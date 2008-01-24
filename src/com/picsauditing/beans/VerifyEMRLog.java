56d177148a7485712c0aa38391d489247ba66505
Glenn Silverman <glenn@picsauditing.com>
2008-01-24 19:38:04 +0000

    Changes to osha verfication

--- src/com/picsauditing/beans/VerifyEMRLog.java
+++ src/com/picsauditing/beans/VerifyEMRLog.java
@@ -44,21 +44,36 @@ public class VerifyEMRLog extends Verifier<PqfLog, Short> implements Verifiable<
 	}
 	
 	
-	public boolean isCorrect() {
+	public String getCorrect() {
 		if(getEntity().getIsCorrect() == null)
-			return false;
+			return "No";
 		
-		return getEntity().getIsCorrect().equals("Yes")? true : false;
+		return getEntity().getIsCorrect().equals("Yes")? "Yes" : "No";
 	}
 	
-	public void setCorrect(boolean isCorrect){
-		if (isCorrect)
+	public void setCorrect(String isCorrect){
+		if (isCorrect.equals("Yes"))
 			getEntity().setIsCorrect("Yes");
 		else
 			getEntity().setIsCorrect("No");
 		
 		verifyLog();
 		
+	}
+	
+	public String getVerifiedAnswer(){
+		if(getEntity().getIsCorrect().equals("Yes"))
+			return getEntity().getAnswer();		
+		else
+			return getEntity().getVerifiedAnswer();
+	}
+	
+	public void setVerifiedAnswer(String answer){
+		if(getEntity().getIsCorrect().equals("Yes"))
+			getEntity().setVerifiedAnswer(getEntity().getAnswer());
+		else
+			getEntity().setVerifiedAnswer(answer);
+			
 	}
 
 	
