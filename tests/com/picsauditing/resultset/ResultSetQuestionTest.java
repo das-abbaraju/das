4b77cc0c5f574b7efa0bf8c35c979abb854c0e1b
Trevor Allred <tallred@picsauditing.com>
2008-02-11 19:59:06 +0000

    added junit tests for resultsets

---
+++ tests/com/picsauditing/resultset/ResultSetQuestionTest.java
@@ -0,0 +1,33 @@
+package com.picsauditing.resultset;
+
+import junit.framework.TestCase;
+
+public class ResultSetQuestionTest extends TestCase {
+
+	public ResultSetQuestionTest(String name) {
+		super(name);
+	}
+
+	public final void testEqualsAny() {
+		ResultSetQuestion question;
+		question = new ResultSetQuestion(ResultSetOperator.Any, "FOO");
+		assertTrue(question.equals("BAR"));
+	}
+	public final void testEqualsEquals() {
+		ResultSetQuestion question;
+		question = new ResultSetQuestion(ResultSetOperator.Equals, "FOO");
+		assertTrue(question.equals("FOO"));
+
+		question = new ResultSetQuestion(ResultSetOperator.Equals, "FOO");
+		assertFalse(question.equals("BAR"));
+
+		question = new ResultSetQuestion(ResultSetOperator.Equals, 1);
+		assertTrue(question.equals(1));
+		
+	}
+	public final void testEqualsNotEquals() {
+		ResultSetQuestion question;
+		question = new ResultSetQuestion(ResultSetOperator.NotEquals, "FOO");
+		assertTrue(question.equals("BAR"));
+	}
+}
