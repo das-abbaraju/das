82ce3840c2bd82d5ca883e2f5e905a46c75081c1
Trevor Allred <tallred@picsauditing.com>
2008-02-11 19:58:24 +0000

    Created new ResultSet processing (decision tree)

--- src/com/picsauditing/matrix/ResultSet.java
+++
@@ -1,5 +0,0 @@
-package com.picsauditing.matrix;
-
-public class ResultSet {
-
-}
---
+++ src/com/picsauditing/resultset/ResultSetOperator.java
@@ -0,0 +1,11 @@
+package com.picsauditing.resultset;
+
+public enum ResultSetOperator {
+	Any,
+	//Contains,
+	//StartsWith,
+	Equals,
+	NotEquals,
+	GreaterThan,
+	LessThan
+}
