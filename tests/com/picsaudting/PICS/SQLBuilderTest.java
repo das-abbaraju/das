dacaac5a23b630d70aa5c9dc507f06fbede1b773
Trevor Allred <tallred@picsauditing.com>
2008-02-11 20:06:58 +0000

    more jUnit stuff

--- tests/com/picsaudting/PICS/SQLBuilderTest.java
+++ tests/com/picsaudting/PICS/SQLBuilderTest.java
@@ -10,8 +10,18 @@ import com.picsauditing.PICS.SQLBuilder;
 public class SQLBuilderTest extends TestCase {
 	protected SQLBuilder builder = new SQLBuilder();
 	
-	public void testAdd() {
-        //double result= fValue1 + fValue2;
-        //assertTrue(result == 5.0);
+	public void testBasic() {
+		builder.setFromTable("test_from");
+		builder.addField("field1");
+		builder.addField("field2");
+		builder.addJoin("JOIN test_join USING (field1)");
+		builder.addWhere("field1 = 'foo' OR field2 = 'bar'");
+		builder.addOrderBy("field1");
+		builder.addOrderBy("field2 DESC");
+		builder.setSQL_CALC_FOUND_ROWS(true);
+		builder.setStartRow(100);
+		builder.setLimit(10);
+		String sql = builder.toString();
+		assertEquals(178, sql.length());
     }
 }
