613806e48206475a6acf375852d0066f40f85d2e
Jeff Shelley <jshelley@picsauditing.com>
2008-03-21 19:13:01 +0000

    Added UserManage action for the "Manage Users (Beta) screen"

---
+++ src/com/picsauditing/actions/users/UsersManage.java
@@ -0,0 +1,141 @@
+package com.picsauditing.actions.users;
+
+import java.util.List;
+
+import org.apache.commons.beanutils.BasicDynaBean;
+import org.apache.struts2.ServletActionContext;
+
+import com.picsauditing.PICS.Utilities;
+import com.picsauditing.access.OpPerms;
+import com.picsauditing.access.OpType;
+import com.picsauditing.actions.PicsActionSupport;
+import com.picsauditing.search.Report;
+import com.picsauditing.search.SelectUser;
+
+public class UsersManage extends PicsActionSupport 
+{
+	
+	protected String accountId = null;
+	protected String filter = null;
+
+	protected Report search = null;
+	protected List<BasicDynaBean> searchData = null;
+	
+	protected String isGroup = null;
+	protected String isActive = null;
+
+	protected boolean hasAllOperators = false;
+	
+	public String execute() throws Exception
+	{
+		String response = INPUT;
+		
+		 if( getPermissions(OpPerms.EditUsers, OpType.View) )
+		 {
+			 String accountId = permissions.getAccountIdString();
+			 if (permissions.hasPermission(OpPerms.AllOperators) && this.accountId != null) 
+			 {
+					accountId = Utilities.intToDB(this.accountId);
+			 }
+			 
+			 this.accountId = accountId;//accountId is on the instance level AND in the execute method...watch out
+
+			 
+			 SelectUser sql = new SelectUser();
+			 sql.addField("u.lastLogin");
+			 sql.addField("u.isGroup");
+
+			 search = new Report();
+			 search.setSql(sql);
+
+			 if (isActive == null)
+			 {
+				 isActive = "Yes";
+			 }
+			 
+			 if ("Yes".equals(isGroup) || "No".equals(isGroup)) {
+			 	sql.addWhere("isGroup = '"+isGroup+"' ");
+			 }
+			 if ("Yes".equals(isActive) || "No".equals(isActive)) {
+			 	sql.addWhere("isActive = '"+isActive+"' ");
+			 }
+
+			 sql.addWhere("accountID = "+accountId);
+			 // Only search for Auditors and Admins
+			 sql.addOrderBy("u.isGroup, u.name");
+			 search.setPageByResult(ServletActionContext.getRequest());
+			 search.setLimit(3);
+
+			 searchData = search.getPage();
+			 
+			 
+			 if (permissions.hasPermission(OpPerms.AllOperators) )
+			 {
+				 hasAllOperators = true;
+			 }
+			 
+			 response = SUCCESS;
+		 }
+		
+		return response;
+	}
+
+
+	
+	
+
+
+	public String getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(String accountId) {
+		this.accountId = accountId;
+	}
+
+	public String getFilter() {
+		return filter;
+	}
+
+	public void setFilter(String filter) {
+		this.filter = filter;
+	}
+
+	public String getIsGroup() {
+		return isGroup;
+	}
+	public void setIsGroup(String isGroup) {
+		this.isGroup = isGroup;
+	}
+
+	public String getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(String isActive) {
+		this.isActive = isActive;
+	}
+
+	public Report getSearch() {
+		return search;
+	}
+	public void setSearch(Report search) {
+		this.search = search;
+	}
+	
+	public List<BasicDynaBean> getSearchData() {
+		return searchData;
+	}
+	public void setSearchData(List<BasicDynaBean> searchData) {
+		this.searchData = searchData;
+	}
+	
+	public boolean isHasAllOperators() {
+		return hasAllOperators;
+	}
+	public void setHasAllOperators(boolean hasAllOperators) {
+		this.hasAllOperators = hasAllOperators;
+	}
+	
+
+}
