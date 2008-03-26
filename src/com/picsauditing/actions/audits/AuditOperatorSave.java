d5da9efb26389aac8d977ced39c1c2bce7940ab2
Jeff Shelley <jshelley@picsauditing.com>
2008-03-26 18:46:19 +0000

    refactored object model

--- src/com/picsauditing/actions/audits/AuditOperatorSave.java
+++ src/com/picsauditing/actions/audits/AuditOperatorSave.java
@@ -5,10 +5,8 @@ import com.picsauditing.dao.AuditOperatorDAO;
 import com.picsauditing.jpa.entities.AuditOperator;
 
 public class AuditOperatorSave extends PicsActionSupport {
-	private int auditOperatorID = 0;
-	private int operatorID;
-	private int auditTypeID;
-	private int riskLevel;
+
+	private AuditOperator ao = null;
 	private AuditOperatorDAO dao;
 
 	public AuditOperatorSave(AuditOperatorDAO dao) {
@@ -16,54 +14,16 @@ public class AuditOperatorSave extends PicsActionSupport {
 	}
 	
 	public String execute() throws Exception {
-		// PrintWriter writer = ServletActionContext.getResponse().getWriter();
-		// writer.print("Success");
-		// writer.flush();
-		AuditOperator o = new AuditOperator();
-		o.setAuditOperatorID(auditOperatorID);
-		o.setOpID(operatorID);
-		o.setAuditTypeID(auditTypeID);
-		o.setMinRiskLevel(riskLevel);
-		o = dao.save(o);
-		this.auditOperatorID = o.getAuditOperatorID();
+		ao = dao.save(ao);
 		
 		return SUCCESS;
 	}
 
-	public void setOperatorID(int operatorID) {
-		this.operatorID = operatorID;
+	public AuditOperator getAo() {
+		return ao;
 	}
 
-	public void setAuditTypeID(int auditTypeID) {
-		this.auditTypeID = auditTypeID;
-	}
-
-	public void setRiskLevel(int riskLevel) {
-		this.riskLevel = riskLevel;
-	}
-
-	public void setAuditOperatorID(int auditOperatorID) {
-		if (auditOperatorID > 0)
-			this.auditOperatorID = auditOperatorID;
-	}
-
-	public int getAuditOperatorID() {
-		return auditOperatorID;
-	}
-
-	public int getOperatorID() {
-		return operatorID;
-	}
-
-	public int getAuditTypeID() {
-		return auditTypeID;
-	}
-
-	public int getRiskLevel() {
-		return riskLevel;
-	}
-	
-	public int getId() {
-		return (auditTypeID*100000)+operatorID;
+	public void setAo(AuditOperator ao) {
+		this.ao = ao;
 	}
 }
