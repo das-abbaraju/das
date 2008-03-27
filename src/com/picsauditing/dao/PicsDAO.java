c2ae5d4186f887f28c9e86daa78994e3c525e46b
Jeff Shelley <jshelley@picsauditing.com>
2008-03-27 17:55:47 +0000

    Added QueryMetaData possibility for searches

--- src/com/picsauditing/dao/PicsDAO.java
+++ src/com/picsauditing/dao/PicsDAO.java
@@ -2,18 +2,48 @@ package com.picsauditing.dao;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 abstract public class PicsDAO {
 	protected EntityManager em;
+	protected QueryMetaData queryMetaData = null;
+	
 	
 	@PersistenceContext
 	public void setEntityManager( EntityManager em )
 	{
 		this.em = em;
 	}
+
+
+	public QueryMetaData getQueryMetaData() {
+		return queryMetaData;
+	}
+
+	public void setQueryMetaData(QueryMetaData queryMetaData) {
+		this.queryMetaData = queryMetaData;
+	}
 	
+	
+	protected void applyQueryMetaData( Query query )
+	{
+		QueryMetaData qmd = getQueryMetaData();
+		if( qmd != null )
+		{
+			if( qmd.getMaxRows() != -1 )
+			{
+				query.setMaxResults(qmd.getMaxRows());
+			}
+			
+			if( qmd.getStartRow() != -1  )
+			{
+				query.setFirstResult( qmd.getStartRow() );
+			}
+		}
+	}
 	
 	/*
 		private Class<T> persistentClass;
