6c80b8a267f5be8ec215922fe53248ac68ade31c
Trevor Allred <tallred@picsauditing.com>
2008-04-22 22:02:57 +0000

    added facilityID and removed composite key

--- src/com/picsauditing/jpa/entities/Facility.java
+++ src/com/picsauditing/jpa/entities/Facility.java
@@ -1,26 +1,53 @@
 package com.picsauditing.jpa.entities;
 
-import javax.persistence.AttributeOverride;
-import javax.persistence.AttributeOverrides;
+import static javax.persistence.GenerationType.IDENTITY;
+
 import javax.persistence.Column;
-import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "facilities")
-public class Facility implements java.io.Serializable {
+public class Facility {
 
-	private FacilityId id;
+	private int id;
+	private OperatorAccount operator;
+	private OperatorAccount corporate;
 
-	@EmbeddedId
-	@AttributeOverrides( { @AttributeOverride(name = "opId", column = @Column(name = "opID", nullable = false)),
-			@AttributeOverride(name = "corporateId", column = @Column(name = "corporateID", nullable = false)) })
-	public FacilityId getId() {
-		return this.id;
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "facilityID", nullable = false)
+	public int getId() {
+		return id;
 	}
 
-	public void setId(FacilityId id) {
+	public void setId(int id) {
 		this.id = id;
 	}
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "opID", nullable = false, updatable = false)
+	public OperatorAccount getOperator() {
+		return operator;
+	}
+
+	public void setOperator(OperatorAccount operator) {
+		this.operator = operator;
+	}
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "corporateID", nullable = false, updatable = false)
+	public OperatorAccount getCorporate() {
+		return corporate;
+	}
+
+	public void setCorporate(OperatorAccount corporate) {
+		this.corporate = corporate;
+	}
+
 }
