dda8a7cde3fc8bd83b85ecc366609f044138197b
Trevor Allred <tallred@picsauditing.com>
2008-04-28 18:57:27 +0000

    moved to actions/contractors

---
+++ src/com/picsauditing/actions/contractors/ConAuditList.java
@@ -0,0 +1,26 @@
+package com.picsauditing.actions.contractors;
+
+import java.util.List;
+
+import org.apache.struts2.config.Result;
+
+import com.picsauditing.dao.ContractorAccountDAO;
+import com.picsauditing.dao.ContractorAuditDAO;
+import com.picsauditing.jpa.entities.ContractorAudit;
+
+public class ConAuditList extends ContractorActionSupport {
+	public ConAuditList(ContractorAccountDAO accountDao, ContractorAuditDAO auditDao) {
+		super(accountDao, auditDao);
+	}
+
+	public String execute() throws Exception {
+		findContractor();
+
+		return SUCCESS;
+	}
+
+	public List<ContractorAudit> getAudits() {
+		return auditDao.findByContractor(id);
+	}
+
+}
