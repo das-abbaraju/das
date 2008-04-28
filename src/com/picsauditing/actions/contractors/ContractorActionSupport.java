da50e29681df5cb7f3e8da7cb34dd75e4268a8eb
Trevor Allred <tallred@picsauditing.com>
2008-04-28 18:57:10 +0000

    moved to actions/contractors

--- src/com/picsauditing/actions/contractors/ContractorActionSupport.java
+++ src/com/picsauditing/actions/contractors/ContractorActionSupport.java
@@ -1,9 +1,11 @@
-package com.picsauditing.actions;
+package com.picsauditing.actions.contractors;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.picsauditing.PICS.FlagCalculator2;
+import com.picsauditing.actions.PicsActionSupport;
 import com.picsauditing.dao.ContractorAccountDAO;
 import com.picsauditing.dao.ContractorAuditDAO;
 import com.picsauditing.jpa.entities.ContractorAccount;
@@ -12,15 +14,22 @@ import com.picsauditing.jpa.entities.ContractorAudit;
 public class ContractorActionSupport extends PicsActionSupport {
 	protected int id = 0;
 	protected ContractorAccount contractor;
-	@Autowired
+	@Autowired(required=true)
 	protected ContractorAccountDAO accountDao;
-	@Autowired
 	protected ContractorAuditDAO auditDao;
 
+	public ContractorActionSupport(ContractorAccountDAO accountDao, ContractorAuditDAO auditDao) {
+		this.accountDao = accountDao;
+		this.auditDao = auditDao;
+	}
+	
 	protected void findContractor() throws Exception {
+		loadPermissions();
 		contractor = accountDao.find(id);
 		if (contractor.getId() == 0)
 			throw new Exception("Contractor " + this.id + " not found");
+		
+		// TODO Check permissions to view this contractor
 	}
 
 	public int getId() {
@@ -42,5 +51,5 @@ public class ContractorActionSupport extends PicsActionSupport {
 	public List<ContractorAudit> getActiveAudits() {
 		return auditDao.findNonExpiredByContractor(contractor.getId());
 	}
-
+	
 }
