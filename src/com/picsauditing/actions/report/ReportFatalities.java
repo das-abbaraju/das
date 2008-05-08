472e884d24e3c96e653019272f6fb01968a7fef6
Keerthi Nannapaneni <knannapaneni@picsauditing.com>
2008-05-08 18:22:18 +0000

    extends reportAccount.

--- src/com/picsauditing/actions/report/ReportFatalities.java
+++ src/com/picsauditing/actions/report/ReportFatalities.java
@@ -2,14 +2,19 @@ package com.picsauditing.actions.report;
 
 import com.picsauditing.PICS.DateBean;
 
-public class ReportFatalities extends ReportContractorAudits {
+public class ReportFatalities extends ReportAccount {
 	protected int year;
 
 	public String execute() throws Exception {
 		sql.addJoin("INNER JOIN OSHA os ON os.conID = a.id");
+		sql.addWhere("os.fatalities1 >0 OR os.fatalities2 >0 OR os.fatalities3 >0");
 		sql.addField("os.fatalities1");
 		sql.addField("os.fatalities2");
 		sql.addField("os.fatalities3");
+		sql.addJoin("JOIN contractor_audit ca ON ca.conID = a.id");
+		sql.addField("ca.auditID");
+		sql.addWhere("ca.auditTypeID = 1");
+		
 		return super.execute();
 	}
 
