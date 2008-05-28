594922e3695cbca623a711ca4d2575831d371a83
Keerthi Nannapaneni <knannapaneni@picsauditing.com>
2008-05-28 21:29:42 +0000

    changed the selectFilter to selectFilterInteger and like clause to equal clause.

--- src/com/picsauditing/actions/report/ReportAccountQuick.java
+++ src/com/picsauditing/actions/report/ReportAccountQuick.java
@@ -1,28 +1,32 @@
 package com.picsauditing.actions.report;
 
 import com.picsauditing.search.SelectFilter;
+import com.picsauditing.search.SelectFilterInteger;
 
 public class ReportAccountQuick extends ReportAccount {
 	protected String accountName;
+
 	public ReportAccountQuick() {
 		this.forwardSingleResults = true;
 	}
+
 	public String getAccountName() {
 		return accountName;
 	}
+
 	public void setAccountName(String accountName) {
 		this.accountName = accountName;
-		if(accountName == null || accountName.length() == 0)
+		if (accountName == null || accountName.length() == 0)
 			accountName = "";
 		try {
-		 int id = Integer.parseInt(accountName);
-		 report.addFilter(new SelectFilter("id", "a.id LIKE '%?%'",
-			accountName, "", ""));
-			 
-		} catch(NumberFormatException nfe) {
-			
-		report.addFilter(new SelectFilter("accountName", "a.name LIKE '%?%'",
-				accountName, "", ""));
+			int id = Integer.parseInt(accountName);
+			report.addFilter(new SelectFilterInteger("id", "a.id = ?",
+					id));
+
+		} catch (NumberFormatException nfe) {
+
+			report.addFilter(new SelectFilter("accountName",
+					"a.name LIKE '%?%'", accountName, "", ""));
 		}
 	}
 }
