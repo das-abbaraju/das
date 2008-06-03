a10df8c09255659dfc2408fc6891b459d35a1b18
Jeff Shelley <jshelley@picsauditing.com>
2008-06-03 21:51:42 +0000

    added reasonable stab at add/update logic

--- src/com/picsauditing/actions/audits/AuditDataSave.java
+++ src/com/picsauditing/actions/audits/AuditDataSave.java
@@ -7,6 +7,8 @@ import com.opensymphony.xwork2.Preparable;
 import com.picsauditing.actions.PicsActionSupport;
 import com.picsauditing.dao.AuditDataDAO;
 import com.picsauditing.jpa.entities.AuditData;
+import com.picsauditing.jpa.entities.AuditQuestion;
+import com.picsauditing.jpa.entities.ContractorAudit;
 import com.picsauditing.jpa.entities.User;
 import com.picsauditing.jpa.entities.YesNo;
 
@@ -14,6 +16,7 @@ public class AuditDataSave extends PicsActionSupport implements Preparable
 {
 	AuditData auditData = null;
 	AuditDataDAO dao = null;
+	protected AuditData oldCopy;
 	
 	public AuditDataSave( AuditDataDAO dao )
 	{
@@ -28,36 +31,73 @@ public class AuditDataSave extends PicsActionSupport implements Preparable
 				return LOGIN;
 			
 			
-			if( auditData.getDataID() != 0 )
+			if( oldCopy.getDataID() != 0 )  //check if this is a currently existing record or not
 			{
-				AuditData oldCopy = dao.find(auditData.getDataID());		
-			
-					if( auditData.getIsCorrect() != oldCopy.getIsCorrect() )
+				if( auditData.getAnswer() != null )  //if answer is being set, then we are not currently verifying
+				{
+					if( oldCopy.getAnswer() == null || ! oldCopy.getAnswer().equals(auditData.getAnswer()))
 					{
-						if( auditData.getIsCorrectBoolean() )
+						oldCopy.setDateVerified(null);
+						oldCopy.setIsCorrect(null);
+						oldCopy.setVerifiedAnswer(null);
+						oldCopy.setWasChanged(YesNo.Yes);
+						oldCopy.setAnswer(auditData.getAnswer());
+					}
+				}
+				else  //we were handed the verification parms instead of the edit parms
+				{
+					if( auditData.getVerifiedAnswer() != null )
+					{
+						oldCopy.setVerifiedAnswer(auditData.getVerifiedAnswer());
+					}
+
+					
+					if( ActionContext.getContext().getParameters().get("auditData.isCorrectBoolean") != null
+							|| ActionContext.getContext().getParameters().get("auditData.isCorrect") != null )
+					{
+						if( auditData.getIsCorrect() != oldCopy.getIsCorrect() )
 						{
-							auditData.setDateVerified(new Date());
-						}
-						else
-						{
-							auditData.setDateVerified(null);
+							if( auditData.getIsCorrectBoolean() )
+							{
+								oldCopy.setDateVerified(new Date());
+							}
+							else
+							{
+								oldCopy.setDateVerified(null);
+							}
+							
+							oldCopy.setIsCorrectBoolean(auditData.getIsCorrectBoolean());
 						}
 					}
-	
-					if( (auditData.getAnswer() == null ^ oldCopy.getAnswer() == null ) 
-							|| (auditData.getAnswer() != null && oldCopy.getAnswer() != null && ! auditData.getAnswer().equals( oldCopy.getAnswer() ) ) 
-					)
-					{
-						auditData.setWasChanged(YesNo.Yes);
-					}
+				}
 			}
 			else
 			{
-				auditData.setAuditor(new User());
-				auditData.getAuditor().setId(permissions.getUserId());
+				oldCopy.setAuditor(new User());
+				oldCopy.getAuditor().setId(permissions.getUserId());
+				
+				oldCopy.setAudit(new ContractorAudit());
+				oldCopy.getAudit().setId(auditData.getAudit().getId());
+				
+				oldCopy.setQuestion(new AuditQuestion() );
+				oldCopy.getQuestion().setQuestionID(auditData.getQuestion().getQuestionID());
+				
+				oldCopy.setAnswer(auditData.getAnswer());
+				
+				oldCopy.setDateVerified(null);
+				oldCopy.setIsCorrect(null);
+				oldCopy.setVerifiedAnswer(null);
+				oldCopy.setWasChanged(YesNo.No);
 			}
-	
-			dao.save(auditData);
+
+			
+			if( auditData.getComment() != null )
+			{
+				oldCopy.setComment(auditData.getComment() );
+			}
+			
+			dao.save(oldCopy);
+			setMessage("Saved");
 		}
 		catch( Exception e )
 		{
@@ -77,7 +117,8 @@ public class AuditDataSave extends PicsActionSupport implements Preparable
 		{
 			int id = new Integer(ids[0]).intValue();
 			
-			auditData = dao.find(id);
+			
+			oldCopy = dao.find(id);
 		}
 	}
 	
