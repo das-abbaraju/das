05e5e9885e094b3677f71db546bac2c5714c274b
Trevor Allred <tallred@picsauditing.com>
2008-06-06 22:35:34 +0000

    for Keerthi

--- src/com/picsauditing/actions/audits/AuditDataSave.java
+++ src/com/picsauditing/actions/audits/AuditDataSave.java
@@ -1,16 +1,24 @@
 package com.picsauditing.actions.audits;
 
+import java.sql.ResultSet;
 import java.util.Date;
+import java.util.Map;
+import java.util.TreeMap;
 
 import javax.persistence.NoResultException;
 
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.Preparable;
+import com.picsauditing.PICS.pqf.SubCategoryBean;
 import com.picsauditing.actions.PicsActionSupport;
+import com.picsauditing.dao.AuditCategoryDataDAO;
 import com.picsauditing.dao.AuditDataDAO;
+import com.picsauditing.jpa.entities.AuditCatData;
+import com.picsauditing.jpa.entities.AuditCategory;
 import com.picsauditing.jpa.entities.AuditData;
 import com.picsauditing.jpa.entities.AuditQuestion;
 import com.picsauditing.jpa.entities.AuditStatus;
+import com.picsauditing.jpa.entities.AuditSubCategory;
 import com.picsauditing.jpa.entities.ContractorAudit;
 import com.picsauditing.jpa.entities.User;
 import com.picsauditing.jpa.entities.YesNo;
@@ -96,6 +104,72 @@ public class AuditDataSave extends PicsActionSupport {
 
 		return SUCCESS;
 	}
+	
+	private void updatePercentageCompleted() throws Exception {
+		int auditID = auditData.getAudit().getId();
+		AuditCategory category = auditData.getQuestion().getSubCategory().getCategory();
+		int catID = category.getId();
+		
+		AuditCategoryDataDAO catDataDAO = new AuditCategoryDataDAO();
+		AuditCatData catData = dao.find(auditID, catID);
+		/**
+		 * MOVED THIS INTO AuditCategoryDataDAO.find()
+		String selectQuery = "SELECT * FROM pqfCatData "+
+			"WHERE catID="+catID+" AND auditID="+auditID;
+		ResultSet rs = SQLStatement.executeQuery(selectQuery);
+		if (!rs.next() || !"Yes".equals(rs.getString("applies"))){
+			rs.close();
+			return;
+		}
+		rs.close();
+		*/
+		int requiredAnsweredCount = 0;
+		int answeredCount = 0;
+		int requiredCount = 0;
+		int yesNACount = 0;
+		
+		// Get a map of all answers in this audit
+		Map<Integer, AuditData> answers = this.dao.findAnswers(auditID);
+		
+		// Get a list of questions/answers for this category
+		for(AuditSubCategory subCategory : category.getSubCategories()) {
+			for(AuditQuestion question : subCategory.getQuestions()) {
+				String answer = this.getString(rs, "answer");
+				
+				String tempIsRequired = rs.getString("isRequired");
+				boolean isRequired = "Yes".equals(tempIsRequired);
+				if ("Depends".equals(tempIsRequired)){
+					// SEE question.getDependsOnQuestion()
+					int dependsOnQID = rs.getInt("dependsOnQID");
+					String dependsOnAnswer = rs.getString("dependsOnAnswer");
+					if (dependsOnAnswer.equals(tempQAMap.get(dependsOnQID)))
+						isRequired = true;
+				}//if
+				if ("Yes".equals(answer) || "NA".equals(answer))
+					yesNACount++;
+				if (isRequired){
+					requiredCount++;
+					if (!"".equals(answer) && !com.picsauditing.PICS.DateBean.NULL_DATE_DB.equals(answer))
+						requiredAnsweredCount++;
+				}//if
+				if (!"".equals(answer))
+					answeredCount++;
+			}
+		}
+		
+		catData.setPercentCompleted(100*requiredAnsweredCount,requiredCount);
+		String tempPercentCompleted = getShowPercent();
+		String tempPercentVerified = getShowPercent(yesNACount,requiredCount);
+		
+//		String updateQuery = "REPLACE INTO pqfcatdata " +
+//				"(catID, auditID, applies, requiredCompleted, numAnswered, " +
+//					"numRequired, percentCompleted, percentVerified) " +
+//				"VALUES " +
+//				"("+catID+", "+auditID+", 'Yes', "+requiredAnsweredCount+", "+answeredCount+", " +
+//					""+requiredCount+", "+tempPercentCompleted+", "+tempPercentVerified+")";
+		catDataDAO.save(catData);
+	}
+
 
 	public AuditData getAuditData() {
 		return auditData;
