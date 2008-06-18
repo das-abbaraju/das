7b5b23eb65ea602729b6d94fd4d782827ccc7e38
Mark Sloan <msloan@picsauditing.com>
2008-06-18 20:52:42 +0000

    Updated to showLabels vs. showLabel.

--- src/com/picsauditing/util/chart/Category.java
+++ src/com/picsauditing/util/chart/Category.java
@@ -3,14 +3,14 @@ package com.picsauditing.util.chart;
 public class Category extends AbstractElement {
 	protected String index;
 	protected String label;
-	protected boolean showLabel;
+	protected boolean showLabels = true;
 	protected String toolText;
 	
 	public String toString() {
 		StringBuilder xml = new StringBuilder();
 		xml.append("<category");
 		append(xml, "label", label);
-		append(xml, "showLabel", showLabel);
+		append(xml, "showLabels", showLabels);
 		append(xml, "toolText", toolText);
 		xml.append(" />");
 		return xml.toString();
