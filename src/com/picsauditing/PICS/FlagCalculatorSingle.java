c6e015960c00726c4d3fc1c8cbd489e5c2fbf0c7
Trevor Allred <tallred@picsauditing.com>
2008-07-07 21:06:28 +0000

    some formatting/debugging changes
    also fixed problem where cron job missed the override flags and also the osha wasn't calculated correctly during batch mode with more than one operator

--- src/com/picsauditing/PICS/FlagCalculatorSingle.java
+++ src/com/picsauditing/PICS/FlagCalculatorSingle.java
@@ -1,7 +1,6 @@
 package com.picsauditing.PICS;
 
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -10,6 +9,7 @@ import com.picsauditing.jpa.entities.AuditOperator;
 import com.picsauditing.jpa.entities.AuditQuestion;
 import com.picsauditing.jpa.entities.AuditStatus;
 import com.picsauditing.jpa.entities.AuditType;
+import com.picsauditing.jpa.entities.Certificate;
 import com.picsauditing.jpa.entities.ContractorAccount;
 import com.picsauditing.jpa.entities.ContractorAudit;
 import com.picsauditing.jpa.entities.ContractorOperator;
@@ -20,11 +20,11 @@ import com.picsauditing.jpa.entities.OperatorAccount;
 import com.picsauditing.jpa.entities.OshaLog;
 import com.picsauditing.jpa.entities.OshaLogYear;
 import com.picsauditing.jpa.entities.YesNo;
-import com.picsauditing.jpa.entities.Certificate;
 
 /**
- * Determine the Flag color for a single contractor at a given facility.
- * This doesn't persist any data nor does it query the database for the required data.
+ * Determine the Flag color for a single contractor at a given facility. This
+ * doesn't persist any data nor does it query the database for the required
+ * data.
  * 
  * @author Trevor
  */
@@ -37,36 +37,62 @@ public class FlagCalculatorSingle {
 	private Certificate certificate;
 	private List<ContractorAudit> conAudits;
 	private Map<Integer, AuditData> auditAnswers;
-	
+
 	/**
-	 * 1) Check to see all required audits are there
-	 * 2) OSHA Data
-	 * 3) AuditQuestions
+	 * 1) Check to see all required audits are there 2) OSHA Data 3)
+	 * AuditQuestions
 	 * 
-	 * @param contractor The contractor jpa entity to calculate the flag for
-	 * @param operator The operator jpa entity to calculate the contractor flag for 
-	 * @param conAudits A list of audits for this contractor that may be required by this operator
+	 * @param contractor
+	 *            The contractor jpa entity to calculate the flag for
+	 * @param operator
+	 *            The operator jpa entity to calculate the contractor flag for
+	 * @param conAudits
+	 *            A list of audits for this contractor that may be required by
+	 *            this operator
 	 * @param auditAnswers
 	 * @return
 	 */
 	public FlagColor calculate() {
-		debug("FlagCalculator.calculate("+contractor.getId()+","+operator.getId()+")");
+		debug("FlagCalculator.calculate(" + contractor.getId() + "," + operator.getId() + ")");
+		debug(" answerOnly=" + answerOnly);
 		
 		// Start positive and see if there are any violations
 		FlagColor flagColor = FlagColor.Green;
+		FlagColor overrideColor = null;
+
+		// First see if there are any forced flags for this operator
+		for (ContractorOperator co : contractor.getOperators()) {
+			if (co.getOperatorAccount().equals(operator)) {
+				// Found the operator, is it forced?
+				debug(" isForcedFlag=" + co.isForcedFlag() + " " + co.getForceFlag());
+				if (co.isForcedFlag())
+					overrideColor = co.getForceFlag();
+			}
+		}
+		if (answerOnly && overrideColor != null)
+			// Things can't get worse, just exit
+			return overrideColor;
 		
-		for(AuditOperator audit : operator.getAudits()) {
+		debug(" flagColor=" + flagColor);
+
+		for (AuditOperator audit : operator.getAudits()) {
+			// Always start with Green
 			audit.setContractorFlag(FlagColor.Green);
-			if (contractor.getRiskLevel().ordinal() >= audit.getMinRiskLevel() ) {
-				// The contractor requires this audit, make sure they have an active one
+			if (contractor.getRiskLevel().ordinal() >= audit.getMinRiskLevel() 
+					&& audit.getRequiredForFlag() != null) {
+				debug(" -- " + audit.getAuditType().getAuditName() + " - " + audit.getRequiredForFlag().toString());
+				// The contractor requires this audit, make sure they have an
+				// active one
 				audit.setContractorFlag(audit.getRequiredForFlag());
-				for(ContractorAudit conAudit : conAudits) {
+				for (ContractorAudit conAudit : conAudits) {
 					if (conAudit.getAuditStatus().equals(AuditStatus.Active)
-						|| conAudit.getAuditStatus().equals(AuditStatus.Exempt)) {
+							|| conAudit.getAuditStatus().equals(AuditStatus.Exempt)) {
 						if (conAudit.getAuditType().equals(audit.getAuditType())
-							|| (conAudit.getAuditType().getAuditTypeID() == AuditType.NCMS
-									&& audit.getAuditType().getAuditTypeID() == AuditType.DESKTOP)) {
-							// We found a matching active audit for this contractor
+								|| (conAudit.getAuditType().getAuditTypeID() == AuditType.NCMS && audit.getAuditType()
+										.getAuditTypeID() == AuditType.DESKTOP)) {
+							// We found a matching active audit for this
+							// contractor
+							debug(" ---- found");
 							audit.setContractorFlag(FlagColor.Green);
 						}
 					}
@@ -79,11 +105,19 @@ public class FlagCalculatorSingle {
 				// Things can't get worse, just exit
 				return flagColor;
 		}
-		
-		for(FlagOshaCriteria criteria : operator.getFlagOshaCriteria()) {
+
+		debug(" flagColor=" + flagColor);
+
+		for (FlagOshaCriteria criteria : operator.getFlagOshaCriteria()) {
 			if (criteria.isRequired()) {
+				debug(" -- osha ");
 				boolean found = false;
-				for(OshaLog osha : contractor.getOshas()) {
+				for (OshaLog osha : contractor.getOshas()) {
+					// The flag colors should always start Green, but sometimes they are still set from the previous operator's loop
+					osha.getYear1().setFlagColor(FlagColor.Green);
+					osha.getYear2().setFlagColor(FlagColor.Green);
+					osha.getYear3().setFlagColor(FlagColor.Green);
+					
 					if (osha.isCorporate()) {
 						found = true;
 						FlagColor oshaAvgFlag = FlagColor.Green;
@@ -99,33 +133,36 @@ public class FlagCalculatorSingle {
 							if (criteria.getFatalities().isFlagged(osha.getAverageFatalities()))
 								oshaAvgFlag = setFlagColor(oshaAvgFlag, criteria.getFlagColor());
 						}
-						// Set the flag color for the average years and then add it to the "rolling total"
+						// Set the flag color for the average years and then add
+						// it to the "rolling total"
 						osha.setFlagColor(oshaAvgFlag);
 						flagColor = setFlagColor(flagColor, oshaAvgFlag);
-						
+
 						// Make sure all three years pass the criteria
 						flagColor = setFlagColor(flagColor, verifyOsha(osha.getYear1(), criteria));
 						flagColor = setFlagColor(flagColor, verifyOsha(osha.getYear2(), criteria));
 						flagColor = setFlagColor(flagColor, verifyOsha(osha.getYear3(), criteria));
-						
-						
+
 					}
 				}
 				if (!found)
-					// We didn't find the required Corporate Osha record, so flag this contractor 
-					// We may actually not want to do this if for some reason a contractor
-					// is allowed to pass their 
+					// We didn't find the required Corporate Osha record, so
+					// flag this contractor
+					// We may actually not want to do this if for some reason a
+					// contractor
+					// is allowed to pass their
 					flagColor = setFlagColor(flagColor, criteria.getFlagColor());
-				
+
 				if (answerOnly && flagColor.equals(FlagColor.Red))
 					// Things can't get worse, just exit
 					return flagColor;
 			}
 		}
-		
-		// For each operator criteria, get the contractor's 
+		debug(" flagColor=" + flagColor);
+
+		// For each operator criteria, get the contractor's
 		// answer and see if it triggers the flag color
-		for(FlagQuestionCriteria criteria : operator.getFlagQuestionCriteria()) {
+		for (FlagQuestionCriteria criteria : operator.getFlagQuestionCriteria()) {
 			if (criteria.getChecked().equals(YesNo.Yes)) {
 				// This question is required by the operator
 				if (criteria.getAuditQuestion().getQuestionID() == AuditQuestion.EMR_AVG) {
@@ -148,9 +185,10 @@ public class FlagCalculatorSingle {
 						years++;
 						emrRateTotal = emrRateTotal + emrRate;
 					}
-					
+
 					if (years > 0) {
-						// The contractor has answered this question so it needs to be correct
+						// The contractor has answered this question so it needs
+						// to be correct
 						Float avgRate = emrRateTotal / years;
 						if (criteria.isFlagged(avgRate.toString()))
 							flagColor = setFlagColor(flagColor, criteria.getFlagColor());
@@ -158,36 +196,38 @@ public class FlagCalculatorSingle {
 				} else {
 					// Check all other audit data answers
 					AuditData data = auditAnswers.get(criteria.getAuditQuestion().getQuestionID());
-					if (data != null && data.getVerifiedAnswerOrAnswer() != null && data.getVerifiedAnswerOrAnswer().length() > 0) {
-						// The contractor has answered this question so it needs to be correct
+					if (data != null && data.getVerifiedAnswerOrAnswer() != null
+							&& data.getVerifiedAnswerOrAnswer().length() > 0) {
+						// The contractor has answered this question so it needs
+						// to be correct
 						data.setFlagColor(FlagColor.Green);
 
 						if (criteria.isValidationRequired() && !data.isVerified()) {
-								data.setFlagColor(criteria.getFlagColor());
-								flagColor = setFlagColor(flagColor, criteria.getFlagColor());
+							data.setFlagColor(criteria.getFlagColor());
+							flagColor = setFlagColor(flagColor, criteria.getFlagColor());
 						}
-						
+
 						if (criteria.isFlagged(data.getVerifiedAnswerOrAnswer())) {
 							data.setFlagColor(criteria.getFlagColor());
 							flagColor = setFlagColor(flagColor, criteria.getFlagColor());
 						}
 					}
-					
+
 					if (answerOnly && flagColor.equals(FlagColor.Red))
 						// Things can't get worse, just exit
 						return flagColor;
 				}
 			}
 		}
-		
+		debug(" flagColor=" + flagColor);
+
 		// Calculate the insurance certificate flags colors
 		if (operator.getCanSeeInsurance().equals(YesNo.Yes)) {
 			FlagColor certFlagColor = null;
 
 			for (Certificate certificate : contractor.getCertificates()) {
-				certFlagColor = setFlagColor(certFlagColor, certificate
-						.getFlagColor());
-				
+				debug(" -- certificate" + certificate.getType() + " " + certificate.getOperatorAccount().getName());
+				certFlagColor = setFlagColor(certFlagColor, certificate.getFlagColor());
 
 				if (answerOnly && certFlagColor.equals(FlagColor.Red))
 					// Things can't get worse, just exit
@@ -195,20 +235,17 @@ public class FlagCalculatorSingle {
 			}
 
 			flagColor = setFlagColor(flagColor, certFlagColor);
+			debug(" flagColor=" + flagColor);
 		}
 
-		// First see if there are any forced flags for this operator
-		for (ContractorOperator co : contractor.getOperators()) {
-			if (co.getOperatorAccount().equals(operator)) {
-				// Found the operator, is it forced?
-				if (co.isForcedFlag())
-					flagColor = co.getForceFlag();
-		    }
-		}
+		debug(" flagColor=" + flagColor);
+		
+		if (overrideColor != null)
+			return overrideColor;
 
 		return flagColor;
 	}
-	
+
 	private float getEmrRate(AuditData auditData) {
 		float NA = -1;
 		if (auditData == null)
@@ -216,7 +253,7 @@ public class FlagCalculatorSingle {
 		String value = auditData.getVerifiedAnswerOrAnswer();
 		if (value == null)
 			return NA;
-		
+
 		try {
 			return Float.parseFloat(value);
 		} catch (Exception e) {
@@ -229,20 +266,29 @@ public class FlagCalculatorSingle {
 		if (oshaYearFlag == null)
 			oshaYearFlag = FlagColor.Green;
 
+		debug(" ---- criteria.getFlagColor() " + criteria.getFlagColor() + osha.isApplicable());
 		if (osha.isApplicable()) {
+			debug(" ---- LWCR " + criteria.getLwcr().isFlagged(osha.getLostWorkCasesRate()) + 
+					" criteria:" + criteria.getLwcr().toString() + " value:" + osha.getLostWorkCasesRate());
+			debug(" ---- TRIR " + criteria.getTrir().isFlagged(osha.getRecordableTotalRate()) + 
+					" criteria:" + criteria.getTrir().toString() + " value:" + osha.getRecordableTotalRate());
+			debug(" ---- Fatal " + criteria.getFatalities().isFlagged(osha.getFatalities()) + 
+					" criteria:" + criteria.getFatalities().toString() + " value:" + osha.getFatalities());
 			if (criteria.getLwcr().isFlagged(osha.getLostWorkCasesRate())
-				|| criteria.getTrir().isFlagged(osha.getRecordableTotalRate())
-				|| criteria.getFatalities().isFlagged(osha.getFatalities())) {
+					|| criteria.getTrir().isFlagged(osha.getRecordableTotalRate())
+					|| criteria.getFatalities().isFlagged(osha.getFatalities())) {
 				oshaYearFlag = setFlagColor(oshaYearFlag, criteria.getFlagColor());
 			}
 		}
 		osha.setFlagColor(oshaYearFlag);
+		debug(" -- oshaYearFlag = " + oshaYearFlag);
 		return oshaYearFlag;
 	}
 
 	/**
-	 * Set the flag color, but only let it get worse
-	 * Green to Red but not reverse
+	 * Set the flag color, but only let it get worse Green to Red but not
+	 * reverse
+	 * 
 	 * @param oldColor
 	 * @param newColor
 	 * @return
@@ -251,17 +297,17 @@ public class FlagCalculatorSingle {
 		if (oldColor == null)
 			// Never set it to null, the default is Green
 			oldColor = FlagColor.Green;
-		
+
 		if (newColor == null)
 			// Don't change anything
 			return oldColor;
 
-		// If 2-Red is greater (worse) than 0-Green, then change the color 
+		// If 2-Red is greater (worse) than 0-Green, then change the color
 		if (newColor.ordinal() > oldColor.ordinal())
 			oldColor = newColor;
 		return oldColor;
 	}
-	
+
 	protected void debug(String message) {
 		if (!debug)
 			return;
@@ -316,6 +362,5 @@ public class FlagCalculatorSingle {
 	public void setAnswerOnly(boolean answerOnly) {
 		this.answerOnly = answerOnly;
 	}
-	
-	
+
 }
