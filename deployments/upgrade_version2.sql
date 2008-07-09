4f1063dcdc52af5e140b96f0f181f9595a820d6d
Trevor Allred <tallred@picsauditing.com>
2008-07-09 06:36:14 +0000

    performed upgrade tonight on database

--- deployments/upgrade_version2.sql
+++
@@ -1,9 +0,0 @@
-/*
- * Inserted rows into WidgetUser table for corporate user
- */
-insert into widget_user values
-(27,10,646,1,1,1,null),
-(25,4,646,1,2,10,null),
-(26,5,646,1,1,15,null),
-(23,2,646,1,1,20,null),
-(24,3,646,1,2,20,null);
