15424c254ed6bbfb52b0b7f54fa0620ec5a399a3
Jeff Shelley <jshelley@picsauditing.com>
2008-07-09 21:23:09 +0000

    added some extra functionality to integrate with Audit CRUD screen

--- src/com/picsauditing/actions/audits/AuditSave.java
+++ src/com/picsauditing/actions/audits/AuditSave.java
@@ -1,9 +1,13 @@
 package com.picsauditing.actions.audits;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.Preparable;
 import com.picsauditing.actions.PicsActionSupport;
+import com.picsauditing.dao.ContractorAccountDAO;
 import com.picsauditing.dao.ContractorAuditDAO;
+import com.picsauditing.dao.OperatorAccountDAO;
 import com.picsauditing.jpa.entities.ContractorAudit;
 
 // Samples
@@ -13,17 +17,42 @@ import com.picsauditing.jpa.entities.ContractorAudit;
 public class AuditSave extends PicsActionSupport implements Preparable {
 	ContractorAudit audit;
 	ContractorAuditDAO dao;
+	OperatorAccountDAO opDao;
+	ContractorAccountDAO conDao;
 
-	public AuditSave(ContractorAuditDAO dao) {
+	protected int contractorID = 0;
+	
+	public AuditSave(ContractorAuditDAO dao, OperatorAccountDAO opDao, ContractorAccountDAO conDao) {
 		this.dao = dao;
+		this.opDao = opDao;
+		this.conDao = conDao;
 	}
 
+	
 	public String execute() throws Exception {
 		if (!forceLogin())
 			return LOGIN;
 		// TODO add security
 		// TODO return an appropriate message
 
+		if (audit.getAuditor() != null
+				&& audit.getAuditor().getId() != 0 ) {
+			audit.setAuditor(getUser(audit.getAuditor().getId()) );
+		} else {
+			audit.setAuditor(null);
+		}
+		//weird workaround.  conAudit.contractorAccount.id was not properly setting off the request,
+		// so we're doing it manually.  if we changed the name of the parameter to:
+		//	conAudit.contractorAccount.trade, which is in the ContractorAccount class, it sets fine.
+		//	conAudit.contractorAccount.name, which is in the Account class, it sets fine
+		//	conAudit.contractorAccount.id, which is in the Account class, the id is not set.  
+		// We think it might be related to the fact that it's either not nullable or a join column in 
+		//	a mapped inheritance relationship. 
+		if( contractorID != 0 )
+		{
+			audit.setContractorAccount(conDao.find(contractorID));
+		}
+		
 		dao.save(audit);
 		this.message = "Success";
 
@@ -32,8 +61,12 @@ public class AuditSave extends PicsActionSupport implements Preparable {
 
 	public void prepare() throws Exception {
 		String[] ids = (String[]) ActionContext.getContext().getParameters().get("audit.id");
-		int id = new Integer(ids[0]).intValue();
-		audit = dao.find(id);
+
+		if (ids != null && ids.length > 0) {
+			int id = new Integer(ids[0]).intValue();
+			audit = dao.find(id);
+			dao.clear();
+		}
 	}
 
 	public ContractorAudit getAudit() {
@@ -43,5 +76,24 @@ public class AuditSave extends PicsActionSupport implements Preparable {
 	public void setAudit(ContractorAudit audit) {
 		this.audit = audit;
 	}
+
+	public ContractorAudit getConAudit() {
+		return audit;
+	}
+
+	public void setConAudit(ContractorAudit audit) {
+		this.audit = audit;
+	}
+
+
+	public int getContractorID() {
+		return contractorID;
+	}
+
+
+	public void setContractorID(int contractorID) {
+		this.contractorID = contractorID;
+	}
+
 
 }
