e0bba68fa450bb5a62145ab9892659add8ef4926
Jeff Shelley <jshelley@picsauditing.com>
2008-07-21 16:50:30 +0000

    added the int convertor

---
+++ src/com/picsauditing/actions/converters/IntConverter.java
@@ -0,0 +1,45 @@
+package com.picsauditing.actions.converters;
+
+import java.util.Map;
+
+public class IntConverter extends EnumConverter {
+	public IntConverter() {
+		enumClass = int.class;
+	}
+
+	@Override
+	public String convertToString(Map arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+		return (String) performFallbackConversion(arg0, arg1, String.class);
+	}
+	
+	@Override
+	public Object convertFromString(Map arg0, String[] arg1, Class arg2) {
+		Object test = null;
+		try {
+			test = super.convertFromString(arg0, arg1, arg2);
+		} catch (Exception itllJustStayNull) { }
+
+		if( test == null ) {
+			if( arg1.length > 0 ) {
+				String temp = arg1[0];
+				temp = temp.replaceAll( "\\." , "" );
+				temp = temp.replaceAll( "\\," , "" );
+				try {
+					test = Integer.parseInt(temp);
+				}
+				catch( Exception e ) {
+					test = performFallbackConversion(arg0, arg1, arg2);
+				}
+			}
+		}
+
+		if( test == null ) {
+			test = performFallbackConversion(arg0, arg1, arg2);
+		}
+		
+		return test;
+	}
+
+}
