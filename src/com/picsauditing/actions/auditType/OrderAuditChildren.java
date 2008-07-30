2184af5601da6b4bd4ffce5e71b15039dd1d75a9
Mark Sloan <msloan@picsauditing.com>
2008-07-30 17:36:29 +0000

    Updates for puts

--- src/com/picsauditing/actions/auditType/OrderAuditChildren.java
+++ src/com/picsauditing/actions/auditType/OrderAuditChildren.java
@@ -2,6 +2,7 @@ package com.picsauditing.actions.auditType;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import com.opensymphony.xwork2.ActionContext;
@@ -14,13 +15,27 @@ public class OrderAuditChildren extends PicsActionSupport {
 	public String execute() {
 		String[] listString = (String[]) ActionContext.getContext().getParameters().get("list[]");
 		for(int i=0; i < listString.length; i++) {
+			
+			//System.out.println("listString[" + i + "]" + "   " + listString[i]);
+			
 			try {
 				int id = Integer.parseInt(listString[i]);
 				list.put(id, i+1);
 			} catch(Exception e){}
 		}
-		
+
 		// TODO iterate over children and update num for each child id in "list"
+		
+		Map<Integer, Integer> tempList = new HashMap<Integer, Integer>();
+		
+		for(Integer i : list.keySet()) {
+			Integer value = list.get(i);
+			tempList.put(i, value);
+			System.out.print(i);
+			System.out.println(" " + value);
+		}
+		
+		
 
 		return SUCCESS;
 	}
