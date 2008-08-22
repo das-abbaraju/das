53ea914de1a1c638894a8ad343cdef83ee54611a
Trevor Allred <tallred@picsauditing.com>
2008-08-22 21:27:56 +0000

    added converter for OpPerms enum used by xworks

---
+++ src/com/picsauditing/actions/converters/OpPermsConverter.java
@@ -0,0 +1,9 @@
+package com.picsauditing.actions.converters;
+
+import com.picsauditing.access.OpPerms;
+
+public class OpPermsConverter extends EnumConverter {
+	public OpPermsConverter() {
+		enumClass = OpPerms.class;
+	}
+}
