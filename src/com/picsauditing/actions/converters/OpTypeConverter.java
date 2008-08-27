502a40288bcf23597321b4775bc418801ed15dd9
Jeff Shelley <jshelley@picsauditing.com>
2008-08-27 19:10:47 +0000

    added

---
+++ src/com/picsauditing/actions/converters/OpTypeConverter.java
@@ -0,0 +1,9 @@
+package com.picsauditing.actions.converters;
+
+import com.picsauditing.access.OpType;
+
+public class OpTypeConverter extends EnumConverter {
+	public OpTypeConverter() {
+		enumClass = OpType.class;
+	}
+}
