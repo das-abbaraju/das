0879eb28b1980aa4b4486722eede49e113e6c4d5
Keerthi Nannapaneni <knannapaneni@picsauditing.com>
2008-09-03 16:51:36 +0000

    changed the extension of the file to lowercase.

--- src/com/picsauditing/actions/audits/AuditDataFileUpload.java
+++ src/com/picsauditing/actions/audits/AuditDataFileUpload.java
@@ -7,19 +7,17 @@ import javax.persistence.NoResultException;
 import com.opensymphony.xwork2.ActionContext;
 import com.picsauditing.actions.FileUploadActionSupport;
 import com.picsauditing.dao.AuditDataDAO;
-import com.picsauditing.dao.ContractorAccountDAO;
 import com.picsauditing.jpa.entities.AuditData;
 import com.picsauditing.jpa.entities.AuditStatus;
-import com.picsauditing.jpa.entities.ContractorAccount;
 import com.picsauditing.jpa.entities.YesNo;
 
 public class AuditDataFileUpload extends FileUploadActionSupport {
-	
+
 	AuditData auditData = null;
 	int contractorID = 0;
 	AuditDataDAO dao = null;
 
-	public AuditDataFileUpload(AuditDataDAO dao ) {
+	public AuditDataFileUpload(AuditDataDAO dao) {
 		this.dao = dao;
 	}
 
@@ -29,95 +27,90 @@ public class AuditDataFileUpload extends FileUploadActionSupport {
 			if (!forceLogin())
 				return LOGIN;
 
-			
-				if( file != null && file.length() != 0 )
-				{
-					String extension = fileFileName.substring( fileFileName.lastIndexOf(".") + 1 );
-					
-					
-					String newFileName= getFtpDir() + "/files/pqf/qID_" + auditData.getQuestion().getQuestionID() + "/" + auditData.getQuestion().getQuestionID() + "_" + contractorID + "." + extension; 
-					
-					
-					if( copyFile(file, newFileName, true))
-					{
-					
-						auditData.setAnswer(extension);	
-					
-						AuditData newCopy = null;
-						try
-						{
-							newCopy = dao.findAnswerToQuestion(auditData.getAudit().getId(), auditData.getQuestion().getQuestionID());
-						}
-						catch( NoResultException newCopyWillStayNullSoWeWillJustInsert ) {}
-						
-						
-						if (newCopy == null) // insert mode
-						{
-								dao.save(auditData);
-						} else // update mode
-						{
-							if (auditData.getAnswer() != null) // if answer is being set,
-																// then we are not currently
-																// verifying
-							{
-								if (auditData.getAnswer() == null
-										|| !newCopy.getAnswer().equals(
-												auditData.getAnswer())) {
-									newCopy.setDateVerified(null);
-									newCopy.setIsCorrect(null);
-									newCopy.setVerifiedAnswer(null);
-									newCopy.setAnswer(auditData.getAnswer());
-			
-									if (newCopy.getAudit().getAuditStatus().equals(
-											AuditStatus.Submitted)) // double check this
-									{
-										newCopy.setWasChanged(YesNo.Yes);
-									}
-								}
-							} else // we were handed the verification parms instead of the
-									// edit parms
-							{
-								if (auditData.getVerifiedAnswer() != null) {
-									newCopy
-											.setVerifiedAnswer(auditData
-													.getVerifiedAnswer());
-								}
-			
-								if (ActionContext.getContext().getParameters().get(
-										"auditData.isCorrect") != null) {
-									if (auditData.getIsCorrect() != newCopy.getIsCorrect()) {
-										if (auditData.isVerified()) {
-											newCopy.setDateVerified(new Date());
-										} else {
-											newCopy.setDateVerified(null);
-										}
-			
-										newCopy.setVerified(auditData.isVerified());
-									}
-								}
-							}
-			
-							if (auditData.getComment() != null) {
-								newCopy.setComment(auditData.getComment());
-							}
-			
-							dao.save(newCopy);
-						}
-						
-						output = "<html><body id=\"response\">" + extension + "</body></html>";
-					}
-					else
-					{
-						//there was an error copying the file
-						System.out.println("there was an error copying the file");
-					}
-				}
+			if (file != null && file.length() != 0) {
+				String extension = fileFileName.substring(fileFileName.lastIndexOf(".") + 1);
 
-			} catch (Exception e) {
-				e.printStackTrace();
-				output = "An Error has Occurred";
+				if (extension == null || extension.equals(""))
+					throw new java.lang.IllegalArgumentException("Bad File Extension");
+
+				extension = extension.toLowerCase();
+				String newFileName = getFtpDir() + "/files/pqf/qID_" + auditData.getQuestion().getQuestionID() + "/"
+						+ auditData.getQuestion().getQuestionID() + "_" + contractorID + "." + extension;
+
+				if (copyFile(file, newFileName, true)) {
+
+					auditData.setAnswer(extension);
+
+					AuditData newCopy = null;
+					try {
+						newCopy = dao.findAnswerToQuestion(auditData.getAudit().getId(), auditData.getQuestion()
+								.getQuestionID());
+					} catch (NoResultException newCopyWillStayNullSoWeWillJustInsert) {
+					}
+
+					if (newCopy == null) // insert mode
+					{
+						dao.save(auditData);
+					} else // update mode
+					{
+						if (auditData.getAnswer() != null) // if answer is
+															// being set,
+						// then we are not currently
+						// verifying
+						{
+							if (auditData.getAnswer() == null || !newCopy.getAnswer().equals(auditData.getAnswer())) {
+								newCopy.setDateVerified(null);
+								newCopy.setIsCorrect(null);
+								newCopy.setVerifiedAnswer(null);
+								newCopy.setAnswer(auditData.getAnswer());
+
+								if (newCopy.getAudit().getAuditStatus().equals(AuditStatus.Submitted)) // double
+																										// check
+																										// this
+								{
+									newCopy.setWasChanged(YesNo.Yes);
+								}
+							}
+						} else // we were handed the verification parms instead
+								// of the
+						// edit parms
+						{
+							if (auditData.getVerifiedAnswer() != null) {
+								newCopy.setVerifiedAnswer(auditData.getVerifiedAnswer());
+							}
+
+							if (ActionContext.getContext().getParameters().get("auditData.isCorrect") != null) {
+								if (auditData.getIsCorrect() != newCopy.getIsCorrect()) {
+									if (auditData.isVerified()) {
+										newCopy.setDateVerified(new Date());
+									} else {
+										newCopy.setDateVerified(null);
+									}
+
+									newCopy.setVerified(auditData.isVerified());
+								}
+							}
+						}
+
+						if (auditData.getComment() != null) {
+							newCopy.setComment(auditData.getComment());
+						}
+
+						dao.save(newCopy);
+					}
+
+					output = "<html><body id=\"response\">" + extension + "</body></html>";
+				} else {
+					// there was an error copying the file
+					System.out.println("there was an error copying the file");
+				}
 			}
-		
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			output = "An Error has Occurred";
+		}
+
 		return SUCCESS;
 	}
 
@@ -136,6 +129,5 @@ public class AuditDataFileUpload extends FileUploadActionSupport {
 	public void setContractorID(int contractorID) {
 		this.contractorID = contractorID;
 	}
-	
-	
+
 }
