bdfa24ebcb9acd1017da6194ed749802a816cdfb
Mark Sloan <msloan@picsauditing.com>
2008-09-26 18:23:00 +0000

    Token class

--- src/com/picsauditing/jpa/entities/Token.java
+++ src/com/picsauditing/jpa/entities/Token.java
@@ -1,7 +1,6 @@
 package com.picsauditing.jpa.entities;
 
 import static javax.persistence.GenerationType.IDENTITY;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
