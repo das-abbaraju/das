384735e540df2e381cd3d53083f70399bd34ea8a
Keerthi Nannapaneni <knannapaneni@picsauditing.com>
2008-10-01 20:45:38 +0000

    added setter methods for key, auditID and type.

--- src/com/picsauditing/actions/report/AuditScheduleUpdate.java
+++ src/com/picsauditing/actions/report/AuditScheduleUpdate.java
@@ -26,14 +26,14 @@ public class AuditScheduleUpdate extends PicsActionSupport implements ServletReq
 			addActionError("Missing auditID, invalid URL");
 			return SUCCESS;
 		}
-		
+
 		int auditID = Integer.parseInt(auditIDString);
 		String type = request.getParameter("type");
 		if (type == null || type.length() != 1) {
 			addActionError("Missing type, invalid URL");
 			return SUCCESS;
 		}
-		
+
 		contractorAudit = dao.find(auditID);
 		if (contractorAudit == null) {
 			addActionError("Missing audit, invalid URL");
@@ -43,12 +43,12 @@ public class AuditScheduleUpdate extends PicsActionSupport implements ServletReq
 		if (type.equals("c")) {
 			contractorAudit.setContractorConfirm(new Date());
 			String note = " Confirmed the " + contractorAudit.getAuditType().getAuditName();
-			
+
 			contractorAudit.getContractorAccount().addNote(null, note);
 		}
 		if (type.equals("a"))
 			contractorAudit.setAuditorConfirm(new Date());
-		
+
 		dao.save(contractorAudit);
 		return SUCCESS;
 	}
@@ -57,5 +57,14 @@ public class AuditScheduleUpdate extends PicsActionSupport implements ServletReq
 	public void setServletRequest(HttpServletRequest request) {
 		this.request = request;
 
+	}
+
+	public void setAuditID(String auditID) {
+	}
+
+	public void setType(String type) {
+	}
+
+	public void setKey(String key) {
 	}
 }
