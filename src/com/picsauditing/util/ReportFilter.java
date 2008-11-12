b41af09186e7fc553da9345c2891e0c428b34e14
Keerthi Nannapaneni <knannapaneni@picsauditing.com>
2008-11-12 20:39:55 +0000

    added action name of the page if destination Action is ""

--- src/com/picsauditing/util/ReportFilter.java
+++ src/com/picsauditing/util/ReportFilter.java
@@ -1,54 +1,51 @@
 package com.picsauditing.util;
 
+import org.apache.struts2.ServletActionContext;
+
+import com.opensymphony.xwork2.ActionSupport;
 import com.picsauditing.access.Permissions;
 
-public class ReportFilter {
+public class ReportFilter extends ActionSupport {
 	protected String destinationAction = "";
 	protected boolean ajax = false;
 	protected boolean allowCollapsed = true;
 	protected boolean allowMailMerge = false;
-	
+
 	public boolean isAllowMailMerge() {
 		return allowMailMerge;
 	}
-
 
 	public void setAllowMailMerge(boolean allowMailMerge) {
 		this.allowMailMerge = allowMailMerge;
 	}
 
-
 	public boolean isAllowCollapsed() {
 		return allowCollapsed;
 	}
-
 
 	public void setAllowCollapsed(boolean allowCollapsed) {
 		this.allowCollapsed = allowCollapsed;
 	}
 
-
 	public String getDestinationAction() {
+		if (destinationAction.equals(""))
+			return ServletActionContext.getActionMapping().getName();
 		return destinationAction;
 	}
-
 
 	public void setDestinationAction(String destinationAction) {
 		this.destinationAction = destinationAction;
 	}
 
-
 	public boolean isAjax() {
 		return ajax;
 	}
-
 
 	public void setAjax(boolean ajax) {
 		this.ajax = ajax;
 	}
 
-
 	public void setPermissions(Permissions permissions) {
-		
+
 	}
 }
