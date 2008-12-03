40f79d2660d070ad6576ec833ca6f505da79a836
Keerthi Nannapaneni <knannapaneni@picsauditing.com>
2008-12-03 17:48:00 +0000

    refactored questionID

--- src/com/picsauditing/dao/DesktopMatrixDAO.java
+++ src/com/picsauditing/dao/DesktopMatrixDAO.java
@@ -6,8 +6,6 @@ import javax.persistence.Query;
 
 import org.springframework.transaction.annotation.Transactional;
 
-import com.picsauditing.access.Permissions;
-import com.picsauditing.jpa.entities.AuditType;
 import com.picsauditing.jpa.entities.DesktopMatrix;
 import com.picsauditing.util.Strings;
 
@@ -21,26 +19,26 @@ public class DesktopMatrixDAO extends PicsDAO {
 		}
 		return o;
 	}
-	
+
 	public void remove(int id) {
 		DesktopMatrix row = find(id);
 		remove(row);
-    }
+	}
 
 	public void remove(DesktopMatrix row) {
-        if (row != null) {
-            em.remove(row);
-        }
-    }
+		if (row != null) {
+			em.remove(row);
+		}
+	}
 
 	public DesktopMatrix find(int id) {
-        return em.find(DesktopMatrix.class, id);
-    }
-	
+		return em.find(DesktopMatrix.class, id);
+	}
+
 	@SuppressWarnings("unchecked")
-    public List<DesktopMatrix> findByQuestions(int[] questionIDs) {
+	public List<DesktopMatrix> findByQuestions(int[] questionIDs) {
 		String list = Strings.implode(questionIDs, ",");
-		Query query = em.createQuery("FROM DesktopMatrix t WHERE t.question.questionID IN ("+list+")");
+		Query query = em.createQuery("FROM DesktopMatrix t WHERE t.question.id IN (" + list + ")");
 		return query.getResultList();
 	}
 }
