39b8a2af2851ffaed0e82b02315ea9da4da2a2d6
Trevor Allred <tallred@picsauditing.com>
2008-12-03 17:26:46 +0000

    fix some perms with getting operators if you aren't an admin...not sure why to be honest

--- src/com/picsauditing/actions/operators/OperatorActionSupport.java
+++ src/com/picsauditing/actions/operators/OperatorActionSupport.java
@@ -1,10 +1,12 @@
 package com.picsauditing.actions.operators;
 
+import com.picsauditing.access.OpPerms;
 import com.picsauditing.actions.PicsActionSupport;
 import com.picsauditing.dao.OperatorAccountDAO;
 import com.picsauditing.jpa.entities.OperatorAccount;
 
 public class OperatorActionSupport extends PicsActionSupport {
+	private static final long serialVersionUID = 8967320010000259378L;
 	protected int id;
 	protected OperatorAccountDAO operatorDao;
 	protected OperatorAccount operator;
@@ -23,8 +25,14 @@ public class OperatorActionSupport extends PicsActionSupport {
 
 	public OperatorAccount getOperator() {
 		loadPermissions();
-		if (operator == null)
-			operatorDao.find(id);
+		if (operator == null) {
+			if (!permissions.hasPermission(OpPerms.AllOperators))
+				if (permissions.isOperator())
+					id = permissions.getAccountId();
+				else
+					return null;
+			operator = operatorDao.find(id);
+		}
 		return operator;
 	}
 
