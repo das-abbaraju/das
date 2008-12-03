e888f94331eb0f7615ad89ce4afd69257b79ca9d
Trevor Allred <tallred@picsauditing.com>
2008-12-03 20:56:13 +0000

    implement new report structure

--- src/com/picsauditing/actions/report/ReportEmrRates.java
+++ src/com/picsauditing/actions/report/ReportEmrRates.java
@@ -11,27 +11,22 @@ public class ReportEmrRates extends ReportAnnualAddendum {
 		permissions.tryPermission(OpPerms.EMRReport);
 		super.checkPermissions();
 	}
-	
+
 	@Override
 	public void buildQuery() {
 		super.buildQuery();
+		
+		forwardSingleResults = false;
+		getFilter().setShowEmrRange(true);
+		getFilter().setShowAuditFor(true);
+		
 		sql.addJoin("JOIN pqfdata d ON d.auditID = ca.auditID");
 		sql.addField("d.answer");
-		
-		int questionID = AuditQuestion.EMR;
-		
-		sql.addWhere("d.questionID = " + questionID);
+
+		sql.addWhere("d.questionID = " + AuditQuestion.EMR);
 		sql.addWhere("d.answer >= " + getFilter().getMinEMR());
 		sql.addWhere("d.answer < " + getFilter().getMaxEMR());
 		sql.addWhere("d.answer > ''");
 	}
-	
-	public String execute() throws Exception {
-	
-		getFilter().setShowEmrRange(true);
-		getFilter().setShowAuditFor(true);
-		forwardSingleResults = false;
 
-		return super.execute2();
-	}
 }
