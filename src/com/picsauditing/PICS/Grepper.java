8d418328ef80d542548485c4eca1bc55afa55651
Jeff Shelley <jshelley@picsauditing.com>
2008-12-04 23:40:44 +0000

    added grepper

---
+++ src/com/picsauditing/PICS/Grepper.java
@@ -0,0 +1,24 @@
+package com.picsauditing.PICS;
+
+import java.util.List;
+import java.util.Vector;
+
+public abstract class Grepper <T> {
+	public abstract boolean check( T t );
+	
+	
+	public List<T> grep( List<T> in ) {
+		
+		List<T> output = new Vector<T>();
+		
+		for( T t : in ) {
+			if( check(t)) {
+				output.add(t);
+			}
+		}
+		return output;
+	}
+	
+	
+	
+}
