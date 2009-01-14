d352d1ef1fde37571633b598e2239e0621ce3e2b
Jeff Shelley <jshelley@picsauditing.com>
2009-01-14 22:05:52 +0000

    added for pruning

---
+++ src/com/picsauditing/util/AnswerMapByAuditPruner.java
@@ -0,0 +1,12 @@
+package com.picsauditing.util;
+
+import com.picsauditing.jpa.entities.ContractorAudit;
+import com.picsauditing.jpa.entities.OperatorAccount;
+
+public class AnswerMapByAuditPruner {
+
+	public AnswerMapByAuditIds<ContractorAudit> prune( AnswerMapByAuditIds<ContractorAudit> data, OperatorAccount operator ) {
+		
+		return data;
+	}
+}
