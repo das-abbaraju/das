2f802608a3515cfb4d6c9e79e8b749cdaa68d8ee
Jeff Shelley <jshelley@picsauditing.com>
2009-01-22 00:35:33 +0000

    moved JPA exception into the DAO and made it @Transactional

--- src/com/picsauditing/dao/ContractorAuditOperatorDAO.java
+++ src/com/picsauditing/dao/ContractorAuditOperatorDAO.java
@@ -1,7 +1,14 @@
 package com.picsauditing.dao;
 
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+
+import org.springframework.transaction.annotation.Transactional;
+
 import com.picsauditing.jpa.entities.ContractorAuditOperator;
 
+@Transactional
+@SuppressWarnings("unchecked")
 public class ContractorAuditOperatorDAO extends PicsDAO {
 	public ContractorAuditOperator save(ContractorAuditOperator o) {
 		if (o.getId() == 0) {
@@ -12,6 +19,22 @@ public class ContractorAuditOperatorDAO extends PicsDAO {
 		return o;
 	}
 
+	
+	public ContractorAuditOperator find(int auditId, int operatorId) {
+		Query query = em
+				.createQuery("SELECT t FROM ContractorAuditOperator t where t.audit.id = ? and t.operator.id = ? " );
+		query.setParameter(1, auditId);
+		query.setParameter(2, operatorId);
+		
+		try {
+			return (ContractorAuditOperator) query.getSingleResult();
+		}
+		catch( NoResultException nre ) {
+			return null;
+		}
+	}
+	
+	
 	public void remove(int id) {
 		ContractorAuditOperator row = find(id);
 		remove(row);
