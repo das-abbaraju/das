88fa8882c86ad9e3fef9d2ba0b9f6960d5242561
Jeff Shelley <jshelley@picsauditing.com>
2009-01-22 18:11:29 +0000

    filled in some details

--- src/com/picsauditing/PICS/FlagCalculationBuilder.java
+++ src/com/picsauditing/PICS/FlagCalculationBuilder.java
@@ -1,5 +1,25 @@
 package com.picsauditing.PICS;
 
+import java.util.List;
+
+import com.picsauditing.jpa.entities.FlagQuestionCriteria;
+import com.picsauditing.util.AnswerMapByAudits;
+
 public class FlagCalculationBuilder {
+	
+	protected AnswerMapByAudits answerMapByAudits = null;
+	protected List<FlagQuestionCriteria> criteria = null;
+	
+	
+	public FlagCalculationBuilder( AnswerMapByAudits answerMapByAudits, List<FlagQuestionCriteria> criteria) {
+		this.answerMapByAudits = answerMapByAudits;
+		this.criteria = criteria;
+	}
+	
+	
+	public List<FlagCalculationUnit> getFlagCalculationUnits() {
+		
+		return null;
+	}
 
 }
