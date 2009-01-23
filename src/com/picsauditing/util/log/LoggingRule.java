3c5c3acc38add80ce67ffb36faad4e1a7d031d90
Jeff Shelley <jshelley@picsauditing.com>
2009-01-23 23:14:21 +0000

    added compare method which sorts by: 		( name.length() descending, name )

--- src/com/picsauditing/util/log/LoggingRule.java
+++ src/com/picsauditing/util/log/LoggingRule.java
@@ -1,6 +1,6 @@
 package com.picsauditing.util.log;
 
-public class LoggingRule {
+public class LoggingRule implements Comparable<LoggingRule> {
 	protected String name = null;
 	protected boolean logged = false;
 
@@ -14,5 +14,17 @@ public class LoggingRule {
 	}
 	public boolean isLogged() {
 		return logged;
+	}
+	
+	@Override
+	public int compareTo(LoggingRule o) {
+		if( o == null || o.getName() == null ) return 1;
+		
+		int cmp = new Integer(this.getName().length()).compareTo(o.getName().length());
+		if( cmp == 0 ) {
+			return this.getName().compareTo( o.getName() );
+		}
+		
+		return -1 * cmp;
 	}
 }
