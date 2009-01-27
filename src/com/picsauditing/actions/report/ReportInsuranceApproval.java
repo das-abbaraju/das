f0f569dc8f340253be97fa09fec4fa35690220bd
Jeff Shelley <jshelley@picsauditing.com>
2009-01-27 20:14:55 +0000

    removed sort to accept the default sorting.

--- src/com/picsauditing/actions/report/ReportInsuranceApproval.java
+++ src/com/picsauditing/actions/report/ReportInsuranceApproval.java
@@ -41,8 +41,6 @@ public class ReportInsuranceApproval extends ReportContractorAudits {
 		// sql = new SelectContractorAudit();
 		this.auditDataDao = auditDataDao;
 		this.auditQuestionDao = auditQuestionDao;
-		
-		orderByDefault = "ca.createdDate";
 	}
 
 	@Override
