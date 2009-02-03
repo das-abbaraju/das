ae6002a6bc5850b5347a5ca934106aba4e3d8d03
Keerthi Nannapaneni <knannapaneni@picsauditing.com>
2009-02-03 23:40:31 +0000

    added Resubmitted status to policy verification report.

--- src/com/picsauditing/access/PicsMenu.java
+++ src/com/picsauditing/access/PicsMenu.java
@@ -115,7 +115,7 @@ public class PicsMenu {
 		if(permissions.hasPermission(OpPerms.InsuranceCerts))
 			subMenu.addChild("Policy List", "ReportPolicyList.action");
 		if(permissions.hasPermission(OpPerms.InsuranceVerification))
-			subMenu.addChild("InsureGuard Verification", "PolicyVerification.action?filter.visible=Y&filter.auditStatus=Submitted");
+			subMenu.addChild("InsureGuard Verification", "PolicyVerification.action?filter.visible=Y&filter.auditStatus=Submitted&filter.auditStatus=Resubmitted");
 		if (permissions.hasPermission(OpPerms.InsuranceApproval))
 			subMenu.addChild("Insurance Approval", "ReportInsuranceApproval.action?filter.caoStatus=Awaiting&filter.auditStatus=Submitted&filter.auditStatus=Active&filter.auditStatus=Resubmitted");
 		if (permissions.hasPermission(OpPerms.InsuranceApproval))
