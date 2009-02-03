b6c7b145184231fe409503f97ea8f5069e08727b
Mark Sloan <msloan@picsauditing.com>
2009-02-03 00:33:45 +0000

    Billing pages.

--- src/com/picsauditing/actions/report/ReportBilling.java
+++ src/com/picsauditing/actions/report/ReportBilling.java
@@ -10,17 +10,9 @@ public class ReportBilling extends ReportAccount {
 	
 	public void buildQuery() {
 		super.buildQuery();
-		sql.addField("c.payingFacilities");
-		sql.addField("c.paymentMethod");
-		sql.addField("c.paymentMethodStatus");
 		sql.addField("c.membershipDate");
-		sql.addField("c.paymentExpires");
-		sql.addField("c.lastInvoiceDate");
-		sql.addField("c.lastPayment");
-		sql.addField("c.billingAmount");
 		sql.addField("c.newBillingAmount");
 		
-		sql.addWhere("mustPay='Yes'");
 		sql.addWhere("a.active = 'Y'");
 	}
 }
