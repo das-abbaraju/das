fa9cf1c8fc3163c1e419dd23f9b8d7435351a2dc
Trevor Allred <tallred@picsauditing.com>
2009-02-04 18:22:25 +0000

    added where clause

--- src/com/picsauditing/actions/report/ReportBilling.java
+++ src/com/picsauditing/actions/report/ReportBilling.java
@@ -11,8 +11,23 @@ public class ReportBilling extends ReportAccount {
 	public void buildQuery() {
 		super.buildQuery();
 		sql.addField("c.membershipDate");
-		sql.addField("c.newBillingAmount");
+		sql.addField("f2.fee");
+		sql.addJoin("LEFT JOIN invoice_fee f1 ON c.membershipLevelID = f1.id");
+		sql.addJoin("LEFT JOIN invoice_fee f2 ON c.newMembershipLevelID = f2.id");
 		
-		sql.addWhere("a.active = 'Y'");
+		sql.addWhere("c.mustPay = 'Yes'");
+		
+		// Show activations and reactivations
+		String where = "(a.active = 'N' AND c.renew = 1)";
+		// Show renewals
+		where += " OR (a.active = 'Y' AND c.renew = 1 AND c.paymentExpires < NOW())";
+		// Show upgrades
+		where += " OR (a.active = 'Y' AND f2.fee > f1.fee)";
+		sql.addWhere(where);
+		
+		// A note about non-renewal upgrades: just because a contractor doesn't want to renew
+		// at the end of the year, doesn't mean they shouldn't be charged an upgrade fee.
+		// However, we (the billing department) should be vigilent about reviewing upgrades 
+		// on non-renewable contractor accounts.
 	}
 }
