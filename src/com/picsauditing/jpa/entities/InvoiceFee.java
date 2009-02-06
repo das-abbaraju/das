f3c50e81430017addc4c79121281e811b4aefd50
Mark Sloan <msloan@picsauditing.com>
2009-02-06 20:50:35 +0000

    Billing Detail iteration.

--- src/com/picsauditing/jpa/entities/InvoiceFee.java
+++ src/com/picsauditing/jpa/entities/InvoiceFee.java
@@ -10,9 +10,11 @@ import javax.persistence.Table;
 @Table(name = "invoice_fee")
 public class InvoiceFee extends BaseTable implements java.io.Serializable {
 	public final static int ACTIVATION = 1;
+	public final static int REACTIVATION = 2;
 	private String fee;
 	private int amount;
 	private boolean visible = true;
+	private String feeClass;
 
 	public String getFee() {
 		return fee;
@@ -37,6 +39,14 @@ public class InvoiceFee extends BaseTable implements java.io.Serializable {
 
 	public void setVisible(boolean visible) {
 		this.visible = visible;
+	}
+
+	public String getFeeClass() {
+		return feeClass;
+	}
+
+	public void setFeeClass(String feeClass) {
+		this.feeClass = feeClass;
 	}
 
 }
