b938bc95a6a2e909f9f93b9da4c0226710ddf38d
Mark Sloan <msloan@picsauditing.com>
2009-02-06 20:49:50 +0000

    Billing Detail iteration.

--- src/com/picsauditing/actions/contractors/BillingDetail.java
+++ src/com/picsauditing/actions/contractors/BillingDetail.java
@@ -1,11 +1,15 @@
 package com.picsauditing.actions.contractors;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import com.picsauditing.PICS.DateBean;
 import com.picsauditing.dao.AccountDAO;
 import com.picsauditing.dao.ContractorAccountDAO;
 import com.picsauditing.dao.ContractorAuditDAO;
 import com.picsauditing.dao.InvoiceDAO;
+import com.picsauditing.dao.InvoiceFeeDAO;
 import com.picsauditing.dao.InvoiceItemDAO;
 import com.picsauditing.jpa.entities.Invoice;
 import com.picsauditing.jpa.entities.InvoiceFee;
@@ -18,12 +22,22 @@ public class BillingDetail extends ContractorActionSupport {
 	private InvoiceFee currentMemebershipFee = null;
 	private InvoiceFee newMembershipFee = null;
 	private InvoiceFee activationFee = null;
+	private InvoiceDAO invoiceDAO = new InvoiceDAO();
+	private InvoiceItemDAO invoiceItemDAO = new InvoiceItemDAO();
+	private InvoiceFeeDAO invoiceFeeDAO;
+	private int invoiceTotal = 0;
+	
+	private List<InvoiceItem> invoiceItems = new ArrayList<InvoiceItem>();
 	 
 	private OperatorAccount requestedBy = null;
 
 	public BillingDetail(ContractorAccountDAO accountDao,
-			ContractorAuditDAO auditDao) {
+			ContractorAuditDAO auditDao, InvoiceDAO invoiceDAO,
+			InvoiceItemDAO invoiceItemDAO, InvoiceFeeDAO invoiceFeeDAO) {
 		super(accountDao, auditDao);
+		this.invoiceDAO = invoiceDAO;
+		this.invoiceItemDAO = invoiceItemDAO;
+		this.invoiceFeeDAO = invoiceFeeDAO;
 	}
 
 	public String execute() throws Exception {
@@ -34,34 +48,86 @@ public class BillingDetail extends ContractorActionSupport {
 		currentMemebershipFee = contractor.getMembershipLevel();
 		newMembershipFee = contractor.getNewMembershipLevel();
 		
-		this.subHeading = "Billing Detail";		
+		String billingStatus = contractor.getBillingStatus();
+		boolean firstUpgrade = true;
 		
-		if (button != null) {
-			if (button.equalsIgnoreCase("Create")) {
-				List<Invoice> contractorInvoices = contractor.getInvoices();
-				Invoice invoice = new Invoice();
-				InvoiceItem invoiceItem = new InvoiceItem();
-				InvoiceDAO invoiceDAO = new InvoiceDAO();
-				InvoiceItemDAO invoiceItemDAO = new InvoiceItemDAO();
+		invoiceItems.clear();
+		
+		// For Activation Fee and New Membership
+		if ("Activation".equals(billingStatus)) {
+			InvoiceFee fee = invoiceFeeDAO.find(InvoiceFee.ACTIVATION); // create Activation fee
+			
+			invoiceItems.add(new InvoiceItem(newMembershipFee)); // add the Membership fee
+			invoiceItems.add(new InvoiceItem(fee)); // add the Activation fee
+		}
+		
+		// For Reactivation Fee and Reactivating Membership
+		if ("Reactivation".equals(contractor.getBillingStatus())) {
+			InvoiceFee fee = invoiceFeeDAO.find(InvoiceFee.REACTIVATION); // create Reactivation fee
+			
+			invoiceItems.add(new InvoiceItem(newMembershipFee)); // add the Membership fee
+			invoiceItems.add(new InvoiceItem(fee)); // add the Reactivation fee
+		}
+		
+		// For Upgrades
+		if ("Upgrade".equals(contractor.getBillingStatus())) {
+			int currAmt = currentMemebershipFee.getAmount();
+			int newAmt = newMembershipFee.getAmount();
+			int currId = currentMemebershipFee.getId();
+			
+			// Loop through currentMembershipFee and newMembershipFee to grab
+			// Upgrade levels
+			while (currAmt < newAmt){
+				if (firstUpgrade) {					
+					currId += 7;
+					firstUpgrade = false;
+				}
+				else
+					currId += 1;
 				
-				contractor.setNewMembershipLevel(newMembershipFee);
-				invoiceItem.setInvoiceFee(newMembershipFee);
+				InvoiceFee fee = invoiceFeeDAO.find(currId);
+				invoiceItems.add(new InvoiceItem(fee));
 				
-				contractorInvoices.add(invoice);
-				
-				invoiceItemDAO.save(invoiceItem);
-				invoiceDAO.save(invoice);
-				accountDao.save(contractor);
-				addActionMessage("Invoice " + invoice.getId() + " has been created.");
-			}
-			else{
-				// Because there are anomalies between browsers and how they
-				// pass in the button values, this is a catch all so we can get
-				// notified when the button name isn't set correctly
-				throw new Exception("no button action found called " + button);
+				currAmt += fee.getAmount();
 			}
 		}
+		
+		// For Renewals
+		if ("Renewal".equals(contractor.getBillingStatus())){
+			invoiceItems.add(new InvoiceItem(currentMemebershipFee));
+		}
 
+		invoiceTotal = 0;
+		for (InvoiceItem item : invoiceItems)
+			invoiceTotal += item.getAmount();
+		
+		if ("Create".equalsIgnoreCase(button)) {
+			
+			Invoice invoice = new Invoice();
+			invoice.setAccount(contractor);
+			invoice.setDueDate(DateBean.addDays(new Date(), Invoice.daysUntilDue));
+			invoice.setPaid(false);
+			invoice.setItems(invoiceItems);
+			invoice.setAuditColumns(getUser());
+			
+			invoiceDAO.save(invoice);
+			contractor.getInvoices().add(invoice);
+			
+			for (InvoiceItem item : invoiceItems) {
+				item.setInvoice(invoice);
+				item.setAuditColumns(getUser());
+				invoiceItemDAO.save(item);
+			}
+			invoice.setTotalAmount(invoiceTotal);
+			
+			//contractor.setMembershipLevel(contractor.getNewMembershipLevel());
+			//TODO contractor.updateBalance();
+			accountDao.save(contractor);
+			
+			addActionMessage("Invoice " + invoice.getId() + " has been created.");
+		}
+
+		this.subHeading = "Billing Detail";
 		return SUCCESS;
 	}
 
@@ -98,4 +164,18 @@ public class BillingDetail extends ContractorActionSupport {
 	public void setActivationFee(InvoiceFee activationFee) {
 		this.activationFee = activationFee;
 	}
+
+	public List<InvoiceItem> getInvoiceItems() {
+		return invoiceItems;
+	}
+
+	public void setInvoiceItems(List<InvoiceItem> invoiceItems) {
+		this.invoiceItems = invoiceItems;
+	}
+
+	public int getInvoiceTotal() {
+		return invoiceTotal;
+	}
+
+
 }
