ac7ae6bb9a5f002b7b6dc376fee4e90158f4a5db
Mark Sloan <msloan@picsauditing.com>
2009-02-07 00:56:51 +0000

    Invoice Detail

--- src/com/picsauditing/actions/contractors/InvoiceDetail.java
+++ src/com/picsauditing/actions/contractors/InvoiceDetail.java
@@ -1,22 +1,66 @@
 package com.picsauditing.actions.contractors;
 
-import com.picsauditing.dao.ContractorAccountDAO;
-import com.picsauditing.dao.ContractorAuditDAO;
+import java.util.Date;
+
+import com.picsauditing.access.NoRightsException;
+import com.picsauditing.access.OpPerms;
+import com.picsauditing.actions.PicsActionSupport;
+import com.picsauditing.dao.InvoiceDAO;
+import com.picsauditing.dao.NoteDAO;
+import com.picsauditing.jpa.entities.Invoice;
 
 @SuppressWarnings("serial")
-public class InvoiceDetail extends ContractorActionSupport {
-	public InvoiceDetail(ContractorAccountDAO accountDao,
-			ContractorAuditDAO auditDao) {
-		super(accountDao, auditDao);
+public class InvoiceDetail extends PicsActionSupport {
+	private int id;
+	private InvoiceDAO invoiceDAO;
+	private NoteDAO noteDAO;
+	private Invoice invoice;
+	
+	public InvoiceDetail(InvoiceDAO invoiceDAO) {
+		this.invoiceDAO = invoiceDAO;
 	}
 
 	public String execute() throws Exception {
 		if (!forceLogin())
 			return LOGIN;
-
-		this.findContractor();
-
-		this.subHeading = "Invoice Detail";
+		
+		invoice = invoiceDAO.find(id);
+		
+		if (!permissions.hasPermission(OpPerms.AllContractors)
+				&& permissions.getAccountId() != invoice.getAccount().getId()) {
+			throw new NoRightsException("You can't view this invoice");
+		}
+		
+		if ("ChargeCard".equals(button)) {
+			// TODO process the credit card
+			String transactionID = "RETURN IT HERE";
+			invoice.setPaid(true);
+			invoice.setPaidDate(new Date());
+			invoice.setTransactionID(transactionID);
+			invoice.setAuditColumns(getUser());
+			invoiceDAO.save(invoice);
+			// also set the notecategory to Billing
+			noteDAO.addPicsAdminNote(invoice.getAccount(), getUser(), "Paid the invoice");
+		}
+		
 		return SUCCESS;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Invoice getInvoice() {
+		return invoice;
+	}
+
+	public void setInvoice(Invoice invoice) {
+		this.invoice = invoice;
+	}
+	
+	
 }
