2ae938a67c7f2039b1156bdec4fa9d38762da850
Mark Sloan <msloan@picsauditing.com>
2009-02-10 21:38:22 +0000

    Invoice detail.

--- src/com/picsauditing/actions/contractors/InvoiceDetail.java
+++ src/com/picsauditing/actions/contractors/InvoiceDetail.java
@@ -6,12 +6,13 @@ import com.picsauditing.PICS.BrainTreeService;
 import com.picsauditing.access.NoRightsException;
 import com.picsauditing.access.OpPerms;
 import com.picsauditing.actions.PicsActionSupport;
+import com.picsauditing.dao.AccountDAO;
 import com.picsauditing.dao.AppPropertyDAO;
+import com.picsauditing.dao.ContractorAccountDAO;
 import com.picsauditing.dao.InvoiceDAO;
 import com.picsauditing.dao.NoteDAO;
-import com.picsauditing.jpa.entities.Account;
+import com.picsauditing.jpa.entities.ContractorAccount;
 import com.picsauditing.jpa.entities.Invoice;
-import com.picsauditing.jpa.entities.PaymentMethod;
 import com.picsauditing.util.Strings;
 
 @SuppressWarnings("serial")
@@ -20,15 +21,21 @@ public class InvoiceDetail extends PicsActionSupport {
 	private InvoiceDAO invoiceDAO;
 	private String response;	
 	private String responsetext;
-	private String transactionId;	
+	private String transactionid;	
 	private NoteDAO noteDAO;
 	private Invoice invoice;
+	private ContractorAccount contractor = new ContractorAccount();
+	private AccountDAO acctDAO;
+	private ContractorAccountDAO conAccountDAO;
 	
 	AppPropertyDAO appPropDao;
 	
-	public InvoiceDetail(InvoiceDAO invoiceDAO, AppPropertyDAO appPropDao) {
+	public InvoiceDetail(InvoiceDAO invoiceDAO, AppPropertyDAO appPropDao, AccountDAO acctDAO, NoteDAO noteDAO, ContractorAccountDAO conAccountDAO) {
 		this.invoiceDAO = invoiceDAO;
 		this.appPropDao = appPropDao;
+		this.acctDAO = acctDAO;
+		this.noteDAO = noteDAO;
+		this.conAccountDAO = conAccountDAO;
 	}
 
 	public String execute() throws Exception {
@@ -42,12 +49,14 @@ public class InvoiceDetail extends PicsActionSupport {
 			throw new NoRightsException("You can't view this invoice");
 		}
 		
-		BrainTreeService processPayment = new BrainTreeService();
-		processPayment.setUserName(appPropDao.find("brainTree.username").getValue());
-		processPayment.setPassword(appPropDao.find("brainTree.password").getValue());		
+		contractor = (ContractorAccount) acctDAO.find(invoice.getAccount().getId()); // do i have to do this or can i access it some other way?
 		
-		if ("Charge".equals(button)) {
-			processPayment.procesPayment(invoice.getAccount().getId(), invoice.getTotalAmount());
+		BrainTreeService paymentService = new BrainTreeService();
+		paymentService.setUserName(appPropDao.find("brainTree.username").getValue());
+		paymentService.setPassword(appPropDao.find("brainTree.password").getValue());		
+		
+		if ("Charge".equals(button) && !contractor.getPaymentMethodStatus().equals("Missing")) {
+			paymentService.procesPayment(contractor.getId(), invoice.getTotalAmount());
 
 			if (!Strings.isEmpty(responsetext) && !response.equals("1")) {
 				String errorMessage = responsetext;
@@ -61,9 +70,13 @@ public class InvoiceDetail extends PicsActionSupport {
 			} else {
 				invoice.setPaid(true);
 				invoice.setPaidDate(new Date());
-				invoice.setTransactionID(transactionId);
+				invoice.setTransactionID(transactionid);
 				invoice.setAuditColumns(getUser());
 				invoiceDAO.save(invoice);
+				
+				int conBalance = contractor.getBalance();
+				contractor.setBalance(conBalance - invoice.getTotalAmount());
+				conAccountDAO.save(contractor);
 				
 				noteDAO.addPicsAdminNote(invoice.getAccount(), getUser(), "Paid the invoice");
 				addActionMessage("Transaction Successfully Processed. Invoice " + invoice.getId() + " has been Paid.");
@@ -105,12 +118,16 @@ public class InvoiceDetail extends PicsActionSupport {
 		this.responsetext = responsetext;
 	}
 
-	public String getTransactionId() {
-		return transactionId;
+	public String getTransactionid() {
+		return transactionid;
 	}
 
-	public void setTransactionId(String transactionId) {
-		this.transactionId = transactionId;
+	public void setTransactionid(String transactionid) {
+		this.transactionid = transactionid;
+	}
+
+	public ContractorAccount getContractor() {
+		return contractor;
 	}
 
 }
