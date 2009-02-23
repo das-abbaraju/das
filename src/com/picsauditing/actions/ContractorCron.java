b8b98eceb7f43a6ba638b7bfa33601c2f4346b37
Jeff Shelley <jshelley@picsauditing.com>
2009-02-23 21:02:48 +0000

    removed metrics code for push

--- src/com/picsauditing/actions/ContractorCron.java
+++ src/com/picsauditing/actions/ContractorCron.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import java.util.List;
 
 import com.picsauditing.PICS.FlagCalculator2;
+import com.picsauditing.cron.CronMetricsAggregator;
 import com.picsauditing.dao.ContractorAccountDAO;
 import com.picsauditing.util.log.PicsLogger;
 
@@ -14,37 +15,43 @@ public class ContractorCron extends PicsActionSupport {
 
 	protected FlagCalculator2 flagCalculator = null;
 	protected ContractorAccountDAO contractorAccountDAO = null;
+	//protected CronMetricsAggregator cronMetrics = null;
 
 	protected long startTime = 0L;
 
-	public ContractorCron(FlagCalculator2 fc2, ContractorAccountDAO contractorAccountDAO) {
+	public ContractorCron(FlagCalculator2 fc2, ContractorAccountDAO contractorAccountDAO /*, CronMetricsAggregator cronMetrics */) {
 		this.flagCalculator = fc2;
 		this.contractorAccountDAO = contractorAccountDAO;
+//		this.cronMetrics = cronMetrics;
 	}
 
 	public String execute() throws Exception {
 
-		PicsLogger.start("contractorCron");
-
-		PicsLogger.log("Starting ContractorCron Job at: ");
-		PicsLogger.log(new Date().toString());
-		try {
-			startTask("Running contractorCron...");
-
-			List<Integer> conIDsList = contractorAccountDAO.findContractorsNeedingRecalculation();
-
-			if( conIDsList != null && conIDsList.size() > 0 ) {
-				flagCalculator.runByContractors(conIDsList);
+		//if( cronMetrics.startJob() ) {
+		
+			PicsLogger.start("contractorCron");
+	
+			PicsLogger.log("Starting ContractorCron Job at: ");
+			PicsLogger.log(new Date().toString());
+			try {
+				startTask("Running contractorCron...");
+	
+				List<Integer> conIDsList = contractorAccountDAO.findContractorsNeedingRecalculation();
+	
+				if( conIDsList != null && conIDsList.size() > 0 ) {
+					flagCalculator.runByContractors(conIDsList);
+				}
+	
+				endTask();
+			} catch (Throwable t) {
+				handleException(t);
 			}
-
-			endTask();
-		} catch (Throwable t) {
-			handleException(t);
-		}
-		PicsLogger.log("Completed ContractorCron Job at: ");
-		PicsLogger.log(new Date().toString());
-
-		PicsLogger.stop();
+			PicsLogger.log("Completed ContractorCron Job at: ");
+			PicsLogger.log(new Date().toString());
+	
+			PicsLogger.stop();
+			//cronMetrics.stopJob();
+		//}
 		return SUCCESS;
 	}
 
