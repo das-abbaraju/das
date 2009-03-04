20f011ce49cc287cb72bf7c6595901f13302d45d
Keerthi Nannapaneni <knannapaneni@picsauditing.com>
2009-03-04 22:09:09 +0000

    updated the script to remove the account_name for legal names

--- deployments/upgrade_version2.sql
+++ deployments/upgrade_version2.sql
@@ -5,25 +5,8 @@ update pqfquestions set isGroupedWithPrevious = CASE isGroupedWithPrevious WHEN 
 update pqfquestions set isRedFlagQuestion = CASE isRedFlagQuestion WHEN 2 THEN 1 ELSE 0 END;
 */
 
-
 /**
-* adding the contractor count flag for corporate users
-*/
-insert into widget_user
-values (null,1,646,1,1,10,null)
-
-/**
- * add a new widget for Contractor Cron Statistics for admins  
+ * delete all the account_name All.
  */
-insert into widget values 
-(null,"Contractor Cron Statistics","Html",0,"ContractorCronStatisticsAjax.action","DevelopmentEnvironment",null);
 
-insert into widget_user
-values (null,16,941,1,2,40,null);
-
-/**
-update Delinquent Contractor Accounts permisission on widget 
-**/
-update widget set requiredPermission = "DelinquentAccounts"
-where widgetID = 15;
-
+delete from account_name where name = 'All'
