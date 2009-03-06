71667ffafc4b6dfa6f359f6fe6eafcdee77236e0
Jeff Shelley <jshelley@picsauditing.com>
2009-03-06 22:13:39 +0000

    took out some of the extra, vestigial stuff.

--- src/com/picsauditing/actions/contractors/ContractorCronStatistics.java
+++ src/com/picsauditing/actions/contractors/ContractorCronStatistics.java
@@ -1,17 +1,12 @@
 package com.picsauditing.actions.contractors;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.picsauditing.actions.PicsActionSupport;
-import com.picsauditing.cron.CronMetrics;
 import com.picsauditing.cron.CronMetricsAggregator;
 import com.picsauditing.cron.CronReportingPeriod;
 
 @SuppressWarnings("serial")
 public class ContractorCronStatistics extends PicsActionSupport {
 	CronMetricsAggregator metricsAggregator;
-	List<CronMetrics> cronMList = new ArrayList<CronMetrics>(); 
 
 	public ContractorCronStatistics(CronMetricsAggregator metricsAggregator) {
 		this.metricsAggregator = metricsAggregator;
@@ -22,24 +17,7 @@ public class ContractorCronStatistics extends PicsActionSupport {
 		if (!permissions.isLoggedIn())
 			return LOGIN_AJAX;
 		
-		cronMList.add(0, metricsAggregator.getMetrics(CronReportingPeriod.TenMinutes));
-		cronMList.add(1, metricsAggregator.getMetrics(CronReportingPeriod.ThirtyMinutes));
-		cronMList.add(2, metricsAggregator.getMetrics(CronReportingPeriod.OneHour));
-		cronMList.add(3, metricsAggregator.getMetrics(CronReportingPeriod.TwoHours));
-		cronMList.add(4, metricsAggregator.getMetrics(CronReportingPeriod.ThreeHours));
-		cronMList.add(5, metricsAggregator.getMetrics(CronReportingPeriod.SixHours));
-		cronMList.add(6, metricsAggregator.getMetrics(CronReportingPeriod.TwelveHours)); 		
-		cronMList.add(7, metricsAggregator.getMetrics(CronReportingPeriod.TwentyFourHours)); 		
-
 		return SUCCESS;
-	}
-
-	public List<CronMetrics> getCronMList() {
-		return cronMList;
-	}
-
-	public void setCronMList(List<CronMetrics> cronMList) {
-		this.cronMList = cronMList;
 	}
 
 	public CronMetricsAggregator getMetricsAggregator() {
