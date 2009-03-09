6763566c36fb3f94cd5e2b66c1679c63067c886b
Jeff Shelley <jshelley@picsauditing.com>
2009-03-09 23:17:43 +0000

    added SOAP service

---
+++ src/com/picsauditing/quickbooks/QBWebConnectorSvc.java
@@ -0,0 +1,16 @@
+/**
+ * QBWebConnectorSvc.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package com.picsauditing.quickbooks;
+
+public interface QBWebConnectorSvc extends javax.xml.rpc.Service {
+    public java.lang.String getQBWebConnectorSvcSoapAddress();
+
+    public com.picsauditing.quickbooks.QBWebConnectorSvcSoap getQBWebConnectorSvcSoap() throws javax.xml.rpc.ServiceException;
+
+    public com.picsauditing.quickbooks.QBWebConnectorSvcSoap getQBWebConnectorSvcSoap(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+}
