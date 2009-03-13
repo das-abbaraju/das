6e37bb4289a0e719eab04f1ffde00035adafe63d
Trevor Allred <tallred@picsauditing.com>
2009-03-13 23:09:46 +0000

    not ready...started the chart stuff

--- src/com/picsauditing/actions/chart/ChartFlagCountCorp.java
+++ src/com/picsauditing/actions/chart/ChartFlagCountCorp.java
@@ -5,56 +5,53 @@ import java.util.List;
 import com.picsauditing.access.OpPerms;
 import com.picsauditing.jpa.entities.FlagColor;
 import com.picsauditing.search.SelectSQL;
-import com.picsauditing.util.chart.ChartSingleSeries;
+import com.picsauditing.util.chart.ChartMultiSeries;
 import com.picsauditing.util.chart.DataRow;
 import com.picsauditing.util.chart.Set;
 
-/**
- * Get a count of each flag color for this operator
- * 
- * @author Trevor
- */
 @SuppressWarnings("serial")
-public class ChartFlagCount extends ChartSSAction {
+public class ChartFlagCountCorp extends ChartMSAction {
 
-	@Override
-	public ChartSingleSeries buildChart() throws Exception {
-
+	public ChartMultiSeries buildChart() {
+		if (!permissions.isCorporate() && !permissions.seesAllContractors())
+			return chart;
+		
 		chart.setShowValues(true);
 
 		SelectSQL sql = new SelectSQL("accounts a");
 		sql.addField("flag as label");
+		sql.addField("o.name as series");
 		sql.addField("count(*) as value");
-		sql.addGroupBy("label");
-		sql.addOrderBy("label");
+		sql.addGroupBy("series, label");
+		sql.addOrderBy("series, label");
 
 		sql.addJoin("JOIN generalcontractors gc ON a.id = gc.subID");
+		sql.addJoin("JOIN accounts o ON o.id = gc.genID");
 		sql.addJoin("JOIN flags f ON a.id = f.conID AND f.opID = gc.genID");
 		sql.addWhere("a.active = 'Y'");
-		if (permissions.isCorporate()) {
-		// // TODO Handle unapproved contractors for operators who manually
-		// approve
-		// sql.addJoin("JOIN facilities fac ON fac.opID = f.opID AND
-		// fac.corporateID = " + permissions.getAccountId());
+		if (permissions.isCorporate())
 			sql.addJoin("JOIN facilities fac ON fac.opID = gc.genID AND fac.corporateID = " + permissions.getAccountId());
-		}
-
-		if (permissions.isOperator()) {
-			sql.addWhere("gc.genID = " + permissions.getAccountId());
-				if (permissions.isApprovesRelationships() && !permissions.hasPermission(OpPerms.ViewUnApproved)) {
-					sql.addWhere("gc.workStatus = 'Y'");
-				}
-		}
 
 		ChartDAO db = new ChartDAO();
 		List<DataRow> data = db.select(sql.toString());
+		// Get the category set first
+		MultiSeriesConverter 
 		for (DataRow row : data) {
 			Set set = new Set(row);
 			set.setColor(FlagColor.valueOf(row.getLabel()).getHex());
-			if(permissions.isOperator())
-				set.setLink("ContractorList.action?filter.flagStatus=" + row.getLabel());
+			//set.setLink("ContractorList.action?filter.flagStatus=" + row.getLabel());
+			chart.addCategory(category)
 			chart.addSet(set);
 		}
+		
+		for (DataRow row : data) {
+			Set set = new Set(row);
+			set.setColor(FlagColor.valueOf(row.getLabel()).getHex());
+			//set.setLink("ContractorList.action?filter.flagStatus=" + row.getLabel());
+			chart.addCategory(category)
+			chart.addSet(set);
+		}
+
 		return chart;
 	}
 }
--- src/com/picsauditing/actions/chart/ChartMSAction.java
+++ src/com/picsauditing/actions/chart/ChartMSAction.java
@@ -2,6 +2,7 @@ package com.picsauditing.actions.chart;
 
 import com.picsauditing.util.chart.ChartMultiSeries;
 
+@SuppressWarnings("serial")
 public class ChartMSAction extends ChartAction {
 	protected ChartMultiSeries chart = new ChartMultiSeries();
 }
