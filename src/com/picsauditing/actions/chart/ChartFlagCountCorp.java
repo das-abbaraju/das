2a6fc652c6639c5fc0ef9b46708a38369d0a3940
Keerthi Nannapaneni <knannapaneni@picsauditing.com>
2009-03-14 00:30:21 +0000

    commented out the corp flag class.

--- src/com/picsauditing/actions/chart/ChartFlagCountCorp.java
+++ src/com/picsauditing/actions/chart/ChartFlagCountCorp.java
@@ -1,21 +1,14 @@
 package com.picsauditing.actions.chart;
 
-import java.util.List;
-
-import com.picsauditing.access.OpPerms;
-import com.picsauditing.jpa.entities.FlagColor;
 import com.picsauditing.search.SelectSQL;
 import com.picsauditing.util.chart.ChartMultiSeries;
-import com.picsauditing.util.chart.DataRow;
-import com.picsauditing.util.chart.Set;
+import com.picsauditing.util.chart.MultiSeriesConverter;
 
 @SuppressWarnings("serial")
 public class ChartFlagCountCorp extends ChartMSAction {
 
 	public ChartMultiSeries buildChart() {
-		if (!permissions.isCorporate() && !permissions.seesAllContractors())
-			return chart;
-		
+
 		chart.setShowValues(true);
 
 		SelectSQL sql = new SelectSQL("accounts a");
@@ -25,32 +18,31 @@ public class ChartFlagCountCorp extends ChartMSAction {
 		sql.addGroupBy("series, label");
 		sql.addOrderBy("series, label");
 
+		sql.addJoin("JOIN facilities fac ON fac.opID = a.id AND fac.corporateID = " + permissions.getAccountId());
 		sql.addJoin("JOIN generalcontractors gc ON a.id = gc.subID");
-		sql.addJoin("JOIN accounts o ON o.id = gc.genID");
+
 		sql.addJoin("JOIN flags f ON a.id = f.conID AND f.opID = gc.genID");
-		sql.addWhere("a.active = 'Y'");
-		if (permissions.isCorporate())
-			sql.addJoin("JOIN facilities fac ON fac.opID = gc.genID AND fac.corporateID = " + permissions.getAccountId());
 
 		ChartDAO db = new ChartDAO();
-		List<DataRow> data = db.select(sql.toString());
+//		List<DataRow> data = db.select(sql.toString());
 		// Get the category set first
-		MultiSeriesConverter 
-		for (DataRow row : data) {
-			Set set = new Set(row);
-			set.setColor(FlagColor.valueOf(row.getLabel()).getHex());
-			//set.setLink("ContractorList.action?filter.flagStatus=" + row.getLabel());
-			chart.addCategory(category)
-			chart.addSet(set);
-		}
+		MultiSeriesConverter multiSeriesConverter = new MultiSeriesConverter();
 		
-		for (DataRow row : data) {
-			Set set = new Set(row);
-			set.setColor(FlagColor.valueOf(row.getLabel()).getHex());
-			//set.setLink("ContractorList.action?filter.flagStatus=" + row.getLabel());
-			chart.addCategory(category)
-			chart.addSet(set);
-		}
+//		for (DataRow row : data) {
+//			Set set = new Set(row);
+//			set.setColor(FlagColor.valueOf(row.getLabel()).getHex());
+//			//set.setLink("ContractorList.action?filter.flagStatus=" + row.getLabel());
+//			chart.addCategory(category)
+//			chart.addSet(set);
+//		}
+//		
+//		for (DataRow row : data) {
+//			Set set = new Set(row);
+//			set.setColor(FlagColor.valueOf(row.getLabel()).getHex());
+//			//set.setLink("ContractorList.action?filter.flagStatus=" + row.getLabel());
+//			chart.addCategory(category)
+//			chart.addSet(set);
+//		}
 
 		return chart;
 	}
