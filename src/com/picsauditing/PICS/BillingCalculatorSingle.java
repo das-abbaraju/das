fde6f7655c402a7fed93b01a62bbbcd37fd7e204
Trevor Allred <tallred@picsauditing.com>
2009-04-01 18:27:56 +0000

    added the createInvoiceItem method

--- src/com/picsauditing/PICS/BillingCalculatorSingle.java
+++ src/com/picsauditing/PICS/BillingCalculatorSingle.java
@@ -1,5 +1,7 @@
 package com.picsauditing.PICS;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Vector;
@@ -11,6 +13,7 @@ import com.picsauditing.jpa.entities.ContractorAccount;
 import com.picsauditing.jpa.entities.ContractorOperator;
 import com.picsauditing.jpa.entities.Invoice;
 import com.picsauditing.jpa.entities.InvoiceFee;
+import com.picsauditing.jpa.entities.InvoiceItem;
 import com.picsauditing.jpa.entities.OperatorAccount;
 
 public class BillingCalculatorSingle {
@@ -99,6 +102,83 @@ public class BillingCalculatorSingle {
 		if (billable >= 2)
 			return InvoiceFee.FACILITIES2;
 		return InvoiceFee.FACILITIES1;
+	}
+
+	static public List<InvoiceItem> createInvoiceItems(ContractorAccount contractor) {
+		List<InvoiceItem> items = new ArrayList<InvoiceItem>();
+		// TODO add in the activation fee if the date is ever missing
+		if (contractor.getMembershipDate() == null) {
+			InvoiceFee fee = new InvoiceFee(InvoiceFee.ACTIVATION);
+			items.add(new InvoiceItem(fee));
+			
+		}
+	
+
+		// For Activation Fee and New Membership
+		if ("Activation".equals(contractor.getBillingStatus())) {
+			if (contractor.getNewMembershipLevel().getId() != InvoiceFee.FREE
+					&& contractor.getMembershipLevel().getId() == InvoiceFee.FREE) {
+				items.add(new InvoiceItem(contractor.getNewMembershipLevel()));
+			}
+		}
+
+		// For Reactivation Fee and Reactivating Membership
+		if ("Reactivation".equals(contractor.getBillingStatus())) {
+			InvoiceFee fee = new InvoiceFee(InvoiceFee.REACTIVATION);
+
+			if (contractor.getNewMembershipLevel() != null)
+				items.add(new InvoiceItem(contractor.getNewMembershipLevel()));
+
+			items.add(new InvoiceItem(fee));
+		}
+
+		// For Renewals
+		if (contractor.getBillingStatus().startsWith("Renew")) {
+			if (contractor.getMembershipLevel() != null)
+				items.add(new InvoiceItem(contractor.getMembershipLevel()));
+		}
+
+		// For Upgrades
+		// Calculate a prorated amount depending on when the upgrade happens
+		// and when the actual membership expires
+		if ("Upgrade".equals(contractor.getBillingStatus())) {
+			if (contractor.getNewMembershipLevel() != null && contractor.getMembershipLevel() != null) {
+				int upgradeAmount = 0;
+				String description = "";
+
+				if (contractor.getMembershipLevel().getAmount() == 0) {
+					// Starting from scratch/Free Membership Level
+					upgradeAmount = contractor.getNewMembershipLevel().getAmount();
+					description = "Membership Level is: $" + contractor.getNewMembershipLevel().getAmount();
+					
+				} else if (DateBean.getDateDifference(contractor.getPaymentExpires()) < 0) {
+					// Their membership has already expired so we need to do a full renewal amount
+					upgradeAmount = contractor.getNewMembershipLevel().getAmount();
+					description = "Membership Level is: $" + contractor.getNewMembershipLevel().getAmount();
+					
+				} else {
+					// Actual prorated Upgrade
+					Date upgradeDate = (contractor.getLastUpgradeDate() == null) ? new Date() : contractor.getLastUpgradeDate();
+					double daysUntilExpiration = DateBean.getDateDifference(upgradeDate, contractor.getPaymentExpires());
+					double upgradeAmountDifference = contractor.getNewMembershipLevel().getAmount()
+							- contractor.getMembershipLevel().getAmount();
+
+					double proratedCalc = upgradeAmountDifference / 365;
+					upgradeAmount = (int)Math.round(daysUntilExpiration * proratedCalc);
+
+					description = "Upgrading from $" + contractor.getMembershipLevel().getAmount() + ". Prorated $"
+							+ upgradeAmount;
+				}
+
+				InvoiceItem invoiceItem = new InvoiceItem();
+				invoiceItem.setInvoiceFee(contractor.getNewMembershipLevel());
+				invoiceItem.setAmount(upgradeAmount);
+				invoiceItem.setDescription(description);
+				items.add(invoiceItem);
+			}
+		}
+
+		return items;
 	}
 
 	public ContractorAccount calculateCurrentBalance(ContractorAccount contractor, boolean invoiceIncludesMembership,
