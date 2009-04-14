ef47523acae585a1df8432f582ade13c09007e95
Keerthi Nannapaneni <knannapaneni@picsauditing.com>
2009-04-14 22:30:23 +0000

    added a csv file on the project.

--- src/com/picsauditing/actions/Trevor.java
+++ src/com/picsauditing/actions/Trevor.java
@@ -1,12 +1,15 @@
 package com.picsauditing.actions;
 
+import org.apache.struts2.ServletActionContext;
+
 @SuppressWarnings("serial")
 public class Trevor extends PicsActionSupport {
 	@Override
 	public String execute() throws Exception {
 		if (!forceLogin())
 			return LOGIN;
-		output = "Hello World";
-		return super.execute();
+		
+		ServletActionContext.getResponse().sendRedirect("images/ReportAccountAudits.csv");
+		return BLANK;
 	}
 }
