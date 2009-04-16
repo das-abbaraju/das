cf69a8d7fda2c4c63bceda6c48c5efb006562472
Trevor Allred <tallred@picsauditing.com>
2009-04-16 20:19:08 +0000

    added icon methods

--- src/com/picsauditing/jpa/entities/CaoStatus.java
+++ src/com/picsauditing/jpa/entities/CaoStatus.java
@@ -2,7 +2,7 @@ package com.picsauditing.jpa.entities;
 
 public enum CaoStatus {
 	NotApplicable, Pending, Submitted, Verified, Approved, Rejected;
-	
+
 	/**
 	 * 
 	 * @return TRUE if NotApplicable or Pending
@@ -14,24 +14,39 @@ public enum CaoStatus {
 			return true;
 		return false;
 	}
-	
+
 	public boolean isPending() {
 		return this.equals(CaoStatus.Pending);
 	}
-	
+
 	public boolean isAwaiting() {
 		return this.equals(CaoStatus.Submitted);
 	}
-	
+
 	public boolean isVerified() {
 		return this.equals(CaoStatus.Verified);
 	}
-	
+
 	public boolean isApproved() {
 		return this.equals(CaoStatus.Approved);
 	}
-	
+
 	public boolean isRejected() {
 		return this.equals(CaoStatus.Rejected);
 	}
+
+	public String getIcon() {
+		boolean approved = true;
+		
+		return "<img src=\"images/icon_" + (approved ? "check" : "x") +
+				".gif\" width=\"32\" height=\"32\" border=\"0\" title=\"" + this.toString() + "\" />";
+	}
+
+	static public String getIcon(String status) {
+		boolean approved = true;
+		
+		return "<img src=\"images/icon_" + (approved ? "check" : "x") +
+				".gif\" width=\"32\" height=\"32\" border=\"0\" title=\"" + status + "\" />";
+	}
+
 }
