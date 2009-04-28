cdd58548d3d3db992846cbee0a9b467b8b57282b
Kyle Partridge <kpartridge@picsauditing.com>
2009-04-28 01:25:13 +0000

    javascript for question link

--- WebContent/js/op_flag_criteria.js
+++ WebContent/js/op_flag_criteria.js
@@ -1,4 +1,19 @@
 
 function showCriteria(opID , questionID) {
-	alert("showing " + opID + " " + questionID);
+	var pars = { 'id': opID, 'question.id':questionID };
+	var myAjax = new Ajax.Updater('criteriaEdit','FlagCriteriaActionAjax.action', {
+		method : 'post',
+		parameters : pars,
+		onComplete: function() { }
+	});
+}
+
+function addNewCriteria(questionID) {
+	var pars = { 'id': <s:property value="id"/>, 'question.id':questionID };
+	
+	var myAjax = new Ajax.Updater('criteriaEdit','FlagCriteriaActionAjax.action', {
+		method : 'post',
+		parameters : pars,
+		onComplete: function() { }
+	});
 }
