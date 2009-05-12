ab4c37fb23e28d8e3b1a23635661d8fcb936fbeb
Keerthi Nannapaneni <knannapaneni@picsauditing.com>
2009-05-12 22:34:41 +0000

    updated the DB script with splitting the PQF into operator specific audits.

--- deployments/upgrade_version2.sql
+++ deployments/upgrade_version2.sql
@@ -54,3 +54,107 @@ insert into widget_user values
 (newwidgetid, 616, 1,1,5, null);
 insert into widget_user values
 (newwidgetid, 646, 1,1,5, null);
+
+/**
+ * Splitting up the pqf operator specific categories into their audits 
+ * 
+ */
+select * from temp_category_audittype;
+
+-- update the categories to the new auditTypeID 
+update pqfcategories pc, temp_category_audittype tca set pc.auditTypeID = tca.auditTypeID
+where pc.id = tca.categoryID
+and pc.auditTypeID = 1;  
+
+-- delete from contractor_audit any audits with the new auditTypeID
+delete from contractor_audit  
+where auditTypeID IN (select tca.auditTypeID from temp_category_audittype tca);
+
+-- Create audits for these new Categories
+insert into contractor_audit
+select null, tca.auditTypeID, canew.conID, canew.creationDate,canew.auditStatus,
+  canew.expiresDate,canew.auditorID,canew.assignedDate,canew.scheduledDate,
+  canew.completedDate,canew.closedDate,canew.requestedByOpID,
+  canew.auditLocation,canew.percentComplete,0,
+  canew.contractorConfirm,canew.auditorConfirm,canew.manuallyAdded,
+  canew.auditFor,canew.createdBy,canew.updatedBy,NOW(),canew.score,
+  null from contractor_audit canew
+join pqfcatdata pcd on pcd.auditid = canew.id
+join temp_category_audittype tca on tca.categoryID = pcd.catID
+where canew.auditTypeID = 1
+AND pcd.applies = 'Yes';
+
+-- insert into temp_auditconverison the old auditid, new auditid and catid
+insert into temp_auditconversion
+select null, caold.id,canew.id, pcd.catid from contractor_audit canew
+join contractor_audit caold on canew.conid = caold.conid
+join audit_type at on at.id = canew.auditTypeID
+join pqfcatdata pcd on pcd.auditid = caold.id
+join temp_category_audittype tca on tca.categoryID = pcd.catID
+where caold.auditTypeID = 1
+and at.id = tca.auditTypeID
+and pcd.applies = 'Yes';
+
+-- Also update the new auditid on the pqfcatData
+update pqfcatdata pcd, temp_auditconversion tca set pcd.auditID = tca.customID
+where pcd.auditID = tca.pqfID
+and pcd.applies = 'Yes'
+and pcd.catId = tca.catID;
+
+-- update the pqfdata with the new audits
+update pqfdata pd, temp_auditconversion tca, pqfquestions pqf, pqfsubcategories ps, 
+pqfcategories pc, pqfcatdata pcd  
+set pd.auditid = tca.customID
+where pd.auditid = tca.pqfID
+and pd.questionid = pqf.id
+and ps.id = pqf.subcategoryID
+and pc.id = ps.categoryID
+and pc.id = tca.catID
+and pcd.auditid = tca.customid
+and pcd.applies = 'Yes'
+and pcd.catid = pc.id;
+
+
+-- Remove all the other categories on the pqfcatData where applies = 'No'
+select count(*) from contractor_audit ca
+join pqfcatdata pcd on ca.id = pcd.auditid
+join temp_category_audittype tca on pcd.catid = tca.categoryID
+where ca.audittypeId = 1;
+ 
+delete from pqfcatdata 
+where auditid in (select ca.id from contractor_audit ca where ca.audittypeId = 1)
+and catid in (select tca.categoryID from temp_category_audittype tca);
+
+-- update the new audits with percent Complete and auditStatus
+update contractor_audit ca,temp_category_audittype tca, pqfcatdata pcd 
+set ca.auditstatus = 'Pending', ca.percentComplete = pcd.percentCompleted
+where tca.audittypeID = ca.auditTypeid
+and ca.id = pcd.auditid
+and pcd.percentCompleted < 100;
+
+update contractor_audit ca,temp_category_audittype tca, pqfcatdata pcd 
+set ca.auditstatus = 'Active', ca.percentComplete = pcd.percentCompleted
+where tca.audittypeID = ca.auditTypeid
+and ca.id = pcd.auditid
+and pcd.percentCompleted = 100;
+ 
+-- update it to audit_operator matrix for these operators looking at the pqf of this operator.
+insert into audit_operator 
+select null, tca.auditTypeid, ao.opID, ao.canSee, ao.canEdit, 
+	min(pm.risklevel), ao.orderedCount,ao.orderDate, 
+	ao.requiredForFlag, 
+	ao.requiredAuditStatus, 
+	ao.additionalInsuredFlag, 
+	ao.waiverSubFlag, 
+	ao.createdBy, 
+	ao.updatedBy, 
+	ao.creationDate, 
+	Now()
+from audit_operator ao 
+join pqfopmatrix pm on ao.opID = pm.opID
+join temp_category_audittype tca on tca.categoryid = pm.catid
+where ao.auditTypeid = 1
+group by pm.catid, pm.opid; 
+
+-- Remove these categories from pqfopmatrix
+delete from pqfopmatrix where catid in (select tca.categoryID from temp_category_audittype tca);
