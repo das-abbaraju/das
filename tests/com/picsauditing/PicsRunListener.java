d83bad22b448b55bc2e9d985cb133543d0ccbf6b
Kyle Partridge <kpartridge@picsauditing.com>
2009-06-03 00:56:13 +0000

    JUnitNightly Test

---
+++ tests/com/picsauditing/PicsRunListener.java
@@ -0,0 +1,23 @@
+package com.picsauditing;
+
+import java.util.List;
+
+import org.junit.runner.Description;
+import org.junit.runner.notification.Failure;
+import org.junit.runner.notification.RunListener;
+
+public class PicsRunListener extends RunListener {
+	List<Failure> failures;
+
+	public PicsRunListener(List<Failure> failures) {
+		super();
+		this.failures = failures;
+	}
+
+	@Override
+	public void testFailure(Failure failure) throws Exception {
+		super.testFailure(failure);
+		// Do the specific PICS run method here
+		failures.add(failure);
+	}
+}
