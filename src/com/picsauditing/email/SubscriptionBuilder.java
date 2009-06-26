b4c81d815985457d4d5a195a7b779fac0622f594
Kyle Partridge <kpartridge@picsauditing.com>
2009-06-26 19:58:57 +0000

    added a getter to the SubscriptionTimePeriod enum to get a date used for comparison with each time period

--- src/com/picsauditing/email/SubscriptionBuilder.java
+++ src/com/picsauditing/email/SubscriptionBuilder.java
@@ -1,16 +1,20 @@
 package com.picsauditing.email;
 
 import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.beanutils.BasicDynaBean;
+
 import com.picsauditing.dao.EmailSubscriptionDAO;
 import com.picsauditing.jpa.entities.Account;
 import com.picsauditing.jpa.entities.EmailQueue;
 import com.picsauditing.jpa.entities.EmailSubscription;
+import com.picsauditing.search.SelectSQL;
 import com.picsauditing.util.SpringUtils;
 
 public abstract class SubscriptionBuilder {
@@ -19,43 +23,17 @@ public abstract class SubscriptionBuilder {
 	protected Subscription subscription;
 	protected List<EmailSubscription> subscriptions;
 
+	protected SelectSQL sql;
+
 	public SubscriptionBuilder(Subscription subscription) {
 		subscriptionDAO = (EmailSubscriptionDAO) SpringUtils.getBean("EmailSubscriptionDAO");
 		this.subscription = subscription;
 	}
 
 	public boolean isSendEmail(EmailSubscription sub) {
-		Calendar calendar = Calendar.getInstance();
-
-		boolean send = true;
-
-		switch (sub.getTimePeriod()) {
-		case None:
-			send = false;
-			break;
-		case Daily:
-			calendar.add(Calendar.DAY_OF_YEAR, -1);
-			send = !sub.getLastSent().after(calendar.getTime());
-			break;
-		case Weekly:
-			calendar.add(Calendar.WEEK_OF_YEAR, -1);
-			send = !sub.getLastSent().after(calendar.getTime());
-			break;
-		case Monthly:
-			calendar.add(Calendar.MONTH, -1);
-			send = !sub.getLastSent().after(calendar.getTime());
-			break;
-		case Quarterly:
-			calendar.add(Calendar.MONTH, -3);
-			send = !sub.getLastSent().after(calendar.getTime());
-			break;
-
-		default:
-			send = true;
-			break;
-		}
-
-		return send;
+		if (sub.getTimePeriod().equals(SubscriptionTimePeriod.None))
+			return false;
+		return !sub.getLastSent().after(sub.getTimePeriod().getCompaisonDate());
 	}
 
 	protected List<EmailSubscription> getSubscriptions() {
@@ -74,20 +52,22 @@ public abstract class SubscriptionBuilder {
 		return result;
 	}
 
-	protected Map<Account, Set<String>> getRecipientsMap() {
-		Map<Account, Set<String>> result = new HashMap<Account, Set<String>>();
+	protected Map<Account, Set<EmailSubscription>> getSubscriptionsByLastSentAndAccount() {
+		Map<Account, Set<EmailSubscription>> result = new HashMap<Account, Set<EmailSubscription>>();
 
 		for (EmailSubscription sub : getSubscriptions()) {
 			if (result.get(sub.getUser().getAccount()) == null)
-				result.put(sub.getUser().getAccount(), new HashSet<String>());
-			
-			result.get(sub.getUser().getAccount()).add(sub.getUser().getEmail());
+				result.put(sub.getUser().getAccount(), new HashSet<EmailSubscription>());
+
+			result.get(sub.getUser().getAccount()).add(sub);
 		}
 
 		return result;
 	}
-	
-	protected abstract EmailQueue buildEmail(Map<String, ? extends Object> parameters);	
+
+	protected abstract List<BasicDynaBean> runSql();
+
+	protected abstract EmailQueue buildEmail();
 
 	public abstract void process();
 }
--- src/com/picsauditing/email/SubscriptionTimePeriod.java
+++ src/com/picsauditing/email/SubscriptionTimePeriod.java
@@ -1,5 +1,29 @@
 package com.picsauditing.email;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public enum SubscriptionTimePeriod {
 	None, Daily, Weekly, Monthly, Quarterly;
+	
+	public Date getCompaisonDate() {
+		Calendar calendar = Calendar.getInstance();
+		
+		switch (this) {
+		case Daily:
+			calendar.add(Calendar.DAY_OF_YEAR, -1);
+			break;
+		case Weekly:
+			calendar.add(Calendar.WEEK_OF_YEAR, -1);
+			break;
+		case Monthly:
+			calendar.add(Calendar.MONTH, -1);
+			break;
+		case Quarterly:
+			calendar.add(Calendar.MONTH, -3);
+			break;
+		}
+		
+		return calendar.getTime();
+	}
 }
