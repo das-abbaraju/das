0f407f855a8ba68df652f6a540667255c299f5d0
Keerthi Nannapaneni <knannapaneni@picsauditing.com>
2009-07-07 16:58:45 +0000

    added SubscriptionConverter.

---
+++ src/com/picsauditing/actions/converters/SubscriptionConverter.java
@@ -0,0 +1,9 @@
+package com.picsauditing.actions.converters;
+
+import com.picsauditing.mail.Subscription;
+
+public class SubscriptionConverter extends EnumConverter {
+	public SubscriptionConverter() {
+		enumClass = Subscription.class;
+	}
+}
--- src/com/picsauditing/actions/converters/SubscriptionTimePeriodConverter.java
+++ src/com/picsauditing/actions/converters/SubscriptionTimePeriodConverter.java
@@ -2,7 +2,6 @@ package com.picsauditing.actions.converters;
 
 import com.picsauditing.mail.SubscriptionTimePeriod;
 
-@SuppressWarnings("unchecked")
 public class SubscriptionTimePeriodConverter extends EnumConverter {
 	public SubscriptionTimePeriodConverter() {
 		enumClass = SubscriptionTimePeriod.class;
