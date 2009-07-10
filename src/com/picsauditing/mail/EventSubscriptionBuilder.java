030819fe9a77ef48c5fb28368e81bf5515e22864
Kyle Partridge <kpartridge@picsauditing.com>
2009-07-10 01:28:49 +0000

    should build emails per user for this event subscription

--- src/com/picsauditing/mail/EventSubscriptionBuilder.java
+++ src/com/picsauditing/mail/EventSubscriptionBuilder.java
@@ -1,14 +1,11 @@
 package com.picsauditing.mail;
 
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import com.picsauditing.dao.EmailSubscriptionDAO;
 import com.picsauditing.jpa.entities.ContractorOperator;
 import com.picsauditing.jpa.entities.EmailSubscription;
-import com.picsauditing.util.Strings;
 
 public class EventSubscriptionBuilder {
 
@@ -19,21 +16,18 @@ public class EventSubscriptionBuilder {
 		List<EmailSubscription> subscriptions = subscriptionDAO.find(Subscription.ContractorFinished,
 				SubscriptionTimePeriod.Event, co.getOperatorAccount().getId());
 
-		Set<String> emails = new HashSet<String>();
 		for (EmailSubscription subscription : subscriptions) {
-			emails.add(subscription.getUser().getEmail());
+			EmailBuilder builder = new EmailBuilder();
+			builder.setTemplate(63);
+			builder.setFromAddress("info@picsauditing.com");
+			builder.addToken("contractor", co.getContractorAccount());
+			builder.addToken("operator", co.getOperatorAccount());
+			builder.setUser(subscription.getUser());
+
+			EmailSender.send(builder.build());
+
 			subscription.setLastSent(now);
 			subscriptionDAO.save(subscription);
 		}
-		String recipients = Strings.implode(emails, ",");
-
-		EmailBuilder builder = new EmailBuilder();
-		builder.setTemplate(63);
-		builder.setToAddresses(recipients);
-		builder.setFromAddress("info@picsauditing.com");
-		builder.addToken("contractor", co.getContractorAccount());
-		builder.addToken("operator", co.getOperatorAccount());
-
-		EmailSender.send(builder.build());
 	}
 }
