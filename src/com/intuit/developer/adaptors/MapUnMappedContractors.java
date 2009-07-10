4c29632d0ec9e0ec41820baf1f7ed511c054e950
Kyle Partridge <kpartridge@picsauditing.com>
2009-07-10 19:10:57 +0000

    dump out contractors based on contractor name

--- src/com/intuit/developer/adaptors/MapUnMappedContractors.java
+++ src/com/intuit/developer/adaptors/MapUnMappedContractors.java
@@ -1,12 +1,12 @@
 package com.intuit.developer.adaptors;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.Writer;
-import java.util.Map;
 
 import javax.xml.bind.Marshaller;
 
 import com.intuit.developer.QBSession;
-import com.picsauditing.jpa.entities.ContractorAccount;
 import com.picsauditing.quickbooks.qbxml.CustomerQueryRqType;
 import com.picsauditing.quickbooks.qbxml.ObjectFactory;
 import com.picsauditing.quickbooks.qbxml.QBXML;
@@ -32,6 +32,10 @@ public class MapUnMappedContractors extends CustomerAdaptor {
 		customerQuery.getIncludeRetElement().add("AccountNumber");
 		customerQuery.getIncludeRetElement().add("CompanyName");
 
+		customerQuery.setNameFilter(factory.createNameFilter());
+		customerQuery.getNameFilter().setMatchCriterion("StartsWith");
+		customerQuery.getNameFilter().setName(getAppPropertyDao().find("DumpContractors.ContractorName").getValue());
+
 		request.getHostQueryRqOrCompanyQueryRqOrCompanyActivityQueryRq().add(customerQuery);
 		xml.setQBXMLMsgsRq(request);
 
@@ -44,38 +48,25 @@ public class MapUnMappedContractors extends CustomerAdaptor {
 	@Override
 	public Object parseQbXml(QBSession currentSession, String qbXml) throws Exception {
 
-		Map<String, Map<String, Object>> parsedResponses = parseCustomerQueryResponse(qbXml);
+		FileWriter fw = null;
 
-		for (String listId : parsedResponses.keySet()) {
-			Map<String, Object> dataForThisListId = parsedResponses.get(listId);
+		try {
+			File fileBase = new File(System.getProperty("pics.ftpDir"));
 
-			ContractorAccount contractor = (ContractorAccount) dataForThisListId.get("contractor");
+			File outputFile = new File(fileBase, "contractors.out");
 
-			if (contractor != null && contractor.getId() != 0) {
-
-				try {
-					ContractorAccount contractor2 = getContractorDao().find(contractor.getId());
-					if (contractor2 != null) {
-
-						if (contractor2.getQbListID() == null || contractor2.getQbListID().length() == 0) {
-							contractor2.setQbListID(listId);
-							getContractorDao().save(contractor2);
-						} else {
-							currentSession.getErrors().add(
-									"update accounts set qbListID = '" + listId + "' where id = " + contractor.getId()
-											+ " AND qbListID is null;");
-						}
-					}
-				} catch (Exception e) {
-					currentSession.getErrors().add(
-							"update accounts set qbListID = '" + listId + "' where id = " + contractor.getId()
-									+ " AND qbListID is null;");
-				}
-
+			if (outputFile.isFile()) {
+				outputFile.delete();
 			}
+
+			fw = new FileWriter(outputFile);
+
+			fw.write(qbXml);
+		} finally {
+			if (fw != null)
+				fw.close();
 		}
 
 		return null;
 	}
-
 }
