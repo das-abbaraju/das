4d9614df03c1a475c10b8379115d5bacd30da7bb
Kyle Partridge <kpartridge@picsauditing.com>
2009-07-10 00:25:14 +0000

    subscription enumeration enhancements

--- src/com/picsauditing/mail/Subscription.java
+++ src/com/picsauditing/mail/Subscription.java
@@ -1,34 +1,31 @@
 package com.picsauditing.mail;
 
 public enum Subscription {
-	PICSSystemNotification("PICS System Notification"),
-	PICSAnnouncements("PICS Announcements"),
-	ContractorRegistration("Contractor Registration",true,false),
-	ContractorFinished("Contractors completes the PICS process", true, false),
-	FlagChanges("FlagChanges",true,false),
-	PendingInsuranceCerts("Pending Insurance Certs",true,false),
-	VerifiedInsuranceCerts("Verified Insurance Certs",true,false),
-	QuarterlyExecutiveReport("Quarterly Executive Report",new SubscriptionTimePeriod[] {SubscriptionTimePeriod.Quarterly},true,false),
-	ContractorInvoices("Contractor Invoices",false,true),
-	InsuranceExpiration("Insurance Expiration",false,true),
-	AuditOpenRequirements("Audit Open Requirements",false,true),
+	PICSSystemNotification("PICS System Notification", new SubscriptionTimePeriod[] { SubscriptionTimePeriod.None,
+			SubscriptionTimePeriod.Event }),
+	PICSAnnouncements("PICS Announcements", new SubscriptionTimePeriod[] { SubscriptionTimePeriod.None,
+			SubscriptionTimePeriod.Event }),
+	ContractorRegistration("Contractor Registration", true, false),
+	ContractorFinished("Contractors completes the PICS process", new SubscriptionTimePeriod[] {
+			SubscriptionTimePeriod.None, SubscriptionTimePeriod.Event }, true, false),
+	FlagChanges("FlagChanges", true, false),
+	PendingInsuranceCerts("Pending Insurance Certs", true, false),
+	VerifiedInsuranceCerts("Verified Insurance Certs", true, false),
+	QuarterlyExecutiveReport("Quarterly Executive Report", new SubscriptionTimePeriod[] { SubscriptionTimePeriod.None,
+			SubscriptionTimePeriod.Quarterly }, true, false),
+	ContractorInvoices("Contractor Invoices", false, true),
+	InsuranceExpiration("Insurance Expiration", false, true),
+	AuditOpenRequirements("Audit Open Requirements", false, true),
 	FinishPICSProcess("Contractor Completes the PICS process", false, true);
 
 	private String description;
-	private SubscriptionTimePeriod[] supportedTimePeriods = {SubscriptionTimePeriod.Daily, SubscriptionTimePeriod.Monthly, SubscriptionTimePeriod.Weekly };
-
-	// Whether or not contractor/operator account is required for this
-	// subscription
+	private SubscriptionTimePeriod[] supportedTimePeriods = { SubscriptionTimePeriod.None,
+			SubscriptionTimePeriod.Daily, SubscriptionTimePeriod.Monthly, SubscriptionTimePeriod.Weekly };
 	private boolean requiredForOperator = true;
 	private boolean requiredForContractor = true;
 
-	Subscription() {
-		this.supportedTimePeriods = SubscriptionTimePeriod.values();
-	}
-
 	Subscription(String description) {
 		this.description = description;
-		this.supportedTimePeriods = SubscriptionTimePeriod.values();
 	}
 
 	Subscription(String description, SubscriptionTimePeriod[] supportedTimePeriods) {
@@ -40,7 +37,6 @@ public enum Subscription {
 		this.description = description;
 		this.requiredForOperator = requiredForOperator;
 		this.requiredForContractor = requiredForContractor;
-		this.supportedTimePeriods = SubscriptionTimePeriod.values();
 	}
 
 	Subscription(String description, SubscriptionTimePeriod[] supportedTimePeriods, boolean requiredForOperator,
