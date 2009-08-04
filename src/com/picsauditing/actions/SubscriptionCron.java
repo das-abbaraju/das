45061c5b3d02c71b672bef587a3ad7f6c889f2f7
Kyle Partridge <kpartridge@picsauditing.com>
2009-08-04 22:22:46 +0000

    Added flag color subcription to the cron. using app_properties to select which subscriptions to run on the cron

--- src/com/picsauditing/actions/SubscriptionCron.java
+++ src/com/picsauditing/actions/SubscriptionCron.java
@@ -2,19 +2,24 @@ package com.picsauditing.actions;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.struts2.interceptor.ServletRequestAware;
 
 import com.opensymphony.xwork2.ActionContext;
+import com.picsauditing.dao.AppPropertyDAO;
 import com.picsauditing.dao.ContractorAccountDAO;
 import com.picsauditing.dao.ContractorAuditOperatorDAO;
 import com.picsauditing.dao.ContractorOperatorFlagDAO;
 import com.picsauditing.dao.EmailSubscriptionDAO;
+import com.picsauditing.jpa.entities.AppProperty;
 import com.picsauditing.mail.ContractorRegistrationSubscription;
 import com.picsauditing.mail.FlagChangesSubscription;
+import com.picsauditing.mail.FlagColorSubscription;
 import com.picsauditing.mail.InsuranceCertificateSubscription;
 import com.picsauditing.mail.Subscription;
 import com.picsauditing.mail.SubscriptionBuilder;
@@ -23,14 +28,18 @@ import com.picsauditing.mail.SubscriptionTimePeriod;
 @SuppressWarnings("serial")
 public class SubscriptionCron extends PicsActionSupport implements ServletRequestAware {
 
+	private AppPropertyDAO appPropDAO;
 	private EmailSubscriptionDAO subscriptionDAO;
 	private ContractorOperatorFlagDAO flagDAO;
 	private ContractorAuditOperatorDAO caoDAO;
 	private ContractorAccountDAO conDAO;
 	protected HttpServletRequest request;
 
-	public SubscriptionCron(EmailSubscriptionDAO subscriptionDAO, ContractorOperatorFlagDAO flagDAO,
-			ContractorAuditOperatorDAO caoDAO, ContractorAccountDAO conDAO) {
+	Set<Subscription> subs = new HashSet<Subscription>();
+
+	public SubscriptionCron(AppPropertyDAO appPropDAO, EmailSubscriptionDAO subscriptionDAO,
+			ContractorOperatorFlagDAO flagDAO, ContractorAuditOperatorDAO caoDAO, ContractorAccountDAO conDAO) {
+		this.appPropDAO = appPropDAO;
 		this.subscriptionDAO = subscriptionDAO;
 		this.flagDAO = flagDAO;
 		this.caoDAO = caoDAO;
@@ -42,6 +51,12 @@ public class SubscriptionCron extends PicsActionSupport implements ServletReques
 		Calendar calendar = Calendar.getInstance();
 		String name = request.getRequestURL().toString();
 		String serverName = name.replace(ActionContext.getContext().getName() + ".action", "");
+
+		for (Subscription subscription : Subscription.values()) {
+			AppProperty app = appPropDAO.find(subscription.getAppPropertyKey());
+			if (app != null && "1".equals(app.getValue()))
+				subs.add(subscription);
+		}
 
 		List<SubscriptionTimePeriod> timePeriods = new ArrayList<SubscriptionTimePeriod>();
 
@@ -58,28 +73,55 @@ public class SubscriptionCron extends PicsActionSupport implements ServletReques
 
 		for (SubscriptionTimePeriod timePeriod : timePeriods) {
 			SubscriptionBuilder builder;
-			builder = new FlagChangesSubscription(timePeriod, subscriptionDAO, flagDAO);
-			builder.setServerName(serverName);
-			builder.process();
 
-			builder = new InsuranceCertificateSubscription(Subscription.PendingInsuranceCerts, timePeriod,
-					subscriptionDAO, caoDAO);
-			builder.setServerName(serverName);
-			builder.process();
+			if (subs.contains(Subscription.FlagChanges)) {
+				builder = new FlagChangesSubscription(timePeriod, subscriptionDAO, flagDAO);
+				builder.setServerName(serverName);
+				builder.process();
+			}
 
-			builder = new InsuranceCertificateSubscription(Subscription.VerifiedInsuranceCerts, timePeriod,
-					subscriptionDAO, caoDAO);
-			builder.setServerName(serverName);
-			builder.process();
+			if (subs.contains(Subscription.PendingInsuranceCerts)) {
+				builder = new InsuranceCertificateSubscription(Subscription.PendingInsuranceCerts, timePeriod,
+						subscriptionDAO, caoDAO);
+				builder.setServerName(serverName);
+				builder.process();
+			}
 
-			builder = new ContractorRegistrationSubscription(timePeriod, subscriptionDAO, conDAO);
-			builder.setServerName(serverName);
-			builder.process();
+			if (subs.contains(Subscription.VerifiedInsuranceCerts)) {
+				builder = new InsuranceCertificateSubscription(Subscription.VerifiedInsuranceCerts, timePeriod,
+						subscriptionDAO, caoDAO);
+				builder.setServerName(serverName);
+				builder.process();
+			}
+
+			if (subs.contains(Subscription.ContractorRegistration)) {
+				builder = new ContractorRegistrationSubscription(timePeriod, subscriptionDAO, conDAO);
+				builder.setServerName(serverName);
+				builder.process();
+			}
+
+			if (subs.contains(Subscription.RedFlags)) {
+				builder = new FlagColorSubscription(Subscription.RedFlags, timePeriod, subscriptionDAO, flagDAO);
+				builder.setServerName(serverName);
+				builder.process();
+			}
+
+			if (subs.contains(Subscription.AmberFlags)) {
+				builder = new FlagColorSubscription(Subscription.AmberFlags, timePeriod, subscriptionDAO, flagDAO);
+				builder.setServerName(serverName);
+				builder.process();
+			}
+
+			if (subs.contains(Subscription.GreenFlags)) {
+				builder = new FlagColorSubscription(Subscription.GreenFlags, timePeriod, subscriptionDAO, flagDAO);
+				builder.setServerName(serverName);
+				builder.process();
+			}
 		}
 
 		return SUCCESS;
 	}
-	
+
 	@Override
 	public void setServletRequest(HttpServletRequest request) {
 		this.request = request;
