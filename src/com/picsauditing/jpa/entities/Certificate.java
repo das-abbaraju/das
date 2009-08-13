5c2a10f5af0cf2ceaafbf17e7d6010a65b20655e
Kyle Partridge <kpartridge@picsauditing.com>
2009-08-13 00:28:59 +0000

    added updateExpirationDate transient method

--- src/com/picsauditing/jpa/entities/Certificate.java
+++ src/com/picsauditing/jpa/entities/Certificate.java
@@ -1,6 +1,7 @@
 package com.picsauditing.jpa.entities;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -10,6 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
@@ -77,5 +79,27 @@ public class Certificate extends BaseTable {
 
 	public void setCaos(List<ContractorAuditOperator> caos) {
 		this.caos = caos;
+	}
+
+	@Transient
+	public void updateExpirationDate() {
+		if (caos.size() > 0) {
+			// if there are caos - use the latest expiration date
+			expirationDate = null;
+			for (ContractorAuditOperator cao : caos) {
+				if (expirationDate == null
+						|| (cao.getAudit().getExpiresDate() != null && cao.getAudit().getExpiresDate().after(
+								expirationDate)))
+					expirationDate = cao.getAudit().getExpiresDate();
+			}
+		}
+
+		// 1 - there are no caos
+		// 2 - caos do not expire
+		if (expirationDate == null) {
+			Calendar cal = Calendar.getInstance();
+			cal.add(Calendar.MONTH, 6);
+			expirationDate = cal.getTime();
+		}
 	}
 }
