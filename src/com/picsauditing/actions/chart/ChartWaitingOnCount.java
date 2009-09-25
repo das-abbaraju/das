377a46b878827ed5ffdaa250d66bca0f61798e29
Kyle Partridge <kpartridge@picsauditing.com>
2009-09-25 01:21:30 +0000

    bug#0001523: waiting on widget

---
+++ src/com/picsauditing/actions/chart/ChartWaitingOnCount.java
@@ -0,0 +1,42 @@
+package com.picsauditing.actions.chart;
+
+import java.util.List;
+
+import com.picsauditing.jpa.entities.WaitingOn;
+import com.picsauditing.search.SelectSQL;
+import com.picsauditing.util.chart.ChartSingleSeries;
+import com.picsauditing.util.chart.DataRow;
+import com.picsauditing.util.chart.Set;
+
+/**
+ * Get a count of each flag color for this operator
+ * 
+ * @author Trevor
+ */
+@SuppressWarnings("serial")
+public class ChartWaitingOnCount extends ChartSSAction {
+
+	@Override
+	public ChartSingleSeries buildChart() throws Exception {
+
+		chart.setShowValues(true);
+
+		SelectSQL sql = new SelectSQL("generalcontractors gc");
+		sql.addJoin("JOIN flags f ON gc.genID = f.opID AND gc.subID = f.conID");
+		sql.addGroupBy("waitingOn");
+
+		sql.addField("waitingOn AS label");
+		sql.addField("count(*) AS value");
+
+		ChartDAO db = new ChartDAO();
+		List<DataRow> data = db.select(sql.toString());
+		for (DataRow row : data) {
+			Set set = new Set(row);
+			WaitingOn waitingOn = WaitingOn.valueOf(Integer.parseInt(row.getLabel()));
+			String wait = waitingOn.isNone() ? "None" : waitingOn.toString();
+			set.setLabel(wait);
+			chart.addSet(set);
+		}
+		return chart;
+	}
+}
