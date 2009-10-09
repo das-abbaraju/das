8cc1c1fa6d9900c016103da89d0246f27cb0bc91
Kyle Partridge <kpartridge@picsauditing.com>
2009-10-09 23:03:10 +0000

    added a ContractorDetails to design the new ContractorView page

---
+++ WebContent/struts/contractors/con_details.jsp
@@ -0,0 +1,131 @@
+<%@ taglib prefix="s" uri="/struts-tags"%>
+<%@ taglib prefix="pics" uri="pics-taglib"%>
+<%@ page language="java" errorPage="exception_handler.jsp"%>
+<html>
+<head>
+<title><s:property value="contractor.name" /></title>
+
+<link rel="stylesheet" type="text/css" media="screen"
+	href="css/reports.css" />
+<script type="text/javascript" src="js/prototype.js"></script>
+<script type="text/javascript">
+	function removeTag(tagId) {
+		var pars = "button=RemoveTag&tagId=" + tagId+'&id='+<s:property value="id"/>;
+		var divName ='conoperator_tags';
+		$(divName).innerHTML="<img src='images/ajax_process.gif' />";
+		var myAjax = new Ajax.Updater(divName, 'TagNameEditAjax.action', 
+		{
+			method: 'post', 
+			parameters: pars,
+			onSuccess: function(transport) { 
+				new Effect.Highlight($(divName), {duration: 0.75, startcolor:'#FFFF11', endcolor:'#EEEEEE'});
+			}
+		});
+		return false;
+	}
+	
+	function addTag() {
+		var tagId = $('tagName').value;
+		var pars = "button=AddTag&tagId=" + tagId+'&id='+<s:property value="id"/>;
+		var divName ='conoperator_tags';
+		$(divName).innerHTML="<img src='images/ajax_process.gif' />";
+		var myAjax = new Ajax.Updater(divName, 'TagNameEditAjax.action', 
+		{
+			method: 'post', 
+			parameters: pars,
+			onSuccess: function(transport) { 
+				
+				new Effect.Highlight($(divName), {duration: 0.75, startcolor:'#FFFF11', endcolor:'#EEEEEE'});
+			}
+		});
+		return false;
+	}
+</script>
+<style>
+#content div.column {
+	width: 47%;
+	margin: 0px 5px;
+	padding: 5px;
+	float: left;
+	border: 1px solid #000;
+	position: relative;
+}
+div.contractor_block {
+	border: 1px solid #000;
+	float: left;
+	width: 98%;
+}
+div.contractor_info {
+	float: left;
+	clear: right;
+}
+div.contractor_info ul {
+	list-style: none;
+	margin-left: 0px;
+}
+#content div.contractor_description {
+	background-color:#797B7A;
+	color:#F7F7F7;
+	padding:5px 10px;
+}
+img.contractor_logo {
+	float: left;
+	max-width: 45%;
+	/* IE Image max-width */
+	width: expression(this.width > 225 ? 225 : true);
+}
+</style>
+</head>
+<body>
+<s:include value="conHeader.jsp" />
+<s:if test="!contractor.activeB">
+	<div id="alert">This contractor is not active.
+	<s:if test="contractor.lastPayment != null">They last paid on <s:property value="contractor.lastPayment"/>.</s:if>
+	</div>
+</s:if>
+<s:if test="contractor.acceptsBids">
+	<s:if test="canUpgrade">
+		<div id="info">This is a Trial Only Account and will expire on <strong><s:date name="contractor.paymentExpires" format="M/d/yyyy" /></strong><br/>
+		Click <a href="ContractorView.action?id=<s:property value="id" />" class="picsbutton positive">Upgrade to Full Membership</a> to continue working at your selected facilities.</div>
+	</s:if>
+	<s:else>
+		<div id="alert">This is a Trial Contractor Account.</div>
+	</s:else>
+</s:if>
+<s:elseif test="contractor.paymentOverdue && (permissions.admin || permissions.contractor)">
+	<div id="alert">This contractor has an outstanding invoice due</div>
+</s:elseif>
+<s:if test="permissions.admin && !contractor.mustPayB">
+	<div id="alert">This account has a lifetime free membership</div>
+</s:if>
+
+
+<div class="column">
+	<div class="contractor_block">
+		<img src="ContractorLogo.action?id=<s:property value="id"/>" class="contractor_logo" />
+		<div class="contractor_info">
+			<div><h4><s:property value="contractor.name"/></h4></div>
+			<div><s:property value="contractor.address"/>, <s:property value="contractor.city"/>, <s:property value="contractor.state"/>, <s:property value="contractor.zip"/></div>
+			<div>
+				<ul>
+					<li>Contact: <span class="value"><s:property value="contractor.contact" /></span></li>
+					<li>Phone: <span class="value"><s:property value="contractor.phone" /></span></li>
+					<s:if test="contractor.phone2"><li>Other Phone: <span class="value"><s:property value="contractor.phone2" /></span></li></s:if>
+					<s:if test="contractor.fax"><li>Fax: <span class="value"><s:property value="contractor.fax" /></span></li></s:if>
+					<li>Email: <strong><a href="mailto:<s:property value="contractor.email" />" class="value"><s:property value="contractor.email" /></a></strong></li>
+					<s:if test="contractor.webUrl.length() > 0"><li>Web site: <strong><a href="http://<s:property value="contractor.webUrl" />" class="value" target="_blank"><s:property value="contractor.webUrl" /></a></strong></li></s:if>
+					<s:if test="@com.picsauditing.util.Strings@isEmpty(contractor.brochureFile) == false">
+						<li><a href="DownloadContractorFile.action?id=<s:property value="id" />" target="_BLANK">Company Brochure</a></li>
+					</s:if>
+				</ul>
+			</div>
+		</div>
+		<div class="contractor_info contractor_description"><s:property value="contractor.descriptionHTML" escape="false" /></div>
+	</div>
+</div>
+<div class="column"></div>
+
+<br clear="all" />
+
+</body>
+</html>
---
+++ src/com/picsauditing/actions/contractors/ContractorDetails.java
@@ -0,0 +1,181 @@
+package com.picsauditing.actions.contractors;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.picsauditing.PICS.AuditBuilder;
+import com.picsauditing.access.OpPerms;
+import com.picsauditing.access.OpType;
+import com.picsauditing.dao.ContractorAccountDAO;
+import com.picsauditing.dao.ContractorAuditDAO;
+import com.picsauditing.dao.ContractorOperatorDAO;
+import com.picsauditing.dao.ContractorTagDAO;
+import com.picsauditing.dao.OperatorTagDAO;
+import com.picsauditing.jpa.entities.AuditStatus;
+import com.picsauditing.jpa.entities.ContractorAudit;
+import com.picsauditing.jpa.entities.ContractorOperator;
+import com.picsauditing.jpa.entities.ContractorTag;
+import com.picsauditing.jpa.entities.EmailQueue;
+import com.picsauditing.jpa.entities.NoteCategory;
+import com.picsauditing.jpa.entities.OperatorTag;
+import com.picsauditing.mail.EmailBuilder;
+import com.picsauditing.mail.EmailSender;
+import com.picsauditing.util.Images;
+
+@SuppressWarnings("serial")
+public class ContractorDetails extends ContractorActionSupport {
+	private AuditBuilder auditBuilder;
+	private OperatorTagDAO operatorTagDAO;
+	private ContractorTagDAO contractorTagDAO;
+	private ContractorOperatorDAO contractorOperatorDAO;
+	public List<OperatorTag> operatorTags = new ArrayList<OperatorTag>();
+	public int tagId;
+
+	private int logoWidth = 0;
+
+	public ContractorDetails(ContractorAccountDAO accountDao, ContractorAuditDAO auditDao, AuditBuilder auditBuilder,
+			OperatorTagDAO operatorTagDAO, ContractorTagDAO contractorTagDAO, ContractorOperatorDAO contractorOperatorDAO) {
+		super(accountDao, auditDao);
+		this.auditBuilder = auditBuilder;
+		this.operatorTagDAO = operatorTagDAO;
+		this.contractorTagDAO = contractorTagDAO;
+		this.contractorOperatorDAO = contractorOperatorDAO;
+	}
+
+	public String execute() throws Exception {
+		if (!forceLogin())
+			return LOGIN;
+		limitedView = true;
+		findContractor();
+
+		if ("AddTag".equals(button)) {
+			if (tagId > 0) {
+				ContractorTag cTag = new ContractorTag();
+				cTag.setContractor(contractor);
+				cTag.setTag(new OperatorTag());
+				cTag.getTag().setId(tagId);
+				cTag.setAuditColumns(permissions);
+				contractor.getOperatorTags().add(cTag);
+				accountDao.save(contractor);
+			}
+			
+		}
+
+		if ("RemoveTag".equals(button)) {
+			contractorTagDAO.remove(tagId);
+		}
+		
+		if("Upgrade to Full Membership".equals(button)) {
+			contractor.setAcceptsBids(false);
+			contractor.setRenew(true);
+			for (ContractorAudit cAudit : contractor.getAudits()) {
+				if (cAudit.getAuditType().isPqf() && !cAudit.getAuditStatus().isPending()) {
+					cAudit.changeStatus(AuditStatus.Pending, getUser());
+					auditDao.save(cAudit);
+					break;
+				}
+			}
+			if(permissions.isOperator()) {
+				for(ContractorOperator cOperator : contractor.getOperators()) {
+					if(cOperator.getOperatorAccount().getId() == permissions.getAccountId()) {
+						cOperator.setWorkStatus("Y");
+						cOperator.setAuditColumns(permissions);
+						contractorOperatorDAO.save(cOperator);
+						break;
+					}
+				}
+			}
+
+			contractor.setNeedsRecalculation(true);
+			contractor.setAuditColumns(permissions);
+			accountDao.save(contractor);
+			
+			addNote(contractor, "Upgraded the Trial account to a full membership.", NoteCategory.General);
+			
+			// Sending a Email to the contractor for upgrade
+			EmailBuilder emailBuilder = new EmailBuilder();
+			emailBuilder.setTemplate(73); // Trial Contractor Account Approval 
+			emailBuilder.setPermissions(permissions);
+			emailBuilder.setContractor(contractor);
+			emailBuilder.addToken("permissions", permissions);
+			EmailQueue emailQueue = emailBuilder.build();
+			emailQueue.setPriority(60);
+			EmailSender.send(emailQueue);
+		}
+		
+		if (permissions.isOperator()) {
+			operatorTags = getOperatorTagNamesList();
+
+			for (ContractorTag contractorTag : contractor.getOperatorTags()) {
+				if (operatorTags.contains(contractorTag.getTag()))
+					operatorTags.remove(contractorTag.getTag());
+			}
+		}
+		if (contractor.getOperators() != null && contractor.getOperators().size() > 0) {
+			auditBuilder.setUser(getUser());
+			auditBuilder.buildAudits(this.contractor);
+		}
+
+		this.subHeading = "Contractor Details";
+
+		return SUCCESS;
+	}
+
+	public int getLogoWidth() {
+		// System.out.println("getLogoWidth for " + contractor.getId());
+		if (contractor.getLogoFile() == null)
+			return 0;
+		if (contractor.getLogoFile().equals("No"))
+			return 0;
+
+		if (logoWidth == 0) {
+			String filename = getFtpDir() + "/logos/" + contractor.getLogoFile();
+			// System.out.println("filename = " + filename);
+			try {
+				logoWidth = Images.getWidth(filename);
+			} catch (IOException e) {
+				System.out.println("failed to get logo width of " + filename + ": " + e.getMessage());
+			}
+			// System.out.println("width = " + width);
+			if (logoWidth > 300)
+				logoWidth = 300;
+		}
+		return logoWidth;
+	}
+
+	public List<OperatorTag> getOperatorTagNamesList() throws Exception {
+		if (operatorTags != null && operatorTags.size() > 0)
+			return operatorTags;
+
+		return operatorTagDAO.findByOperator(permissions.getAccountId());
+	}
+
+	public int getTagId() {
+		return tagId;
+	}
+
+	public void setTagId(int tagId) {
+		this.tagId = tagId;
+	}
+
+	public List<OperatorTag> getOperatorTags() {
+		return operatorTags;
+	}
+
+	public void setOperatorTags(List<OperatorTag> operatorTags) {
+		this.operatorTags = operatorTags;
+	}
+	
+	public boolean isCanUpgrade() {
+		if(permissions.isContractor())
+			return true;
+		if(permissions.seesAllContractors())
+			return true;
+		if(permissions.isOperator() 
+				&& permissions.hasPermission(OpPerms.ViewTrialAccounts, OpType.Edit))
+			return true;
+		
+		return false;
+	}
+}
