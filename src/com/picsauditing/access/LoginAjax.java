9fb7583ccce19cd648cfb0a3364b6209a1c3ad6c
Kyle Partridge <kpartridge@picsauditing.com>
2009-11-18 19:02:12 +0000

    fixed bug#0001590

--- src/com/picsauditing/access/LoginAjax.java
+++ src/com/picsauditing/access/LoginAjax.java
@@ -1,12 +1,22 @@
 package com.picsauditing.access;
 
+import org.json.simple.JSONObject;
+
 import com.picsauditing.actions.PicsActionSupport;
 
 @SuppressWarnings("serial")
 public class LoginAjax extends PicsActionSupport {
+	private JSONObject json = new JSONObject();
+
+	@SuppressWarnings("unchecked")
 	@Override
 	public String execute() throws Exception {
 		loadPermissions(false);
-		return SUCCESS;
+		json.put("loggedin", permissions.isLoggedIn());
+		return JSON;
+	}
+	
+	public JSONObject getJson() {
+		return json;
 	}
 }
