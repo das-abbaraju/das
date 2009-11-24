6ab475217e60fdea9eeeaf2b683080343f1552f5
Kyle Partridge <kpartridge@picsauditing.com>
2009-11-24 18:32:15 +0000

    bug#0001597 reword schedule office audit message

--- src/com/picsauditing/actions/contractors/ContractorWidget.java
+++ src/com/picsauditing/actions/contractors/ContractorWidget.java
@@ -177,7 +177,7 @@ public class ContractorWidget extends ContractorActionSupport {
 					String text;
 					if (conAudit.getAuditType().getId() == AuditType.OFFICE && conAudit.getScheduledDate() == null) {
 						text = "Please <a href='ScheduleAudit.action?auditID=" + conAudit.getId()
-								+ "'>schedule your Office Audit</a>";
+								+ "'>click here to schedule your Office Audit</a>";
 					} else {
 						text = "Prepare for an <a href=\"Audit.action?auditID=" + conAudit.getId() + "\">upcoming "
 								+ conAudit.getAuditType().getAuditName() + "</a>";
