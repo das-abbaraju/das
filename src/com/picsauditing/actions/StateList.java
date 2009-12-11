4129b2187b12a49c4fbe5238d3e129f749ee9d93
Kyle Partridge <kpartridge@picsauditing.com>
2009-12-11 00:12:20 +0000

    fixed alpha stateList

--- src/com/picsauditing/actions/StateList.java
+++ src/com/picsauditing/actions/StateList.java
@@ -2,5 +2,9 @@ package com.picsauditing.actions;
 
 @SuppressWarnings("serial")
 public class StateList extends AccountActionSupport {
+	@Override
+	public String execute() throws Exception {
 
+		return SUCCESS;
+	}
 }
