6a46a7ff7d81bd95b5ef937db70331006dc60948
Tom Baker <tbaker@picsauditing.com>
2010-01-30 01:03:26 +0000

    PICS-32: Needed to move a WHERE clause item out of generalcontractor JOIN

--- src/com/picsauditing/mail/FlagChangesSubscription.java
+++ src/com/picsauditing/mail/FlagChangesSubscription.java
@@ -37,7 +37,7 @@ public class FlagChangesSubscription extends SubscriptionBuilder {
 			// Only send out results on the contractors in the general contractor table that have approved work status
 			sql.addJoin("JOIN flags f1 ON a.id = f1.conID AND f1.opID = " + o.getId());
 			sql.addJoin("JOIN flag_archive f2 ON f1.conID = f2.conID AND f1.opID = f2.opID AND f1.flag <> f2.flag");
-			sql.addJoin("JOIN generalcontractors gc ON f1.conID = gc.subID AND f1.opID = gc.genID AND gc.workStatus = 'Y'");
+			sql.addJoin("JOIN generalcontractors gc ON f1.conID = gc.subID AND f1.opID = gc.genID");
 			sql.addJoin("JOIN operators o ON gc.genID = o.id");
 			
 			sql.addWhere("f2.creationDate = '" + df.format(timePeriod.getComparisonDate()) + "'");
