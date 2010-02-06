bfc340ae3312734353b708511cba6bb4cef1bce5
Trevor Allred <tallred@picsauditing.com>
2010-02-06 00:11:37 +0000

    PICS-163

--- src/com/picsauditing/jpa/entities/FlagCriteriaOperator.java
+++ src/com/picsauditing/jpa/entities/FlagCriteriaOperator.java
@@ -11,4 +11,17 @@ import org.hibernate.annotations.CacheConcurrencyStrategy;
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE, region = "daily")
 public class FlagCriteriaOperator extends BaseTable {
 
+	/**
+	 * Determine if a contractor's answer to this criteria should be flagged
+	 * and if so, what color. If the contractor criteria is not the same as the operator
+	 * criteria, then throw an exception.
+	 * 
+	 * @param contractorCriteria
+	 * @return
+	 */
+	public FlagColor evaluate(FlagCriteriaContractor contractorCriteria) {
+		
+		return null;
+	}
+
 }
