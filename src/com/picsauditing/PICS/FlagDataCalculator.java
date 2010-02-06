b4a6c808e53cd985ab7eebee3a3b200134c4b494
Trevor Allred <tallred@picsauditing.com>
2010-02-06 00:01:48 +0000

    PICS-163 started

---
+++ src/com/picsauditing/PICS/FlagDataCalculator.java
@@ -0,0 +1,53 @@
+package com.picsauditing.PICS;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.picsauditing.jpa.entities.FlagColor;
+import com.picsauditing.jpa.entities.FlagCriteria;
+import com.picsauditing.jpa.entities.FlagCriteriaContractor;
+import com.picsauditing.jpa.entities.FlagCriteriaOperator;
+import com.picsauditing.jpa.entities.FlagData;
+
+public class FlagDataCalculator {
+	private Map<FlagCriteria, FlagCriteriaContractor> contractorCriteria = null;
+	private Map<FlagCriteria, FlagCriteriaOperator> operatorCriteria = null;
+	
+	public FlagDataCalculator(List<FlagCriteriaContractor> contractorCriteria, List<FlagCriteriaOperator> operatorCriteria) {
+		setContractorCriteria(contractorCriteria);
+		setOperatorCriteria(operatorCriteria);
+	}
+
+	public List<FlagData> calculate() {
+		List<FlagData> list = new ArrayList<FlagData>();
+		
+		for (FlagCriteria criteria : operatorCriteria.keySet()) {
+			if (contractorCriteria.containsKey(criteria)) {
+				FlagColor flag = criteria.evaluate(contractorCriteria);
+				if (!flag.equals(FlagColor.Green)) {
+					FlagData data = new FlagData();
+					// TODO set the data fields
+					list.add(data);
+				}
+			}
+		}
+		
+		return list;
+	}
+	
+	public void setContractorCriteria(List<FlagCriteriaContractor> list) {
+		contractorCriteria = new HashMap<FlagCriteria, FlagCriteriaContractor>();
+		for (FlagCriteriaContractor value : list) {
+			contractorCriteria.put(null, value);
+		}
+	}
+
+	public void setOperatorCriteria(List<FlagCriteriaOperator> list) {
+		operatorCriteria = new HashMap<FlagCriteria, FlagCriteriaOperator>();
+		for (FlagCriteriaOperator value : list) {
+			operatorCriteria.put(null, value);
+		}
+	}
+}
