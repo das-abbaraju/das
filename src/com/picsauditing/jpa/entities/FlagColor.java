38cf214141cedfb3cb3ab449e8f620c7882ff81c
Kyle Partridge <kpartridge@picsauditing.com>
2010-02-24 23:14:03 +0000

    PICS-214: enum enhancements. added insuranceStatus field

--- src/com/picsauditing/jpa/entities/FlagColor.java
+++ src/com/picsauditing/jpa/entities/FlagColor.java
@@ -3,14 +3,19 @@ package com.picsauditing.jpa.entities;
 import java.util.ArrayList;
 
 public enum FlagColor {
-	Green("#339900"), Amber("#FFCC33"), Red("#CC0000");
+
+	Green("#339900", "Approve"),
+	Amber("#FFCC33", "Review"),
+	Red("#CC0000", "Reject");
 
 	private String hex;
-	
-	private FlagColor(String hex) {
+	private String insuranceStatus;
+
+	private FlagColor(String hex, String insuranceStatus) {
 		this.hex = hex;
+		this.insuranceStatus = insuranceStatus;
 	}
-	
+
 	public static ArrayList<String> getValuesWithDefault() {
 		ArrayList<String> values = new ArrayList<String>();
 		for (FlagColor value : FlagColor.values())
@@ -21,27 +26,31 @@ public enum FlagColor {
 	public String getHex() {
 		return hex;
 	}
-	
+
+	public String getInsuranceStatus() {
+		return insuranceStatus;
+	}
+
 	public String getBigIcon() {
-		return "<img src=\"images/icon_"+this.toString().toLowerCase()+
-			"FlagBig.gif\" width=\"32\" height=\"32\" border=\"0\" title=\""+this.toString()+"\" />";
+		return "<img src=\"images/icon_" + this.toString().toLowerCase()
+				+ "FlagBig.gif\" width=\"32\" height=\"32\" border=\"0\" title=\"" + this.toString() + "\" />";
 	}
-	
+
 	public String getSmallIcon() {
-			return "<img src=\"images/icon_"+this.toString().toLowerCase()+
-				"Flag.gif\" width=\"10\" height=\"12\" border=\"0\" title=\""+this.toString()+"\" />";
+		return "<img src=\"images/icon_" + this.toString().toLowerCase()
+				+ "Flag.gif\" width=\"10\" height=\"12\" border=\"0\" title=\"" + this.toString() + "\" />";
 	}
-	
+
 	static public String getSmallIcon(String flagColor) {
 		return valueOf(flagColor).getSmallIcon();
 	}
-	
-	public static FlagColor getWorseColor( FlagColor color1, FlagColor color2) {
+
+	public static FlagColor getWorseColor(FlagColor color1, FlagColor color2) {
 		if (color2 == null)
 			return color1;
 
 		if (color1 == null) {
-//			System.out.println("WARNING: oldColor == null");
+			// System.out.println("WARNING: oldColor == null");
 			return color2;
 		}
 
