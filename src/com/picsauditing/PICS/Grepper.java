479607ef6d0af82949f7c3973a7068a3ade2aa35
Kyle Partridge <kpartridge@picsauditing.com>
2010-03-01 18:55:45 +0000

    Format

--- src/com/picsauditing/PICS/Grepper.java
+++ src/com/picsauditing/PICS/Grepper.java
@@ -3,26 +3,24 @@ package com.picsauditing.PICS;
 import java.util.List;
 import java.util.Vector;
 
-public abstract class Grepper <T> {
-	public abstract boolean check( T t );
-	
-	
-	public List<T> grep( List<T> in ) {
-		
+public abstract class Grepper<T> {
+
+	public abstract boolean check(T t);
+
+	public List<T> grep(List<T> in) {
+
 		List<T> output = new Vector<T>();
 
-		if( in != null ) {
-		
-			for( T t : in ) {
-				if( check(t)) {
+		if (in != null) {
+
+			for (T t : in) {
+				if (check(t)) {
 					output.add(t);
 				}
 			}
 		}
-		
+
 		return output;
 	}
-	
-	
-	
+
 }
