d553170451a6d1f11ab01d9a846e191e42bd5f17
Kyle Partridge <kpartridge@picsauditing.com>
2010-03-03 23:50:19 +0000

    PICS-233

--- src/com/picsauditing/actions/Home.java
+++ src/com/picsauditing/actions/Home.java
@@ -5,16 +5,23 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
+import javax.servlet.http.Cookie;
+
+import org.apache.struts2.ServletActionContext;
+
+import com.ibm.icu.util.Calendar;
 import com.picsauditing.access.OpPerms;
 import com.picsauditing.actions.contractors.ContractorActionSupport;
 import com.picsauditing.dao.ContractorAccountDAO;
 import com.picsauditing.dao.ContractorAuditDAO;
 import com.picsauditing.dao.WidgetUserDAO;
+import com.picsauditing.jpa.entities.User;
 import com.picsauditing.jpa.entities.Widget;
 import com.picsauditing.jpa.entities.WidgetUser;
 
 @SuppressWarnings("serial")
 public class Home extends ContractorActionSupport {
+
 	private Map<Integer, List<Widget>> columns = new TreeMap<Integer, List<Widget>>();
 
 	private WidgetUserDAO dao;
@@ -41,12 +48,12 @@ public class Home extends ContractorActionSupport {
 
 		for (WidgetUser widgetUser : widgetsToShowForUser) {
 			Widget widget = widgetUser.getWidget();
-			
-			if(widget.getRequiredPermission() != null) {
-				if(!permissions.hasPermission(widget.getRequiredPermission()))
+
+			if (widget.getRequiredPermission() != null) {
+				if (!permissions.hasPermission(widget.getRequiredPermission()))
 					continue;
 			}
-			
+
 			// Set custom parameters from the user
 			widget.setExpanded(widgetUser.isExpanded());
 			widget.setCustomConfig(widgetUser.getCustomConfig());
@@ -76,5 +83,38 @@ public class Home extends ContractorActionSupport {
 
 	public int getColumnWidth() {
 		return Math.round(90 / columns.size());
+	}
+
+	public boolean isShowRebranding() {
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.MONTH, Calendar.MARCH);
+		cal.set(Calendar.DAY_OF_MONTH, 1);
+		cal.set(Calendar.YEAR, 2010);
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+
+		User u = getUser();
+
+		if (u.getCreationDate() == null || u.getCreationDate().before(cal.getTime())) {
+
+			Cookie[] cookies = ServletActionContext.getRequest().getCookies();
+
+			if (cookies != null) {
+				for (Cookie cookie : cookies) {
+					if ("PICS_NEW".equals(cookie.getName())) {
+						if ("1".equals(cookie.getValue()))
+							return false;
+					}
+				}
+			}
+
+			// The account has not seen the notice
+			Cookie PICS_NEW = new Cookie("PICS_NEW", "1");
+			ServletActionContext.getResponse().addCookie(PICS_NEW);
+			return true;
+		}
+
+		return false;
 	}
 }
