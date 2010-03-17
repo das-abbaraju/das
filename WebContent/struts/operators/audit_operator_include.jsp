aea7cffce90827e383738eb386e660cd3b7045f3
Lani Aung <uaung@picsauditing.com>
2010-03-17 18:40:25 +0000

    AuditOperator changes to minimum risk level now updates the FlagCriteriaOperator with the same auditType. An info message appears if the FCO does not exist.

--- WebContent/struts/operators/audit_operator_include.jsp
+++ WebContent/struts/operators/audit_operator_include.jsp
@@ -1,20 +1,21 @@
 <%@ taglib prefix="s" uri="/struts-tags"%>
 <div style="padding: 0px; margin: 0px; white-space: nowrap;">
-<form id="ao<s:property value="htmlID" />"><s:if test="id > 0">
-	<s:hidden name="ao.id" value="%{id}" />
-</s:if><s:else>
-	<s:hidden name="ao.auditType.id" value="%{auditType.id}" />
-	<s:hidden name="ao.operatorAccount.id" value="%{operatorAccount.id}" />
-</s:else> <s:checkbox name="ao.canSee" value="%{canSee}" title="View"
-	onclick="$('#button%{htmlID}').show()" /> | <s:checkbox
-	name="ao.canEdit" value="canEdit" title="Edit"
-	onclick="$('#button%{htmlID}').show()" /> | <s:radio
-	id="aorisk%{htmlID}" name="ao.minRiskLevel" list="riskLevelList"
-	value="minRiskLevel" onclick="$('#button%{htmlID}').show()" />
-	<s:if
-	test="auditType.classType.policy">
-	<br>
-	<s:textarea name="ao.help" value="%{help}" rows="3" cols="70"
-		onkeyup="$('#button%{htmlID}').show()"></s:textarea>
-</s:if></form>
+	<form id="ao<s:property value="htmlID" />">
+		<s:if test="id > 0">
+			<s:hidden name="ao.id" value="%{id}" />
+		</s:if>
+		<s:else>
+			<s:hidden name="ao.auditType.id" value="%{auditType.id}" />
+			<s:hidden name="ao.operatorAccount.id" value="%{operatorAccount.id}" />
+		</s:else>
+		<s:checkbox name="ao.canSee" value="%{canSee}" title="View" onclick="$('#button%{htmlID}').show()" /> | 
+		<s:checkbox name="ao.canEdit" value="canEdit" title="Edit" onclick="$('#button%{htmlID}').show()" /> | 
+		<s:radio id="aorisk%{htmlID}" name="ao.minRiskLevel" list="riskLevelList"
+			value="minRiskLevel" onclick="$('#button%{htmlID}').show()" />
+		<s:if test="auditType.classType.policy"><br />
+			<s:textarea name="ao.help" value="%{help}" rows="3" cols="70" 
+				onkeyup="$('#button%{htmlID}').show()"></s:textarea>
+		</s:if>
+	</form>
+<div style="margin-right: 10px"><s:include value="../actionMessages.jsp" /></div>
 </div>
--- src/com/picsauditing/actions/audits/AuditOperatorSave.java
+++ src/com/picsauditing/actions/audits/AuditOperatorSave.java
@@ -1,14 +1,18 @@
 package com.picsauditing.actions.audits;
 
+import java.util.List;
 import java.util.Map;
 
 import com.opensymphony.xwork2.Preparable;
+import com.picsauditing.access.OpPerms;
 import com.picsauditing.actions.PicsActionSupport;
 import com.picsauditing.dao.AuditOperatorDAO;
 import com.picsauditing.dao.ContractorAccountDAO;
+import com.picsauditing.dao.FlagCriteriaOperatorDAO;
 import com.picsauditing.jpa.entities.AuditOperator;
 import com.picsauditing.jpa.entities.AuditStatus;
 import com.picsauditing.jpa.entities.FlagColor;
+import com.picsauditing.jpa.entities.FlagCriteriaOperator;
 import com.picsauditing.jpa.entities.LowMedHigh;
 
 @SuppressWarnings("serial")
@@ -17,12 +21,14 @@ public class AuditOperatorSave extends PicsActionSupport implements Preparable {
 	private AuditOperator ao = null;
 	private AuditOperatorDAO dao;
 	private ContractorAccountDAO cAccountDAO;
+	private FlagCriteriaOperatorDAO fcoDAO;
 	private int operatorID;
 	private int auditTypeID;
 
-	public AuditOperatorSave(AuditOperatorDAO dao, ContractorAccountDAO cAccountDAO) {
+	public AuditOperatorSave(AuditOperatorDAO dao, ContractorAccountDAO cAccountDAO, FlagCriteriaOperatorDAO fcoDAO) {
 		this.dao = dao;
 		this.cAccountDAO = cAccountDAO;
+		this.fcoDAO = fcoDAO;
 	}
 
 	public String execute() {
@@ -30,10 +36,35 @@ public class AuditOperatorSave extends PicsActionSupport implements Preparable {
 			return LOGIN;
 		
 		cAccountDAO.updateContractorByOperator(ao.getOperatorAccount());
-
+		boolean foundCriteria = false;
+		
+		try {
+			List<FlagCriteriaOperator> opCriteria = ao.getOperatorAccount().getFlagCriteriaInherited();
+			
+			for (FlagCriteriaOperator fco : opCriteria) {
+				if (fco.getCriteria().getAuditType() != null && fco.getCriteria().getAuditType().equals(ao.getAuditType())) {
+					foundCriteria = true;
+					fco.setMinRiskLevel(LowMedHigh.getMap().get(ao.getMinRiskLevel()));
+					fcoDAO.save(fco);
+				}
+			}
+		} catch (Exception e) {
+			// Do nothing?
+		}
+		
 		ao.setAuditColumns(permissions);
 		ao = dao.save(ao);
 
+		if (!foundCriteria) {
+			String message = ao.getOperatorAccount().getName() + " has no flag criteria for "
+				+ ao.getAuditType().getAuditName();
+			
+			if (permissions.hasPermission(OpPerms.ManageOperators))
+				message += "<br />To set up flag criteria, <a href=\"ManageFlagCriteria.action\">click here</a>";
+			
+			addActionMessage(message);
+		}
+		
 		return SUCCESS;
 	}
 
