356c460503337b07795c88be7acba44100233dd6
Kyle Partridge <kpartridge@picsauditing.com>
2010-03-18 19:37:20 +0000

    fuzzy date pluralization fix

--- src/com/picsauditing/PICS/DateBean.java
+++ src/com/picsauditing/PICS/DateBean.java
@@ -331,18 +331,25 @@ public class DateBean {
 		String fuzzy = "";
 		if (diff < MINUTES)
 			fuzzy = "Today";
-		else if (diff < HOURS)
-			fuzzy = diff / MINUTES + " minutes ago";
-		else if (diff < DAYS)
-			fuzzy = diff / HOURS + " hours ago";
-		else if (diff < WEEKS)
-			fuzzy = diff / DAYS + " days ago";
-		else if (diff < MONTHS)
-			fuzzy = diff / WEEKS + " weeks ago";
-		else if (diff < YEARS)
-			fuzzy = diff / MONTHS + " months ago";
-		else
-			fuzzy = diff / YEARS + " years ago";
+		else if (diff < HOURS) {
+			long n = diff / MINUTES;
+			fuzzy = n + ((n == 1) ? " minute ago" : " minutes ago");
+		} else if (diff < DAYS) {
+			long n = diff / HOURS;
+			fuzzy = n + ((n == 1) ? " hour ago" : " hours ago");
+		} else if (diff < WEEKS) {
+			long n = diff / DAYS;
+			fuzzy = n + ((n == 1) ? " day ago" : " days ago");
+		} else if (diff < MONTHS) {
+			long n = diff / WEEKS;
+			fuzzy = n + ((n == 1) ? " week ago" : " weeks ago");
+		} else if (diff < YEARS) {
+			long n = diff / MONTHS;
+			fuzzy = n + ((n == 1) ? " month ago" : " months ago");
+		} else {
+			long n = diff / YEARS;
+			fuzzy = n + ((n == 1) ? " year ago" : " years ago");
+		}
 
 		return fuzzy;
 
