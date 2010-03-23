7691e0264945324bb238d78f08e59140fe81305a
Keerthi Nannapaneni <knannapaneni@picsauditing.com>
2010-03-23 22:34:13 +0000

    Fixed the contractors tab in opheader to use operatorids for corporate accounts. PICS-335

--- src/com/picsauditing/actions/operators/OperatorActionSupport.java
+++ src/com/picsauditing/actions/operators/OperatorActionSupport.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import com.picsauditing.actions.AccountActionSupport;
 import com.picsauditing.dao.OperatorAccountDAO;
 import com.picsauditing.jpa.entities.AmBest;
+import com.picsauditing.jpa.entities.Facility;
 import com.picsauditing.jpa.entities.FlagCriteriaOperator;
 import com.picsauditing.jpa.entities.OperatorAccount;
 
@@ -21,7 +22,7 @@ public class OperatorActionSupport extends AccountActionSupport {
 	public OperatorActionSupport(OperatorAccountDAO operatorDao) {
 		this.operatorDao = operatorDao;
 	}
-	
+
 	public String execute() throws Exception {
 		findOperator();
 		return SUCCESS;
@@ -30,22 +31,23 @@ public class OperatorActionSupport extends AccountActionSupport {
 	protected void findOperator() throws Exception {
 		loadPermissions();
 		if (operator == null) {
-			
+
 			if (id == 0) {
-				// Check the parameter list just in case it hasn't been set yet 
+				// Check the parameter list just in case it hasn't been set yet
 				// ie this is being called from a Prepare method
 				id = this.getParameter("id");
 			}
 
 			if (permissions.isOperatorCorporate())
 				id = permissions.getAccountId();
-			
+
 			if (id == 0)
 				throw new Exception("Missing operator id");
-//			else if (permissions.isCorporate()) {
-//				if (!permissions.getOperatorChildren().contains(id))
-//					throw new Exception("Corporate account doesn't have access to that operator");
-			
+			// else if (permissions.isCorporate()) {
+			// if (!permissions.getOperatorChildren().contains(id))
+			// throw new
+			// Exception("Corporate account doesn't have access to that operator");
+
 			operator = operatorDao.find(id);
 			account = operator;
 		}
@@ -54,10 +56,11 @@ public class OperatorActionSupport extends AccountActionSupport {
 	public OperatorAccount getOperator() {
 		return operator;
 	}
-	
+
 	public List<OperatorAccount> getInheritsFlagCriteria() {
 		if (inheritsFlagCriteria == null) {
-			inheritsFlagCriteria = operatorDao.findWhere(true, "a.status IN ('Active','Demo') AND a.inheritFlagCriteria.id = " + operator.getId());
+			inheritsFlagCriteria = operatorDao.findWhere(true,
+					"a.status IN ('Active','Demo') AND a.inheritFlagCriteria.id = " + operator.getId());
 			inheritsFlagCriteria.remove(operator);
 		}
 		return inheritsFlagCriteria;
@@ -65,7 +68,8 @@ public class OperatorActionSupport extends AccountActionSupport {
 
 	public List<OperatorAccount> getInheritsInsuranceCriteria() {
 		if (inheritsInsuranceCriteria == null) {
-			inheritsInsuranceCriteria = operatorDao.findWhere(true, "a.status IN ('Active','Demo') AND a.inheritInsuranceCriteria.id = " + operator.getId());
+			inheritsInsuranceCriteria = operatorDao.findWhere(true,
+					"a.status IN ('Active','Demo') AND a.inheritInsuranceCriteria.id = " + operator.getId());
 			inheritsInsuranceCriteria.remove(operator);
 		}
 		return inheritsInsuranceCriteria;
@@ -73,7 +77,8 @@ public class OperatorActionSupport extends AccountActionSupport {
 
 	public List<OperatorAccount> getInheritsAudits() {
 		if (inheritsAudits == null) {
-			inheritsAudits = operatorDao.findWhere(true, "a.status IN ('Active','Demo') AND a.inheritAudits.id = " + operator.getId());
+			inheritsAudits = operatorDao.findWhere(true, "a.status IN ('Active','Demo') AND a.inheritAudits.id = "
+					+ operator.getId());
 			inheritsAudits.remove(operator);
 		}
 		return inheritsAudits;
@@ -81,12 +86,13 @@ public class OperatorActionSupport extends AccountActionSupport {
 
 	public List<OperatorAccount> getInheritsInsurance() {
 		if (inheritsInsurance == null) {
-			inheritsInsurance = operatorDao.findWhere(true, "a.status IN ('Active','Demo') AND a.inheritInsurance.id = " + operator.getId());
+			inheritsInsurance = operatorDao.findWhere(true,
+					"a.status IN ('Active','Demo') AND a.inheritInsurance.id = " + operator.getId());
 			inheritsInsurance.remove(operator);
 		}
 		return inheritsInsurance;
 	}
-	
+
 	public Map<Integer, String> getAMBestClassList() {
 		return AmBest.financialMap;
 	}
@@ -94,13 +100,25 @@ public class OperatorActionSupport extends AccountActionSupport {
 	public Map<Integer, String> getAMBestRatingsList() {
 		return AmBest.ratingMap;
 	}
-	
+
 	public FlagCriteriaOperator getFlagCriteriaOperatorByAudit(int auditTypeID) {
-		for(FlagCriteriaOperator flagCriteriaOperator : operator.getFlagAuditCriteriaInherited()) {
-			if(flagCriteriaOperator.getCriteria().getAuditType().getId() == auditTypeID) {
+		for (FlagCriteriaOperator flagCriteriaOperator : operator.getFlagAuditCriteriaInherited()) {
+			if (flagCriteriaOperator.getCriteria().getAuditType().getId() == auditTypeID) {
 				return flagCriteriaOperator;
 			}
 		}
 		return null;
+	}
+
+	public String getOperatorIds() {
+		String where = "";
+		if (operator.isCorporate()) {
+			for (Facility facility : operator.getOperatorFacilities()) {
+				where += "&filter.operator=" + facility.getOperator().getId();
+			}
+		} else {
+			where += "&filter.operator=" + operator.getId();
+		}
+		return where;
 	}
 }
