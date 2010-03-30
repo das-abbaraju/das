ad2178a5a40c3b7053d57c3654ad67b1bef1b17d
Lani Aung <uaung@picsauditing.com>
2010-03-30 00:05:19 +0000

    PICS-366: Updated labels to 0 days, 30 days, 60 days and 90 days ago.

--- src/com/picsauditing/actions/chart/OperatorFlagHistory.java
+++ src/com/picsauditing/actions/chart/OperatorFlagHistory.java
@@ -23,14 +23,14 @@ public class OperatorFlagHistory extends ChartMSAction {
 		int operatorID = permissions.getAccountId();
 		Date yesterday =  DateBean.addField(new Date(), Calendar.DAY_OF_WEEK, -1);
 
-		String sql = getOperatorFlagHistorySQL(yesterday, operatorID)
+		String sql = getOperatorFlagHistorySQL(yesterday, "0 days", operatorID)
 			+ " UNION " +	
-			getOperatorFlagHistorySQL(DateBean.getFirstofMonthOrClosestSunday(DateBean.addMonths(yesterday, -1)), operatorID)
+			getOperatorFlagHistorySQL(DateBean.getFirstofMonthOrClosestSunday(DateBean.addDays(yesterday, -30)), "30 days", operatorID)
 			+ " UNION " +
-			getOperatorFlagHistorySQL(DateBean.getFirstofMonthOrClosestSunday(DateBean.addMonths(yesterday, -2)), operatorID)
+			getOperatorFlagHistorySQL(DateBean.getFirstofMonthOrClosestSunday(DateBean.addDays(yesterday, -60)), "60 days", operatorID)
 			+ " UNION " +
-			getOperatorFlagHistorySQL(DateBean.getFirstofMonthOrClosestSunday(DateBean.addMonths(yesterday, -3)), operatorID)
-			+ " ORDER BY label";
+			getOperatorFlagHistorySQL(DateBean.getFirstofMonthOrClosestSunday(DateBean.addDays(yesterday, -90)), "90 days", operatorID)
+			+ " ORDER BY creationDate DESC";
 
 		ChartDAO db = new ChartDAO();
 		List<DataRow> data = db.select(sql.toString());
@@ -49,10 +49,10 @@ public class OperatorFlagHistory extends ChartMSAction {
 		return chart;
 	}
 	
-	private String getOperatorFlagHistorySQL(Date date, int operatorID) throws Exception {
+	private String getOperatorFlagHistorySQL(Date date, String label, int operatorID) throws Exception {
 		String dbDate = DateBean.toDBFormat(date);
-		String creationDate = DateBean.prettyDate(date);
-		String sql = "SELECT '"+ creationDate +"' AS label, flag AS series, count(*) AS value FROM flag_archive WHERE opID = "+ operatorID + " AND creationDate = '" + dbDate +"' GROUP BY flag";	
+		//String creationDate = DateBean.prettyDate(date);
+		String sql = "SELECT '"+ label +"' AS label, flag AS series, count(*) AS value, creationDate FROM flag_archive WHERE opID = "+ operatorID + " AND creationDate = '" + dbDate +"' GROUP BY flag";	
 		return sql;
 	}
 }
