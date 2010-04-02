65d07f657754e66ad704f968614e8c8ce21ffc55
Trevor Allred <tallred@picsauditing.com>
2010-04-02 20:51:16 +0000

    testing static vars for threading

--- src/com/picsauditing/actions/Trevor.java
+++ src/com/picsauditing/actions/Trevor.java
@@ -1,17 +1,57 @@
 package com.picsauditing.actions;
 
-import com.picsauditing.access.OpPerms;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.picsauditing.util.Strings;
 
 @SuppressWarnings("serial")
 public class Trevor extends PicsActionSupport {
 
+	static private Set<Trevor> manager = new HashSet<Trevor>();
+	private String hash;
+	private int id;
+
 	@Override
 	public String execute() throws Exception {
-		if (!forceLogin())
-			return LOGIN_AJAX;
-
-		tryPermissions(OpPerms.DevelopmentEnvironment);
+		try {
+			manager.add(this);
+			process();
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			manager.remove(this);
+		}
 
 		return SUCCESS;
 	}
+
+	private void process() {
+		System.out.println("managers running now = " + manager.size());
+		hash = Strings.hash(manager.size() + "hi" + new Date().toString());
+		for (Trevor trevor : manager) {
+			System.out.println(" - " + id + ": " + trevor.getHash() + (this.equals(trevor) ? " self" : ""));
+		}
+		output = "hello world";
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public String getHash() {
+		return hash;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 }
