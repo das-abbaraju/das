8dd7dfee54230db897e89a2f8ecfedff665b7bb5
Kyle Partridge <kpartridge@picsauditing.com>
2010-04-06 19:50:32 +0000

    fix test case

--- tests/com/picsauditing/dao/AuditCategoryDataTestDAO.java
+++ tests/com/picsauditing/dao/AuditCategoryDataTestDAO.java
@@ -1,5 +1,7 @@
 package com.picsauditing.dao;
 
+import junit.framework.TestCase;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +10,13 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/tests.xml")
-public class AuditCategoryDataTestDAO {
+public class AuditCategoryDataTestDAO extends TestCase {
 	@Autowired
 	private AuditCategoryDataDAO dao;
 
 	@Test
 	public final void testFind() {
-
+		assertNotNull(dao.find(1));
 	}
 
 }
