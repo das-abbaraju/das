aac344f969dfcdaf6075b69ea871e70d0600c628
Tom Baker <tbaker@picsauditing.com>
2010-04-07 19:07:23 +0000

    Current version of Job Tasks

--- src/com/picsauditing/jpa/entities/JobTask.java
+++ src/com/picsauditing/jpa/entities/JobTask.java
@@ -1,20 +1,34 @@
 package com.picsauditing.jpa.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.json.simple.JSONObject;
 
 @SuppressWarnings("serial")
 @Entity
 @Table(name = "job_task")
+@PrimaryKeyJoinColumn(name = "id")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE, region = "temp")
 public class JobTask extends BaseTable {
 
 	private OperatorAccount operator;
 	private String label;
 	private String name;
 	private boolean active;
+	
+	private List<JobTaskCriteria> jobTaskCriteria = new ArrayList<JobTaskCriteria>();
 
 	@ManyToOne
 	@JoinColumn(name = "opID", nullable = false, updatable = false)
@@ -50,5 +64,22 @@ public class JobTask extends BaseTable {
 	public void setActive(boolean active) {
 		this.active = active;
 	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public JSONObject toJSON(boolean full) {
+		JSONObject json = super.toJSON(full);
 
+		json.put("label", label);
+		json.put("name", name);
+		if(full)
+			json.put("operator", operator.toJSON());
+		
+		return json;
+	}
+
+	@OneToMany(mappedBy = "task", cascade = { CascadeType.ALL })
+	public List<JobTaskCriteria> getCriteria() {
+		return jobTaskCriteria;
+	}
 }
--- src/com/picsauditing/jpa/entities/JobTaskCriteria.java
+++ src/com/picsauditing/jpa/entities/JobTaskCriteria.java
@@ -3,7 +3,11 @@ package com.picsauditing.jpa.entities;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 @SuppressWarnings("serial")
 @Entity
