9987dd87cfebf7d9c5e47ecd8f31a53c0f6dfce6
Trevor Allred <tallred@picsauditing.com>
2010-04-13 22:43:02 +0000

    initial check in of Employee Competency classes

---
+++ src/com/picsauditing/jpa/entities/JobCompetency.java
@@ -0,0 +1,20 @@
+package com.picsauditing.jpa.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "pqfdata")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE, region = "temp")
+public class JobCompetency extends BaseTable {
+
+	private String comment;
+	private Date dateVerified;
+
+}
