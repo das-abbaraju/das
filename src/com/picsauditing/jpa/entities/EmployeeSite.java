d8726e05d09093fa533d61667a187f730d301c82
Tom Baker <tbaker@picsauditing.com>
2010-04-16 21:41:55 +0000

    EmployeeSite entity

---
+++ src/com/picsauditing/jpa/entities/EmployeeSite.java
@@ -0,0 +1,58 @@
+package com.picsauditing.jpa.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.json.simple.JSONObject;
+
+@SuppressWarnings("serial")
+@Entity
+public class EmployeeSite extends BaseTable implements JSONable {
+
+	private OperatorAccount operator;
+	private JobSite jobSite;
+	private Employee employee;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "employeeID", nullable = false)
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "opID", nullable = false)
+	public OperatorAccount getOperator() {
+		return operator;
+	}
+
+	public void setOperator(OperatorAccount operator) {
+		this.operator = operator;
+	}
+
+	@ManyToOne
+	@JoinColumn(name = "jobSiteID", nullable = true)
+	public JobSite getJobSite() {
+		return jobSite;
+	}
+
+	public void setJobSite(JobSite jobSite) {
+		this.jobSite = jobSite;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public JSONObject toJSON(boolean full) {
+		JSONObject json = super.toJSON(full);
+
+		json.put("operator", operator.toJSON());
+		json.put("jobSite", jobSite.toJSON());
+		json.put("employee", employee.toJSON());
+
+		return json;
+	}
+}
