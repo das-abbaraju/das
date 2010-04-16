5cf43c7a8838e94b07705a62cc98dbb8af4d37fe
Kyle Partridge <kpartridge@picsauditing.com>
2010-04-16 23:44:57 +0000

    should use the record not found exception

--- src/com/picsauditing/actions/employees/EmployeeCompetencies.java
+++ src/com/picsauditing/actions/employees/EmployeeCompetencies.java
@@ -3,6 +3,7 @@ package com.picsauditing.actions.employees;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.picsauditing.access.RecordNotFoundException;
 import com.picsauditing.actions.AccountActionSupport;
 import com.picsauditing.dao.ContractorAccountDAO;
 import com.picsauditing.dao.EmployeeCompetencyDAO;
@@ -31,7 +32,7 @@ public class EmployeeCompetencies extends AccountActionSupport {
 	protected int jobRoleID;
 	protected boolean canEdit = false;
 	protected boolean checked;
-	
+
 	protected ContractorAccount contractor;
 	protected Employee employee = null;
 	protected List<Employee> employees;
@@ -40,8 +41,8 @@ public class EmployeeCompetencies extends AccountActionSupport {
 
 	protected DoubleMap<Employee, OperatorCompetency, EmployeeCompetency> map;
 
-	public EmployeeCompetencies(ContractorAccountDAO conDAO, EmployeeDAO employeeDAO,
-			EmployeeCompetencyDAO ecDAO, EmployeeRoleDAO erDAO, OperatorCompetencyDAO opCompDAO) {
+	public EmployeeCompetencies(ContractorAccountDAO conDAO, EmployeeDAO employeeDAO, EmployeeCompetencyDAO ecDAO,
+			EmployeeRoleDAO erDAO, OperatorCompetencyDAO opCompDAO) {
 		this.conDAO = conDAO;
 		this.employeeDAO = employeeDAO;
 		this.ecDAO = ecDAO;
@@ -53,27 +54,27 @@ public class EmployeeCompetencies extends AccountActionSupport {
 	public String execute() throws Exception {
 		if (!forceLogin())
 			return LOGIN;
-		
+
 		if (permissions.isContractor()) {
 			// Contractors should view and edit the competencies
 			canEdit = true;
 			conID = permissions.getAccountId();
 		}
-		
+
 		if (permissions.isAdmin())
 			canEdit = true;
-		
+
 		if (conID == 0)
-			throw new Exception("Missing conID");
-		
+			throw new RecordNotFoundException("Missing conID");
+
 		contractor = conDAO.find(conID);
-		
+
 		if (employeeID > 0)
 			employee = employeeDAO.find(employeeID);
-		
+
 		if (jobRoleID > 0)
 			roles = erDAO.findByJobRole(jobRoleID);
-		
+
 		if (button != null) {
 			if (button.equalsIgnoreCase("AddSkill")) {
 				// A checkbox has been checked
@@ -84,12 +85,12 @@ public class EmployeeCompetencies extends AccountActionSupport {
 					ecDAO.save(ec);
 					json.put("title", "Added Skill");
 					json.put("msg", "Successfully added " + ec.getCompetency().getLabel() + " skill to "
-						+ ec.getEmployee().getDisplayName());
+							+ ec.getEmployee().getDisplayName());
 					return JSON;
 				} else
 					addActionError("Missing employee competency ID");
 			}
-			
+
 			if (button.equalsIgnoreCase("RemoveSkill")) {
 				// A checkbox has been unchecked
 				if (ecID > 0) {
@@ -99,18 +100,18 @@ public class EmployeeCompetencies extends AccountActionSupport {
 					ecDAO.save(ec);
 					json.put("title", "Removed Skill");
 					json.put("msg", "Successfully removed " + ec.getCompetency().getLabel() + " skill from "
-						+ ec.getEmployee().getDisplayName());
+							+ ec.getEmployee().getDisplayName());
 					return JSON;
 				} else
 					addActionError("Missing employee competency ID");
 			}
-			
+
 			if (button.equalsIgnoreCase("Update List")) {
 				if (selectedCompetencies == null || selectedCompetencies.length == 0)
 					addActionError("Please select competencies");
 			}
 		}
-		
+
 		return SUCCESS;
 	}
 
@@ -121,59 +122,59 @@ public class EmployeeCompetencies extends AccountActionSupport {
 	public void setConID(int conID) {
 		this.conID = conID;
 	}
-	
+
 	public int getEmployeeID() {
 		return employeeID;
 	}
-	
+
 	public void setEmployeeID(int employeeID) {
 		this.employeeID = employeeID;
 	}
-	
+
 	public int getEcID() {
 		return ecID;
 	}
-	
+
 	public void setEcID(int ecID) {
 		this.ecID = ecID;
 	}
-	
+
 	public int getJobRoleID() {
 		return jobRoleID;
 	}
-	
+
 	public void setJobRoleID(int jobRoleID) {
 		this.jobRoleID = jobRoleID;
 	}
-	
+
 	public boolean isCanEdit() {
 		return canEdit;
 	}
-	
+
 	public void setCanEdit(boolean canEdit) {
 		this.canEdit = canEdit;
 	}
-	
+
 	public boolean isChecked() {
 		return checked;
 	}
-	
+
 	public void setChecked(boolean checked) {
 		this.checked = checked;
 	}
-	
+
 	public ContractorAccount getContractor() {
 		return contractor;
 	}
-	
+
 	public Employee getEmployee() {
 		return employee;
 	}
-	
+
 	public List<EmployeeRole> getRoles() {
 		return roles;
 	}
-	
+
 	public int[] getSelectedCompetencies() {
 		return selectedCompetencies;
 	}
@@ -181,20 +182,20 @@ public class EmployeeCompetencies extends AccountActionSupport {
 	public void setSelectedCompetencies(int[] selectedCompetencies) {
 		this.selectedCompetencies = selectedCompetencies;
 	}
-	
+
 	public List<OperatorCompetency> getSelectedOC() {
 		if (selectedCompetencies != null)
 			return opCompDAO.findWhere("id IN (0," + Strings.implode(selectedCompetencies) + ")");
-		
+
 		return null;
 	}
-	
+
 	public List<Employee> getEmployees() {
 		// Find ALL employees or just the ones with roles?
-		//return conDAO.find(conID).getEmployees();
+		// return conDAO.find(conID).getEmployees();
 		if (employees == null) {
 			employees = new ArrayList<Employee>();
-			
+
 			if (selectedCompetencies == null || selectedCompetencies.length == 0) {
 				List<EmployeeRole> roles = erDAO.findByContractor(conID);
 				for (EmployeeRole role : roles) {
@@ -204,27 +205,27 @@ public class EmployeeCompetencies extends AccountActionSupport {
 				employees = employeeDAO.findByCompetencies(selectedCompetencies);
 			}
 		}
-		
+
 		return employees;
 	}
-	
+
 	public List<OperatorCompetency> getCompetencies() {
 		return opCompDAO.findByContractor(conID);
 	}
-	
+
 	public List<EmployeeCompetency> getCompetencies(Employee employee) {
 		return ecDAO.findByEmployee(employee.getId());
 	}
-	
+
 	public List<EmployeeRole> getEmployeeRoles(int employeeID) {
 		return erDAO.findByEmployee(employeeID);
 	}
-	
+
 	public DoubleMap<Employee, OperatorCompetency, EmployeeCompetency> getMap() {
 		if (map == null && conID > 0) {
 			map = opCompDAO.findEmployeeCompetencies(getEmployees(), getCompetencies());
 		}
-		
+
 		return map;
 	}
 }
