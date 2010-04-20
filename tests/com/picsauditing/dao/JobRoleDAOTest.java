fb681e693c8173ad6487ffda2cf14873d336f934
Kyle Partridge <kpartridge@picsauditing.com>
2010-04-20 20:32:05 +0000

    JobRoleDAOTest

---
+++ tests/com/picsauditing/dao/JobRoleDAOTest.java
@@ -0,0 +1,29 @@
+package com.picsauditing.dao;
+
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/tests.xml")
+@TransactionConfiguration(defaultRollback = true)
+@Transactional
+public class JobRoleDAOTest extends TestCase {
+	@Autowired
+	private JobRoleDAO jobRoleDAO;
+
+	@Test
+	public void testFindDistinctRolesOrderByPercent() throws Exception {
+		List<String> roleOrder = jobRoleDAO.findDistinctRolesOrderByPercent();
+
+		assertTrue(roleOrder.size() > 0);
+	}
+}
