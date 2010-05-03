7f85a0bc3ad974788781f3f9f024ed63a4d8062b
Trevor Allred <tallred@picsauditing.com>
2010-05-03 20:58:58 +0000

    MANTIS-1201

---
+++ src/com/picsauditing/jpa/entities/ContractorWatch.java
@@ -0,0 +1,36 @@
+package com.picsauditing.jpa.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "contractor_watch")
+public class ContractorWatch extends BaseTable {
+
+	private ContractorAccount contractor;
+	private User user;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "conID", nullable = false, updatable = false)
+	public ContractorAccount getContractor() {
+		return contractor;
+	}
+
+	public void setContractor(ContractorAccount contractor) {
+		this.contractor = contractor;
+	}
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "userID", nullable = false, updatable = false)
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
