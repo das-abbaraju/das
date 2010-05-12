ffce7c5d25469c226fe5e2cc164d7eda276f4f13
Keerthi Nannapaneni <knannapaneni@picsauditing.com>
2010-05-12 22:25:55 +0000

    added ChartFlagCount to CSR dashboard.

--- src/com/picsauditing/actions/chart/ChartFlagCount.java
+++ src/com/picsauditing/actions/chart/ChartFlagCount.java
@@ -3,6 +3,7 @@ package com.picsauditing.actions.chart;
 import java.util.List;
 
 import com.picsauditing.jpa.entities.FlagColor;
+import com.picsauditing.jpa.entities.User;
 import com.picsauditing.search.SelectSQL;
 import com.picsauditing.util.chart.ChartSingleSeries;
 import com.picsauditing.util.chart.DataRow;
@@ -29,10 +30,18 @@ public class ChartFlagCount extends ChartSSAction {
 
 		sql.addJoin("JOIN generalcontractors gc ON a.id = gc.subID");
 		sql.addWhere("a.status IN ('Active','Demo')");
-
-		sql.addWhere("gc.genID = " + permissions.getAccountId());
-		if (permissions.isApprovesRelationships()) {
-			sql.addWhere("gc.workStatus = 'Y'");
+		
+		if(permissions.isOperatorCorporate()) {
+			sql.addWhere("gc.genID = " + permissions.getAccountId());
+			if (permissions.isApprovesRelationships()) {
+				sql.addWhere("gc.workStatus = 'Y'");
+			}
+		}
+		else if(permissions.hasGroup(User.GROUP_CSR)) {
+			sql.addJoin("JOIN contractor_info c ON a.id = c.id");
+			sql.addWhere("c.welcomeAuditor_id = "+ permissions.getUserId());
+			sql.addJoin("JOIN accounts oper ON oper.id = gc.genID");
+			sql.addWhere("oper.type = 'Operator'");
 		}
 
 		ChartDAO db = new ChartDAO();
@@ -40,7 +49,10 @@ public class ChartFlagCount extends ChartSSAction {
 		for (DataRow row : data) {
 			Set set = new Set(row);
 			set.setColor(FlagColor.valueOf(row.getLabel()).getHex());
-			set.setLink("ContractorList.action?filter.flagStatus=" + row.getLabel());
+			if(permissions.isOperatorCorporate())
+				set.setLink("ContractorList.action?filter.flagStatus=" + row.getLabel());
+			if(permissions.hasGroup(User.GROUP_CSR))
+				set.setLink("ReportContractorOperatorFlag.action?filter.flagStatus=" + row.getLabel());
 			chart.addSet(set);
 		}
 		return chart;
--- src/com/picsauditing/actions/report/ReportContractorOperatorFlag.java
+++ src/com/picsauditing/actions/report/ReportContractorOperatorFlag.java
@@ -5,39 +5,43 @@ import java.util.Vector;
 
 import com.picsauditing.jpa.entities.Facility;
 import com.picsauditing.jpa.entities.OperatorAccount;
+import com.picsauditing.jpa.entities.User;
 import com.picsauditing.util.Strings;
 import com.picsauditing.util.excel.ExcelCellType;
 import com.picsauditing.util.excel.ExcelColumn;
 
 @SuppressWarnings("serial")
 public class ReportContractorOperatorFlag extends ReportAccount {
-	
+
 	public ReportContractorOperatorFlag() {
 		setReportName("Contractor Operator Flag");
 		orderByDefault = "a.name, operator.name";
 	}
-	
+
 	@Override
 	protected void buildQuery() {
 		skipPermissions = true;
 		super.buildQuery();
-		
+
 		getFilter().setShowFlagStatus(true);
-		String opIds;
+		String opIds = "";
 		List<Integer> ops = new Vector<Integer>();
-		if(filterOn(getFilter().getOperator())) {
+
+		if (filterOn(getFilter().getOperator())) {
 			opIds = Strings.implode(getFilter().getOperator(), ",");
-		}
-		else {
-			OperatorAccount corporate = (OperatorAccount) getAccount() ;
-			for( Facility child : corporate.getOperatorFacilities() ) {
-				if(child.getOperator().getStatus().isActiveDemo()) {
+		} else if (permissions.isCorporate()) {
+			OperatorAccount corporate = (OperatorAccount) getAccount();
+			for (Facility child : corporate.getOperatorFacilities()) {
+				if (child.getOperator().getStatus().isActiveDemo()) {
 					ops.add(child.getOperator().getId());
 				}
 			}
 			opIds = Strings.implode(ops, ",");
-		}	
-		
+		}
+		if (permissions.hasGroup(User.GROUP_CSR)) {
+			sql.addWhere("c.welcomeAuditor_id = " + permissions.getUserId());
+		}
+
 		sql.addJoin("JOIN generalcontractors gc on gc.subid = a.id");
 		sql.addJoin("JOIN accounts operator on operator.id = gc.genid");
 		sql.addField("operator.name AS opName");
@@ -45,13 +49,17 @@ public class ReportContractorOperatorFlag extends ReportAccount {
 		sql.addField("gc.flag");
 		sql.addField("lower(gc.flag) AS lflag");
 		sql.addWhere("a.status IN ('Active','Demo')");
-		sql.addWhere("operator.id in (" + opIds + ")");
+		sql.addWhere("operator.type = 'Operator'");
+		if(!Strings.isEmpty(opIds))
+			sql.addWhere("operator.id in (" + opIds + ")");
 	}
-	
+
 	@Override
 	protected void addExcelColumns() {
 		super.addExcelColumns();
-		excelSheet.addColumn(new ExcelColumn("opName", "Operator Name", ExcelCellType.String), 30);
-		excelSheet.addColumn(new ExcelColumn("flag", "Flag", ExcelCellType.String), 40);
+		excelSheet.addColumn(new ExcelColumn("opName", "Operator Name",
+				ExcelCellType.String), 30);
+		excelSheet.addColumn(new ExcelColumn("flag", "Flag",
+				ExcelCellType.String), 40);
 	}
-}	
+}
