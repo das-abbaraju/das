092bf6d4a1c9279f03bcba49c0052e60ec0fb649
Keerthi Nannapaneni <knannapaneni@picsauditing.com>
2010-05-14 16:10:27 +0000

    updated the upgrade script.

--- deployments/upgrade_version2.sql
+++ deployments/upgrade_version2.sql
@@ -13,21 +13,3 @@ join pqfdata pd on ca.id = pd.auditid
 where pd.questionid = 894
 and pd.answer = 'Yes');
 **/
-
-/** Adding existing widgets to CSR users**/
-insert into widget_user 
-select null,widgetID,959,expanded, 
-wu.column,sortOrder,customConfig from widget_user wu 
-where 
-(userid = 941 and widgetid in (2,16,18))
-or (userid = 616 and widgetid = 1);
-
-/** Make sure that the widgetIDs are correct! **/
-insert into widget_user
-(id, widgetID, userID, column, sortOrder)
-values
-(null, 26, 959, 2, 30),
-(null, 28, 959, 1, 30),
-(null, 29, 959, 2, 15),
-(null, 30, 959, 2, 35),
-(null, 31, 959, 1, 15);
