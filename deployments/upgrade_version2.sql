72a0ffe1160c7876fc843559301602cf8ea442a8
Tom Baker <tbaker@picsauditing.com>
2010-06-29 18:07:55 +0000

    PICS-737: Updating CSRs in ref_state based on assignment script data

--- deployments/upgrade_version2.sql
+++ deployments/upgrade_version2.sql
@@ -14,6 +14,39 @@ where pd.questionid = 894
 and pd.answer = 'Yes');
 **/
 
+-- PICS-737: Updating CSRs in ref_state based on assignment script data
+-- Ashley Prather
+update ref_state set csrID = 22223
+WHERE isoCode in ('AK','AZ','HI','ID','MT','NM','NV','OR','UT','WY') 
+AND countryCode = 'US';
+
+-- Derrick Piper
+update ref_state set csrID = 23542
+where (isoCode in ('TX') OR countryCode = 'CA');
+
+-- Estevan Orozco
+update ref_state set csrID = 940
+WHERE (isoCode in ('CA','GU','PR','WA') OR countryCode not in ('US','CA'));
+
+-- Kaitlyn O'Malley
+update ref_state set csrID = 11067
+where isoCode in ('AR','CO','IA','KS','LA','MO','ND','NE','OK','SD','WI')
+AND countryCode = 'US';
+
+-- Neal Chawla
+update ref_state set csrID = 23550
+where isoCode in ('AL','FL','GA','KY','OH','TN')
+AND countryCode = 'US';
+
+-- Tiffany Roberson
+update ref_state set csrID = 22222
+where isoCode in ('IL','IN','MI','MN','MS')
+AND countryCode = 'US';
+
+-- Valeree Claudio
+update ref_state set csrID = 8397
+where isoCode in ('CT','DE','MA','MD','ME','NC','NH','NJ','NY','PA','RI','SC','VA','VT','WV')
+AND countryCode = 'US';
 
 /* Deactive unused assessment centers except for OQSG, and set NACE and NCCER to pending */
 update accounts set status = 'Deactivated' where type = 'Assessment' and name !='OQSG';
