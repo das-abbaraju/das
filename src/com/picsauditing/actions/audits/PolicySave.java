36895420b6108b006adb771ae00dda9d0644f08a
Kyle Partridge <kpartridge@picsauditing.com>
2010-06-30 23:40:45 +0000

    Formatting only

--- src/com/picsauditing/actions/audits/PolicySave.java
+++ src/com/picsauditing/actions/audits/PolicySave.java
@@ -40,10 +40,9 @@ public class PolicySave extends AuditActionSupport implements Preparable {
 	protected Certificate certificate;
 	protected String mode;
 
-	public PolicySave(ContractorAccountDAO accountDao,
-			ContractorAuditDAO auditDao, AuditCategoryDataDAO catDataDao,
-			AuditDataDAO auditDataDao, ContractorAuditDAO contractorAuditDAO,
-			ContractorAuditOperatorDAO caoDAO, CertificateDAO certificateDao) {
+	public PolicySave(ContractorAccountDAO accountDao, ContractorAuditDAO auditDao, AuditCategoryDataDAO catDataDao,
+			AuditDataDAO auditDataDao, ContractorAuditDAO contractorAuditDAO, ContractorAuditOperatorDAO caoDAO,
+			CertificateDAO certificateDao) {
 		super(accountDao, auditDao, catDataDao, auditDataDao);
 		this.caoDAO = caoDAO;
 		this.certificateDao = certificateDao;
@@ -70,15 +69,13 @@ public class PolicySave extends AuditActionSupport implements Preparable {
 				contractor = cao.getAudit().getContractorAccount();
 
 				if ("Submit".equals(button) || "Resubmit".equals(button)) {
-					if (cao.getAudit().getPercentComplete() == 100
-							&& cao.isCanContractorSubmit()) {
+					if (cao.getAudit().getPercentComplete() == 100 && cao.isCanContractorSubmit()) {
 						cao.setStatus(CaoStatus.Submitted);
 						statusChanged = true;
 						button = "Save";
-						addActionMessage("The <strong>"
-								+ cao.getAudit().getAuditType().getAuditName()
-								+ "</strong> Policy has been submitted for <strong>"
-								+ cao.getOperator().getName() + "</strong>.");
+						addActionMessage("The <strong>" + cao.getAudit().getAuditType().getAuditName()
+								+ "</strong> Policy has been submitted for <strong>" + cao.getOperator().getName()
+								+ "</strong>.");
 					} else {
 						addActionError("Please enter all required questions before submitting the policy.");
 						caoDAO.refresh(cao);
@@ -89,11 +86,14 @@ public class PolicySave extends AuditActionSupport implements Preparable {
 				if ("Verify".equals(button)) {
 					cao.setStatus(CaoStatus.Verified);
 					if (cao.getOperator().isAutoApproveInsurance()) {
-						if(cao.getFlag() == null) {
+						if (cao.getFlag() == null) {
 							caoDAO.save(cao);
-							String redirectURL = URLEncoder.encode(String.format("PolicySaveAjax.action?cao.id=%d&button=Verify&certID=%d", cao.getId(), certID), "UTF-8");
-							return redirect("ContractorCronAjax.action?conID=" + contractor.getId() + 
-							"&opID=0&steps=ContractorETL&steps=Flag&steps=Policies&redirectUrl="+redirectURL);
+							String redirectURL = URLEncoder.encode(String.format(
+									"PolicySaveAjax.action?cao.id=%d&button=Verify&certID=%d", cao.getId(), certID),
+									"UTF-8");
+							return redirect("ContractorCronAjax.action?conID=" + contractor.getId()
+									+ "&opID=0&steps=ContractorETL&steps=Flag&steps=Policies&redirectUrl="
+									+ redirectURL);
 						}
 						if (cao.getFlag() == FlagColor.Green)
 							cao.setStatus(CaoStatus.Approved);
@@ -104,9 +104,9 @@ public class PolicySave extends AuditActionSupport implements Preparable {
 					statusChanged = true;
 					button = "Save";
 
-					addActionMessage(String.format("The <strong>%s</strong> Policy has been %s for <strong>%s</strong>.",
-							cao.getAudit().getAuditType().getAuditName(), 
-							cao.getStatus(),cao.getOperator().getName()));
+					addActionMessage(String.format(
+							"The <strong>%s</strong> Policy has been %s for <strong>%s</strong>.", cao.getAudit()
+									.getAuditType().getAuditName(), cao.getStatus(), cao.getOperator().getName()));
 				}
 
 				if ("Reject".equals(button)) {
@@ -124,23 +124,17 @@ public class PolicySave extends AuditActionSupport implements Preparable {
 						// Insurance Policy rejected by PICS
 						emailBuilder.setTemplate(52);
 						emailBuilder.setPermissions(permissions);
-						emailBuilder.setFromAddress("\""
-								+ permissions.getName() + "\"<"
-								+ permissions.getEmail() + ">");
-						emailBuilder.setContractor(cao.getAudit()
-								.getContractorAccount(),
-								OpPerms.ContractorInsurance);
+						emailBuilder
+								.setFromAddress("\"" + permissions.getName() + "\"<" + permissions.getEmail() + ">");
+						emailBuilder.setContractor(cao.getAudit().getContractorAccount(), OpPerms.ContractorInsurance);
 						emailBuilder.addToken("cao", cao);
 						EmailQueue email = emailBuilder.build();
 						email.setViewableBy(cao.getOperator());
 						EmailSender.send(email);
 
-						addActionMessage("The <strong>"
-								+ cao.getAudit().getAuditType().getAuditName()
-								+ "</strong> Policy has been rejected for <strong>"
-								+ cao.getOperator().getName()
-								+ "</strong>. Note: "
-								+ Strings.htmlStrip(cao.getNotes()));
+						addActionMessage("The <strong>" + cao.getAudit().getAuditType().getAuditName()
+								+ "</strong> Policy has been rejected for <strong>" + cao.getOperator().getName()
+								+ "</strong>. Note: " + Strings.htmlStrip(cao.getNotes()));
 					}
 				}
 
@@ -148,10 +142,9 @@ public class PolicySave extends AuditActionSupport implements Preparable {
 					cao.setStatus(CaoStatus.Approved);
 					statusChanged = true;
 					button = "Save";
-					addActionMessage("The <strong>"
-							+ cao.getAudit().getAuditType().getAuditName()
-							+ "</strong> Policy has been approved for <strong>"
-							+ cao.getOperator().getName() + "</strong>");
+					addActionMessage("The <strong>" + cao.getAudit().getAuditType().getAuditName()
+							+ "</strong> Policy has been approved for <strong>" + cao.getOperator().getName()
+							+ "</strong>");
 				}
 
 				if ("NotApplicable".equals(button)) {
@@ -181,7 +174,7 @@ public class PolicySave extends AuditActionSupport implements Preparable {
 
 				if ("Save".equals(button)) {
 					if (cao != null) {
-						
+
 						if (certID > 0)
 							cao.setCertificate(certificateDao.find(certID));
 						else
@@ -195,14 +188,10 @@ public class PolicySave extends AuditActionSupport implements Preparable {
 						if (statusChanged) {
 							cao.setStatusChangedBy(getUser());
 							cao.setStatusChangedDate(new Date());
-							String text = " Changed "
-									+ cao.getAudit().getAuditType()
-											.getAuditName() + " status to "
-									+ cao.getStatus() + " for "
-									+ cao.getOperator().getName();
-							addNote(contractor, text, NoteCategory.Insurance,
-									LowMedHigh.Low, true, cao.getOperator()
-											.getId(), null);
+							String text = " Changed " + cao.getAudit().getAuditType().getAuditName() + " status to "
+									+ cao.getStatus() + " for " + cao.getOperator().getName();
+							addNote(contractor, text, NoteCategory.Insurance, LowMedHigh.Low, true, cao.getOperator()
+									.getId(), null);
 						}
 						caoDAO.save(cao);
 
