31bd8bf720ecf643d72d4158e88aa43270ba3d77
Tom Baker <tbaker@picsauditing.com>
2010-06-30 18:39:16 +0000

    PICS-727: Contractors had previously green flag forced notes for other facilities and corrupted operator notes which didn't list the facility in question. Query looks like it just picked the first of these.

--- deployments/upgrade_version2.sql
+++ deployments/upgrade_version2.sql
@@ -14,39 +14,16 @@ where pd.questionid = 894
 and pd.answer = 'Yes');
 **/
 
--- PICS-737: Updating CSRs in ref_state based on assignment script data
--- Ashley Prather
-update ref_state set csrID = 22223
-WHERE isoCode in ('AK','AZ','HI','ID','MT','NM','NV','OR','UT','WY') 
-AND countryCode = 'US';
-
--- Derrick Piper
-update ref_state set csrID = 23542
-where (isoCode in ('TX') OR countryCode = 'CA');
-
--- Estevan Orozco
-update ref_state set csrID = 940
-WHERE (isoCode in ('CA','GU','PR','WA') OR countryCode not in ('US','CA'));
-
--- Kaitlyn O'Malley
-update ref_state set csrID = 11067
-where isoCode in ('AR','CO','IA','KS','LA','MO','ND','NE','OK','SD','WI')
-AND countryCode = 'US';
-
--- Neal Chawla
-update ref_state set csrID = 23550
-where isoCode in ('AL','FL','GA','KY','OH','TN')
-AND countryCode = 'US';
-
--- Tiffany Roberson
-update ref_state set csrID = 22222
-where isoCode in ('IL','IN','MI','MN','MS')
-AND countryCode = 'US';
-
--- Valeree Claudio
-update ref_state set csrID = 8397
-where isoCode in ('CT','DE','MA','MD','ME','NC','NH','NJ','NY','PA','RI','SC','VA','VT','WV')
-AND countryCode = 'US';
+-- PICS-727: Begin --
+update generalcontractors set forcedBy = 776 where id = 5716;
+update generalcontractors set forcedBy = 987 where id in (748, 9867, 11077, 11078, 12815, 23831);
+update generalcontractors set forcedBy = 556 where id = 12764;
+update generalcontractors set forcedBy = 2173 where id in (16317, 17795, 17797, 17798);
+update generalcontractors set forcedBy = 2175 where id in (17842, 17843, 17844);
+update generalcontractors set forcedBy = 8597 where id = 28914;
+update generalcontractors set forcedBy = 11429 where id in (34391, 35382, 36494, 36864, 40366);
+update generalcontractors set forcedBy = 10569 where id = 35912;
+-- PICS-727: END --
 
 /* Deactive unused assessment centers except for OQSG, and set NACE and NCCER to pending */
 update accounts set status = 'Deactivated' where type = 'Assessment' and name !='OQSG';
