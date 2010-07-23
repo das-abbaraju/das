cb4e7f8e3d4787421ba54ab044bf454b7efd3856
Lani Aung <uaung@picsauditing.com>
2010-07-23 23:08:29 +0000

    PICS-703

--- WebContent/struts/widgets/corporate_statistics.jsp
+++ WebContent/struts/widgets/corporate_statistics.jsp
@@ -1,6 +1,32 @@
 <%@ taglib prefix="s" uri="/struts-tags"%>
+<%@ taglib prefix="pics" uri="pics-taglib"%>
 <p><label>Number of Contractors:</label> <s:property value="totalContractorCount" /></p>
 <p><label>Number of Active Contractors:</label> <s:property value="activeContractorCount" /></p>
 <p><label>Number of Active Operators:</label> <s:property value="operatorCount" /><br />
 <p><label>Number of Active Corporate:</label> <s:property value="corporateCount" /><br />
 <p><label>Number of Active Users:</label> <s:property value="userCount" /></p>
+<s:if test="permissions.hasGroup(981) || permissions.hasGroup(10801)">
+	<pics:permission perm="DevelopmentEnvironment" negativeCheck="true">
+		<br />
+		<p>
+			<div id="corpStatAM1">
+				Total number of contractors registered for all your accounts
+			</div>
+			<label>Number of Your Contractors:</label>
+			<s:property value="amContractorCount" />
+			<a href="#" onclick="return false;" class="help cluetip" rel="#corpStatAM1" title="Number of Your Contractors"></a>
+			<script type="text/javascript">
+			$('.cluetip').cluetip({
+				closeText: "<img src='images/cross.png' width='16' height='16'>",
+				arrows: true,
+				cluetipClass: 'jtip',
+				local: true,
+				clickThrough: false
+			});
+			</script>
+		</p>
+		<p><label>Number of Your Accounts:</label> <s:property value="amAccountCount" /><br />
+		<p><label>Number of Your Corporate Accounts:</label> <s:property value="amCorporateCount" /><br />
+		<p><label>Number of Your Users:</label> <s:property value="amUserCount" /></p>
+	</pics:permission>
+</s:if>
--- src/com/picsauditing/actions/CorporateWidget.java
+++ src/com/picsauditing/actions/CorporateWidget.java
@@ -1,8 +1,14 @@
 package com.picsauditing.actions;
 
+import java.util.List;
+
+import com.picsauditing.access.OpPerms;
+import com.picsauditing.dao.AccountUserDAO;
 import com.picsauditing.dao.ContractorAccountDAO;
 import com.picsauditing.dao.OperatorAccountDAO;
 import com.picsauditing.dao.UserDAO;
+import com.picsauditing.jpa.entities.AccountUser;
+import com.picsauditing.jpa.entities.User;
 
 @SuppressWarnings("serial")
 public class CorporateWidget extends PicsActionSupport {
@@ -11,14 +17,21 @@ public class CorporateWidget extends PicsActionSupport {
 	protected int activeContractorCount;
 	protected int userCount;
 	protected int totalContractorCount;
+	protected int amContractorCount;
+	protected int amAccountCount;
+	protected int amCorporateCount;
+	protected int amUserCount;
 	ContractorAccountDAO cAccountDAO;
 	OperatorAccountDAO opAccountDAO;
 	UserDAO userDAO;
+	AccountUserDAO amDAO;
 
-	public CorporateWidget(ContractorAccountDAO cAccountDAO, OperatorAccountDAO opAccountDAO, UserDAO userDAO) {
+	public CorporateWidget(ContractorAccountDAO cAccountDAO, OperatorAccountDAO opAccountDAO, UserDAO userDAO,
+			AccountUserDAO amDAO) {
 		this.cAccountDAO = cAccountDAO;
 		this.opAccountDAO = opAccountDAO;
 		this.userDAO = userDAO;
+		this.amDAO = amDAO;
 	}
 
 	public String execute() throws Exception {
@@ -31,6 +44,28 @@ public class CorporateWidget extends PicsActionSupport {
 		operatorCount = opAccountDAO.getOperatorCounts("o.type = 'Operator'");
 		corporateCount = opAccountDAO.getOperatorCounts("o.type = 'Corporate'");
 		userCount = userDAO.getUsersCounts();
+		
+		if ((permissions.hasGroup(981) || permissions.hasGroup(10801)) 
+				&& !permissions.hasPermission(OpPerms.DevelopmentEnvironment)) {
+			// Account managers and Sales Reps
+			List<AccountUser> amRoles = amDAO.findByUserSalesAM(permissions.getUserId());
+			amAccountCount = amRoles.size();
+			amCorporateCount = 0;
+			
+			for (AccountUser au : amRoles) {
+				if (au.getAccount().isCorporate())
+					amCorporateCount++;
+			}
+			
+			amContractorCount = cAccountDAO.getActiveContractorCounts("c IN (SELECT contractorAccount FROM " +
+					"ContractorOperator WHERE operatorAccount IN (SELECT account FROM AccountUser " +
+					"WHERE user.id = " + permissions.getUserId() + "))");
+			
+			List<User> amUsers = userDAO.findWhere("u.account IN (SELECT account FROM AccountUser " +
+					"WHERE user.id = " + permissions.getUserId() + ")");
+			amUserCount = amUsers.size();
+		}
+		
 		return SUCCESS;
 	}
 
@@ -52,5 +87,22 @@ public class CorporateWidget extends PicsActionSupport {
 
 	public int getCorporateCount() {
 		return corporateCount;
+	}
+	
+	// For Account Managers and Sales Reps
+	public int getAmAccountCount() {
+		return amAccountCount;
+	}
+	
+	public int getAmContractorCount() {
+		return amContractorCount;
+	}
+	
+	public int getAmCorporateCount() {
+		return amCorporateCount;
+	}
+	
+	public int getAmUserCount() {
+		return amUserCount;
 	}
 }
