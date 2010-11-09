4484a6ec58fe89c78ed9d0293b7fddbf64d586f4
Lani Aung <uaung@picsauditing.com>
2010-11-09 00:30:42 +0000

    PICS-1071

--- WebContent/struts/operators/op_config.jsp
+++ WebContent/struts/operators/op_config.jsp
@@ -7,8 +7,27 @@
 <link rel="stylesheet" type="text/css" media="screen" href="css/forms.css?v=<s:property value="version"/>" />
 <link rel="stylesheet" type="text/css" media="screen" href="css/reports.css?v=<s:property value="version"/>" />
 <s:include value="../jquery.jsp" />
+<script type="text/javascript">
+function toggleType(ruleID) {
+	var div = "#type_" + ruleID;
+	var link = "#type_link_" + ruleID;
+	
+	$(div).toggle();
+
+	if ($(div).is(':hidden'))
+		$(link).text("Show Rules");
+	else
+		$(link).text("Hide Rules");
+}
+</script>
+<style type="text/css">
+.auditTypeRule {
+	display: none;
+}
+</style>
 </head>
 <body>
+<s:include value="opHeader.jsp" />
 <fieldset class="form">
 	<h2 class="formLegend">General</h2>
 	<ol>
@@ -23,6 +42,105 @@
 					</s:iterator>
 				</tbody>
 			</table>
+		</li>
+	</ol>
+</fieldset>
+<fieldset class="form">
+	<h2 class="formLegend">Likely Included Audits</h2>
+	<ol>
+		<li>
+			<table class="report">
+				<thead>
+					<tr>
+						<th>Category</th>
+						<th>Audit</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					<s:iterator value="typeMap.keySet()" id="type">
+						<tr class="clickable">
+							<td><s:property value="#type.classType" /></td>
+							<td><s:property value="#type.auditName" /></td>
+							<td>
+								<a href="#" onclick="toggleType(<s:property value="#type.id" />); return false;" id="type_link_<s:property value="#type.id" />">Show Rules</a>
+								<table id="type_<s:property value="#type.id" />" class="auditTypeRule">
+									<s:include value="../audits/rules/audit_rule_header.jsp"/>
+									<s:set name="ruleURL" value="'AuditRuleEditor.action'"/>
+									<s:set name="categoryRule" value="false"/>
+									<s:iterator value="typeMap.get(#type)" id="r">
+										<s:include value="../audits/rules/audit_rule_view.jsp"/>
+									</s:iterator>
+								</table>
+							</td>
+						</tr>
+					</s:iterator>
+				</tbody>
+			</table>
+		</li>
+	</ol>
+</fieldset>
+<fieldset class="form">
+	<h2 class="formLegend">Rules to Explicitly Remove Audits</h2>
+	<ol>
+		<li>
+			<s:if test="excludeTypes.size > 0">
+				<table class="report">
+					<thead>
+						<tr>
+							<th>Include</th>
+							<th>Audit Type</th>
+							<th>Operator</th>
+						</tr>
+					</thead>
+					<tbody>
+						<s:iterator value="excludeTypes" id="type">
+							<tr class="clickable">
+								<td><s:property value="#type.include ? 'Yes' : 'No'" /></td>
+								<td><s:property value="#type.auditType == null ? '*' : #type.auditType.auditName" /></td>
+								<td><s:property value="#type.operatorAccount == null ? '*' : #type.operatorAccount.name" /></td>
+							</tr>
+						</s:iterator>
+					</tbody>
+				</table>
+			</s:if>
+		</li>
+	</ol>
+</fieldset>
+<fieldset class="form">
+	<h2 class="formLegend">Likely Included PQF Categories</h2>
+	<ol>
+		<li><label>Categories</label>
+			<s:iterator value="categoryMap.keySet()">
+				<s:property value="name" /><br />
+			</s:iterator>
+		</li>
+	</ol>
+</fieldset>
+<fieldset class="form">
+	<h2 class="formLegend">Rules to Explicitly Remove PQF Categories</h2>
+	<ol>
+		<li>
+			<s:if test="excludeCategories.size > 0">
+				<table class="report">
+					<thead>
+						<tr>
+							<th>Include</th>
+							<th>Category</th>
+							<th>Operator</th>
+						</tr>
+					</thead>
+					<tbody>
+						<s:iterator value="excludeCategories" id="cat">
+							<tr class="clickable">
+								<td><s:property value="#cat.include ? 'Yes' : 'No'" /></td>
+								<td><s:property value="#cat.auditCategory == null ? '*' : #cat.auditCategory.name" /></td>
+								<td><s:property value="#cat.operatorAccount == null ? '*' : #cat.operatorAccount.name" /></td>
+							</tr>
+						</s:iterator>
+					</tbody>
+				</table>
+			</s:if>
 		</li>
 	</ol>
 </fieldset>
--- src/com/picsauditing/actions/operators/OperatorConfiguration.java
+++ src/com/picsauditing/actions/operators/OperatorConfiguration.java
@@ -1,35 +1,107 @@
 package com.picsauditing.actions.operators;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import com.opensymphony.xwork2.Preparable;
+import com.picsauditing.dao.AuditDecisionTableDAO;
 import com.picsauditing.dao.OperatorAccountDAO;
+import com.picsauditing.jpa.entities.AuditCategory;
+import com.picsauditing.jpa.entities.AuditCategoryRule;
+import com.picsauditing.jpa.entities.AuditType;
+import com.picsauditing.jpa.entities.AuditTypeRule;
 import com.picsauditing.jpa.entities.OperatorAccount;
 import com.picsauditing.util.Strings;
 
 @SuppressWarnings("serial")
 public class OperatorConfiguration extends OperatorActionSupport implements Preparable {
+	protected AuditDecisionTableDAO adtDAO;
+
+	private Map<AuditType, List<AuditTypeRule>> typeMap = new TreeMap<AuditType, List<AuditTypeRule>>();
+	private Map<AuditCategory, List<AuditCategoryRule>> categoryMap = new TreeMap<AuditCategory, List<AuditCategoryRule>>();
+	private List<AuditTypeRule> excludeTypes = new ArrayList<AuditTypeRule>();
+	private List<AuditCategoryRule> excludeCategories = new ArrayList<AuditCategoryRule>();
+
 	private List<OperatorAccount> allParents = null;
-	
-	public OperatorConfiguration(OperatorAccountDAO operatorDao) {
+
+	public OperatorConfiguration(OperatorAccountDAO operatorDao, AuditDecisionTableDAO adtDAO) {
 		super(operatorDao);
+		this.adtDAO = adtDAO;
 	}
-	
+
 	public void prepare() throws Exception {
 		findOperator();
-		subHeading = "Edit " + operator.getType();
+		subHeading = "Operator Configuration";
 	}
-	
+
 	public String execute() throws Exception {
 		if (!forceLogin())
 			return LOGIN;
-		
-		allParents = operatorDao.findWhere(true, "a.id IN (" + Strings.implode(operator.getOperatorHeirarchy()) + ")", permissions);
-		
+
+		List<Integer> inheritance = operator.getOperatorHeirarchy();
+		inheritance.remove((Integer) operator.getId());
+
+		allParents = operatorDao.findWhere(true, "a.id IN (" + Strings.implode(inheritance) + ")", permissions);
+
+		Collections.sort(allParents, new Comparator<OperatorAccount>() {
+			public int compare(OperatorAccount o1, OperatorAccount o2) {
+				return (o1.getId() - o2.getId());
+			}
+		});
+
+		List<AuditTypeRule> typeRules = adtDAO.findAuditTypeRulesByOperator(operator.getId());
+		List<AuditCategoryRule> categoryRules = adtDAO.findAuditCategoryRulesByOperator(operator.getId());
+
+		for (AuditTypeRule typeRule : typeRules) {
+			if (typeRule.getAuditType() != null) {
+				if (typeMap.get(typeRule.getAuditType()) == null)
+					typeMap.put(typeRule.getAuditType(), new ArrayList<AuditTypeRule>());
+
+				typeMap.get(typeRule.getAuditType()).add(typeRule);
+			}
+
+			if (!typeRule.isInclude() && typeRule.getOperatorAccount() != null
+					&& typeRule.getOperatorAccount().getId() == operator.getId())
+				excludeTypes.add(typeRule);
+		}
+
+		for (AuditCategoryRule catRule : categoryRules) {
+			if (catRule.getAuditCategory() != null) {
+				if (categoryMap.get(catRule.getAuditCategory()) == null)
+					categoryMap.put(catRule.getAuditCategory(), new ArrayList<AuditCategoryRule>());
+
+				categoryMap.get(catRule.getAuditCategory()).add(catRule);
+			}
+
+			if (!catRule.isInclude() && catRule.getOperatorAccount() != null
+					&& catRule.getOperatorAccount().getId() == operator.getId())
+				excludeCategories.add(catRule);
+		}
+
 		return SUCCESS;
 	}
-	
+
 	public List<OperatorAccount> getAllParents() {
 		return allParents;
+	}
+
+	public Map<AuditType, List<AuditTypeRule>> getTypeMap() {
+		return typeMap;
+	}
+
+	public Map<AuditCategory, List<AuditCategoryRule>> getCategoryMap() {
+		return categoryMap;
+	}
+
+	public List<AuditTypeRule> getExcludeTypes() {
+		return excludeTypes;
+	}
+
+	public List<AuditCategoryRule> getExcludeCategories() {
+		return excludeCategories;
 	}
 }
