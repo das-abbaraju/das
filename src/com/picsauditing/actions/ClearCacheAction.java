be0c65e4b434058c7cd6d081f18c657cbbfb7066
Kyle Partridge <kpartridge@picsauditing.com>
2010-12-01 00:30:30 +0000

    added the rules caches to the ClearCache.action menu item

--- src/com/picsauditing/actions/ClearCacheAction.java
+++ src/com/picsauditing/actions/ClearCacheAction.java
@@ -3,23 +3,37 @@ package com.picsauditing.actions;
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheManager;
 
+import com.picsauditing.PICS.AuditCategoryRuleCache;
+import com.picsauditing.PICS.AuditTypeRuleCache;
+
 public class ClearCacheAction extends PicsActionSupport {
+
+	AuditTypeRuleCache auditTypeRuleCache;
+	AuditCategoryRuleCache auditCategoryRuleCache;
+
+	public ClearCacheAction(AuditTypeRuleCache auditTypeRuleCache, AuditCategoryRuleCache auditCategoryRuleCache) {
+		this.auditTypeRuleCache = auditTypeRuleCache;
+		this.auditCategoryRuleCache = auditCategoryRuleCache;
+	}
 
 	@Override
 	public String execute() throws Exception {
 		String[] cacheNames = CacheManager.getInstance().getCacheNames();
-	
-		for( String cacheName : cacheNames ) {
+
+		for (String cacheName : cacheNames) {
 			System.out.println(cacheName);
 			addActionMessage("Cleared cache named " + cacheName);
-			
+
 			Cache cache = CacheManager.getInstance().getCache(cacheName);
-			for( Object key : cache.getKeys() ) {
+			for (Object key : cache.getKeys()) {
 				addActionMessage("Cleared object " + key);
 				cache.remove(key);
 			}
 		}
-		
+
+		auditTypeRuleCache.clear();
+		auditCategoryRuleCache.clear();
+
 		return SUCCESS;
 	}
 }
