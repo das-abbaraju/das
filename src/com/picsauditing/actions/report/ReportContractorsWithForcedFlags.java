3726ed3605a713e24f5996baa768649cd55dd9f1
Tom Baker <tbaker@picsauditing.com>
2010-12-13 22:35:46 +0000

    PICS-912

--- src/com/picsauditing/actions/report/ReportContractorsWithForcedFlags.java
+++ src/com/picsauditing/actions/report/ReportContractorsWithForcedFlags.java
@@ -26,11 +26,12 @@ public class ReportContractorsWithForcedFlags extends ReportAccount {
 		skipPermissions = true;
 		super.buildQuery();
 		
-		String forceFlagsJoin = "JOIN (SELECT conid,opid,forcebegin,forceend,forcedby,forceflag, cID FROM"+ 
-		" (SELECT gc.subid as conid, gc.genid as opid, gc.forcebegin, gc.forceend, gc.forcedBy, gc.forceflag, 0 cID FROM generalcontractors gc" +
+		String forceFlagsJoin = "JOIN (SELECT conid,opid,forcebegin,forceend,forcedby,forceflag,label FROM"+ 
+		" (SELECT gc.subid as conid, gc.genid as opid, gc.forcebegin, gc.forceend, gc.forcedBy, gc.forceflag, 'Overall' AS label FROM generalcontractors gc" +
 		" WHERE gc.forceFlag IS NOT NULL" +
 		" UNION" +
-		" SELECT fdo.conid, fdo.opid, fdo.updateDate as forcebegin, fdo.forceend, fdo.updatedBy as forcedby, fdo.forceflag, fdo.criteriaID as cID FROM flag_data_override fdo"+
+		" SELECT fdo.conid, fdo.opid, fdo.updateDate as forcebegin, fdo.forceend, fdo.updatedBy as forcedby, fdo.forceflag, fc1.label FROM flag_data_override fdo"+
+		" JOIN flag_criteria fc1 ON fdo.criteriaID = fc1.id"+
 		" WHERE fdo.forceFlag IS NOT NULL) t"+
 		" ) ff ON a.id = ff.conid";
 		
@@ -39,7 +40,6 @@ public class ReportContractorsWithForcedFlags extends ReportAccount {
 		sql.addJoin("LEFT JOIN users u ON u.id = ff.forcedBy");
 		sql.addJoin("LEFT JOIN accounts fa ON fa.id = u.accountID");
 		sql.addJoin("LEFT JOIN generalcontractors gc ON gc.genid = o.id and gc.subid = ff.conid");
-		sql.addJoin("LEFT JOIN flag_criteria fc on fc.id = ff.cID");
 		sql.addWhere("a.status = 'Active'");
 		sql.addWhere("o.status = 'Active'");
 		sql.addWhere("ff.forceFlag IS NOT NULL");
@@ -52,10 +52,10 @@ public class ReportContractorsWithForcedFlags extends ReportAccount {
 			sql.addWhere(opIds);
 		}
 		sql.addField("o.name AS opName");
-		sql.addField("fc.label AS fLabel");
 		sql.addField("o.type AS opType");
 		sql.addField("o.id AS opId");
 		sql.addField("lower(ff.forceFlag) AS lflag");
+		sql.addField("label AS fLabel");
 		sql.addField("ff.forceend");
 		sql.addField("ff.forceBegin");
 		sql.addField("u.id as forcedById"); 
