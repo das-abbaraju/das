d2053f1aff5d45e64914734df61de83f49c47d0d
Kyle Partridge <kpartridge@picsauditing.com>
2010-12-16 20:53:29 +0000

    PICS-1571

---
+++ src/com/picsauditing/actions/autocomplete/AuditCategoryAutocomplete.java
@@ -0,0 +1,59 @@
+package com.picsauditing.actions.autocomplete;
+
+import java.util.Iterator;
+
+import com.picsauditing.dao.AuditCategoryDAO;
+import com.picsauditing.jpa.entities.AuditCategory;
+
+@SuppressWarnings("serial")
+public class AuditCategoryAutocomplete extends AutocompleteActionSupport<AuditCategory> {
+
+	protected AuditCategoryDAO auditCategoryDAO;
+	protected Integer auditTypeID;
+
+	public AuditCategoryAutocomplete(AuditCategoryDAO auditCategoryDAO) {
+		this.auditCategoryDAO = auditCategoryDAO;
+	}
+
+	@Override
+	protected void findItems() {
+		String where = "";
+		if (auditTypeID != null && auditTypeID > 0)
+			where = "t.auditType.id =" + auditTypeID + " AND ";
+		if (isSearchDigit())
+			items = auditCategoryDAO.findWhere(where + "t.id LIKE '" + q + "%'");
+		else
+			items = auditCategoryDAO.findWhere(where + "t.name LIKE '%" + q + "%'");
+	}
+
+	@Override
+	protected void createOutput() {
+		for (AuditCategory category : items) {
+			// The ID of the category we are searching for
+			outputBuffer.append(category.getId()).append("|");
+
+			// The display of the category
+			if (isSearchDigit())
+				outputBuffer.append("(").append(category.getId()).append(") ");
+			outputBuffer.append(category.getAuditType().getAuditName()).append(" &gt; ");
+
+			Iterator<AuditCategory> ancestors = category.getAncestors().iterator();
+			while (ancestors.hasNext()) {
+				outputBuffer.append(ancestors.next().getName());
+				if (ancestors.hasNext())
+					outputBuffer.append(" &gt; ");
+			}
+
+			outputBuffer.append("\n");
+		}
+	}
+
+	public Integer getAuditTypeID() {
+		return auditTypeID;
+	}
+
+	public void setAuditTypeID(Integer auditTypeID) {
+		this.auditTypeID = auditTypeID;
+	}
+
+}
