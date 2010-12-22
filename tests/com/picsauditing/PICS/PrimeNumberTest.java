9cfeb43827564cf4d659b6ea6506e88cd30b5924
Trevor Allred <tallred@picsauditing.com>
2010-12-22 22:09:05 +0000

    adjusted baseline for prime number test

--- tests/com/picsauditing/PICS/PrimeNumberTest.java
+++ tests/com/picsauditing/PICS/PrimeNumberTest.java
@@ -5,25 +5,13 @@ import junit.framework.TestCase;
 import org.junit.Test;
 
 public class PrimeNumberTest extends TestCase {
+	
 	@Test
 	public void testPrimeNumber() {
-		int i = 2;
-		long starttime = System.currentTimeMillis();
-		while( i < 100000) {
-			if(isPrime(i))
-				System.out.println(i);
-			i++;
-		}
-		long endtime = System.currentTimeMillis();
-		System.out.println("Time Taken :" + (endtime-starttime));
+		assertEquals(false, isPrime(5));
 	}
 	
-	private boolean isPrime(int candidate) {
-		for(int i=2; i<=  Math.ceil(Math.sqrt(candidate)); i++) {
-			if(candidate%i == 0)
-				return false;
-		}
+	private boolean isPrime(int n) {
 		return true;
 	}
-
 }
