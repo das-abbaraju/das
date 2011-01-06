a5b08b94c0f885b2f277fbd85ac5062066d64996
Kyle Partridge <kpartridge@picsauditing.com>
2011-01-06 00:59:06 +0000

    Don't scroll down when visiting the audit page

--- src/com/picsauditing/actions/contractors/ContractorActionSupport.java
+++ src/com/picsauditing/actions/contractors/ContractorActionSupport.java
@@ -250,7 +250,7 @@ public class ContractorActionSupport extends AccountActionSupport {
 
 		if (!permissions.isContractor() || permissions.hasPermission(OpPerms.ContractorSafety)) { // Add
 			// All Other Audits
-			MenuComponent subMenu = new MenuComponent("Audits", "ContractorDocuments.action?id=" + id + "#Audit");
+			MenuComponent subMenu = new MenuComponent("Audits", "ContractorDocuments.action?id=" + id);
 			for (ContractorAudit audit : auditList) {
 				if (audit.getAuditType().getClassType().equals(AuditTypeClass.Audit)) {
 					if (!permissions.isContractor() || audit.getCurrentOperators().size() > 0) {
