2297161d7dc36dbfc3fa13d596cb5a66a8d64924
Trevor Allred <tallred@picsauditing.com>
2011-01-14 06:21:59 +0000

    removed testing

--- tests/com/picsauditing/PICS/PrimeNumberTest.java
+++ tests/com/picsauditing/PICS/PrimeNumberTest.java
@@ -5,13 +5,9 @@ import junit.framework.TestCase;
 import org.junit.Test;
 
 public class PrimeNumberTest extends TestCase {
-	
+
 	@Test
 	public void testPrimeNumber() {
-		assertEquals(false, isPrime(5));
-	}
-	
-	private boolean isPrime(int n) {
-		return true;
+		
 	}
 }
