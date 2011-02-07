4a63b7218441f57fbd97936d7ec7c7d34c3471ad
Trevor Allred <tallred@picsauditing.com>
2011-02-07 18:59:06 +0000

    added in clean data so we can save and edit this file easier for the alpha database setup process

---
+++ deployments/clean_data.sql
@@ -0,0 +1,15 @@
+update users set password = SHA1(CONCAT('kkttl5',id));
+update contractor_info set taxID = '123456789';
+update users set email = 'tester@picsauditing.com' where email > '';
+update employee set email = 'tester@picsauditing.com' where email > '';
+update employee set ssn = '123456789' where ssn > '';
+update email_queue set toAddresses = 'tester@picsauditing.com', ccAddresses = null, bccAddresses = null, fromPassword = null, fromAddress = null;
+
+update generalcontractors set baselineFlag = flag;
+
+update app_properties set value = '2J862r8678vrx3d3evk6m44B9RHXcWUv' where property = 'brainTree.key';
+update app_properties set value = '1884502' where property = 'brainTree.key_id';
+update app_properties set value = 'picstest' where property = 'brainTree.username';
+update app_properties set value = 'password1' where property = 'brainTree.password';
+update app_properties set value = 'ccprocessora' where property = 'brainTree.processor_id.us';
+update app_properties set value = 'ccprocessorb' where property = 'brainTree.processor_id.canada';
