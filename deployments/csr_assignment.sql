ac472cfb6b9de5f9ba813a6953c49bdead8aa897
Trevor Allred <tallred@picsauditing.com>
2011-02-16 05:39:07 +0000

    finished the prune script

--- deployments/csr_assignment.sql
+++ deployments/csr_assignment.sql
@@ -103,7 +103,7 @@ update contractor_info c set welcomeAuditor_id = 27274
 where c.id in (5593,11417,11270,10281,7471,6086,7892,13577,6177,13520,6918,4530);
 
 -- rebuild the stats for ContractorOperators
-TRUNCATE TABLE stats_gco_count; 
+TRUNCATE TABLE stats_gco_count;
 
 INSERT INTO stats_gco_count 
 SELECT gc.genID opID, null, count(*) total FROM generalcontractors gc 
--- deployments/upgrade.sql
+++ deployments/upgrade.sql
@@ -2,3 +2,6 @@
 update ref_country rc set rc.english = 'United States Minor Islands', rc.spanish = 'United States Minor Islands', rc.french = 'United States Minor Islands' where rc.isoCode = 'UM';
 update ref_country rc set rc.english = 'South Georgia & Sandwich Islands', rc.spanish = 'South Georgia & Sandwich Islands', rc.french = 'South Georgia & Sandwich Islands' where rc.isoCode = 'GS';
 -- END --
+
+alter table `users` add column `locale` varchar(5) DEFAULT 'en' NULL;
+alter table `accounts` add column `locale` varchar(5) DEFAULT 'en' NULL;
