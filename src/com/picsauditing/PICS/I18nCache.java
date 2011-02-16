bbaee8d321a062baf603633add56ee7077a87b8c
Kyle Partridge <kpartridge@picsauditing.com>
2011-02-16 23:35:38 +0000

    fallback conversion and better message formatting

--- src/com/picsauditing/PICS/I18nCache.java
+++ src/com/picsauditing/PICS/I18nCache.java
@@ -1,6 +1,7 @@
 package com.picsauditing.PICS;
 
 import java.sql.SQLException;
+import java.text.MessageFormat;
 import java.util.List;
 import java.util.Locale;
 
@@ -8,9 +9,14 @@ import org.apache.commons.beanutils.BasicDynaBean;
 
 import com.google.common.collect.Table;
 import com.google.common.collect.TreeBasedTable;
+import com.opensymphony.xwork2.util.LocalizedTextUtil;
+import com.picsauditing.jpa.entities.AppTranslation;
 import com.picsauditing.search.Database;
 
 public class I18nCache {
+
+	public final String DEFAULT_LANGUAGE = "en";
+	public final String DEFAULT_TRANSLATION = "Translation missing";
 
 	private final static I18nCache INSTANCE = new I18nCache();
 
@@ -29,7 +35,7 @@ public class I18nCache {
 	}
 
 	public boolean hasKey(String key, Locale locale) {
-		return hasKey(key, locale.toString());
+		return hasKey(key, getLocaleFallback(key, locale));
 	}
 
 	public String getText(String key, String locale) {
@@ -37,24 +43,22 @@ public class I18nCache {
 	}
 
 	public String getText(String key, Locale locale) {
-		return getText(key, locale.toString());
+		return getText(key, getLocaleFallback(key, locale));
 	}
 
 	public String getText(String key, String locale, Object... args) {
 		if (hasKey(key, locale))
-			return String.format(getText(key, locale), args);
+			return MessageFormat.format(getText(key, locale), args);
 		else
 			return null;
 	}
 
 	public String getText(String key, Locale locale, Object... args) {
-		if (args == null)
-			return getText(key, locale);
-		return getText(key, locale.toString(), args);
+		return getText(key, getLocaleFallback(key, locale), args);
 	}
 
 	public Table<String, String, String> getCache() {
-		if (cache == null || "1".equals(System.getProperty("pics.debug"))) {
+		if (cache == null || true) {
 			try {
 				cache = TreeBasedTable.create();
 				Database db = new Database();
@@ -69,5 +73,41 @@ public class I18nCache {
 		}
 
 		return cache;
+	}
+
+	public void clear() {
+		cache = null;
+	}
+
+	public String getLocaleFallback(String key, Locale locale) {
+		String localeString = locale.toString();
+		if (!hasKey(key, localeString)) {
+			localeString = locale.getLanguage();
+			if (!hasKey(key, localeString)) {
+				localeString = DEFAULT_LANGUAGE;
+				if (!hasKey(key, localeString)) {
+					// insert the default msg into the table and the cache
+					try {
+						Database db = new Database();
+						String sql = "INSERT INTO app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, lastUsed)"
+								+ " VALUES ('"
+								+ key
+								+ "', '"
+								+ localeString
+								+ "', '"
+								+ DEFAULT_TRANSLATION
+								+ "', 1, 1, NOW(), NOW(), NOW())";
+						db.executeInsert(sql);
+						cache.put(key, localeString, DEFAULT_TRANSLATION);
+					} catch (SQLException e) {
+						// In case the translation already existed, it might be
+						// best to clear the cache
+						clear();
+					}
+				}
+			}
+		}
+
+		return localeString;
 	}
 }
