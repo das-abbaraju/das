78cd67719c03f549f9d848725d0b082c2d4b00dc
Lani Aung <uaung@picsauditing.com>
2011-02-24 18:09:42 +0000

    i18n

--- src/com/picsauditing/actions/contractors/ContractorDocuments.java
+++ src/com/picsauditing/actions/contractors/ContractorDocuments.java
@@ -46,8 +46,6 @@ public class ContractorDocuments extends ContractorActionSupport {
 		this.auditTypeDAO = auditTypeDAO;
 		this.caoDAO = caoDAO;
 		this.auditTypeRuleCache = auditTypeRuleCache;
-
-		subHeading = "Document Index";
 	}
 
 	@Override
@@ -55,6 +53,7 @@ public class ContractorDocuments extends ContractorActionSupport {
 		if (!forceLogin())
 			return LOGIN;
 
+		subHeading = getText(this.getClass().getSimpleName() + ".title");
 		findContractor();
 
 		Map<String, List<ContractorAudit>> allIMAudits = new TreeMap<String, List<ContractorAudit>>();
