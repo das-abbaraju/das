e8074cc690a6cc4e677d6561411565ee4ad4d88d
Trevor Allred <tallred@picsauditing.com>
2011-03-01 02:26:17 +0000

    debugging stuff

--- src/com/picsauditing/actions/Trevor.java
+++ src/com/picsauditing/actions/Trevor.java
@@ -1,85 +1,25 @@
 package com.picsauditing.actions;
 
 import java.sql.SQLException;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
 
-import com.google.common.base.Strings;
-import com.google.common.collect.Collections2;
-import com.picsauditing.PICS.AuditCategoryRuleCache;
-import com.picsauditing.PICS.AuditTypeRuleCache;
-import com.picsauditing.dao.AppPropertyDAO;
-import com.picsauditing.dao.AuditDecisionTableDAO;
 import com.picsauditing.dao.ContractorAccountDAO;
-import com.picsauditing.jpa.entities.AppProperty;
-import com.picsauditing.jpa.entities.AuditCategoryRule;
-import com.picsauditing.jpa.entities.ContractorAccount;
+import com.picsauditing.jpa.entities.ContractorAudit;
 
 @SuppressWarnings("serial")
 public class Trevor extends PicsActionSupport {
 
-	private AuditDecisionTableDAO auditRuleDAO;
 	private ContractorAccountDAO accountDAO;
-	private AppPropertyDAO appPropertyDAO;
-	private AuditCategoryRuleCache categoryCache;
-	private AuditTypeRuleCache typeCache;
-	private int conID;
-	private Set<AuditCategoryRule> applicable;
-	public String rules;
 
-	public Trevor(ContractorAccountDAO accountDAO, AuditDecisionTableDAO auditRuleDAO, AuditCategoryRuleCache categoryCache, AuditTypeRuleCache typeCache, AppPropertyDAO appPropertyDAO) {
+	public Trevor(ContractorAccountDAO accountDAO) {
 		this.accountDAO = accountDAO;
-		this.auditRuleDAO = auditRuleDAO;
-		this.categoryCache = categoryCache;
-		this.typeCache = typeCache;
-		this.appPropertyDAO = appPropertyDAO;
 	}
 
 	public String execute() throws SQLException {
 
-		if ("clear".equals(button)) {
-			List<AuditCategoryRule> rules = auditRuleDAO.findCategoryRules();
-			categoryCache.clear();
-			typeCache.clear();
-			AppProperty appProp = appPropertyDAO.find("clear_cache");
-			if (appProp != null) {
-				appProp.setValue("true");
-				appPropertyDAO.save(appProp);
-			}
-		}
-		if ("print".equals(button)) {
-			output = categoryCache.print();
-		}
-		if (conID > 0) {
-			ContractorAccount contractor = accountDAO.find(conID);
-			//applicable = cache.getApplicable(contractor, null);
-		}
-		if ("showlist".equals(button)) {
-			String[] rulesList = rules.split(",");
-			applicable = new LinkedHashSet<AuditCategoryRule>();
-			for (String string : rulesList) {
-				try {
-					applicable.add(auditRuleDAO.findAuditCategoryRule(Integer.parseInt(string)));
-				} catch (NumberFormatException e) {
-					// ignore
-					System.out.println("problem: " + string);
-				}
-			}
-		}
-
+		System.out.println("Querying the first time");
+		ContractorAudit t = (ContractorAudit) accountDAO.find(ContractorAudit.class, 1688);
+		
 		return SUCCESS;
 	}
 
-	public int getConID() {
-		return conID;
-	}
-
-	public void setConID(int conID) {
-		this.conID = conID;
-	}
-
-	public Set<AuditCategoryRule> getApplicable() {
-		return applicable;
-	}
 }
