8da936b5f89b86d72468375a08e09d4105ed0817
Kyle Partridge <kpartridge@picsauditing.com>
2011-03-24 18:37:22 +0000

    this cron should be anonymouse

--- src/com/picsauditing/actions/cron/PyContractorCron.java
+++ src/com/picsauditing/actions/cron/PyContractorCron.java
@@ -3,22 +3,24 @@ package com.picsauditing.actions.cron;
 import java.util.HashSet;
 import java.util.List;
 
+import com.picsauditing.access.Anonymous;
 import com.picsauditing.actions.PicsActionSupport;
 import com.picsauditing.dao.ContractorAccountDAO;
 import com.picsauditing.util.Strings;
 
 @SuppressWarnings("serial")
 public class PyContractorCron extends PicsActionSupport {
-	
+
 	private ContractorAccountDAO dao;
-	
-	public PyContractorCron(ContractorAccountDAO dao){
-		this.dao = dao;		
+
+	public PyContractorCron(ContractorAccountDAO dao) {
+		this.dao = dao;
 	}
-	
-	public String execute(){
+
+	@Anonymous
+	public String execute() {
 		List<Integer> ids = dao.findContractorsNeedingRecalculation(15, new HashSet<Integer>());
-		if(ids!=null && !ids.isEmpty())
+		if (ids != null && !ids.isEmpty())
 			output = Strings.implode(ids);
 		return BLANK;
 	}
