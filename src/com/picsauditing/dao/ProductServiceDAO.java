8d110a5fb4a98496eef1417faeba8d5afa43c940
Trevor Allred <tallred@picsauditing.com>
2011-03-28 19:26:03 +0000

    added Root query

--- src/com/picsauditing/dao/ProductServiceDAO.java
+++ src/com/picsauditing/dao/ProductServiceDAO.java
@@ -8,6 +8,7 @@ import com.picsauditing.jpa.entities.ProductService;
 
 @SuppressWarnings("unchecked")
 public class ProductServiceDAO extends PicsDAO {
+
 	public ProductService find(int id) {
 		ProductService a = em.find(ProductService.class, id);
 		return a;
@@ -16,6 +17,11 @@ public class ProductServiceDAO extends PicsDAO {
 	public List<ProductService> findByParent(int parentID) {
 		Query query = em.createQuery("SELECT p FROM ProductService p WHERE p.parent.id =  ?");
 		query.setParameter(1, parentID);
+		return query.getResultList();
+	}
+
+	public List<ProductService> findRoot() {
+		Query query = em.createQuery("SELECT p FROM ProductService p WHERE p.parent IS NULL");
 		return query.getResultList();
 	}
 }
