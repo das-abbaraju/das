1fe65e03a0d694e3ec31c6845fba5b72d0828b96
Kyle Partridge <kpartridge@picsauditing.com>
2011-03-28 18:44:05 +0000

    created service taxonomy stub

---
+++ src/com/picsauditing/actions/productservices/ServiceTaxonomy.java
@@ -0,0 +1,14 @@
+package com.picsauditing.actions.productservices;
+
+import com.picsauditing.actions.PicsActionSupport;
+
+@SuppressWarnings("serial")
+public class ServiceTaxonomy extends PicsActionSupport {
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return super.execute();
+	}
+
+}
