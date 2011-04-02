e30a2c523e04057c7bfa163627c92f4de1748d96
Trevor Allred <tallred@picsauditing.com>
2011-04-02 00:08:29 +0000

    more converter stuff

--- src/com/picsauditing/actions/converters/TranslatableStringConverter.java
+++ src/com/picsauditing/actions/converters/TranslatableStringConverter.java
@@ -1,5 +1,6 @@
 package com.picsauditing.actions.converters;
 
+import java.lang.reflect.Field;
 import java.util.Locale;
 import java.util.Map;
 
@@ -9,6 +10,8 @@ import com.opensymphony.xwork2.ActionContext;
 import com.picsauditing.jpa.entities.TranslatableString;
 import com.picsauditing.util.Strings;
 
+import freemarker.template.utility.StringUtil;
+
 @SuppressWarnings("unchecked")
 public class TranslatableStringConverter extends StrutsTypeConverter {
 
@@ -17,8 +20,16 @@ public class TranslatableStringConverter extends StrutsTypeConverter {
 		TranslatableString response = new TranslatableString();
 
 		if (values.length > 0 && !Strings.isEmpty(values[0])) {
-			Locale locale = ActionContext.getContext().getLocale();
-			response.putTranslation(locale.toString(), values[0]);
+			try {
+				Object action = ActionContext.getContext().getActionInvocation().getAction();
+				Field object = action.getClass().getDeclaredField(context.get("current.property.path").toString());
+				action.getClass().getMethod("get" + StringUtil.capitalize(context.get("current.property.path").toString()));
+				context.get("conversion.property.fullName");
+				Locale locale = ActionContext.getContext().getLocale();
+				response.putTranslation(locale.toString(), values[0]);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		} else {
 			response = null;
 		}
