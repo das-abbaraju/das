56f50028dccabad63cc66b115abf6357219e74a9
David Tomberlin <dtomberlin@picsauditing.com>
2011-05-03 18:16:50 +0000

    PICS-2278

--- src/com/picsauditing/actions/chart/ChartFlagCountCorp.java
+++ src/com/picsauditing/actions/chart/ChartFlagCountCorp.java
@@ -18,35 +18,39 @@ public class ChartFlagCountCorp extends ChartMSAction {
 		chart.setShowLegend(false);
 		chart.setShowValues(false);
 		chart.setAnimation(false);
-		
+
 		SelectSQL sql = new SelectSQL("accounts a");
 		sql.addField("flag as series");
 		sql.addField("op.name as label");
 		sql.addField("count(*) as value");
-		sql.addField("CONCAT('ReportContractorOperatorFlag.action?button=Search&filter.flagStatus=',gc.flag,'&filter.operator=',op.id) as link");
+		sql
+				.addField("CONCAT('ReportContractorOperatorFlag.action?button=Search&filter.flagStatus=',gc.flag,'&filter.operator=',op.id) as link");
 		sql.addGroupBy("series, label");
 		sql.addOrderBy("series, label");
 		sql.addJoin("JOIN generalcontractors gc ON a.id = gc.subID");
+		sql.addJoin("JOIN operators o on o.id = gc.genID");
 		sql.addJoin("JOIN accounts op ON op.id = gc.genID");
-		sql.addWhere("a.status IN ('Active','Demo')");
-		sql.addWhere("op.status IN ('Active','Demo')"); 
-		sql.addWhere("gc.genID IN (SELECT fac.opID from facilities fac where fac.corporateID = "+ permissions.getAccountId()+ ")");
+		sql.addWhere("a.status IN ('Active'" + (permissions.getAccountStatus().isDemo() ? ",'Demo')" : ")"));
+		sql.addWhere("op.status IN ('Active'" + (permissions.getAccountStatus().isDemo() ? ",'Demo')" : ")"));
+		sql.addWhere("gc.genID IN (SELECT fac.opID from facilities fac where fac.corporateID = "
+				+ permissions.getAccountId() + ")");
 		sql.addWhere("gc.flag != 'Clear'");
-		
+		sql.addWhere("IF(o.approvesRelationships='Yes',gc.workStatus='Y',1)");
+
 		ChartDAO db = new ChartDAO();
 		List<DataRow> data = db.select(sql.toString());
-		
+
 		MultiSeriesConverter converter = new MultiSeriesConverter();
 		converter.setChart(chart);
 		converter.addData(data);
 		Map<String, DataSet> dataSet = converter.getChart().getDataSets();
 		for (DataSet row : dataSet.values()) {
 			row.setShowValues(false);
-			for(Set set : row.getSets().values()) {
+			for (Set set : row.getSets().values()) {
 				set.setColor(FlagColor.valueOf(row.getSeriesName()).getHex());
 			}
 		}
-		
+
 		return chart;
 	}
 }
