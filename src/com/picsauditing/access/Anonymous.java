14ba9368352ffe31a589e75adaf614772967377e
Trevor Allred <tallred@picsauditing.com>
2011-05-10 20:09:26 +0000

    added comments

--- src/com/picsauditing/access/Anonymous.java
+++ src/com/picsauditing/access/Anonymous.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Add this to any Action class method that do NOT require a logged in user. By default, users must be logged in.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Anonymous {
