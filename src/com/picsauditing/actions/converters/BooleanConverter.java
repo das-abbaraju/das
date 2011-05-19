c439f67b4e9a3e9c4d3d1254ca6a1626f1ed439b
Kyle Partridge <kpartridge@picsauditing.com>
2011-05-19 18:10:55 +0000

    boolean converter fix

---
+++ src/com/picsauditing/actions/converters/BooleanConverter.java
@@ -0,0 +1,31 @@
+package com.picsauditing.actions.converters;
+
+import java.util.Map;
+
+import org.apache.struts2.util.StrutsTypeConverter;
+
+import com.picsauditing.util.Strings;
+
+@SuppressWarnings("rawtypes")
+public class BooleanConverter extends StrutsTypeConverter {
+
+	@Override
+	public Object convertFromString(Map context, String[] values, Class toClass) {
+		Object test = null;
+
+		if (test == null) {
+			if (values.length > 0 && !Strings.isEmpty(values[0])) {
+				String temp = values[0];
+				test = Boolean.valueOf(temp);
+			}
+		}
+
+		return test;
+	}
+
+	@Override
+	public String convertToString(Map context, Object o) {
+		return String.valueOf(o);
+	}
+
+}
