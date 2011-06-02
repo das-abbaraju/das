7a8aae1625e68f9e8b1c94c666c2b22d88771542
Tom Baker <tbaker@picsauditing.com>
2011-06-02 17:37:11 +0000

    PICS-2516

--- src/com/picsauditing/jpa/entities/Currency.java
+++ src/com/picsauditing/jpa/entities/Currency.java
@@ -1,12 +1,14 @@
 package com.picsauditing.jpa.entities;
 
 public enum Currency {
-	USD("USD $"), CAD("CAD $");
+	USD("USD $","$"), CAD("CAD $","$"), GBP("GBP £","£");
 
 	private String display;
+	private String icon;
 
-	private Currency(String display) {
+	private Currency(String display, String icon) {
 		this.display = display;
+		this.setIcon(display);
 	}
 
 	public String getDisplay() {
@@ -16,6 +18,8 @@ public enum Currency {
 	public static Currency getFromISO(String isoCode) {
 		if (isoCode.equals("CA"))
 			return CAD;
+		if (isoCode.equals("GB"))
+			return GBP;
 
 		return USD;
 	}
@@ -26,5 +30,13 @@ public enum Currency {
 
 	public boolean isUs() {
 		return this.equals(USD);
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+
+	public String getIcon() {
+		return icon;
 	}
 }
