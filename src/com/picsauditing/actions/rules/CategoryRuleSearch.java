23eac8151c0c8a79e9d4df43f77522a3ed6c4e47
Trevor Allred <tallred@picsauditing.com>
2011-06-05 05:02:25 +0000

    still needs testing, but this looks correct

--- src/com/picsauditing/actions/rules/CategoryRuleSearch.java
+++ src/com/picsauditing/actions/rules/CategoryRuleSearch.java
@@ -2,18 +2,15 @@ package com.picsauditing.actions.rules;
 
 import com.picsauditing.PICS.Utilities;
 import com.picsauditing.access.OpPerms;
+import com.picsauditing.access.RequiredPermission;
 import com.picsauditing.search.SelectSQL;
 
 @SuppressWarnings("serial")
 public class CategoryRuleSearch extends AuditRuleSearch {
 
 	@Override
+	@RequiredPermission(value = OpPerms.ManageCategoryRules)
 	public String execute() throws Exception {
-		if (!forceLogin())
-			return LOGIN;
-		
-		tryPermissions(OpPerms.ManageCategoryRules);
-
 		categoryRule = true;
 		sql = new SelectSQL("audit_category_rule a_search");
 		return super.execute();
