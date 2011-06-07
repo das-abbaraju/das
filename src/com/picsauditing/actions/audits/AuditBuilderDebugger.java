7d321a5d57a57a917cc475924c8069ee56d78a10
Trevor Allred <tallred@picsauditing.com>
2011-06-07 00:20:21 +0000

    debug categories

--- src/com/picsauditing/actions/audits/AuditBuilderDebugger.java
+++ src/com/picsauditing/actions/audits/AuditBuilderDebugger.java
@@ -1,9 +1,32 @@
 package com.picsauditing.actions.audits;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.picsauditing.actions.contractors.ContractorActionSupport;
+import com.picsauditing.auditBuilder.AuditCategoriesBuilder;
+import com.picsauditing.auditBuilder.AuditCategoryRuleCache;
 
 @SuppressWarnings("serial")
 public class AuditBuilderDebugger extends ContractorActionSupport {
 
-	// TODO Create a new Debugger class
+	private int catID;
+
+	@Autowired
+	private AuditCategoryRuleCache auditCategoryRuleCache;
+
+	public String category() {
+
+		AuditCategoriesBuilder categoriesBuilder = new AuditCategoriesBuilder(auditCategoryRuleCache, contractor);
+		// categoriesBuilder.calculate(conAudit);
+		return "category";
+	}
+
+	public int getCatID() {
+		return catID;
+	}
+
+	public void setCatID(int catID) {
+		this.catID = catID;
+	}
+
 }
