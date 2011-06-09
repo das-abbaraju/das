57f33be9cefdec05c47485a245d36d83c7ed6205
Lani Aung <uaung@picsauditing.com>
2011-06-09 18:50:29 +0000

    Added "Previous Expired" column for My Audits page

--- src/com/picsauditing/actions/report/ReportContractorAuditAuditor.java
+++ src/com/picsauditing/actions/report/ReportContractorAuditAuditor.java
@@ -24,26 +24,30 @@ public class ReportContractorAuditAuditor extends ReportContractorAuditOperator 
 	public void buildQuery() {
 		super.buildQuery();
 
+		sql.addJoin("LEFT JOIN (SELECT ca.conID, ca.auditTypeID, MAX(ca.expiresDate) expired FROM contractor_audit ca "
+				+ "WHERE ca.auditTypeID = 2 AND expiresDate < NOW() GROUP BY ca.conID "
+				+ "ORDER BY ca.expiresDate DESC) ca2 ON ca2.conID = ca.conID AND ca2.auditTypeID = ca.auditTypeID");
+		sql.addField("ca2.expired");
+
 		sql.addWhere("ca.auditorID=" + permissions.getUserId());
 		sql.addWhere("a.status IN ('Active','Demo')");
 		if (getFilter().isNotRenewingContractors())
 			sql.addWhere("c.renew = 0");
 		if (getFilter().isContractorsWithPendingMembership())
-			sql.addWhere("c.id in (" + "select c.id from contractor_info c "
-					+ "join invoice i on i.accountID = c.id "
+			sql.addWhere("c.id in (" + "select c.id from contractor_info c " + "join invoice i on i.accountID = c.id "
 					+ "join invoice_item ii on i.id = ii.invoiceID join invoice_fee invf on ii.feeID = invf.id "
 					+ "where invf.feeClass = 'Membership' and invf.id != 100 and invf.id != 4 and i.status = 'Unpaid'"
 					+ " and (ii.amount = invf.defaultAmount or i.totalAmount >= 450))");
-		if(!getFilter().isNotRenewingContractors() && !getFilter().isContractorsWithPendingMembership()){
+		if (!getFilter().isNotRenewingContractors() && !getFilter().isContractorsWithPendingMembership()) {
 			sql.addWhere("c.renew = 1");
 			sql.addWhere("c.id not in (" + "select c.id from contractor_info c "
 					+ "join invoice i on i.accountID = c.id "
 					+ "join invoice_item ii on i.id = ii.invoiceID join invoice_fee invf on ii.feeID = invf.id "
 					+ "where invf.feeClass = 'Membership' and invf.id != 100 and invf.id != 4 and i.status = 'Unpaid'"
 					+ " and (ii.amount = invf.defaultAmount or i.totalAmount >= 450))");
-			
+
 		}
-		orderByDefault = "expiresDate, ca.assignedDate DESC";
+		orderByDefault = "ISNULL(ca2.expired), ca2.expired, ca.assignedDate DESC";
 
 		getFilter().setShowAuditor(false);
 		getFilter().setShowStatus(false);
