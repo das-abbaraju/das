1d107b257f6556e6e4f336b1d0c96cc151cfdf96
Trevor Allred <tallred@picsauditing.com>
2011-07-06 18:09:51 +0000

    comments from code review

--- src/com/picsauditing/jpa/entities/FeeClass.java
+++ src/com/picsauditing/jpa/entities/FeeClass.java
@@ -1,6 +1,7 @@
 package com.picsauditing.jpa.entities;
 
 public enum FeeClass {
+	// TODO combine some of these fees
 	Deprecated, Free, BidOnly, ListOnly, DocuGUARD, InsureGUARD, AuditGUARD, EmployeeGUARD, Activation, Reactivation, LateFee, ReschedulingFee, ScanningFee, WebcamFee, ExpediteFee, ImportFee, SuncorDiscount, GST, Misc;
 
 	public boolean isPaymentExpiresNeeded() {
