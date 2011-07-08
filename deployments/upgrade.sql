3172e483df80410382e65199d8cfd0bb8c844e07
Kyle Partridge <kpartridge@picsauditing.com>
2011-07-08 01:55:44 +0000

    Ran these upgrades.

--- deployments/upgrade.sql
+++ deployments/upgrade.sql
@@ -1,7 +1,3 @@
-update audit_type set scoreType='Percent' where scoreable;
-
-update invoice_fee set visible = 0 where id in (3,297,299,301,309,317,325);
-
 -- PICS-2787
 update app_translation 
 set msgValue = replace(msgValue,'(US)','(US/Canada)')
