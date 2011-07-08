5c59487bf1b2f22069ddc43189614e05934325cf
Lani Aung <uaung@picsauditing.com>
2011-07-08 19:18:51 +0000

    Show Locale as a column

--- WebContent/struts/i18n/translation_etl_table.jsp
+++ WebContent/struts/i18n/translation_etl_table.jsp
@@ -4,35 +4,28 @@
 <table class="report">
 	<thead>
 		<tr>
-			<s:iterator value="allLocales">
-				<td colspan="3"><s:property /></td>
-			</s:iterator>
-		</tr>
-		<tr>
-			<s:iterator value="allLocales">
-				<td>Key</td>
-				<td>New</td>
-				<td>Old</td>
-			</s:iterator>
+			<td>Key</td>
+			<td>Locale</td>
+			<td>New</td>
+			<td>Old</td>
 		</tr>
 	</thead>
 	<tbody>
 		<s:iterator value="allKeys" id="key">
 			<tr>
-				<s:iterator value="allLocales" id="locale">
-					<td><s:property value="importedTranslations.get(#key, #locale).get(0).key" /></td>
-					<td><s:property value="importedTranslations.get(#key, #locale).get(0).value" /></td>
-					<td><s:property value="importedTranslations.get(#key, #locale).get(1).value" /></td>
-				</s:iterator>
+				<td><s:property value="importedTranslations.get(#key).get(0).key" /></td>
+				<td><s:property value="importedTranslations.get(#key).get(0).locale" /></td>
+				<td><s:property value="importedTranslations.get(#key).get(0).value" /></td>
+				<td><s:property value="importedTranslations.get(#key).get(1).value" /></td>
 			</tr>
 		</s:iterator>
 		<s:if test="allKeys.size > 250">
 			<tr>
-				<td colspan="<s:property value="allLocales.size * 3" />" class="center">...</td>
+				<td colspan="4" class="center">...</td>
 			</tr>
 		</s:if>
 		<tr>
-			<td colspan="<s:property value="allLocales.size * 3" />" class="center" style="font-weight: bold;">
+			<td colspan="4" class="center" style="font-weight: bold;">
 				<s:property value="allKeys.size" /> total changes
 			</td>
 		</tr>
--- src/com/picsauditing/util/TranslationETL.java
+++ src/com/picsauditing/util/TranslationETL.java
@@ -54,9 +54,8 @@ public class TranslationETL extends PicsActionSupport {
 	private boolean download = false;
 	private Date startDate;
 	private String translations;
-	private DoubleMap<String, String, List<AppTranslation>> importedTranslations;
+	private Map<String, List<AppTranslation>> importedTranslations;
 	private List<String> allKeys;
-	private List<String> allLocales;
 	// Import
 	private File file;
 	protected String fileContentType = null;
@@ -83,22 +82,20 @@ public class TranslationETL extends PicsActionSupport {
 
 		List<AppTranslation> translations = new ArrayList<AppTranslation>();
 		for (String key : allKeys) {
-			for (String locale : allLocales) {
-				if (importedTranslations.get(key, locale) != null) {
-					AppTranslation t = importedTranslations.get(key, locale).get(0);
+			if (importedTranslations.get(key) != null) {
+				AppTranslation t = importedTranslations.get(key).get(0);
 
-					if (importedTranslations.get(key, locale).size() > 1) {
-						AppTranslation t2 = importedTranslations.get(key, locale).get(1);
+				if (importedTranslations.get(key).size() > 1) {
+					AppTranslation t2 = importedTranslations.get(key).get(1);
 
-						if (t2 != null && t2.getId() > 0)
-							t.setId(t2.getId());
-					}
+					if (t2 != null && t2.getId() > 0)
+						t.setId(t2.getId());
+				}
 
-					translations.add(t);
-					if (translations.size() == 100) {
-						saveTranslations(translations);
-						translations.clear();
-					}
+				translations.add(t);
+				if (translations.size() == 100) {
+					saveTranslations(translations);
+					translations.clear();
 				}
 			}
 		}
@@ -276,9 +273,8 @@ public class TranslationETL extends PicsActionSupport {
 		db = new Database();
 		sql = new SelectSQL("app_translation t");
 
-		importedTranslations = new DoubleMap<String, String, List<AppTranslation>>();
+		importedTranslations = new TreeMap<String, List<AppTranslation>>();
 		Set<String> allKeysSet = new HashSet<String>();
-		Set<String> allLocalesSet = new HashSet<String>();
 
 		sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		sdf2 = new SimpleDateFormat("yyyy-MM-dd");
@@ -300,13 +296,12 @@ public class TranslationETL extends PicsActionSupport {
 					addField(t, children.item(j).getNodeName(), children.item(j).getTextContent());
 
 				allKeysSet.add(t.getKey());
-				allLocalesSet.add(t.getLocale());
 
 				if (t.getKey() != null) {
-					if (importedTranslations.get(t.getKey(), t.getLocale()) == null)
-						importedTranslations.put(t.getKey(), t.getLocale(), new ArrayList<AppTranslation>());
+					if (importedTranslations.get(t.getKey()) == null)
+						importedTranslations.put(t.getKey(), new ArrayList<AppTranslation>());
 
-					importedTranslations.get(t.getKey(), t.getLocale()).add(t);
+					importedTranslations.get(t.getKey()).add(t);
 				}
 			}
 		}
@@ -319,8 +314,10 @@ public class TranslationETL extends PicsActionSupport {
 			String locale = d.get("locale").toString();
 			String msgValue = d.get("msgValue").toString();
 
-			if (importedTranslations.get(msgKey, locale) != null) {
-				if (msgValue.equals(importedTranslations.get(msgKey, locale).get(0).getValue()))
+			if (importedTranslations.get(msgKey) != null) {
+				// Check if the locale and msgKey are the same
+				AppTranslation first = importedTranslations.get(msgKey).get(0);
+				if (msgValue.equals(first.getValue()) && locale.equals(first.getLocale()))
 					allKeysSet.remove(msgKey);
 				else {
 					AppTranslation t = new AppTranslation();
@@ -334,13 +331,12 @@ public class TranslationETL extends PicsActionSupport {
 					addField(t, "updateDate", d.get("updateDate") == null ? null : d.get("updateDate").toString());
 					addField(t, "lastUsed", d.get("lastUsed") == null ? null : d.get("lastUsed").toString());
 
-					importedTranslations.get(t.getKey(), t.getLocale()).add(t);
+					importedTranslations.get(t.getKey()).add(t);
 				}
 			}
 		}
 
 		allKeys = new ArrayList<String>(allKeysSet);
-		allLocales = new ArrayList<String>(allLocalesSet);
 	}
 
 	private void setupSQL(String where) {
@@ -424,12 +420,8 @@ public class TranslationETL extends PicsActionSupport {
 		return foundRows;
 	}
 
-	public DoubleMap<String, String, List<AppTranslation>> getImportedTranslations() {
+	public Map<String, List<AppTranslation>> getImportedTranslations() {
 		return importedTranslations;
-	}
-
-	public List<String> getAllLocales() {
-		return allLocales;
 	}
 
 	public List<String> getAllKeys() {
