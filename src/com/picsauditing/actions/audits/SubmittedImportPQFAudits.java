09b2fc8dadfe5988a5d46d171b80a34bb78a2dff
Tom Baker <tbaker@picsauditing.com>
2011-07-20 00:05:59 +0000

    PICS-2851: The ImportPQF can be assigned to CSRs which are not the welcome auditor. Need to go off the auditor assignment on the audit.

--- src/com/picsauditing/actions/audits/SubmittedImportPQFAudits.java
+++ src/com/picsauditing/actions/audits/SubmittedImportPQFAudits.java
@@ -17,23 +17,23 @@ public class SubmittedImportPQFAudits extends PicsActionSupport {
 	public List<BasicDynaBean> getData() throws Exception {
 		db = new Database();
 		sql = new SelectSQL("contractor_audit ca");
-		
+
 		sql.addField("a.id accountID");
 		sql.addField("a.name");
 		sql.addField("i.id invoiceID");
 		sql.addField("i.status invoiceStatus");
 		sql.addField("ca.id auditID");
 		sql.addField("GROUP_CONCAT(DISTINCT cao.status SEPARATOR ', ') auditStatus");
-		
-		sql.addJoin("JOIN contractor_audit_operator cao ON cao.auditID = ca.id AND cao.visible = 1 AND cao.status = 'Submitted'");
+
+		sql.addJoin("JOIN contractor_audit_operator cao ON cao.auditID = ca.id AND cao.visible = 1"
+				+ " AND cao.status = 'Submitted'");
 		sql.addJoin("JOIN accounts a ON a.id = ca.conID AND a.status = 'Active'");
-		sql.addJoin("JOIN contractor_info c ON c.id = a.id AND c.competitorMembership = 1 AND c.welcomeAuditor_id = "
-				+ permissions.getUserId());
+		sql.addJoin("JOIN contractor_info c ON c.id = a.id");
 		sql.addJoin("JOIN invoice i ON i.accountID = a.id AND i.status = 'Paid'");
 		sql.addJoin("JOIN invoice_item ii ON ii.invoiceID = i.id");
 		sql.addJoin("JOIN invoice_fee fee ON fee.id = ii.feeID AND fee.feeClass = 'ImportFee'");
 
-		sql.addWhere("ca.auditTypeID = 232");
+		sql.addWhere("ca.auditTypeID = 232 AND ca.auditorID = " + permissions.getUserId());
 		sql.addOrderBy("ca.creationDate DESC");
 		sql.addGroupBy("a.id, ca.id");
 
