d07863dd7ff4c83ad9cb2c482e47f82b803c319e
Kirk Chase <kchase@picsauditing.com>
2011-08-15 22:27:32 +0000

    PICS-3021 Merge Manual Audit CAOs into a single CAO upgrade script to merge caos

--- deployments/upgrade.sql
+++ deployments/upgrade.sql
@@ -11,3 +11,38 @@ where ca.auditTypeID = 1
 group by ca.conID) d2 on d2.conID = c.id
 set c.productRiskVerified = d2.dateVerified;
 -- END
+
+-- PICS-3021
+-- delete all invisible caos
+delete from contractor_audit_operator where id in (select id from (
+select cao.id from contractor_audit ca
+join accounts a on ca.conID = a.id and a.type = 'Contractor' and a.status = 'Active'
+join contractor_audit_operator cao on ca.id = cao.auditID and cao.visible = 0
+where ca.auditTypeID in (2)
+) aa);
+
+-- insert new pics global cao with data from later on workflow
+insert into contractor_audit_operator (auditID, opID, createdBy, updatedBy, creationDate, updateDate, status, visible, percentVerified, percentComplete, statusChangedDate)
+select distinct ca.id, 4, 37951, 37951, now(), now(), cao.status, 1, max(cao.percentVerified), max(cao.percentComplete), now()
+from contractor_audit ca
+join accounts a on ca.conID = a.id and a.type = 'Contractor' and a.status = 'Active'
+join contractor_audit_operator cao on ca.id = cao.auditID and cao.visible = 1
+where ca.auditTypeID in (2)
+group by ca.id;
+
+-- delete all other caos
+delete from contractor_audit_operator where id in (select id from (
+select cao.id from contractor_audit ca
+join accounts a on ca.conID = a.id and a.type = 'Contractor' and a.status = 'Active'
+join contractor_audit_operator cao on ca.id = cao.auditID and cao.opID != 4 and cao.visible = 1
+where ca.auditTypeID in (2)
+) aa);
+
+-- Script to reset the cron for all those contractors who have a new CAO. 
+update contractor_info ci
+join accounts a on ci.id = a.id
+join contractor_audit ca on ci.id = ca.conID and ca.auditTypeID = 2
+join contractor_audit_operator cao on ca.id = cao.auditID and cao.visible = 1 and cao.opID = 4 and cao.creationDate > curdate()
+set ci.needsRecalculation = ci.needsRecalculation + 2, ci.lastRecalculation = null
+where a.type = 'Contractor' and a.status = 'Active'
+-- END
