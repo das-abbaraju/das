c19aa05f2be703cb308a92967d3a72e52ca8cb11
Lucas Kam <lkam@picsauditing.com>
2011-09-14 21:41:58 +0000

    PICS-3287

--- deployments/upgradeConfig.sql
+++ deployments/upgradeConfig.sql
@@ -6,3 +6,15 @@ update flag_criteria set category="Insurance AMB Rating" where label like '%Rati
 update flag_criteria set optionCode='ExcessEachOccurrence' where category="Insurance Criteria" and description like '%plus Excess Each Occurrence%';
 update flag_criteria set optionCode='ExcessAggregate' where category="Insurance Criteria" and description like '%plus Excess Aggregate%';
 --
+
+-- PICS-3287
+update pqfData pd
+  join contractor_audit ca
+    on ca.id = pd.auditID
+  join contractor_audit ca2
+    on ca2.conID = ca.conID
+set pd.auditID = ca2.id, pd.questionID = (case pd.questionID when 7786 then 9186 when 7787 then 9188 when 7788 then 9187 end)
+where (pd.questionID = 7786
+        or pd.questionID = 7787
+        or pd.questionID = 7788)
+    and ca2.auditTypeID = 279;
