92f32e7dada776fa9d7a1e5869380085f8de5822
Kyle Partridge <kpartridge@picsauditing.com>
2011-09-15 17:58:00 +0000

    this should be in the upgrade script for live

--- deployments/upgradeConfig.sql
+++ deployments/upgradeConfig.sql
@@ -7,19 +7,6 @@ update flag_criteria set optionCode='ExcessEachOccurrence' where category="Insur
 update flag_criteria set optionCode='ExcessAggregate' where category="Insurance Criteria" and description like '%plus Excess Aggregate%';
 --
 
--- PICS-3287
-update pqfData pd
-  join contractor_audit ca
-    on ca.id = pd.auditID
-  join contractor_audit ca2
-    on ca2.conID = ca.conID
-set pd.auditID = ca2.id, pd.questionID = (case pd.questionID when 7786 then 9186 when 7787 then 9188 when 7788 then 9187 end)
-where (pd.questionID = 7786
-        or pd.questionID = 7787
-        or pd.questionID = 7788)
-    and ca2.auditTypeID = 279;
-
-    
 -- PICS-3219
 update app_translation t set t.msgValue = '<SubscriptionHeader>
 Below are all the contractors whose flags have been forced. <br/>
