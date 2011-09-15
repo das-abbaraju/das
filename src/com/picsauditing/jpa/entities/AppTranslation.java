434d52ea998e60ac4ba8a155cadb4bc67cf6c19e
Kyle Partridge <kpartridge@picsauditing.com>
2011-09-15 23:44:20 +0000

    PICS-3322:
    Only showing english, french and spanish

--- src/com/picsauditing/jpa/entities/AppTranslation.java
+++ src/com/picsauditing/jpa/entities/AppTranslation.java
@@ -62,10 +62,10 @@ public class AppTranslation extends BaseTable implements java.io.Serializable {
 	public static List<Locale> getLocales() {
 		if (locales == null) {
 			locales = new ArrayList<Locale>();
-			locales.add(new Locale("ar"));
-			locales.add(new Locale("zh", "CN"));
-			locales.add(new Locale("zh", "TW"));
-			locales.add(new Locale("nl"));
+			// locales.add(new Locale("ar"));
+			// locales.add(new Locale("zh", "CN"));
+			// locales.add(new Locale("zh", "TW"));
+			// locales.add(new Locale("nl"));
 			locales.add(new Locale("en"));
 			locales.add(new Locale("en", "AU"));
 			locales.add(new Locale("en", "CA"));
@@ -75,11 +75,13 @@ public class AppTranslation extends BaseTable implements java.io.Serializable {
 			locales.add(new Locale("fr"));
 			locales.add(new Locale("fr", "CA"));
 			locales.add(new Locale("fr", "FR"));
-			locales.add(new Locale("de"));
-			locales.add(new Locale("ja"));
-			locales.add(new Locale("pt"));
+			// locales.add(new Locale("de"));
+			// locales.add(new Locale("ja"));
+			// locales.add(new Locale("pt"));
 			locales.add(new Locale("es"));
-			locales.add(new Locale("ru"));
+			locales.add(new Locale("es", "MX"));
+			locales.add(new Locale("es", "ES"));
+			// locales.add(new Locale("ru"));
 		}
 		return locales;
 	}
