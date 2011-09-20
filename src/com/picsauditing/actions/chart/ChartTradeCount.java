637893c8eca93ad8418c8a6fdda8e7d056e2c5d3
Tom Baker <tbaker@picsauditing.com>
2011-09-20 01:07:41 +0000

    PICS-3376: Trades showing french when they should be showing english. Couldn't add link because of similarity checking in ContractorList

--- src/com/picsauditing/actions/chart/ChartTradeCount.java
+++ src/com/picsauditing/actions/chart/ChartTradeCount.java
@@ -23,14 +23,13 @@ public class ChartTradeCount extends ChartSSAction {
 		SelectSQL sql = new SelectSQL("accounts a");
 		sql.addJoin("JOIN contractor_info ci ON a.id = ci.id");
 		sql.addJoin("JOIN contractor_audit ca ON a.id = ca.conID AND ca.auditTypeID = 1");
-		sql.addJoin("JOIN contractor_trade ct ON ca.conID = ct.conID AND ct.selfPerformed = 1");
-		sql.addJoin("JOIN app_translation at ON CONCAT('Trade.', ct.tradeID, '.name') = at.msgKey");
+		sql.addJoin("JOIN contractor_trade ct ON ci.id = ct.conID AND ct.selfPerformed = 1");
 		
-		sql.addField("at.msgValue as label");
+		sql.addField("ct.tradeID as label");
 		sql.addField("count(*) as value");
 		sql.addGroupBy("label");
 		sql.addOrderBy("value DESC, label");
-		
+
 		PermissionQueryBuilder permQuery = new PermissionQueryBuilder(permissions);
 		sql.addWhere("1 " + permQuery.toString());
 
@@ -40,7 +39,10 @@ public class ChartTradeCount extends ChartSSAction {
 		for (DataRow row : data) {
 			if (count < 20) {
 				Set set = new Set(row);
-				// set.setLabel(getText("AuditQuestion." + row.getLabel() + ".name"));
+				set.setLabel(getText("Trade." + row.getLabel() + ".name"));
+				// The number between the results returned differ on ContractorList
+				// due to similarity checking on ContractorList that happens invisibly
+				// set.setLink("ContractorList.action?filter.trade="+row.getLabel());
 				chart.getSets().add(set);
 			}
 			count++;
