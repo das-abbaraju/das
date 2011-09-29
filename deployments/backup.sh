f5de97e55e2e5f2047c69e5e1f3af7a6a7fcae6c
Lani Aung <uaung@picsauditing.com>
2011-09-29 23:00:44 +0000

    Updating backup.sh

--- deployments/backup.sh
+++ deployments/backup.sh
@@ -188,6 +188,9 @@ load_config_to_live () {
 	echo "clear cache"
 	/usr/bin/mysql $db_live -e "UPDATE app_properties SET value = '1' WHERE property = 'PICS.clear_cache'"
 	
+	echo "updating configupdatedate on live"
+	/usr/bin/mysql $db_live -e "UPDATE app_properties SET value = NOW() WHERE property = 'ConfigUpdateDate'"
+	
 	echo "Rezip the config data"
 	/bin/gzip -f $dumpfile
 }
