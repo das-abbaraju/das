e054e133f43eb2ef073605872f5fe6bac1b7a702
Tom Baker <tbaker@picsauditing.com>
2011-10-10 21:33:27 +0000

    PICS-3558: Making Force Flag Subscription return exactly the same results as Force Flag Report.

--- src/com/picsauditing/mail/subscription/ForcedFlagsSubscription.java
+++ src/com/picsauditing/mail/subscription/ForcedFlagsSubscription.java
@@ -2,16 +2,13 @@ package com.picsauditing.mail.subscription;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.apache.commons.beanutils.DynaBean;
 
 import com.picsauditing.PICS.I18nCache;
 import com.picsauditing.jpa.entities.EmailSubscription;
-import com.picsauditing.jpa.entities.Facility;
 import com.picsauditing.jpa.entities.OperatorAccount;
 import com.picsauditing.search.SelectAccount;
 import com.picsauditing.search.SelectSQL;
@@ -24,19 +21,12 @@ public class ForcedFlagsSubscription extends SqlSubscriptionBuilder {
 		try {
 			OperatorAccount o = (OperatorAccount) subscription.getUser().getAccount();
 
-			Set<Integer> operators = new HashSet<Integer>();
-			if (!o.isCorporate())
-				operators.add(o.getId());
-			// Adding child facilities and switch tos
-			for (Facility f : o.getOperatorFacilities())
-				operators.add(f.getOperator().getId());
-
 			SelectSQL sql = new SelectAccount();
 			String forceFlagsJoin = "JOIN (SELECT conid,opid,forcebegin,forceend,forcedby,forceflag,label FROM"
 					+ " (SELECT gc.subid as conid, gc.genid as opid, gc.forcebegin, gc.forceend, gc.forcedBy, gc.forceflag, 'FlagCriteria.Overall' AS label FROM generalcontractors gc"
 					+ " WHERE gc.forceFlag IS NOT NULL"
 					+ " UNION"
-					+ " SELECT fdo.conid, fdo.opid, fdo.updateDate as forcebegin, fdo.forceend, fdo.updatedBy as forcedby, fdo.forceflag, CONCAT('FlagCriteria.',fc1.id,'.label') AS label FROM flag_data_override fdo"
+					+ " SELECT fdo.conid, fdo.opid, fdo.updateDate as forcebegin, fdo.forceend, fdo.updatedBy as forcedby, fdo.forceflag, CONCAT('FlagCriteria.', fc1.id, '.label') as label FROM flag_data_override fdo"
 					+ " JOIN flag_criteria fc1 ON fdo.criteriaID = fc1.id" + " WHERE fdo.forceFlag IS NOT NULL) t"
 					+ " ) ff ON a.id = ff.conid";
 
@@ -45,15 +35,15 @@ public class ForcedFlagsSubscription extends SqlSubscriptionBuilder {
 			sql.addJoin("LEFT JOIN users u ON u.id = ff.forcedBy");
 			sql.addJoin("LEFT JOIN accounts fa ON fa.id = u.accountID");
 			sql.addJoin("LEFT JOIN generalcontractors gc ON gc.genid = o.id and gc.subid = ff.conid");
-			sql.addWhere("a.type = 'Contractor'");
 			sql.addWhere("a.status = 'Active'");
 			sql.addWhere("o.status = 'Active'");
 			sql.addWhere("ff.forceFlag IS NOT NULL");
-			if (o.isCorporate()) {
+			if (o.isOperatorCorporate()) {
 				String opIds = " ff.opid = " + o.getId() + " OR ";
-
-				opIds += " ff.opid IN (SELECT corporateID from facilities where opID = " + o.getId() + ")";
-
+				if (o.isOperator())
+					opIds += " ff.opid IN (SELECT corporateID from facilities where opID = " + o.getId() + ")";
+				else
+					opIds += " ff.opid IN (SELECT opID from facilities where corporateID = " + o.getId() + ")";
 				sql.addWhere(opIds);
 			}
 			sql.addField("o.name AS opName");
@@ -63,10 +53,10 @@ public class ForcedFlagsSubscription extends SqlSubscriptionBuilder {
 			sql.addField("label AS fLabel");
 			sql.addField("ff.forceend");
 			sql.addField("ff.forceBegin");
-			sql.addField("u.id AS forcedById");
+			sql.addField("u.id as forcedById");
 			sql.addField("u.name AS forcedBy");
 			sql.addField("fa.name AS forcedByAccount");
-			sql.addOrderBy("o.name, a.name");
+			sql.addField("gc.workStatus");
 
 			report.setLimit(100);
 			report.setSql(sql);
