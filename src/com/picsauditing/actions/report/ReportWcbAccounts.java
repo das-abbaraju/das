b526cd3514c9b6dde49cb406b1d0f865afa25e0f
Lucas Kam <lkam@picsauditing.com>
2011-10-20 20:51:00 +0000

    Updated question ids for WCB report

--- src/com/picsauditing/actions/report/ReportWcbAccounts.java
+++ src/com/picsauditing/actions/report/ReportWcbAccounts.java
@@ -41,9 +41,9 @@ public class ReportWcbAccounts extends ReportAccount {
 		sql.addJoin("JOIN contractor_audit_operator cao ON cao.auditID = ca.id");
 		sql.addJoin("JOIN pqfData p ON cao.auditID = p.auditID");
 		sql.addWhere("ca.auditTypeID in(143,144,145,146,147,148,166,167,168,169,170,261)");
-		sql.addWhere("cao.status IN('Complete','Pending','Submitted')");
+		sql.addWhere("cao.status IN('Complete','Pending','Submitted','Approved')");
 		sql.addWhere("cao.visible = 1");
-		sql.addWhere("p.questionID IN(8129, 8063, 8070, 8077, 8089, 8142, 8147, 1818, 1819, 1820, 1816, 1868)");
+		sql.addWhere("p.questionID IN(8077,8129,8063,8070,8175,8168,8154,8161,8147,8183,8142,8089)");
 
 		orderByDefault = "WCBAccountNumber, a.name";
 		filteredDefault = true;
