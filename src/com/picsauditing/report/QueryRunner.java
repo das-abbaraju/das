68070d1e2f154cce3fdd806d36a1f88c7134e073
Trevor Allred <tallred@picsauditing.com>
2011-12-16 16:38:17 +0000

    lots of refactoring... functionality is basically the same

--- src/com/picsauditing/report/QueryRunner.java
+++ src/com/picsauditing/report/QueryRunner.java
@@ -2,8 +2,10 @@ package com.picsauditing.report;
 
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.beanutils.BasicDynaBean;
 import org.apache.commons.lang.StringUtils;
@@ -12,92 +14,21 @@ import org.jboss.util.Strings;
 import com.picsauditing.access.Permissions;
 import com.picsauditing.search.Database;
 import com.picsauditing.search.SelectSQL;
+import com.picsauditing.util.PermissionQueryBuilder;
 
 public class QueryRunner {
 	private SelectSQL sql = new SelectSQL();
 	private Map<String, QueryField> availableFields = new HashMap<String, QueryField>();
-	// private Permissions permissions;
+	private Map<String, String> joins = new HashMap<String, String>();
+	private Permissions permissions;
 	private String defaultSort = null;
 	private int allRows = 0;
 	private List<String> columns;
 
 	public QueryRunner(QueryBase base, Permissions permissions) {
-		// this.permissions = permissions;
+		this.permissions = permissions;
 
 		buildBase(base);
-	}
-
-	public SelectSQL buildQuery(QueryCommand command) {
-		columns = command.getColumns();
-		if (columns.size() == 0) {
-			columns.addAll(availableFields.keySet());
-		}
-
-		if (command.getPage() > 1)
-			sql.setStartRow((command.getPage() - 1) * command.getRowsPerPage());
-		sql.setLimit(command.getRowsPerPage());
-
-		if (command.getGroupBy().size() > 0) {
-			addQueryField("total", "count(*)");
-			columns.add("total");
-			for (SortableField field : command.getGroupBy()) {
-				String groupBy = field.field;
-				if (!columns.contains(field.field))
-					groupBy = availableFields.get(field.field).sql;
-				if (!field.ascending)
-					groupBy += " DESC";
-				sql.addGroupBy(groupBy);
-			}
-		}
-
-		if (command.getOrderBy().size() == 0) {
-			sql.addOrderBy(defaultSort);
-		} else {
-			for (SortableField field : command.getOrderBy()) {
-				String orderBy = field.field;
-				if (!columns.contains(field.field))
-					orderBy = availableFields.get(field.field).sql;
-				if (!field.ascending)
-					orderBy += " DESC";
-				sql.addOrderBy(orderBy);
-			}
-		}
-
-		for (String column : columns) {
-			if (availableFields.keySet().contains(column)) {
-				String field = availableFields.get(column).sql;
-				sql.addField(field + " AS " + column);
-
-				addLeftJoins(column);
-			}
-		}
-
-		if (command.getFilters().size() > 0) {
-			String where = command.getFilterExpression();
-			if (where == null || Strings.isEmpty(where)) {
-				for (int i = 0; i > command.getFilters().size(); i++) {
-					where = i + " AND ";
-				}
-				where = StringUtils.removeEnd(where, " AND ");
-			}
-
-			for (int i = command.getFilters().size() - 1; i >= 0; i--) {
-				QueryFilter queryFilter = command.getFilters().get(i);
-				String filterExp = queryFilter.toExpression(availableFields);
-
-				if (queryFilter.getOperator().equals(QueryFilterOperator.InReport))
-					// TODO: query the report sql and put down an inner query.
-					where = where.replace(i + "", filterExp);
-				else
-					where = where.replace(i + "", filterExp);
-			}
-			sql.addWhere(where);
-		}
-
-		// We may need to move this to a class field
-		sql.setSQL_CALC_FOUND_ROWS(true);
-
-		return sql;
 	}
 
 	public QueryData run() throws SQLException {
@@ -106,6 +37,104 @@ public class QueryRunner {
 		allRows = db.getAllRows();
 		QueryData data = new QueryData(columns, rows);
 		return data;
+	}
+
+	public SelectSQL buildQuery(QueryCommand command) {
+		columns = command.getColumns();
+		if (columns.size() == 0) {
+			columns.addAll(availableFields.keySet());
+		}
+
+		addLeftJoins();
+		addRuntimeFilters(command);
+
+		addGroupBy(command);
+		addOrderBy(command);
+		addLimits(command);
+
+		// We may need to move this to a class field
+		sql.setSQL_CALC_FOUND_ROWS(true);
+
+		return sql;
+	}
+
+	private void addRuntimeFilters(QueryCommand command) {
+		if (command.getFilters().size() == 0)
+			return;
+
+		String where = command.getFilterExpression();
+		if (where == null || Strings.isEmpty(where)) {
+			for (int i = 0; i > command.getFilters().size(); i++) {
+				where = i + " AND ";
+			}
+			where = StringUtils.removeEnd(where, " AND ");
+		}
+
+		for (int i = command.getFilters().size() - 1; i >= 0; i--) {
+			QueryFilter queryFilter = command.getFilters().get(i);
+			String filterExp = queryFilter.toExpression(availableFields);
+
+			if (queryFilter.getOperator().equals(QueryFilterOperator.InReport))
+				// TODO: query the report sql and put down an inner query.
+				where = where.replace(i + "", filterExp);
+			else
+				where = where.replace(i + "", filterExp);
+		}
+		sql.addWhere(where);
+	}
+
+	private void addLeftJoins() {
+		Set<String> addedJoins = new HashSet<String>();
+		for (String column : columns) {
+			if (availableFields.keySet().contains(column)) {
+				QueryField availableField = availableFields.get(column);
+				if (availableField.requiresJoin()) {
+					if (!addedJoins.contains(availableField.requireJoin)) {
+						sql.addJoin(this.joins.get(availableField.requireJoin));
+					}
+				}
+				String field = availableField.sql;
+				sql.addField(field + " AS " + column);
+			}
+		}
+	}
+
+	private void addOrderBy(QueryCommand command) {
+		if (command.getOrderBy().size() == 0) {
+			sql.addOrderBy(defaultSort);
+			return;
+		}
+
+		for (SortableField field : command.getOrderBy()) {
+			String orderBy = field.field;
+			if (!columns.contains(field.field))
+				orderBy = availableFields.get(field.field).sql;
+			if (!field.ascending)
+				orderBy += " DESC";
+			sql.addOrderBy(orderBy);
+		}
+	}
+
+	private void addGroupBy(QueryCommand command) {
+		if (command.getGroupBy().size() == 0)
+			return;
+
+		addQueryField("total", "count(*)");
+		columns.add("total");
+		for (SortableField field : command.getGroupBy()) {
+			String groupBy = field.field;
+			if (!columns.contains(field.field))
+				groupBy = availableFields.get(field.field).sql;
+			if (!field.ascending)
+				groupBy += " DESC";
+			sql.addGroupBy(groupBy);
+		}
+	}
+
+	private void addLimits(QueryCommand command) {
+		if (command.getPage() > 1)
+			sql.setStartRow((command.getPage() - 1) * command.getRowsPerPage());
+		sql.setLimit(command.getRowsPerPage());
 	}
 
 	private void buildBase(QueryBase base) {
@@ -138,25 +167,6 @@ public class QueryRunner {
 		}
 	}
 
-	private void addLeftJoins(String column) {
-		if (StringUtils.endsWithIgnoreCase(column, "UserID")) {
-			if (StringUtils.startsWithIgnoreCase(column, "accountContact"))
-				sql.addJoin("LEFT JOIN users accountContact ON accountContact.id = a.contactID");
-			else if (StringUtils.startsWithIgnoreCase(column, "customerService"))
-				sql.addJoin("LEFT JOIN users customerService ON customerService.id = c.welcomeAuditor_id");
-			else if (StringUtils.startsWithIgnoreCase(column, "auditor"))
-				sql.addJoin("LEFT JOIN users auditor ON auditor.id = ca.auditorID");
-			else if (StringUtils.startsWithIgnoreCase(column, "closingAuditor"))
-				sql.addJoin("LEFT JOIN users closingAuditor ON closingAuditor.id = ca.closingAuditorID");
-			else if (StringUtils.startsWithIgnoreCase(column, "requestedByOperator"))
-				sql.addJoin("LEFT JOIN users u ON u.id = crr.requestedByUserID");
-			else if (StringUtils.startsWithIgnoreCase(column, "requestedContactedBy"))
-				sql.addJoin("LEFT JOIN users uc ON uc.id = crr.lastContactedBy");
-		} else if (StringUtils.endsWithIgnoreCase(column, "AccountID"))
-			if (StringUtils.startsWithIgnoreCase(column, "requestedExisting"))
-				sql.addJoin("LEFT JOIN accounts requestedExisting ON requestedExisting.id = crr.conID");
-	}
-
 	private QueryField addQueryField(String dataIndex, String sql) {
 		QueryField field = new QueryField(dataIndex, sql);
 		availableFields.put(dataIndex, field);
@@ -182,11 +192,10 @@ public class QueryRunner {
 		addQueryField("accountDBAName", "a.dbaName");
 		addQueryField("accountReason", "a.reason");
 
-		addQueryField("accountContactUserID", "accountContact.id");
-		addQueryField("accountContactUserAccountID", "accountContact.id");
-		addQueryField("accountContactUserName", "accountContact.name");
-		addQueryField("accountContactUserPhone", "accountContact.phone");
-		addQueryField("accountContactUserEmail", "accountContact.email");
+		joinToUser("accountContact", "a.contactID");
+
+		PermissionQueryBuilder permQuery = new PermissionQueryBuilder(permissions);
+		sql.addWhere("1 " + permQuery.toString());
 
 		defaultSort = "a.nameIndex";
 	}
@@ -213,9 +222,7 @@ public class QueryRunner {
 		addQueryField("contractorPaymentExpires", "c.paymentExpires");
 		addQueryField("contractorCreditCardOnFile", "c.ccOnFile");
 
-		addQueryField("customerServiceUserID", "customerService.id");
-		addQueryField("customerServiceUserAccountID", "customerService.accountID");
-		addQueryField("customerServiceUserName", "customerService.name");
+		joinToUser("customerService", "c.welcomeAuditor_id");
 	}
 
 	private void buildContractorAuditBase() {
@@ -233,26 +240,21 @@ public class QueryRunner {
 		auditTypeName.translate("AuditType", "name");
 		addQueryField("auditCreationDate", "ca.creationDate").type(FieldType.Date);
 		addQueryField("auditExpirationDate", "ca.expiresDate").type(FieldType.Date);
-		addQueryField("auditScheduledDate", "ca.expiresDate").type(FieldType.Date);
-		addQueryField("auditAssignedDate", "ca.expiresDate").type(FieldType.Date);
+		addQueryField("auditScheduledDate", "ca.scheduledDate").type(FieldType.Date);
+		addQueryField("auditAssignedDate", "ca.assignedDate").type(FieldType.Date);
 		addQueryField("auditLocation", "ca.auditLocation");
 		addQueryField("auditFor", "ca.auditFor");
 		addQueryField("auditScore", "ca.score");
-		addQueryField("auditContractorConfirmation", "ca.score");
-		addQueryField("auditAuditorConfirmation", "ca.score");
+		addQueryField("auditContractorConfirm", "ca.contractorConfirm").type(FieldType.Date);
+		addQueryField("auditAuditorConfirm", "ca.auditorConfirm").type(FieldType.Date);
 
 		addQueryField("auditTypeIsScheduled", "atype.isScheduled");
 		addQueryField("auditTypeHasAuditor", "atype.hasAuditor");
 		addQueryField("auditTypeScorable", "atype.scoreable");
 
-		addQueryField("auditorUserID", "auditor.id");
-		addQueryField("auditorUserAccountID", "auditor.accountID");
-		addQueryField("auditorUserName", "auditor.name");
+		joinToUser("auditor", "ca.auditorID");
+		joinToUser("closingAuditor", "ca.closingAuditorID");
 
-		addQueryField("closingAuditorUserID", "closingAuditor.id");
-		addQueryField("closingAuditorUserAccountID", "closingAuditor.accountID");
-		addQueryField("closingAuditorUserName", "closingAuditor.name");
-		
 		defaultSort = "ca.creationDate DESC";
 	}
 
@@ -260,12 +262,11 @@ public class QueryRunner {
 		buildContractorAuditBase();
 
 		sql.addJoin("JOIN contractor_audit_operator cao ON cao.auditID = ca.id");
-		sql.addJoin("JOIN accounts caoAccount ON cao.opID = caoAccount.id");
-
 		addQueryField("contractorAuditOperatorID", "cao.id");
 		addQueryField("contractorAuditOperatorStatus", "cao.status");
 		addQueryField("contractorAuditOperatorStatusChangedDate", "cao.statusChangedDate").type(FieldType.Date);
 
+		sql.addJoin("JOIN accounts caoAccount ON cao.opID = caoAccount.id");
 		addQueryField("contractorAuditOperatorAccountID", "caoAccount.id");
 		addQueryField("contractorAuditOperatorAccountName", "caoAccount.name");
 
@@ -276,14 +277,13 @@ public class QueryRunner {
 		buildContractorAuditOperatorBase();
 
 		sql.addJoin("JOIN contractor_audit_operator_workflow cao ON cao.id = caow.caoID");
-
 		addQueryField("contractorAuditOperatorWorkflowStatus", "caow.status");
+
 	}
 
 	private void buildRegistrationRequestsBase() {
 		sql = new SelectSQL();
 		sql.setFromTable("contractor_registration_request crr");
-		sql.addJoin("JOIN accounts op ON op.id = crr.requestedByID");
 
 		addQueryField("requestID", "crr.id");
 		addQueryField("requestedName", "crr.name");
@@ -297,30 +297,39 @@ public class QueryRunner {
 		addQueryField("requestedZip", "crr.zip");
 		addQueryField("requestedCountry", "crr.country");
 		addQueryField("requestedNotes", "crr.notes");
-		addQueryField("requestedByOperatorID", "op.id");
-		addQueryField("requestedByOperatorName", "op.name");
 		addQueryField("requestedDeadline", "crr.deadline");
 		addQueryField("requestedLastContactedByDate", "crr.lastContactDate").type(FieldType.Date);
 		addQueryField("requestedContactCount", "crr.contactCount");
 		addQueryField("requestedMatchCount", "crr.matchCount");
 		addQueryField("requestCreationDate", "crr.creationDate").type(FieldType.Date);
 
-		addQueryField("requestedByOperatorUserID", "u.id");
-		addQueryField("requestedByOperatorUserAccountID", "u.accountID");
-		addQueryField("requestedByOperatorUserName", "u.name");
-		addQueryField("requestedByOperatorUserOther", "crr.requestedByUser");
+		sql.addJoin("JOIN accounts op ON op.id = crr.requestedByID");
+		addQueryField("requestedByOperatorID", "op.id");
+		addQueryField("requestedByOperatorName", "op.name");
 
-		addQueryField("requestedContactedByUserID", "uc.id");
-		addQueryField("requestedContactedByUserAccountID", "uc.accountID");
-		addQueryField("requestedContactedByUserName", "uc.name");
+		joinToUser("requestedBy", "crr.requestedByUserID");
+		joinToUser("contactedBy", "crr.lastContactedBy");
 
-		addQueryField("requestedExistingAccountID", "requestedExisting.id");
-		addQueryField("requestedExistingAccountName", "requestedExisting.name");
+		joinToAccount("requestedExisting", "crr.conID");
+	}
+
+	private void joinToAccount(String joinAlias, String foreignKey) {
+		joins.put(joinAlias, "LEFT JOIN accounts " + joinAlias + " ON " + joinAlias + ".id = " + foreignKey);
+		addQueryField(joinAlias + "ID", joinAlias + ".id").requireJoin(joinAlias);
+		addQueryField(joinAlias + "Name", joinAlias + ".name").requireJoin(joinAlias);
+	}
+
+	private void joinToUser(String joinAlias, String foreignKey) {
+		joins.put(joinAlias, "LEFT JOIN users " + joinAlias + " ON " + joinAlias + ".id = " + foreignKey);
+		addQueryField(joinAlias + "ID", joinAlias + ".id").requireJoin(joinAlias);
+		addQueryField(joinAlias + "AccountID", joinAlias + ".accountID").requireJoin(joinAlias);
+		addQueryField(joinAlias + "Name", joinAlias + ".name").requireJoin(joinAlias);
+		addQueryField(joinAlias + "Phone", joinAlias + ".phone").requireJoin(joinAlias);
+		addQueryField(joinAlias + "Email", joinAlias + ".email").requireJoin(joinAlias);
 	}
 
 	private void buildInvoiceBase() {
 		buildContractorBase();
-
 		sql.addJoin("JOIN invoice i on i.accountID = c.id");
 	}
 
