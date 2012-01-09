1046311304428ebda8a010765e5242c2e94b6435
Carey Hinoki <chinoki@picsauditing.com>
2012-01-09 19:44:10 +0000

    PICS-4157
    translation key upgrade

--- deployments/upgradeConfig.sql
+++ deployments/upgradeConfig.sql
@@ -8,3 +8,7 @@
 
 -- PICS-3967
 update audit_type set classType='Employee' where classType='IM' or id in (29, 99, 100);
+
+
+-- PICS-4157 
+insert into `app_translation` (`id`, `msgKey`, `locale`, `msgValue`, `createdBy`, `updatedBy`, `creationDate`, `updateDate`, `lastUsed`) values('139775','RegistrationMakePayment.Processing','en','Processing Payment - Please wait.','941','941','2012-01-09 11:33:35','2012-01-09 11:33:35',NULL);
