1281143e63900cfdb3f41bb1d7a45049ec28d0fd
Craig Jones <cjones@picsauditing.com>
2012-02-14 01:43:38 +0000

    Converted to JUnit4 style. Added variance factors to floating point tests.

--- tests/com/picsauditing/util/GeoTest.java
+++ tests/com/picsauditing/util/GeoTest.java
@@ -1,15 +1,20 @@
 package com.picsauditing.util;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
 
-public class GeoTest extends TestCase {
+import org.junit.Test;
 
+
+
+public class GeoTest  {
+
+	@Test
 	public void testDistance() {
-		assertEquals(0.0, Geo.distance(0, 0, 0, 0));
-		assertEquals(1568.5205567985759, Geo.distance(10, 10, 0, 0));
-		assertEquals(1568.5205567985759, Geo.distance(-10, -10, 0, 0));
-		assertEquals(1568.5205567985759, Geo.distance(-10, 10, 0, 0));
-		assertEquals(1568.5205567985759, Geo.distance(10, -10, 0, 0));
-		assertEquals(3137.041113597152, Geo.distance(10, 10, -10, -10));
+		assertEquals(0.0, Geo.distance(0, 0, 0, 0), 0.01);
+		assertEquals(1568.5205567985759, Geo.distance(10, 10, 0, 0), 0.01);
+		assertEquals(1568.5205567985759, Geo.distance(-10, -10, 0, 0), 0.01);
+		assertEquals(1568.5205567985759, Geo.distance(-10, 10, 0, 0), 0.01);
+		assertEquals(1568.5205567985759, Geo.distance(10, -10, 0, 0), 0.01);
+		assertEquals(3137.041113597152, Geo.distance(10, 10, -10, -10), 0.01);
 	}
 }
