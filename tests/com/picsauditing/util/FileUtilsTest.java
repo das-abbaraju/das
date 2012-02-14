a483047f435a8ea128efdf367dcb6e3c0dbe9972
Craig Jones <cjones@picsauditing.com>
2012-02-14 01:41:55 +0000

    Converted to JUnit4 style.

--- tests/com/picsauditing/util/FileUtilsTest.java
+++ tests/com/picsauditing/util/FileUtilsTest.java
@@ -1,42 +1,41 @@
 package com.picsauditing.util;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
-public class FileUtilsTest extends TestCase {
+public class FileUtilsTest {
+	@Test
 	public void testGetSimilarFiles() {
-		try {
-			File folder = new File("tests");
-			File[] files = FileUtils.getSimilarFiles(folder, "test_notes");
-			assertTrue(files.length == 2);
-		} catch (Exception e) {
-			fail(e.getMessage());
-		}
+		File folder = new File("tests");
+		File[] files = FileUtils.getSimilarFiles(folder, "test_notes");
+		assertTrue(files.length == 2);
 	}
-	
+
+	@Test
 	public void testGetFtpDir() {
-		try {
-			File file = FileUtils.ensurePathExists("WebContent/struts/trevor/test/trevor.jsp");
-			System.out.println(file.getAbsolutePath());
-			file = new File("WebContent/struts/trevor/test/trevor.jsp");
-			assertTrue("Failed to delete trevor.jsp", file.delete());
-			file = new File("WebContent/struts/trevor/test");
-			assertTrue("Failed to delete test", file.delete());
-			file = new File("WebContent/struts/trevor");
-			assertTrue("Failed to delete trevor", file.delete());
-		} catch (Exception e) {
-			fail(e.getMessage());
-		}
+		File file = FileUtils.ensurePathExists("WebContent/struts/trevor/test/trevor.jsp");
+		System.out.println(file.getAbsolutePath());
+		file = new File("WebContent/struts/trevor/test/trevor.jsp");
+		assertTrue("Failed to delete trevor.jsp", file.delete());
+		file = new File("WebContent/struts/trevor/test");
+		assertTrue("Failed to delete test", file.delete());
+		file = new File("WebContent/struts/trevor");
+		assertTrue("Failed to delete trevor", file.delete());
 	}
-	
+
+	@Test
 	public void testSize() {
 		assertEquals("1 Bytes", FileUtils.size(1));
 		assertEquals("1.2 KB", FileUtils.size(1234));
 		assertEquals("1.2 MB", FileUtils.size(1234000));
 		assertEquals("1.2 GB", FileUtils.size(1234000000));
 	}
-	
+
+	@Test
 	public void testGetExtension() {
 		assertEquals(FileUtils.getExtension("filename.gif"), "gif");
 		assertEquals(FileUtils.getExtension("filename.jpeg"), "jpeg");
