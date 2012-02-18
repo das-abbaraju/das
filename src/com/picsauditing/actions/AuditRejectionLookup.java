6522a16c8129f43d2ca0402ae720fabf0ac3840c
Craig Jones <cjones@picsauditing.com>
2012-02-18 01:33:52 +0000

    PICS-3564 (Paired with Alex) - Changed to use SelectSQL - Comments - Changed to use DBBean to get connection.

--- src/com/picsauditing/actions/AuditRejectionLookup.java
+++ src/com/picsauditing/actions/AuditRejectionLookup.java
@@ -1,32 +1,43 @@
 package com.picsauditing.actions;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.util.List;
 
 import org.apache.commons.beanutils.BasicDynaBean;
 import org.json.simple.JSONObject;
 
 import com.google.common.base.Strings;
+import com.picsauditing.PICS.DBBean;
 import com.picsauditing.search.Database;
+import com.picsauditing.search.SelectSQL;
 
 @SuppressWarnings("serial")
 public class AuditRejectionLookup extends PicsActionSupport { 
 	
 	@Override
 	public String execute() throws Exception {
-		Database database = new Database();
+		// FIXME: restrict this query to the user's preferred language
+		SelectSQL selectSQL = new SelectSQL("app_translation");
+		selectSQL.addField("msgKey");
+		selectSQL.addField("msgValue");
+		selectSQL.addWhere("msgKey like 'Insurance.Rejection.Reason.Code.%'");
+		
+		Connection conn =  DBBean.getDBConnection();
 		// TODO: move this out of the controller and into the DAO Layer without using Dyna Beans
-		List<BasicDynaBean> results = database.select("Select msgKey, msgValue from app_translation where msgKey like 'Insurance.Rejection.Reason.Code.%'", false);
+		ResultSet results = conn.createStatement().executeQuery(selectSQL.toString());
+		
 		populateJsonArray(results);
 		
 		return SUCCESS;
 	}
 	
 	@SuppressWarnings("unchecked")
-	private void populateJsonArray(List<BasicDynaBean> results) {
-		for (BasicDynaBean value : results) {
+	private void populateJsonArray(ResultSet results) throws Exception {
+		while (results.next()) {
 			JSONObject jsonObject = new JSONObject();
-			jsonObject.put("id", parseCode((String) value.get("msgKey")));
-			jsonObject.put("value", value.get("msgValue"));
+			jsonObject.put("id", parseCode(results.getString(0)));
+			jsonObject.put("value", results.getString(1));
 			jsonArray.add(jsonObject);
 		}
 	}
