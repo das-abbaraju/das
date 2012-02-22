06153535f57474327f1eb9043aa5bed7ed1fb6d9
Alex Ananighian <aananighian@picsauditing.com>
2012-02-22 18:40:28 +0000

    Changes from code review

--- src/com/picsauditing/actions/AuditRejectionLookup.java
+++ src/com/picsauditing/actions/AuditRejectionLookup.java
@@ -2,34 +2,36 @@ package com.picsauditing.actions;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.util.List;
 
-import org.apache.commons.beanutils.BasicDynaBean;
 import org.json.simple.JSONObject;
 
 import com.google.common.base.Strings;
 import com.picsauditing.PICS.DBBean;
-import com.picsauditing.search.Database;
 import com.picsauditing.search.SelectSQL;
 
 @SuppressWarnings("serial")
 public class AuditRejectionLookup extends PicsActionSupport { 
 	
+	private static final String TRANSLATION_KEY_FOR_REJECTION_CODE = "Insurance.Rejection.Reason.Code.%";
+	
 	@Override
 	public String execute() throws Exception {
-		// FIXME: restrict this query to the user's preferred language
 		SelectSQL selectSQL = new SelectSQL("app_translation");
 		selectSQL.addField("msgKey");
 		selectSQL.addField("msgValue");
-		selectSQL.addWhere("msgKey like 'Insurance.Rejection.Reason.Code.%'");
+		selectSQL.addWhere(buildWhereClause());
 		
 		Connection conn =  DBBean.getDBConnection();
-		// TODO: move this out of the controller and into the DAO Layer without using Dyna Beans
 		ResultSet results = conn.createStatement().executeQuery(selectSQL.toString());
 		
 		populateJsonArray(results);
 		
 		return SUCCESS;
+	}
+	
+	private String buildWhereClause() {
+		return ("locale = '" + getLocaleStatic().getLanguage() 
+				+  "' AND msgKey LIKE '" + TRANSLATION_KEY_FOR_REJECTION_CODE + "'");
 	}
 	
 	@SuppressWarnings("unchecked")
