1dc030f3e90af4d7fe964ee93fe8286016bec181
Craig Jones <cjones@picsauditing.com>
2012-02-23 22:17:04 +0000

    Added support for the unit-testing (integration testing) of Action classes - Updated POM to specify newer versions of the struts library (2.3.1.2) and to include the Struts/Spring/Junit plugin - Updated POM to downgrade the version of JUnit from 4.8.2 to 4.4 (as required by the 2.3.1.2 version of the Struts/Spring/Junit plugin) - Changed the DBBean class to allow the datasource to be injectable by Spring (if not injected, then it still does a JNDI lookup, as before) - Fixed up tests.xml to be the equivalent of applicationContext.xml for the unit tests.xml

--- src/com/picsauditing/PICS/DBBean.java
+++ src/com/picsauditing/PICS/DBBean.java
@@ -1,30 +1,57 @@
 package com.picsauditing.PICS;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
-public class DBBean {
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class DBBean implements InitializingBean  {
+	private static DataSource staticDataSource;
+	private DataSource dataSource;
+	
+	public DataSource getDataSource() {
+		return dataSource;
+	}
+
+	@Autowired
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
 	public static Connection getDBConnection() throws SQLException {
-		DBBean dbBean = new DBBean();
+		if (staticDataSource != null) {
+			return staticDataSource.getConnection();
+		}
 		try {
-			DataSource ds = dbBean.getJdbcPics();
-			return ds.getConnection();
+			DataSource dataSource = getJdbcPics();
+			return dataSource.getConnection();
 		} catch (NamingException ne) {
 			ne.printStackTrace();
 			return null;
 		}
 	}
 
-	private com.picsauditing.PICS.PICSDBLocator serviceLocator;
 
-	private com.picsauditing.PICS.PICSDBLocator getServiceLocator() {
+	private static com.picsauditing.PICS.PICSDBLocator serviceLocator;
+
+	private static com.picsauditing.PICS.PICSDBLocator getServiceLocator() {
 		if (serviceLocator == null)
 			serviceLocator = new com.picsauditing.PICS.PICSDBLocator();
 		return serviceLocator;
 	}
 
-	private DataSource getJdbcPics() throws NamingException {
+	private static DataSource getJdbcPics() throws NamingException {
 		return (DataSource) getServiceLocator().getDataSource("java:comp/env/jdbc/pics");
 	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		assert dataSource != null;
+		staticDataSource = dataSource;
+	}
+
 }
