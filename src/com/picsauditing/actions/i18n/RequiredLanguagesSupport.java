9f506a370510f682a92dcad187676176af16838e
Lani Aung <uaung@picsauditing.com>
2012-02-23 20:29:38 +0000

    PICS-4717 -- Add more languages to PICS Organizer. Added to required languages as well.

--- src/com/picsauditing/actions/i18n/RequiredLanguagesSupport.java
+++ src/com/picsauditing/actions/i18n/RequiredLanguagesSupport.java
@@ -9,42 +9,42 @@ import com.picsauditing.jpa.entities.BaseTableRequiringLanguages;
 
 @SuppressWarnings("serial")
 public abstract class RequiredLanguagesSupport extends PicsActionSupport {
-	public static String[] DEFAULT_LOCALES = new String[] { "en", "fr", "es" };
+	public static String[] DEFAULT_LOCALES = new String[] { "en", "fr", "es", "de", "nl", "fi", "sv", "zh", "pt" };
 	protected List<Locale> availableLocales;
 	protected List<Locale> selectedLocales;
-	
+
 	private static List<Locale> getDefaultLocales() {
 		List<Locale> locales = new ArrayList<Locale>();
-		
+
 		for (String locale : DEFAULT_LOCALES) {
 			locales.add(new Locale(locale));
 		}
-		
+
 		return locales;
 	}
-	
+
 	public List<Locale> getAvailableLocales() {
 		if (availableLocales == null) {
 			availableLocales = getDefaultLocales();
 			availableLocales.removeAll(getSelectedLocales());
 		}
-		
+
 		return availableLocales;
 	}
 
 	public List<Locale> getSelectedLocales() {
 		if (selectedLocales == null) {
 			selectedLocales = new ArrayList<Locale>();
-			
+
 			fillSelectedLocales();
 		}
-		
+
 		return selectedLocales;
 	}
-	
+
 	public List<Locale> getAvailableLocalesFrom(BaseTableRequiringLanguages entity) {
 		availableLocales = new ArrayList<Locale>();
-		
+
 		for (String language : entity.getLanguages()) {
 			availableLocales.add(new Locale(language));
 		}
@@ -52,6 +52,6 @@ public abstract class RequiredLanguagesSupport extends PicsActionSupport {
 
 		return availableLocales;
 	}
-	
+
 	abstract protected void fillSelectedLocales();
 }
