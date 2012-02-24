30e7aa798c9689a7154c03bc31feb62debc6bf82
Kirk Chase <kchase@picsauditing.com>
2012-02-24 22:25:38 +0000

    PICS-4349 Open Requirements Documents don't copy over when copying audits

--- src/com/picsauditing/actions/audits/ContractorAuditCopy.java
+++ src/com/picsauditing/actions/audits/ContractorAuditCopy.java
@@ -8,13 +8,16 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.picsauditing.PICS.PICSFileType;
 import com.picsauditing.access.OpPerms;
 import com.picsauditing.access.RequiredPermission;
+import com.picsauditing.dao.ContractorAuditFileDAO;
 import com.picsauditing.dao.ContractorAuditOperatorDAO;
 import com.picsauditing.dao.ContractorAuditOperatorWorkflowDAO;
 import com.picsauditing.jpa.entities.AuditData;
 import com.picsauditing.jpa.entities.ContractorAccount;
 import com.picsauditing.jpa.entities.ContractorAudit;
+import com.picsauditing.jpa.entities.ContractorAuditFile;
 import com.picsauditing.jpa.entities.ContractorAuditOperator;
 import com.picsauditing.jpa.entities.ContractorAuditOperatorPermission;
 import com.picsauditing.jpa.entities.ContractorAuditOperatorWorkflow;
@@ -35,18 +38,20 @@ public class ContractorAuditCopy extends AuditActionSupport {
 	private ContractorAuditOperatorWorkflowDAO caowDAO;
 	@Autowired
 	private ContractorAuditOperatorDAO caoDAO;
+	@Autowired
+	private ContractorAuditFileDAO contractorAuditFileDAO;
 
 	@RequiredPermission(value = OpPerms.AuditCopy)
 	public String execute() throws Exception {
 		this.findConAudit();
 		int oldconID = conAudit.getContractorAccount().getId();
 		if (button != null) {
-			ContractorAccount nConAccount = contractorAccountDao.findConID(contractorSelect);
-			if (nConAccount == null) {
+			ContractorAccount newConAudit = contractorAccountDao.findConID(contractorSelect);
+			if (newConAudit == null) {
 				addActionError("No Contractor Found");
 				return SUCCESS;
 			}
-			List<ContractorAudit> auditList = nConAccount.getAudits();
+			List<ContractorAudit> auditList = newConAudit.getAudits();
 			for (ContractorAudit existingAudit : auditList) {
 				if (existingAudit.getAuditType().equals(conAudit.getAuditType()) && !existingAudit.isExpired()
 						&& !existingAudit.getAuditType().isAnnualAddendum()) {
@@ -86,31 +91,11 @@ public class ContractorAuditCopy extends AuditActionSupport {
 			findConAudit();
 
 			Map<Integer, AuditData> preToPostAuditDataIdMapper = new HashMap<Integer, AuditData>();
-			auditDao.copy(conAudit, nConAccount, preToPostAuditDataIdMapper);
+			auditDao.copy(conAudit, newConAudit, preToPostAuditDataIdMapper);
 
-			ContractorAudit oldConAudit = auditDao.find(auditID);
-
-			for (AuditData auditData : oldConAudit.getData()) {
-
-				if (auditData.getQuestion().getQuestionType().equals("File")) {
-
-					AuditData newAnswer = preToPostAuditDataIdMapper.get(auditData.getId());
-
-					String newFileBase = "files/"
-							+ FileUtils.thousandize(preToPostAuditDataIdMapper.get(auditData.getId()).getId());
-					String newFileName = "data_" + auditData.getId();
-
-					String oldFileBase = "files/" + FileUtils.thousandize(auditData.getId());
-					String oldFileName = "data_" + auditData.getId() + "." + newAnswer.getAnswer();
-
-					File oldFile = new File(getFtpDir() + "/" + oldFileBase, oldFileName);
-					try {
-						FileUtils.copyFile(oldFile, getFtpDir(), newFileBase, newFileName, newAnswer.getAnswer(), true);
-					} catch (Exception couldntCopyTheFile) {
-						couldntCopyTheFile.printStackTrace();
-					}
-				}
-			}
+			copyAuditQuestionFiles(preToPostAuditDataIdMapper);
+			
+			copyAuditFiles();			
 
 			String notes = conAudit.getAuditType().getName().toString() + " Copied from Contractor " + oldconID;
 			addNote(conAudit.getContractorAccount(), notes, NoteCategory.Audits, getViewableByAccount(conAudit
@@ -120,6 +105,70 @@ public class ContractorAuditCopy extends AuditActionSupport {
 		}
 
 		return SUCCESS;
+	}
+
+	private void copyAuditQuestionFiles(Map<Integer, AuditData> preToPostAuditDataIdMapper) {
+		ContractorAudit oldConAudit = auditDao.find(auditID);
+
+		for (AuditData auditData : oldConAudit.getData()) {
+
+			if (auditData.getQuestion().getQuestionType().equals("File")) {
+
+				AuditData newAnswer = preToPostAuditDataIdMapper.get(auditData.getId());
+
+				String newFileBase = "files/"
+						+ FileUtils.thousandize(preToPostAuditDataIdMapper.get(auditData.getId()).getId());
+				String newFileName = "data_" + auditData.getId();
+
+				String oldFileBase = "files/" + FileUtils.thousandize(auditData.getId());
+				String oldFileName = "data_" + auditData.getId() + "." + newAnswer.getAnswer();
+
+				File oldFile = new File(getFtpDir() + "/" + oldFileBase, oldFileName);
+				try {
+					FileUtils.copyFile(oldFile, getFtpDir(), newFileBase, newFileName, newAnswer.getAnswer(), true);
+				} catch (Exception couldntCopyTheFile) {
+					couldntCopyTheFile.printStackTrace();
+				}
+			}
+		}
+	}
+
+	private void copyAuditFiles() {
+		List<ContractorAuditFile> auditFiles = contractorAuditFileDAO.findByAudit(auditID);
+		for (ContractorAuditFile caf : auditFiles) {
+			ContractorAuditFile contractorAuditFile = new ContractorAuditFile();
+
+			contractorAuditFile.setAudit(conAudit);
+			contractorAuditFile.setReviewed(caf.isReviewed());
+			contractorAuditFile.setDescription(caf.getDescription());
+			contractorAuditFile.setFileType(caf.getFileType());
+			contractorAuditFile.setAuditColumns(permissions);
+
+			contractorAuditFile = contractorAuditFileDAO.save(contractorAuditFile);
+
+			int fileId = contractorAuditFile.getId();
+
+			String newFileBase = "files/" + FileUtils.thousandize(fileId);
+			String newFileName = getFileName(fileId);
+
+			File[] files = getFiles(caf.getId());
+			if (files != null && files.length == 1) {
+				try {
+					FileUtils.copyFile(files[0], getFtpDir(), newFileBase, newFileName, caf.getFileType(), true);
+				} catch (Exception couldntCopyTheFile) {
+					couldntCopyTheFile.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	private File[] getFiles(int fileID) {
+		File dir = new File(getFtpDir() + "/files/" + FileUtils.thousandize(fileID));
+		return FileUtils.getSimilarFiles(dir, getFileName(fileID));
+	}
+
+	private String getFileName(int fileID) {
+		return PICSFileType.audit + "_" + fileID;
 	}
 
 	public String getContractorSelect() {
