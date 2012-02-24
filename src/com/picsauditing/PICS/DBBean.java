af349c9adafd7b5604f1dc6dc49c95786dca07e7
Craig Jones <cjones@picsauditing.com>
2012-02-24 08:04:50 +0000

    Temporarily reverted the upgrade of Struts.

--- src/com/picsauditing/PICS/DBBean.java
+++ src/com/picsauditing/PICS/DBBean.java
@@ -1,57 +1,30 @@
 package com.picsauditing.PICS;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
+import java.sql.*;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
-
-public class DBBean implements InitializingBean  {
-	private static DataSource staticDataSource;
-	private DataSource dataSource;
-	
-	public DataSource getDataSource() {
-		return dataSource;
-	}
-
-	@Autowired
-	public void setDataSource(DataSource dataSource) {
-		this.dataSource = dataSource;
-	}
-
+public class DBBean {
 	public static Connection getDBConnection() throws SQLException {
-		if (staticDataSource != null) {
-			return staticDataSource.getConnection();
-		}
+		DBBean dbBean = new DBBean();
 		try {
-			DataSource dataSource = getJdbcPics();
-			return dataSource.getConnection();
+			DataSource ds = dbBean.getJdbcPics();
+			return ds.getConnection();
 		} catch (NamingException ne) {
 			ne.printStackTrace();
 			return null;
 		}
 	}
 
+	private com.picsauditing.PICS.PICSDBLocator serviceLocator;
 
-	private static com.picsauditing.PICS.PICSDBLocator serviceLocator;
-
-	private static com.picsauditing.PICS.PICSDBLocator getServiceLocator() {
+	private com.picsauditing.PICS.PICSDBLocator getServiceLocator() {
 		if (serviceLocator == null)
 			serviceLocator = new com.picsauditing.PICS.PICSDBLocator();
 		return serviceLocator;
 	}
 
-	private static DataSource getJdbcPics() throws NamingException {
+	private DataSource getJdbcPics() throws NamingException {
 		return (DataSource) getServiceLocator().getDataSource("java:comp/env/jdbc/pics");
 	}
-
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		assert dataSource != null;
-		staticDataSource = dataSource;
-	}
-
 }
