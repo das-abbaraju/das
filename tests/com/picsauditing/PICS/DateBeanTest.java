b6b830b914c40d349e27a8ca7ff1d26bc3a9ce32
Alex Ananighian <aananighian@picsauditing.com>
2012-02-27 18:59:21 +0000

    Changes for PICS-4153

--- tests/com/picsauditing/PICS/DateBeanTest.java
+++ tests/com/picsauditing/PICS/DateBeanTest.java
@@ -1,6 +1,6 @@
 package com.picsauditing.PICS;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -9,6 +9,7 @@ import java.util.TimeZone;
 import org.junit.Test;
 
 public class DateBeanTest  {
+	
 	@Test
 	public void testShowFormat() throws Exception {
 		String formatted = DateBean.toShowFormat("2007-03-15");
@@ -48,12 +49,14 @@ public class DateBeanTest  {
 		assertEquals("0001-01-01", DateBean.toDBFormat(cal.getTime()));
 	}
 	
+	@SuppressWarnings("deprecation")
 	@Test
 	public void testTimeZone() throws Exception {
 		Date sourceDate = new Date(999999);
 		Date destDate = DateBean.convertTime(sourceDate, TimeZone.getTimeZone("US/Eastern"), TimeZone.getTimeZone("US/Pacific"));
 		assertEquals(sourceDate.getHours() - 3, destDate.getHours());
 	}
+	
 	@Test
 	public void testGetNextDayMidnight() throws Exception {
 		Calendar cal = Calendar.getInstance();
@@ -64,4 +67,86 @@ public class DateBeanTest  {
 		
 		assertEquals(d2, DateBean.getNextDayMidnight(d1));
 	}
+	
+	@Test
+	public void testGetYearFromDate() {
+		Calendar calendar = Calendar.getInstance();
+		
+		calendar.set(Calendar.DATE, 10);
+		calendar.set(Calendar.MONTH, 3);
+		calendar.set(Calendar.YEAR, 2011);
+		
+		assertEquals(2011, DateBean.getYearFromDate(calendar.getTime()));
+		assertEquals(0, DateBean.getYearFromDate(null));
+	}
+	
+	@Test
+	public void testGetMarchOfNextYearWithNull() {
+		Date result = DateBean.getMarchOfNextYear(null);
+		assertNull(result);
+	}
+	
+	@Test
+	public void testGetMarchOfThatYearWithNull() {
+		Date result = DateBean.getMarchOfThatYear(null);
+		assertNull(result);
+	}
+	
+	@Test
+	public void testGetFirstOfNextYearWithNull() {
+		Date result = DateBean.getFirstOfNextYear(null);
+		assertNull(result);
+	}
+	
+	@Test
+	public void testGetMarchOfNextYear() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.DATE, 5);
+		calendar.set(Calendar.MONTH, Calendar.JUNE);
+		calendar.set(Calendar.YEAR, 2011);
+		
+		Date marchOfThatYear = DateBean.getMarchOfNextYear(calendar.getTime());
+		
+		Calendar result = Calendar.getInstance();
+		result.setTime(marchOfThatYear);
+		
+		assertEquals(1, result.get(Calendar.DATE));
+		assertEquals(Calendar.MARCH, result.get(Calendar.MONTH));
+		assertEquals(calendar.get(Calendar.YEAR) + 1, result.get(Calendar.YEAR));
+	}
+	
+	@Test
+	public void testGetMarchOfThatYear() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.DATE, 5);
+		calendar.set(Calendar.MONTH, Calendar.JUNE);
+		calendar.set(Calendar.YEAR, 2011);
+		
+		Date marchOfThatYear = DateBean.getMarchOfThatYear(calendar.getTime());
+		
+		Calendar result = Calendar.getInstance();
+		result.setTime(marchOfThatYear);
+		
+		assertEquals(1, result.get(Calendar.DATE));
+		assertEquals(Calendar.MARCH, result.get(Calendar.MONTH));
+		assertEquals(calendar.get(Calendar.YEAR), result.get(Calendar.YEAR));
+	}
+	
+	@Test
+	public void getFirstOfNextYear() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.DATE, 5);
+		calendar.set(Calendar.MONTH, Calendar.JUNE);
+		calendar.set(Calendar.YEAR, 2011);
+		
+		Date marchOfThatYear = DateBean.getFirstOfNextYear(calendar.getTime());
+		
+		Calendar result = Calendar.getInstance();
+		result.setTime(marchOfThatYear);
+		
+		assertEquals(1, result.get(Calendar.DATE));
+		assertEquals(Calendar.JANUARY, result.get(Calendar.MONTH));
+		assertEquals(calendar.get(Calendar.YEAR) + 1, result.get(Calendar.YEAR));
+	}
+	
 }
