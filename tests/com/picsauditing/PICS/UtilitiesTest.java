395a9e4c645c3e3a2f11404138aab3f645188ed7
Alex Ananighian <aananighian@picsauditing.com>
2012-02-28 21:42:14 +0000

    Test case for the toList method in Utilities

--- tests/com/picsauditing/PICS/UtilitiesTest.java
+++ tests/com/picsauditing/PICS/UtilitiesTest.java
@@ -2,8 +2,25 @@ package com.picsauditing.PICS;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
+
 
 public class UtilitiesTest {
 
+	@Test
+	public void testToListNullInput() {
+		assertEquals(0, Utilities.toList().size());
+	}
+	
+	@Test
+	public void testToList() {
+		List<Integer> numbers = Utilities.toList(1, 2, 3, 4);
+		
+		for (int index = 0; index < numbers.size(); index++) {
+			assertTrue((index + 1) == numbers.get(index));
+		}
+	}
+	
 }
