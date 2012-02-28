fd4a991e140501e4a98ea003e5c233104dbc4809
Alex Ananighian <aananighian@picsauditing.com>
2012-02-28 22:06:04 +0000

    Changes to the UtilitiesTest because I have removed my toList() method from the Utilities class

--- tests/com/picsauditing/PICS/UtilitiesTest.java
+++ tests/com/picsauditing/PICS/UtilitiesTest.java
@@ -10,17 +10,8 @@ import org.junit.Test;
 public class UtilitiesTest {
 
 	@Test
-	public void testToListNullInput() {
-		assertEquals(0, Utilities.toList().size());
-	}
-	
-	@Test
-	public void testToList() {
-		List<Integer> numbers = Utilities.toList(1, 2, 3, 4);
-		
-		for (int index = 0; index < numbers.size(); index++) {
-			assertTrue((index + 1) == numbers.get(index));
-		}
+	public void testIsEmptyArray() {
+		assertTrue(Utilities.isEmptyArray(null));
 	}
 	
 }
