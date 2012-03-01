42287a8c4d9fd76d432944c79c78071b71cbc3ba
Alex Ananighian <aananighian@picsauditing.com>
2012-03-01 19:51:36 +0000

    PICS-4813 - Added in the customized multi-year flaggable criteria

---
+++ src/com/picsauditing/PICS/flags/MultiYearValueCalculator.java
@@ -0,0 +1,200 @@
+package com.picsauditing.PICS.flags;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.math.NumberUtils;
+
+import com.picsauditing.jpa.entities.AuditData;
+import com.picsauditing.jpa.entities.ContractorAccount;
+import com.picsauditing.jpa.entities.ContractorAudit;
+import com.picsauditing.jpa.entities.FlagCriteria;
+import com.picsauditing.util.Strings;
+import com.picsauditing.util.Testable;
+
+/**
+ * Users should not extend this class.
+ */
+final public class MultiYearValueCalculator {
+
+	/**
+	 * Private default constructor to discourage creating new instances of this
+	 * class.
+	 */
+	private MultiYearValueCalculator() {}
+
+	public static String returnValueForMultiYear(ContractorAccount contractor, FlagCriteria criteria) {
+		List<ContractorAudit> audits = contractor.getAudits();
+		Number result = null;
+		
+		switch (criteria.getMultiYearScope()) {
+			case LastYearOnly:
+				result = getValueForSpecificYear(contractor, criteria, 1);
+				break;
+
+			case TwoYearsAgo:
+				result = getValueForSpecificYear(contractor, criteria, 2);
+				break;
+
+			case ThreeYearsAgo:
+				result = getValueForSpecificYear(contractor, criteria, 3);
+				break;
+
+			case ThreeYearAverage:
+				audits = getAnnualUpdateAudits(contractor, 3);
+				result = calculateMultiYearAverage(criteria, audits);
+				break;
+
+			case ThreeYearAggregate:
+				audits = getAnnualUpdateAudits(contractor, 3);
+				result = calculateMultiYearAggregate(criteria, audits);
+				break;
+		}
+		
+		if (result != null) {
+			return result.toString();
+		}
+		
+		return null;
+	}
+
+	static Double getValueForSpecificYear(ContractorAccount contractor, FlagCriteria criteria, int yearsBack) {
+		List<ContractorAudit> audits = getAnnualUpdateAudits(contractor, yearsBack);
+		if (CollectionUtils.isEmpty(audits) || audits.size() < yearsBack) {
+			return null;
+		}
+
+		return getValueFromAuditData(criteria, audits.get(yearsBack - 1).getData());
+	}
+
+	@Testable
+	static List<ContractorAudit> getAnnualUpdateAudits(ContractorAccount contractor, int yearsBack) {
+		List<ContractorAudit> audits = contractor.getSortedAnnualUpdates();
+		if (CollectionUtils.isEmpty(audits)) {
+			return Collections.emptyList();
+		}
+
+		if (yearsBack > audits.size()) {
+			return audits;
+		}
+
+		return audits.subList(0, yearsBack);
+	}
+
+	@Testable
+	static Number calculateMultiYearAggregate(FlagCriteria criteria, List<ContractorAudit> audits) {
+		if (!"Check Box".equals(criteria.getQuestion().getQuestionType())) {
+			List<Double> values = getValuesForMathematicalFunction(criteria, audits);
+			return addValues(values);
+		}
+		
+		return getTotalCheckBoxCount(criteria, audits);		
+	}
+
+	/**
+	 * Question: Whether or not we should calculate a "Null" value as part of
+	 * the Average or not (currently, they are calculated in the average).
+	 * 
+	 * @param rule
+	 * @param audits
+	 * @return
+	 */
+	@Testable
+	static Double calculateMultiYearAverage(FlagCriteria criteria, List<ContractorAudit> audits) { 
+		List<Double> values = getValuesForMathematicalFunction(criteria, audits);
+		Double sum = addValues(values);
+
+		if (sum != null) {
+			return (sum / values.size());
+		}
+
+		return null;
+	}
+
+	@Testable
+	static Double addValues(List<Double> values) {
+		if (CollectionUtils.isEmpty(values)) {
+			return null;
+		}
+
+		Double total = null;
+		for (Double number : values) {
+			if (number != null) {
+				if (total == null) {
+					total = new Double(0.0);
+				}
+
+				total += number;
+			}
+		}
+
+		return total;
+	}
+		
+	@Testable
+	static List<Double> getValuesForMathematicalFunction(FlagCriteria criteria, List<ContractorAudit> audits) {
+		if (CollectionUtils.isEmpty(audits)) {
+			return null;
+		}
+
+		List<Double> values = new ArrayList<Double>();
+		for (ContractorAudit audit : audits) {
+			List<AuditData> auditDataList = audit.getData();
+			values.add(getValueFromAuditData(criteria, auditDataList));
+		}
+
+		return values;
+	}
+	
+	static Integer getTotalCheckBoxCount(FlagCriteria criteria, List<ContractorAudit> audits) {
+		if (CollectionUtils.isEmpty(audits)) {
+			return null;
+		}
+		
+		int total = 0;
+		for (ContractorAudit audit : audits) {
+			List<AuditData> auditDataList = audit.getData();
+			total += isCheckBoxSelectedForQuestion(criteria, auditDataList);
+		}
+		
+		return total;
+	}
+	
+	static int isCheckBoxSelectedForQuestion(FlagCriteria criteria, List<AuditData> auditDataList) {
+		for (AuditData auditData : auditDataList) {
+			if(isCheckBoxQuestionWithAnswer(criteria, auditData)) {
+				return 1;
+			}
+		}
+		
+		return 0;
+	}
+
+	@Testable
+	static Double getValueFromAuditData(FlagCriteria criteria, List<AuditData> auditDataList) {
+		Double value = null;
+		for (AuditData auditData : auditDataList) {
+			if (isQuestionWithNumericAnswer(criteria, auditData)) {
+				value = NumberUtils.toDouble(auditData.getAnswer(), 0.0);
+			}
+		}
+
+		return value;
+	}
+
+	private static boolean isQuestionWithNumericAnswer(FlagCriteria criteria, AuditData auditData) {
+		return (criteria.getQuestion().getId() == auditData.getQuestion().getId() 
+				&& FlagCriteria.NUMBER.equals(criteria.getDataType()) 
+				&& !Strings.isEmpty(auditData.getAnswer()));
+	}
+	
+	private static boolean isCheckBoxQuestionWithAnswer(FlagCriteria criteria, AuditData auditData) {
+		return (criteria.getQuestion().getId() == auditData.getQuestion().getId() 
+				&& "Check Box".equals(criteria.getQuestion().getQuestionType())
+				&& !Strings.isEmpty(auditData.getAnswer())
+				&& "X".equals(auditData.getAnswer()));
+	}
+
+}
