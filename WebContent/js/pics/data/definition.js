d5924defcc4c0c09d9855e087cf90b25573cb05e
Trevor Allred <tallred@picsauditing.com>
2012-03-02 06:23:40 +0000

    I think we're going to need some good sample/test data

---
+++ WebContent/js/pics/data/definition.js
@@ -0,0 +1,11 @@
+{
+	"success" : true,
+	"rowsPerPage" : 10,
+	"filterExpression" : "{1} AND ({2} OR {3})",
+	"orderBy" : [ {
+		"column" : "accountCity"
+	}, {
+		"column" : "contractorMembershipDate",
+		"direction" : "DESC"
+	} ]
+}
