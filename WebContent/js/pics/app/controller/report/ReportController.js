bcd48b20cb35f5562ba25f13dda5b3b3822c5082
Trevor Allred <tallred@picsauditing.com>
2012-03-06 23:03:53 +0000

    wow this is messy but it works mostly...added ability to choose columns and update grid and store dynamically

--- WebContent/js/pics/app/controller/report/ReportController.js
+++ WebContent/js/pics/app/controller/report/ReportController.js
@@ -64,17 +64,32 @@ Ext.define('PICS.controller.report.ReportController', {
         	return;
         }
         
+        var columnStore = this.getReportReportsColumnStore();
+        var columns = columnStore.data.items;
+        console.log(columns);
+
         // Setup store fields for the report data
         var dataStore = this.getReportReportDataStore();
-        dataStore.fields = [];
-        for(var i = 0; i < report.columns().data.items.length; i++) {
-        	var storeField = {};
-        	var column = report.columns().data.items[i];
-        	storeField.name = column.get("name");
-        	// storeField.type = report.columns().data[i].get("extType");
-        	dataStore.fields.push(storeField);
+        
+        var fields = [];
+        for(var i = 0; i < columns.length; i++) {
+        	var field = {};
+        	var column = columns[i];
+        	field.name = column.get("name");
+        	// field.type = column.get("extType");
+        	fields.push(field);
         }
-
+        
+        var model = Ext.define('PICS.model.report.ReportRow', {
+            extend: 'Ext.data.Model',
+            fields: fields
+        });
+        dataStore.proxy.reader.setModel(model);
+        
+        // console.log(dataStore);
+//        console.log(this.getReportReportDataStore().model);
+//        console.log(Ext.ModelManager.getModel("PICS.model.report.ReportRow"));
+        
         // Setup the URL
 		var url = "ReportDynamic!data.action?";
 		if (report && report.getId() > 0) {
@@ -88,14 +103,23 @@ Ext.define('PICS.controller.report.ReportController', {
         dataStore.load({
         	callback: function(records, operation, success) {
         		if (success) {
-        			console.log(records);
         			var dataGrid = Ext.getCmp("dataGrid");
         			dataGrid.headerCt.removeAll();
-                    dataGrid.headerCt.add({"width":27,"xtype":"rownumberer"},{"text":"Account Name","width":180,"dataIndex":"accountName"},{"text":"Account Status","dataIndex":"accountStatus"},{"text":"Account ID","dataIndex":"accountID"});
+                    dataGrid.headerCt.add({"width":27,"xtype":"rownumberer"});
+                    for(var i = 0; i < columns.length; i++) {
+                    	var gridColumn = {};
+                    	var column = columns[i];
+                    	gridColumn.dataIndex = column.get("name");
+                    	gridColumn.text = column.get("text");
+                    	if (column.get("width") > 0)
+                    		gridColumn.width = column.get("width");
+                    	dataGrid.headerCt.add(gridColumn);
+                    }
+                    console.log(columns.length);
         		} else {
         			Ext.MessageBox.alert("Failed to read data from Server", "Reason: " + operation.error);
         		}
-            }
+            }, scope: this
         });
     },
     removeColumn: function(button, e, options) {
@@ -111,7 +135,7 @@ Ext.define('PICS.controller.report.ReportController', {
             type = 'reportoptionscolumns';
         }
         grid = Ext.ComponentQuery.query(type +' gridpanel');
-        selected = grid[0].getSelectionModel().getSelection()
+        selected = grid[0].getSelectionModel().getSelection();
         store.remove(selected);
     },
     saveReport: function(button, e, options) {
