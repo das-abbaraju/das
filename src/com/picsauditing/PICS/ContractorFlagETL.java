551a6370d7e61d3682453289d8485c786c0403ef
Alex Ananighian <aananighian@picsauditing.com>
2012-03-15 17:08:05 +0000

    Change the ThreeYearAggregate to ThreeYearSum (missed a change)

--- src/com/picsauditing/PICS/ContractorFlagETL.java
+++ src/com/picsauditing/PICS/ContractorFlagETL.java
@@ -1,5 +1,6 @@
 package com.picsauditing.PICS;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -49,55 +50,6 @@ public class ContractorFlagETL {
 		for (FlagCriteria flagCriteria : distinctFlagCriteria) {
 			PicsLogger.log("Starting to calculate = " + flagCriteria);
 			changes.addAll(executeFlagCriteriaCalculation(flagCriteria, contractor, answerMap));
-//			if (flagCriteria.getAuditType() != null) {
-//				changes.add(new FlagCriteriaContractor(contractor, flagCriteria, "true"));
-//			}
-//
-//			if (flagCriteria.getQuestion() != null) {
-//				if (AuditQuestion.CITATIONS == flagCriteria.getQuestion().getId()) {
-//					
-//				} else if (QUESTION_IDS_FOR_MULTI_YEAR.contains(flagCriteria.getQuestion().getId())) {
-//					FlagCriteriaContractor flagCriteriaContractor = new FlagCriteriaContractor(contractor,
-//							flagCriteria, "");
-//					flagCriteriaContractor.setAnswer(MultiYearValueCalculator.returnValueForMultiYear(contractor, flagCriteria));
-//					changes.add(flagCriteriaContractor);
-//				} else if (runAnnualUpdateFlagging(flagCriteria)) {
-//
-//					/**
-//					 * There is extra check before the Citation Question to make sure that the flag criteria's
-//					 * question category is applicable, before executing the flagging for the Citation Question
-//					 */
-//					FlagCriteriaContractor flagCriteriaContractor = new FlagCriteriaContractor(contractor, flagCriteria, "");
-//					ContractorAudit annualUpdate = contractor.getCompleteAnnualUpdates().get(flagCriteria.getMultiYearScope());
-//
-//					if (annualUpdate != null) {
-//						if (annualUpdate.isCategoryApplicable(flagCriteria.getQuestion().getCategory().getId())) {
-//							for (AuditData data : annualUpdate.getData()) {
-//								if (data.getQuestion().getId() == flagCriteria.getQuestion().getId()) {
-//									flagCriteriaContractor.setAnswer(FlagAnswerParser.parseAnswer(flagCriteria, data));
-//									flagCriteriaContractor.setAnswer2("for Year: " + annualUpdate.getAuditFor());
-//									changes.add(flagCriteriaContractor);
-//									break;
-//								}
-//							}
-//						}
-//					}
-//					
-//						// do our multi year stuff		
-//						// String result = MultiYearValueCalculator.returnValueForMultiYear();
-//						// if (!Strings.isEmpty(result)) {
-//						// then create the fcc
-//					
-//				} else {
-//					changes.addAll(performFlaggingForNonEMR(contractor, answerMap, flagCriteria));
-//				}
-//			} // end of questions
-//
-//			// Checking OSHA
-//			if (flagCriteria.getOshaType() != null) {
-//				performOshaFlagCalculations(contractor, changes, flagCriteria);
-//			}
-
 		}
 
 		persistFlagCriteriaContractorChanges(contractor, changes);
@@ -121,8 +73,8 @@ public class ContractorFlagETL {
 		}
 
 		if (flagCriteria.getQuestion() != null) {
-			if (AuditQuestion.CITATIONS == flagCriteria.getQuestion().getId()) {
-				changes.addAll(MultiYearValueCalculator.doOldStuffForEMR(flagCriteria, contractor));
+			if (AuditQuestion.EMR == flagCriteria.getQuestion().getId()) {
+				changes.addAll(doOldStuffForEMR(flagCriteria, contractor));
 			} else if (flagCriteria.getMultiYearScope() != null && QUESTION_IDS_FOR_MULTI_YEAR.contains(flagCriteria.getQuestion().getId())) {
 				FlagCriteriaContractor flagCriteriaContractor = new FlagCriteriaContractor(contractor,
 						flagCriteria, "");
@@ -148,11 +100,11 @@ public class ContractorFlagETL {
 							}
 						}
 					}
-				}				
+				}
 			} else {
 				changes.addAll(performFlaggingForNonEMR(contractor, answerMap, flagCriteria));
 			}
-		} 
+		}
 
 		if (flagCriteria.getOshaType() != null) {
 			performOshaFlagCalculations(contractor, changes, flagCriteria);
@@ -256,6 +208,123 @@ public class ContractorFlagETL {
 			flagCriteriaContractorDao.remove(criteriaData);
 		}
 	}
+	
+	private Set<FlagCriteriaContractor> doOldStuffForEMR(FlagCriteria flagCriteria, ContractorAccount contractor) {
+		Set<FlagCriteriaContractor> changes = new HashSet<FlagCriteriaContractor>();
+
+		if (flagCriteria.getQuestion().getId() == AuditQuestion.EMR) {
+			Map<String, AuditData> auditsOfThisEMRType = contractor.getEmrs();
+
+			List<AuditData> years = new ArrayList<AuditData>();
+			for (String year : auditsOfThisEMRType.keySet()) {
+				if (!year.equals("Average"))
+					years.add(auditsOfThisEMRType.get(year));
+			}
+
+			if (years != null && years.size() > 0) {
+				Float answer = null;
+				String answer2 = "";
+				boolean verified = true; // Has the data been verified?
+
+				try {
+					switch (flagCriteria.getMultiYearScope()) {
+					case ThreeYearAverage:
+						AuditData average = auditsOfThisEMRType.get("Average");
+						answer = (average != null) ? Float.valueOf(Strings.formatNumber(average.getAnswer())) : null;
+						for (AuditData year : years) {
+							if (year != null) {
+								answer2 += (answer2.isEmpty()) ? "Years: "
+										+ year.getAudit().getAuditFor() : ", "
+										+ year.getAudit().getAuditFor();
+							}
+						}
+						if (average == null || !average.isVerified())
+							verified = false;
+						break;
+					case ThreeYearsAgo:
+						if (years.size() >= 3) {
+							if (years.get(years.size() - 3) != null) {
+								answer = Float.valueOf(Strings.formatNumber(years.get(years.size() - 3).getAnswer()));
+								verified = years.get(years.size() - 3).isVerified();
+								answer2 = "Year: " + years.get(years.size() - 3).getAudit().getAuditFor();
+							}
+						}
+						break;
+					case TwoYearsAgo:
+						if (years.size() >= 2) {
+							if (years.get(years.size() - 2) != null) {
+								answer = Float.valueOf(Strings.formatNumber(years.get(years.size() - 2).getAnswer()));
+								verified = years.get(years.size() - 2).isVerified();
+								answer2 = "Year: " + years.get(years.size() - 2).getAudit().getAuditFor();
+							}
+						}
+						break;
+					case LastYearOnly:
+						if (years.size() >= 1) {
+							AuditData lastYear = years.get(years.size() - 1);
+							if (lastYear != null && isLast2Years(lastYear.getAudit().getAuditFor())) {
+								answer = Float.valueOf(Strings.formatNumber(lastYear.getAnswer()));
+								verified = lastYear.isVerified();
+								answer2 = "Year: " + lastYear.getAudit().getAuditFor();
+							}
+						}
+						break;
+					default:
+						throw new RuntimeException(
+								"Invalid MultiYear scope of "
+										+ flagCriteria.getMultiYearScope().toString()
+										+ " specified for flag criteria id "
+										+ flagCriteria.getId()
+										+ ", contractor id "
+										+ contractor.getId());
+					}
+				} catch (Throwable t) {
+					PicsLogger.log("Could not cast contractor: "
+							+ contractor.getId() + " and answer: "
+							+ ((answer != null) ? answer : "null")
+							+ " to a value for criteria: "
+							+ flagCriteria.getId());
+
+					answer = null; // contractor errors out somewhere
+					// during the process of creating
+					// their data
+					// do not want to enter partially corrupt data
+				}
+
+				if (answer != null) {
+					final FlagCriteriaContractor fcc = new FlagCriteriaContractor(
+							contractor, flagCriteria, answer.toString());
+					fcc.setVerified(verified);
+
+					// conditionally add verified tag
+					if (verified) {
+						answer2 += "<br/><span class=\"verified\">Verified</span>";
+					}
+					fcc.setAnswer2(answer2);
+
+					changes.add(fcc);
+				} else {
+					if (flagCriteria.isFlaggableWhenMissing()) {
+						FlagCriteriaContractor flagCriteriaContractor = new FlagCriteriaContractor(contractor, flagCriteria, null);
+						flagCriteriaContractor.setAnswer2(null);
+						changes.add(flagCriteriaContractor);
+					}
+				}
+			}
+		}
+		
+		
+		return changes;
+	}
+
+	private static boolean isLast2Years(String auditFor) {
+		int lastYear = DateBean.getCurrentYear() - 1;
+		if (Integer.toString(lastYear).equals(auditFor)
+				|| Integer.toString(lastYear - 1).equals(auditFor))
+			return true;
+		return false;
+	}
+	
 	// TODO: Fix Me
 	private void performOshaFlagCalculations(ContractorAccount contractor, Set<FlagCriteriaContractor> changes, FlagCriteria flagCriteria) {
 		/*OshaOrganizer osha = contractor.getOshaOrganizer();
