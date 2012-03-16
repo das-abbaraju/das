0d8c0200bb30943f4b895a26cd3109ed8ea472fd
Lani Aung <uaung@picsauditing.com>
2012-03-16 17:00:27 +0000

    PICS-4209 -- Update widgets on Operator Dashboards. Refactored to use SQL and limit to subcontractors/host operators that have flags. Also added links to contractors and flag colors.

--- WebContent/struts/widgets/subcontractors_flag_matrix.jsp
+++ WebContent/struts/widgets/subcontractors_flag_matrix.jsp
@@ -1,25 +1,43 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="s" uri="/struts-tags"%>
-<head>
-	<link rel="stylesheet" href="css/reports.css" />
-</head>
-<body>
-	<div>
+<div style="width: 100%; height: 400px; overflow: auto;">
+	<s:if test="!gcContractorOperators.empty && !subcontractors.empty">
 		<table class="report">
-			<tr>
-				<th><s:text name="GeneralContractor.SubContractor" /></th>
-				<s:iterator value="gcContractorOperators" var="gcOp">
-					<th><s:property value="#gcOp.name" /></th>
-				</s:iterator>
-			</tr>
-			<s:iterator value="subcontractors" var="sub">
+			<thead>
 				<tr>
-					<td><s:property value="#sub.name" /></td>
-					<s:iterator value="gcContractorOperators" var="gcOp2">
-						<td><s:property value="#sub.getContractorOperatorForOperator(#gcOp2).flagColor.smallIcon" escape="false" /></td>
+					<th>
+						<s:text name="GeneralContractor.SubContractor" />
+					</th>
+					<s:iterator value="gcContractorOperators" var="gcOp">
+						<th><s:property value="#gcOp.name" /></th>
 					</s:iterator>
 				</tr>
-			</s:iterator>	
+			</thead>
+			<s:iterator value="subcontractors" var="sub">
+				<tr>
+					<td>
+						<a href="ContractorView.action?id=<s:property value="#sub.id" />">
+							<s:property value="#sub.name" />
+						</a>
+					</td>
+					<s:iterator value="gcContractorOperators" var="gcOp2">
+						<td class="center">
+							<s:url var="contractor_flag" action="ContractorFlag">
+								<s:param name="id" value="%{#sub.id}" />
+								<s:param name="opID" value="%{#gcOp2.id}" />
+							</s:url>
+							<a href="${contractor_flag}">
+								<s:property value="table.get(#gcOp2.id, #sub.id).smallIcon" escape="false" />
+							</a>
+						</td>
+					</s:iterator>
+				</tr>
+			</s:iterator>
 		</table>
-	</div>
-</body>
+	</s:if>
+	<s:else>
+		<div class="alert">
+			<s:text name="Report.message.NoRowsFound" />
+		</div>
+	</s:else>
+</div>
--- src/com/picsauditing/actions/operators/SubcontractorsFlagMatrixWidget.java
+++ src/com/picsauditing/actions/operators/SubcontractorsFlagMatrixWidget.java
@@ -2,67 +2,97 @@ package com.picsauditing.actions.operators;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
-import com.picsauditing.actions.PicsActionSupport;
+import org.apache.commons.beanutils.BasicDynaBean;
+
+import com.google.common.collect.Table;
+import com.google.common.collect.TreeBasedTable;
+import com.picsauditing.actions.report.ReportActionSupport;
 import com.picsauditing.jpa.entities.ContractorAccount;
-import com.picsauditing.jpa.entities.ContractorOperator;
+import com.picsauditing.jpa.entities.FlagColor;
 import com.picsauditing.jpa.entities.OperatorAccount;
+import com.picsauditing.search.SelectSQL;
 
 @SuppressWarnings("serial")
-public class SubcontractorsFlagMatrixWidget extends PicsActionSupport {
+public class SubcontractorsFlagMatrixWidget extends ReportActionSupport {
+	private SelectSQL sql = new SelectSQL("generalcontractors gc");
 	private OperatorAccount operator;
-	private List<ContractorAccount> subcontractors = null;
-	private List<OperatorAccount> gcContractorOperators = null;
-	
+
+	private List<ContractorAccount> subcontractors = new ArrayList<ContractorAccount>();
+	private List<OperatorAccount> gcContractorOperators = new ArrayList<OperatorAccount>();
+	private Table<Integer, Integer, FlagColor> table = TreeBasedTable.create();
+
 	public String execute() throws Exception {
 		if (!permissions.isLoggedIn())
 			return LOGIN_AJAX;
-		
+
 		operator = dao.find(OperatorAccount.class, permissions.getAccountId());
+
+		if (operator.isGeneralContractor()) {
+			buildQuery();
+			run(sql);
+			buildMap();
+		}
+
 		return SUCCESS;
-	}
-	
-	public OperatorAccount getOperator() {
-		return operator;
-	}
-	
-	public void setOperator(OperatorAccount operator) {
-		this.operator = operator;
 	}
 
 	public List<ContractorAccount> getSubcontractors() {
-		if (subcontractors == null) {
-			subcontractors = new ArrayList<ContractorAccount>();
-			
-			for (ContractorOperator conOp : operator.getContractorOperators()) {
-				subcontractors.add(conOp.getContractorAccount());
-			}
-		}
-		
-		Collections.sort(subcontractors, new Comparator<ContractorAccount>() {
-			public int compare(ContractorAccount contractorOne, ContractorAccount contractorTwo) {
-				return contractorOne.getName().toLowerCase().compareTo(contractorTwo.getName().toLowerCase());
-			}
-		});
-		
 		return subcontractors;
 	}
-	
-	public void setSubcontractors(List<ContractorAccount> subcontractors) {
-		this.subcontractors = subcontractors;
-	}
-	
+
 	public List<OperatorAccount> getGcContractorOperators() {
-		if (gcContractorOperators == null) {
-			gcContractorOperators = operator.getGcContractorOperatorAccounts();
-		}
 		return gcContractorOperators;
 	}
 
-	public void setGcContractorOperators(List<OperatorAccount> gcContractorOperators) {
-		this.gcContractorOperators = gcContractorOperators;
+	public Table<Integer, Integer, FlagColor> getTable() {
+		return table;
 	}
 
+	private void buildQuery() {
+		sql.addJoin("JOIN generalcontractors subCon ON subCon.genID = gc.genID");
+		sql.addJoin("JOIN generalcontractors gcOp ON subCon.subID = gcOp.subID AND gcOp.genID = "
+				+ permissions.getAccountId());
+		sql.addJoin("JOIN accounts o ON o.id = gc.genID AND o.status = 'Active' AND o.type = 'Operator'");
+		sql.addJoin("JOIN accounts c ON c.id = subCon.subID AND c.status = 'Active'");
+
+		sql.addField("o.id opID");
+		sql.addField("o.name opName");
+		sql.addField("c.id conID");
+		sql.addField("c.name conName");
+		sql.addField("subCon.flag");
+
+		sql.addWhere("gc.subID = " + operator.getGcContractor().getContractorAccount().getId());
+		sql.addWhere("gcOp.workStatus IN ('Y','P')");
+
+		sql.addOrderBy("o.name");
+		sql.addOrderBy("c.name");
+	}
+
+	private void buildMap() {
+		for (BasicDynaBean bean : data) {
+			OperatorAccount operator = new OperatorAccount();
+			operator.setId(Integer.parseInt(bean.get("opID").toString()));
+			operator.setName(bean.get("opName").toString());
+			operator.setType("Operator");
+
+			if (!gcContractorOperators.contains(operator))
+				gcContractorOperators.add(operator);
+
+			ContractorAccount contractor = new ContractorAccount();
+			contractor.setId(Integer.parseInt(bean.get("conID").toString()));
+			contractor.setName(bean.get("conName").toString());
+			contractor.setType("Contractor");
+
+			if (!subcontractors.contains(contractor))
+				subcontractors.add(contractor);
+
+			FlagColor flag = FlagColor.valueOf(bean.get("flag").toString());
+			table.put(operator.getId(), contractor.getId(), flag);
+		}
+		
+		Collections.sort(gcContractorOperators);
+		Collections.sort(subcontractors);
+	}
 }
