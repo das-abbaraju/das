feeb0188db6e9032fe2fb7bcb23e6e39c87bda89
Lani Aung <uaung@picsauditing.com>
2012-03-17 01:01:10 +0000

    PICS-4340 -- On the registration request page the default country is Andorra? Using name instead of getText. From code review with tbaker

--- src/com/picsauditing/actions/contractors/RequestNewContractor.java
+++ src/com/picsauditing/actions/contractors/RequestNewContractor.java
@@ -464,7 +464,7 @@ public class RequestNewContractor extends PicsActionSupport {
 		Collections.sort(countries, new Comparator<Country>() {
 			@Override
 			public int compare(Country o1, Country o2) {
-				return getText(o1.getI18nKey()).compareTo(getText(o2.getI18nKey()));
+				return o1.getName().compareTo(o2.getName());
 			}
 		});
 
