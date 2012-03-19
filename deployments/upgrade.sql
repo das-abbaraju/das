eb05bf232b6da95c0964d77213387ef16d7546a9
Lucas Kam <lkam@picsauditing.com>
2012-03-19 17:18:54 +0000

    PICS-3156 Added Osha data conversion.

--- deployments/upgrade.sql
+++ deployments/upgrade.sql
@@ -54,3 +54,145 @@ set c.productRisk = 0
 update contractor_info c JOIN accounts a ON a.id = c.id 
 set c.transportationRisk = 0
  WHERE (c.transportationRisk <> 0 AND a.transportationServices = 0);
+ 
+ 
+ -- PICS-3156 Osha Data Conversion
+ -- Total Man Hours
+replace into pqfdata
+            (auditID,
+             questionID,
+             answer,
+             dateVerified,
+             auditorID,
+             creationDate,
+             updateDate)
+select
+  oa.auditID,
+  8810,
+  oa.manHours,
+  oa.verifiedDate,
+  oa.auditorID,
+  oa.creationDate,
+  oa.updateDate
+from osha_audit oa
+where oa.SHAType = "OSHA";
+
+-- Fatalities
+replace into pqfdata
+            (auditID,
+             questionID,
+             answer,
+             dateVerified,
+             auditorID,
+             creationDate,
+             updateDate)
+select
+  oa.auditID,
+  8812,
+  oa.fatalities,
+  oa.verifiedDate,
+  oa.auditorID,
+  oa.creationDate,
+  oa.updateDate
+from osha_audit oa
+where oa.SHAType = "OSHA";
+
+-- Lost Workday Cases
+replace into pqfdata
+            (auditID,
+             questionID,
+             answer,
+             dateVerified,
+             auditorID,
+             creationDate,
+             updateDate)
+select
+  oa.auditID,
+  8813,
+  oa.lostWorkCases,
+  oa.verifiedDate,
+  oa.auditorID,
+  oa.creationDate,
+  oa.updateDate
+from osha_audit oa
+where oa.SHAType = "OSHA";
+
+-- Lost Workdays
+replace into pqfdata
+            (auditID,
+             questionID,
+             answer,
+             dateVerified,
+             auditorID,
+             creationDate,
+             updateDate)
+select
+  oa.auditID,
+  8814,
+  oa.lostWorkDays,
+  oa.verifiedDate,
+  oa.auditorID,
+  oa.creationDate,
+  oa.updateDate
+from osha_audit oa
+where oa.SHAType = "OSHA";
+
+-- Restricted
+replace into pqfdata
+            (auditID,
+             questionID,
+             answer,
+             dateVerified,
+             auditorID,
+             creationDate,
+             updateDate)
+select
+  oa.auditID,
+  8815,
+  oa.restrictedWorkCases,
+  oa.verifiedDate,
+  oa.auditorID,
+  oa.creationDate,
+  oa.updateDate
+from osha_audit oa
+where oa.SHAType = "OSHA";
+
+-- Job Transfers
+replace into pqfdata
+            (auditID,
+             questionID,
+             answer,
+             dateVerified,
+             auditorID,
+             creationDate,
+             updateDate)
+select
+  oa.auditID,
+  8816,
+  oa.modifiedWorkDay,
+  oa.verifiedDate,
+  oa.auditorID,
+  oa.creationDate,
+  oa.updateDate
+from osha_audit oa
+where oa.SHAType = "OSHA";
+
+-- injuries and illnesses
+replace into pqfdata
+            (auditID,
+             questionID,
+             answer,
+             dateVerified,
+             auditorID,
+             creationDate,
+             updateDate)
+select 
+  oa.auditID,
+  8817,
+  oa.injuryIllnessCases,
+  oa.verifiedDate,
+  oa.auditorID,
+  oa.creationDate,
+  oa.updateDate
+from osha_audit oa
+where oa.SHAType = "OSHA";
