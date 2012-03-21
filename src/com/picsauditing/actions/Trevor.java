50233c902303113b056bee7c87e749ad24f34f92
Tom Baker <tbaker@picsauditing.com>
2012-03-21 12:41:43 -0700

    test 2

--- src/com/picsauditing/actions/Trevor.java
+++ src/com/picsauditing/actions/Trevor.java
@@ -3,7 +3,7 @@ package com.picsauditing.actions;
 @SuppressWarnings("serial")
 public class Trevor extends PicsActionSupport {
 	public String execute() {
-		// test
+		// test 2
 		return SUCCESS;
 	}
 }
