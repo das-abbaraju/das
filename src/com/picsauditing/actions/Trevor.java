accbea53f04c18500fbc6d03b632ed7ece549c20
lkam <lkam@picsauditing.com>
2012-03-21 15:47:33 -0700

    test 7

--- src/com/picsauditing/actions/Trevor.java
+++ src/com/picsauditing/actions/Trevor.java
@@ -3,7 +3,7 @@ package com.picsauditing.actions;
 @SuppressWarnings("serial")
 public class Trevor extends PicsActionSupport {
 	public String execute() {
-		// test 4
+		// test 7
 		return SUCCESS;
 	}
 }
