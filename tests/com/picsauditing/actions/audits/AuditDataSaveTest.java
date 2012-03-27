dfabf904949c26afe177fe3b4a019c1a7c668ac1
Craig Jones <cjones@picsauditing.com>
2012-03-27 16:54:08 -0700

    Reverted the solution for PICS-5146 and implemented it in a different way.  We are now explicitly trimming leading zeros (along with all commas as before), rather than depending on a formatter to do it, which was inadvertently putting commas back in for "Number" question types when we were only doing that for "Decimal Number" types previously.

---
+++ tests/com/picsauditing/actions/audits/AuditDataSaveTest.java
@@ -0,0 +1,24 @@
+package com.picsauditing.actions.audits;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AuditDataSaveTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testTrimWhitespaceLeadingZerosAndAllCommas() throws Exception {
+		assertEquals("10.10", AuditDataSave.trimWhitespaceLeadingZerosAndAllCommas("10.10"));
+		assertEquals("10.10", AuditDataSave.trimWhitespaceLeadingZerosAndAllCommas("  10.10"));
+		assertEquals("10.10", AuditDataSave.trimWhitespaceLeadingZerosAndAllCommas("10.10  "));
+		assertEquals("10.10", AuditDataSave.trimWhitespaceLeadingZerosAndAllCommas("010.10"));
+		assertEquals("10.10", AuditDataSave.trimWhitespaceLeadingZerosAndAllCommas("00010.10"));
+		assertEquals("10.10", AuditDataSave.trimWhitespaceLeadingZerosAndAllCommas(",010.10"));
+		assertEquals("10.10", AuditDataSave.trimWhitespaceLeadingZerosAndAllCommas("0,010.10"));
+	}
+}
