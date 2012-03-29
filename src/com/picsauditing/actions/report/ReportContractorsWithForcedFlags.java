e6e8f335880e5cdae41d3873a6094cd3691b42b2
mdo <mdo@picsauditing.com>
2012-03-29 09:26:57 -0700

    PICS-5014. As per Code review with Tom, Applying the changes listed only if user is from Sales and Marketing.

--- src/com/picsauditing/actions/report/ReportContractorsWithForcedFlags.java
+++ src/com/picsauditing/actions/report/ReportContractorsWithForcedFlags.java
@@ -7,49 +7,54 @@ import com.picsauditing.util.excel.ExcelColumn;
 
 @SuppressWarnings("serial")
 public class ReportContractorsWithForcedFlags extends ReportAccount {
-	
+
 	@Override
 	public void prepare() throws Exception {
 		super.prepare();
 
-		getFilter().setShowStatus(true);
+		if (!permissions.isMarketing())
+			getFilter().setShowStatus(true);
 		getFilter().setShowInsuranceLimits(false);
 		getFilter().setShowOpertorTagName(false);
 	}
-	
+
 	@Override
 	public void checkPermissions() throws Exception {
 		permissions.tryPermission(OpPerms.ForcedFlagsReport);
 	}
-	
+
 	@Override
 	protected void buildQuery() {
 		skipPermissions = true;
 		super.buildQuery();
-		
-		String forceFlagsJoin = "JOIN (SELECT conid,opid,forcebegin,forceend,forcedby,forceflag,label FROM"+ 
-		" (SELECT gc.subid as conid, gc.genid as opid, gc.forcebegin, gc.forceend, gc.forcedBy, gc.forceflag, 'FlagCriteria.Overall' AS label FROM generalcontractors gc" +
-		" WHERE gc.forceFlag IS NOT NULL" +
-		" UNION" +
-		" SELECT fdo.conid, fdo.opid, fdo.updateDate as forcebegin, fdo.forceend, fdo.updatedBy as forcedby, fdo.forceflag, CONCAT('FlagCriteria.', fc1.id, '.label') as label FROM flag_data_override fdo"+
-		" JOIN flag_criteria fc1 ON fdo.criteriaID = fc1.id"+
-		" WHERE fdo.forceFlag IS NOT NULL) t"+
-		" ) ff ON a.id = ff.conid";
-		
+
+		String forceFlagsJoin = "JOIN (SELECT conid,opid,forcebegin,forceend,forcedby,forceflag,label FROM"
+				+ " (SELECT gc.subid as conid, gc.genid as opid, gc.forcebegin, gc.forceend, gc.forcedBy, gc.forceflag, 'FlagCriteria.Overall' AS label FROM generalcontractors gc"
+				+ " WHERE gc.forceFlag IS NOT NULL"
+				+ " UNION"
+				+ " SELECT fdo.conid, fdo.opid, fdo.updateDate as forcebegin, fdo.forceend, fdo.updatedBy as forcedby, fdo.forceflag, CONCAT('FlagCriteria.', fc1.id, '.label') as label FROM flag_data_override fdo"
+				+ " JOIN flag_criteria fc1 ON fdo.criteriaID = fc1.id" + " WHERE fdo.forceFlag IS NOT NULL) t"
+				+ " ) ff ON a.id = ff.conid";
+
 		sql.addJoin(forceFlagsJoin);
 		sql.addJoin("JOIN accounts o ON o.id = ff.opid");
 		sql.addJoin("LEFT JOIN users u ON u.id = ff.forcedBy");
 		sql.addJoin("LEFT JOIN accounts fa ON fa.id = u.accountID");
 		sql.addJoin("LEFT JOIN generalcontractors gc ON gc.genid = o.id and gc.subid = ff.conid");
 		sql.addWhere("ff.forceFlag IS NOT NULL");
-		if(permissions.isOperatorCorporate()) {
+		if (!permissions.isMarketing()) {
+			sql.addWhere("a.status = 'Active'");
+			sql.addWhere("o.status = 'Active'");
+		}
+		if (permissions.isOperatorCorporate()) {
 			String opIds = " ff.opid = " + permissions.getAccountId() + " OR ";
-			if(permissions.isOperator()) 
-				opIds += " ff.opid IN (SELECT corporateID from facilities where opID = " + permissions.getAccountId()+")";
-			else 
-				opIds += " ff.opid IN (SELECT opID from facilities where corporateID = " + permissions.getAccountId()+")";
+			if (permissions.isOperator())
+				opIds += " ff.opid IN (SELECT corporateID from facilities where opID = " + permissions.getAccountId()
+						+ ")";
+			else
+				opIds += " ff.opid IN (SELECT opID from facilities where corporateID = " + permissions.getAccountId()
+						+ ")";
 			sql.addWhere(opIds);
-			sql.addWhere("a.status != 'Demo'");
 		}
 		sql.addField("o.name AS opName");
 		sql.addField("o.type AS opType");
@@ -58,27 +63,27 @@ public class ReportContractorsWithForcedFlags extends ReportAccount {
 		sql.addField("label AS fLabel");
 		sql.addField("ff.forceend");
 		sql.addField("ff.forceBegin");
-		sql.addField("u.id as forcedById"); 
+		sql.addField("u.id as forcedById");
 		sql.addField("u.name AS forcedBy");
 		sql.addField("fa.name AS forcedByAccount");
 		sql.addField("gc.workStatus");
-		
+
 		if (filterOn(getFilter().getOperator())) {
 			String list = Strings.implode(getFilter().getOperator(), ",");
 			sql.addWhere("o.id IN (" + list + ")");
 		}
-		
+
 		orderByDefault = "o.name, a.name";
-		
+
 		if (download) {
 			sql.addField("ff.forceFlag");
 		}
 	}
-	
+
 	@Override
 	protected void addExcelColumns() {
 		super.addExcelColumns();
-		
+
 		excelSheet.addColumn(new ExcelColumn("opName"));
 		excelSheet.addColumn(new ExcelColumn("forceFlag", "Flag"));
 		excelSheet.addColumn(new ExcelColumn("fLabel", "Flag Issue", ExcelCellType.Translated));
