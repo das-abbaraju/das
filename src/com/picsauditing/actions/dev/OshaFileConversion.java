bf54756d9d9853a56ea3234714952810d43c12b3
Craig Jones <cjones@picsauditing.com>
2012-03-29 12:23:32 -0700

    For PICS-3156 - Created a Dev-time utility that generates a shell script that moves OSHA log files around within the file system to correspond with the new structuring.

---
+++ src/com/picsauditing/actions/dev/OshaFileConversion.java
@@ -0,0 +1,16 @@
+package com.picsauditing.actions.dev;
+
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+
+import com.picsauditing.actions.PicsActionSupport;
+
+public class OshaFileConversion extends PicsActionSupport {
+
+	@Override
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+	
+}
