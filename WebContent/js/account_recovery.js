a78eccf939f67e581cad9f31cabb552734e95667
Lani Aung <uaung@picsauditing.com>
2012-04-11 15:51:02 -0700

    PICS-5051 -- CAPTCHA field label is in English. Updated to use custom translations (as lang didn't work?)

--- WebContent/js/account_recovery.js
+++ WebContent/js/account_recovery.js
@@ -1,6 +1,15 @@
 var RecaptchaOptions = {
    theme : 'white',
-   lang	 : '<s:property value="locale.language" />'
+   custom_translations : {
+       instructions_visual : translate('JS.Recaptcha.TypeTheTwoWords'),
+       instructions_audio : translate('JS.Recaptcha.TypeWhatYouHear'),
+       play_again : translate('JS.Recaptcha.PlaySoundAgain'),
+       cant_hear_this : translate('JS.Recaptcha.DownloadSoundAsMP3'),
+       visual_challenge : translate('JS.Recaptcha.GetAVisualChallenge'),
+       audio_challenge : translate('JS.Recaptcha.GetAnAudioChallenge'),
+       refresh_btn : translate('JS.Recaptcha.GetANewChallenge'),
+       help_btn : translate('JS.global.Help')
+   }
 };
 
 $(function() {
