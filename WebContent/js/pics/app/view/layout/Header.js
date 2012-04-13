175670f9c814c897726065b431b9267690d8b655
Carey Hinoki <chinoki@picsauditing.com>
2012-04-13 16:22:51 -0700

    PICS-4962 Formatting

--- WebContent/js/pics/app/view/layout/Header.js
+++ WebContent/js/pics/app/view/layout/Header.js
@@ -9,35 +9,35 @@ Ext.define('PICS.view.layout.Header', {
         text: '<a href="Home.action"><img src="images/logo_sm.png" /></a>',
         width: 100,
         height: 35
-    },{
+    }, {
         xtype: 'tbfill'
-    },{
+    }, {
         xtype: 'tbtext',
-        text: '<a href="Home.action"><img src="images/*.png" /></a>'
-    },{
+        //text: '<a href="Home.action"><img src="images/*.png" /></a>'
+    }, {
         xtype: 'tbspacer',
         width: 15
-    },{
+    }, {
         xtype: 'tbtext',
         cls: 'toolbarBold',
         text: 'Welcome, '
-    },{
+    }, {
         xtype: 'tbtext',
         cls: 'toolbarBold',
         text: '<a href="ProfileEdit.action">Joel</a>'
-    },{
+    }, {
         xtype: 'tbseparator'
-    },{
+    }, {
         xtype: 'tbtext',
         text: '<a href="Home.action">Home</a>'
-    },{
+    }, {
         xtype: 'tbseparator'
-    },{
+    }, {
         xtype: 'tbtext',
         text: '<a href="http://www.picsauditing.com/">PICS</a>'
-    },{
+    }, {
         xtype: 'tbseparator'
-    },{
+    }, {
         xtype: 'tbtext',
         text: '<a href="Login.action?button=logout">Logout</a>'
     }],
