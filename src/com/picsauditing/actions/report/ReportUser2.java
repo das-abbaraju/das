2650b69717ab1cee0eb0bb97f312afdbedf4935d
Lei Wang <lwang@picsauditing.com>
2012-05-08 18:30:00 -0700

    PICS-5381, allow user to enter user id to search user to be added to granting access.

--- src/com/picsauditing/actions/report/ReportUser2.java
+++ src/com/picsauditing/actions/report/ReportUser2.java
@@ -1,6 +1,5 @@
 package com.picsauditing.actions.report;
 
-import com.picsauditing.PICS.Utilities;
 import com.picsauditing.search.SelectUser;
 import com.picsauditing.util.ReportFilterUser;
 import com.picsauditing.util.Strings;
@@ -13,7 +12,7 @@ public class ReportUser2 extends ReportActionSupport {
 
 	@Override
 	public String execute() throws Exception {
-		
+
 		sql.addJoin("JOIN accounts a ON a.id = u.accountID");
 		sql.addField("a.name as accountName");
 		sql.addWhere("u.isActive = 'Yes'");
@@ -31,45 +30,66 @@ public class ReportUser2 extends ReportActionSupport {
 		ReportFilterUser f = getFilter();
 
 		String search = Strings.escapeQuotes(f.getSearch());
-		if (search.length() >= 3) {
+		if (search.length() >= 3) {			
 			StringBuffer where = new StringBuffer();
 			String phone = Strings.stripPhoneNumber(f.getSearch());
 			if (phone.length() > 0)
 				where.append("u.phoneIndex LIKE '").append(search).append("%'");
+
 			if (search.contains("@")) {
 				where.append(where.length() > 0 ? " OR " : "");
 				where.append("u.email LIKE '").append(search).append("%'");
 			}
-			
+			if (isNumeric(search)){
+				where.append(" OR u.id LIKE '").append(search).append("%'");
+				where.append(" OR u.id = ").append(search);
+			}
+
 			if (where.length() == 0) {
 				where.append("u.name LIKE '%").append(search).append("%'");
 				where.append("OR a.name LIKE '%").append(search).append("%'");
 				where.append("OR u.email LIKE '%").append(search).append("%'");
-			}
-			
+				where.append("OR u.id LIKE '").append(search).append("%'");
+			}			
 			sql.addWhere(where.toString());
 		} else {
 			if (filterOn(f.getStartsWith()))
 				sql.addWhere("u.name LIKE '" + Strings.escapeQuotes(f.getStartsWith()) + "%'");
-	
+
 			if (filterOn(f.getContactName()))
 				sql.addWhere("u.name LIKE '%" + Strings.escapeQuotes(f.getContactName()) + "%'");
-	
+
 			String phone = Strings.stripPhoneNumber(f.getPhoneNumber());
 			if (filterOn(phone))
 				sql.addWhere("u.phoneIndex LIKE '" + phone + "%'");
-	
+
 			if (filterOn(f.getEmailAddress()))
 				sql.addWhere("u.email LIKE '%" + Strings.escapeQuotes(f.getEmailAddress()) + "%'");
-	
+
 			if (filterOn(f.getUserName()))
 				sql.addWhere("u.username LIKE '%" + Strings.escapeQuotes(f.getUserName()) + "%'");
-			
+
 			if (filterOn(f.getCompanyName()))
 				sql.addWhere("a.name LIKE '%" + Strings.escapeQuotes(f.getCompanyName()) + "%'");
+			
+			if (isNumeric(f.getSearch())) {
+				if (filterOn(f.getSearch())) {
+					sql.addWhere("u.id LIKE '" + f.getSearch() + "%'");
+				}
+			}			
 		}
 	}
-	
+
+	public boolean isNumeric(String input) {
+		try {
+			Integer.parseInt(input);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+
+	}
+
 	public ReportFilterUser getFilter() {
 		return filter;
 	}
@@ -77,6 +97,5 @@ public class ReportUser2 extends ReportActionSupport {
 	public void setFilter(ReportFilterUser filter) {
 		this.filter = filter;
 	}
-	
-	
+
 }
