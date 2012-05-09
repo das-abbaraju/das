26a196537400698e3622743e2a1a938f7d18a457
Tom Baker <tbaker@picsauditing.com>
2012-05-09 14:32:19 -0700

    PICS-4647: Operator Flag Changes email should have a total on it

--- deployments/upgrade.sql
+++ deployments/upgrade.sql
@@ -6,6 +6,52 @@
 -- SEE upgradeConfig.sql FOR CONFIG CHANGES
 -- -----------------------------------------------------------------------------------------------
 
+-- PICS-4647 Operator Flag Changes email template update for new totals
+update email_template t set t.body = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+</head>
+<body>
+<div style="width: 750px; background-color: #002240; padding: 0 15px 0 15px; margin: 0; color: #6699CC; font-weight: normal; font-family: sans-serif; font-size: 11px; border-bottom: 3px solid #4686bf; border-left: 1px solid #002240; border-right: 1px solid #002240;">
+&nbsp;</div>
+<div style="width: 750px; padding: 15px; margin: 0; background-color: white; line-height: 18px; font-family: Helvetica, Arial, sans-serif; font-size: 14px; border-left: 1px solid #002240; border-right: 1px solid #002240;">
+Below is a list of Operators who have had at least 10 total flag changes as well as 5% of their total flag colors change recently. Please review each one for errors. If you find a flag error, please work with your manager or IT to resolve the problems ASAP. If the flags are due to a valid change, then approve each one and consider reaching out to the operator\'s primary contact to discuss the impact of the changes.
+#if(${changes.size()} > 0)
+<h3 style="color: rgb(168, 77, 16)">Operator Flag Changes</h3>
+<div>
+<strong>Total Flag Differences: </strong>${totalFlagChanges}<strong>  |  Total Operators Affected: </strong>${changes.size()}
+</div>
+<table style="border-collapse: collapse; border: 2px solid #003768; background: #f9f9f9;">
+ <thead>
+  <tr style="vertical-align: middle; font-size: 13px;font-weight: bold; background: #003768; color: #FFF;">
+   <td style="border: 1px solid #e0e0e0; padding: 4px;">Operator</td>
+   <td style="border: 1px solid #e0e0e0; padding: 4px;">Changes</td>
+   <td style="border: 1px solid #e0e0e0; padding: 4px;">Percent</td>
+  </tr>
+ </thead>
+ <tbody>
+  #foreach( $change in $changes )
+  <tr style="margin:0px">
+   <td style="border: 1px solid #A84D10; vertical-align: middle; padding: 4px; font-size: 13px;"><a href="https://www.picsorganizer.com/ReportFlagChanges.action?filter.operator=${change.get(\'id\')}">${change.get(\'operator\')}</a></td>
+   <td style="border: 1px solid #A84D10; vertical-align: middle; padding: 4px; font-size: 13px;">${change.get(\'changes\')}</td>
+   <td style="border: 1px solid #A84D10; vertical-align: middle; padding: 4px; font-size: 13px;">${change.get(\'percent\')}%</td>
+  </tr>
+  #end
+ </tbody>
+</table>	
+#end
+<br />
+<TimeStampDisclaimer>
+</div>
+<div style="width: 750px; background-color: #002240; padding: 15px; margin: 0; color: #6699CC; font-weight: normal; font-family: sans-serif; font-size: 11px; border-top: 3px solid #4686bf; border-left: 1px solid #002240; border-right: 1px solid #002240;">
+Copyright &copy; 2012 PICS
+<a href="http://www.picsauditing.com/" style="font-size: 11px; color: #6699CC; padding: 2px;">http://www.picsauditing.com</a> |
+<a href="http://www.picsauditing.com/app/privacy_policy.jsp" style="font-size: 11px; color: #6699CC; padding: 2px;">Privacy Policy</a>
+</div>
+</body>
+</html>' where t.id = 55;
+--
+
 -- PICS-5567 COHS Stattistics table
 update pqfdata pd 
 join contractor_audit ca on ca.id = pd.auditID 
