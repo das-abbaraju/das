2de92245da45f12d9a2e686cff7a074f1b4039e3
jbrownell <jbrownell@picsauditing.com>
2012-05-23 09:35:10 -0700

    PICS-4962: Removed the writer from report store as it is no longer used.

--- WebContent/js/pics/app/store/report/Reports.js
+++ WebContent/js/pics/app/store/report/Reports.js
@@ -1,6 +1,6 @@
 /**
  * Report Store
- * 
+ *
  * load backend report into local report database via ajax
  * sends backend report from local to server
  */
@@ -14,28 +14,14 @@ Ext.define('PICS.store.report.Reports', {
             root: 'report',
             type: 'json'
         },
-        type: 'ajax',
-        writer: {
-        	xtype: 'writer.base',
-        	write: function(request) {
-        	    // TODO
-        		// See http://docs.sencha.com/ext-js/4-0/source/Json3.html#Ext-data-writer-Json
-        		// writeRecords
-        		var report = request.records[0];
-        		
-                request.params['report.parameters'] = report.parameters;
-                request.url = 'ReportDynamic!save.action?report=' + report.getId();
-                
-                return request;
-        	}
-        }
+        type: 'ajax'
     },
-    
+
     constructor: function () {
         var url = Ext.Object.fromQueryString(document.location.search);
-        
+
         this.proxy.url = 'ReportDynamic!getReportParameters.action?report=' + url.report;
-        
+
         this.callParent(arguments);
     }
 });
