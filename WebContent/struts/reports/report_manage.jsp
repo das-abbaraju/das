0c9ee33d7f11df6b593107cc62766cac77893d2b
Lei Wang <lwang@picsauditing.com>
2012-05-24 16:14:49 -0700

    PICS-4962, allow user to delete report under manage report.  needs to go back and implement the permission setting when permission for report is in place.

--- WebContent/struts/reports/report_manage.jsp
+++ WebContent/struts/reports/report_manage.jsp
@@ -1,12 +1,29 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<%@ taglib prefix="pics" uri="pics-taglib" %>
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
+    <title><s:property value="report.summary"/></title>
+    <!-- <link rel="stylesheet" type="text/css" href="js/pics/resources/css/my-ext-theme.css"> -->
+    <link rel="stylesheet" type="text/css" href="js/pics/resources/css/my-ext-custom.css?v=${version}">
 </head>
 <body>
-report manage
+	<div id="reportTable">
+		<table>
+			<tr>
+				<Th>Report Name</Th>
+				<th></th>
+			</tr>
+		    <s:iterator value="reportsByUser">
+			    <tr>
+			    	<td>
+			    		<s:property value="name" /> created by <s:property value="user.createdBy.name" />
+		    		</td>
+					<td>
+						<a href="ManageReports!deleteReport.action?reportID=<s:property value="id" />">remove</a>
+					</td>
+				</tr>
+			</s:iterator>
+		</table>
+	</div>
 </body>
-</html>
--- src/com/picsauditing/actions/report/ManageReports.java
+++ src/com/picsauditing/actions/report/ManageReports.java
@@ -1,32 +1,70 @@
 package com.picsauditing.actions.report;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.picsauditing.access.NoRightsException;
+import com.picsauditing.access.OpPerms;
+import com.picsauditing.access.OpType;
+import com.picsauditing.access.RequiredPermission;
+import com.picsauditing.access.UserAccess;
 import com.picsauditing.actions.PicsActionSupport;
+import com.picsauditing.dao.UserDAO;
 import com.picsauditing.jpa.entities.Report;
+import com.picsauditing.jpa.entities.User;
 
 @SuppressWarnings("serial")
 public class ManageReports extends PicsActionSupport {
+	private User user;
 	private List<Report> reportsByUser = new ArrayList<Report>();
-	
+	private int reportID;
+
+	@Autowired
+	protected UserDAO userDAO;
+
 	public String execute() throws Exception {
 		super.execute();
-		
-		setReportsByUser(null);
-		
-		return SUCCESS;
-	}
-	
-	public String deleteReport(int ReportID){
+		setUser(permissions.getUserId());
 		
 		return SUCCESS;
 	}
 	
-	private void setReportsByUser(List<Report> reportsByUser){
-		this.reportsByUser = reportsByUser;
+	public String deleteReport() throws NoRightsException {
+		
+		setUser(permissions.getUserId());
+		reportsByUser = user.getReports();
+		for (int i = 0; i < reportsByUser.size(); i++) {
+			if (reportsByUser.get(i).getId() == reportID) {
+				// remove from the DB
+				dao.remove(reportsByUser.get(i));
+				// remove from the list
+				reportsByUser.remove(i);
+			}
+		}
+		return SUCCESS;
 	}
-	public List<Report> getReportsByUser(){		
-		return reportsByUser;			
+
+	private void setUser(int userId) {
+		this.user = userDAO.find(userId);
 	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public List<Report> getReportsByUser() {
+		return user.getReports();
+	}
+
+	public int getReportID() {
+		return reportID;
+	}
+
+	public void setReportID(int reportID) {
+		this.reportID = reportID;
+	}
+
 }
