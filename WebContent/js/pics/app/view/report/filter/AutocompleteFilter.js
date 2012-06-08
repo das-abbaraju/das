09d5ded5267d83ae48e91d8fccd9e5ba2257338d
Joel Brownell <jbrownell@picsauditing.com>
2012-06-08 11:44:24 -0700

    PICS-4962: Updated autocomplete filter.

--- WebContent/js/pics/app/view/report/filter/AutocompleteFilter.js
+++ WebContent/js/pics/app/view/report/filter/AutocompleteFilter.js
@@ -17,49 +17,61 @@ Ext.define('PICS.view.report.filter.AutocompleteFilter', {
         this.child('panel displayfield[name=filterName]').setValue(this.record.get('name'));
 
         this.createFilterCombo();
-   },
+    },
 
-   createFilterCombo: function () {
-       var me = this;
+    createFilterCombo: function () {
+        var me = this;
 
-       var url = Ext.Object.fromQueryString(document.location.search);
+        var url = Ext.Object.fromQueryString(document.location.search);
 
-       var listFilter = {
-            xtype: 'panel',
-            items: [{
-                xtype: 'combo',
-                allowBlank: false,
-                displayField: 'name',
-                forceSelection: true,
+        var list_filter = Ext.create('Ext.ux.form.BoxSelect', {
+            displayField: 'name',
+            delimiter: ',',
+            forceSelection: true,
+            hideTrigger: true,
+            listeners: {
+                beforequery: function (queryEvent, options) {
+                    //prevent empty queries
+                    if (!queryEvent.query) {
+                        return false;
+                    }
+                },
+                change: function (obj, newval, oldval, options) {
+                    newval = newval.replace(/\s/g,"");
+                    this.up('autocompletefilter').record.set('value', newval);
+                }
+            },
+            margin: '0 5 0 0',
+            multiSelect: true,
+            name: 'listValue',
+            minChars: 1,
+            mode: 'remote',
+            queryParam: 'searchQuery',
+            store: {
+                fields: [
+                    {name: 'id', type: 'string'},
+                    {name: 'name',  type: 'string'}
+                ],
+                proxy: {
+                    type: 'ajax',
+                    url: 'ReportDynamic!list.action?report=' + url.report + '&fieldName=' + this.record.get('name'),
+                    reader: {
+                        root: 'result',
+                        type: 'json'
+                    }
+                },
                 listeners: {
-                    change: function (obj, newval, oldval, options) {
-                       this.up('autocompletefilter').record.set('value', newval);
+                    load: function () {
+                        //set valueField to pull from json id
+                        me.child('boxselect').valueField = 'id';
                     }
-                },
-                margin: '0 5 0 0',
-                multiSelect: true,
-                name: 'listValue',
-                minChars: 1,
-                mode: 'remote',
-                queryParam: 'searchQuery',
-                store: {
-                   fields: [
-                       {name: 'id', type: 'string'},
-                       {name: 'name',  type: 'string'}
-                    ],
-                    proxy: {
-                        type: 'ajax',
-                        url: 'ReportDynamic!list.action?report=' + url.report + '&fieldName=' + this.record.get('name'),
-                        reader: {
-                            root: 'result',
-                            type: 'json'
-                        }
-                    }
-                },
-                valueField: 'id'
-            }],
-            layout: 'hbox'
-        };
-        this.add(listFilter);
-   }
+                }
+            },
+            typeAhead: false,
+            valueField: 'searchQuery',
+            value: this.record.get('value')
+        });
+
+        this.add(list_filter);
+    }
 });
