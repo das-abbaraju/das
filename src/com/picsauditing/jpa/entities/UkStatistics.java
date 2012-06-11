8d59455120663b09e6988060a099bfaa325fbe18
Kirk Chase <kchase@picsauditing.com>
2012-06-11 11:43:56 -0700

    PICS-6016 Unable to Complete/Approve UK Annual Update -- added UK HSE verification

--- src/com/picsauditing/jpa/entities/UkStatistics.java
+++ src/com/picsauditing/jpa/entities/UkStatistics.java
@@ -1,18 +1,36 @@
 package com.picsauditing.jpa.entities;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 public class UkStatistics extends SafetyStatistics{
-	
+
+	private List<AuditData> questionsToVerify = new ArrayList<AuditData>();
+	private int[] questionIdsToVerify = {7691, 8867, 8868, 8869, 8870, 8871, 8872, 9967, 9968, 12033};
+
 	public UkStatistics(int year, List<AuditData> data) {
 		super(year, OshaType.UK_HSE, data);
+		
+		Arrays.sort(questionIdsToVerify);
 		
 		answerMap = new HashMap<OshaRateType, AuditData>();
 		for (AuditData answer: data) {
 			if (answer.getQuestion().getId() == 9060) {
 				answerMap.put(OshaRateType.IFR, answer);
 			}
+			if (answer.getQuestion().getId() == 9099) {
+				answerMap.put(OshaRateType.Hours, answer);
+			}
+			if (answer.getQuestion().getId() == 8873) {
+				answerMap.put(OshaRateType.FileUpload, answer);
+				fileUpload = answer;
+			}
+			if (Arrays.binarySearch(questionIdsToVerify, answer.getQuestion().getId()) > 0) {
+				questionsToVerify.add(answer);
+			}
+			
 		}	
 	}
 
@@ -23,7 +41,6 @@ public class UkStatistics extends SafetyStatistics{
 
 	@Override
 	public List<AuditData> getQuestionsToVerify() {
-		// TODO Auto-generated method stub
-		return null;
+		return questionsToVerify;
 	}
 }
