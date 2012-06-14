62d5c55b6374d6d22957ab7e7a8ccaabe78521a9
Lani Aung <uaung@picsauditing.com>
2012-06-13 17:00:43 -0700

    PICS-6048: Need to pass Date as long instead of string for deserializing.

--- src/com/picsauditing/messaging/FlagChange.java
+++ src/com/picsauditing/messaging/FlagChange.java
@@ -16,50 +16,60 @@ public class FlagChange implements JSONable, EnterpriseMessage {
 	private FlagColor toColor;
 	private Date timestamp;
 	private String details;
-	
+
 	public ContractorAccount getContractor() {
 		return contractor;
 	}
+
 	public void setContractor(ContractorAccount contractor) {
 		this.contractor = contractor;
 	}
+
 	public OperatorAccount getOperator() {
 		return operator;
 	}
+
 	public void setOperator(OperatorAccount operator) {
 		this.operator = operator;
 	}
+
 	public FlagColor getFromColor() {
 		return fromColor;
 	}
+
 	public void setFromColor(FlagColor fromColor) {
 		this.fromColor = fromColor;
 	}
+
 	public FlagColor getToColor() {
 		return toColor;
 	}
+
 	public void setToColor(FlagColor toColor) {
 		this.toColor = toColor;
 	}
+
 	public Date getTimestamp() {
 		return timestamp;
 	}
+
 	public void setTimestamp(Date timestamp) {
 		this.timestamp = timestamp;
 	}
-	
+
 	public void setDetails(String details) {
 		this.details = details;
 	}
+
 	public String getDetails() {
 		return details;
 	}
-	
+
 	@Override
 	public void fromJSON(JSONObject o) {
-		
+
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public JSONObject toJSON(boolean full) {
@@ -68,16 +78,16 @@ public class FlagChange implements JSONable, EnterpriseMessage {
 		json.put("operator", operator.toJSON(full));
 		json.put("fromColor", fromColor.toString());
 		json.put("toColor", toColor.toString());
-		json.put("timestamp", timestamp.toString());
+		json.put("timestamp", timestamp.getTime());
 		json.put("details", details);
-		
+
 		return json;
 	}
-	
+
 	public JSONObject toJSON() {
 		return toJSON(false);
 	}
-	
+
 	@Override
 	public String getMessage() {
 		return toJSON().toJSONString();
