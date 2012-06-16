ad3030fb5da31866734637e44a981a6c5d8be78f
Tom Baker <tbaker@picsauditing.com>
2012-06-15 17:11:01 -0700

    PICS-6048: Fixing unit test

--- tests/com/picsauditing/messaging/PublisherRabbitMqTest.java
+++ tests/com/picsauditing/messaging/PublisherRabbitMqTest.java
@@ -2,16 +2,21 @@ package com.picsauditing.messaging;
 
 import static org.mockito.Mockito.*;
 
+import java.util.Date;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
+import com.picsauditing.jpa.entities.ContractorAccount;
+import com.picsauditing.jpa.entities.FlagColor;
+import com.picsauditing.jpa.entities.OperatorAccount;
+
 
 public class PublisherRabbitMqTest {
 	private PublisherRabbitMq publisherRabbitMq;
-	@Mock private EnterpriseMessage message;
 	@Mock private RabbitTemplate amqpTemplate;
 	
 	@Before
@@ -20,15 +25,24 @@ public class PublisherRabbitMqTest {
 		
 		publisherRabbitMq = new PublisherRabbitMq();
 		publisherRabbitMq.setAmqpTemplate(amqpTemplate);
-		
-		when(message.getMessage()).thenReturn("message");
 	}
 
 	@Test
 	public void testSendCriticalAlertMessage() throws Exception {
-		publisherRabbitMq.publish(message);
+		FlagChange fc = getFlagChange();
+		publisherRabbitMq.publish(fc);
 		
-		verify(amqpTemplate, times(0)).convertAndSend(message.getMessage());
+		verify(amqpTemplate, times(0)).convertAndSend(fc);
 	}
 
+	public FlagChange getFlagChange() {
+		FlagChange fc = new FlagChange();
+		fc.setContractor(new ContractorAccount(3));
+		fc.setOperator(new OperatorAccount());
+		fc.setFromColor(FlagColor.Red);
+		fc.setToColor(FlagColor.Green);
+		fc.setTimestamp(new Date());
+		
+		return fc;
+	}
 }
