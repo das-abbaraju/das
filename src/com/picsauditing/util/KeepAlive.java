2d1094d1f6365341bb8397fa31635a353489736e
Lani Aung <uaung@picsauditing.com>
2012-06-21 15:31:45 -0700

    PICS-6162 -- Remove Login.action check from KeepAlive

--- src/com/picsauditing/util/KeepAlive.java
+++ src/com/picsauditing/util/KeepAlive.java
@@ -2,10 +2,7 @@ package com.picsauditing.util;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.OperatingSystemMXBean;
-import java.net.URL;
-import java.net.URLConnection;
 
-import javax.net.ssl.SSLHandshakeException;
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
@@ -17,17 +14,14 @@ public class KeepAlive {
 	private final static Logger logger = LoggerFactory.getLogger(KeepAlive.class);
 
 	private float loadFactor = 3f;
-	private double timeoutInSeconds = 3.0;
 
 	private HttpServletRequest request;
 	private OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
 	private Database database = new Database();
-	private URLConnector urlConnector = new URLConnector();
 
 	public static final String SYSTEM_OK = "SYSTEM OK";
 	public static final String SYSTEM_LOAD = "SYSTEM LOAD = %.2f";
 	public static final String DATABASE_UNACCESSIBLE = "DATABASE UNACCESSIBLE";
-	public static final String PAGE_TIMED_OUT = "PAGE TIMED OUT";
 
 	public KeepAlive(HttpServletRequest request) {
 		this.request = request;
@@ -35,16 +29,12 @@ public class KeepAlive {
 
 	public String getKeepAliveStatus() {
 		setLoadFactor();
-		setTimeoutInSeconds();
 		double systemLoadAverage = operatingSystemMXBean.getSystemLoadAverage();
 
 		if (systemLoadAverage > loadFactor) {
 			return String.format(SYSTEM_LOAD, systemLoadAverage);
 		} else if (!isDatabaseAccessible()) {
 			return DATABASE_UNACCESSIBLE;
-			// Because of PICS-6112, I am temporarily commenting this code out
-//		} else if (!isSiteLoadedBeforeTimeout()) {
-//			return PAGE_TIMED_OUT;
 		}
 
 		return SYSTEM_OK;
@@ -66,22 +56,6 @@ public class KeepAlive {
 		}
 	}
 
-	public double getTimeoutInSeconds() {
-		return timeoutInSeconds;
-	}
-
-	public void setTimeoutInSeconds() {
-		String[] timeout = request.getParameterValues("timeout");
-
-		try {
-			if (timeout != null && timeout.length > 0) {
-				timeoutInSeconds = Double.parseDouble(timeout[0].toString());
-			}
-		} catch (Exception e) {
-			logger.error(e.getMessage());
-		}
-	}
-
 	private boolean isDatabaseAccessible() {
 		try {
 			return database.execute("SELECT 1");
@@ -90,50 +64,5 @@ public class KeepAlive {
 		}
 
 		return false;
-	}
-
-	private boolean isSiteLoadedBeforeTimeout() {
-		if (urlConnector.connect(getLoginPageForEnvironment())) {
-			return true;
-		}
-
-		return false;
-	}
-
-	private String getLoginPageForEnvironment() {
-		String requestURL = request.getRequestURL().toString();
-		String requestURI = request.getRequestURI();
-
-		return requestURL.replaceAll(requestURI, "") + "/Login.action";
-	}
-
-	public class URLConnector {
-		public boolean connect(String urlToConnect) {
-			boolean sslException = false;
-
-			try {
-				URL url = new URL(urlToConnect);
-				URLConnection urlConnection = url.openConnection();
-				urlConnection.setReadTimeout((int) (timeoutInSeconds * 1000));
-				urlConnection.setConnectTimeout((int) (timeoutInSeconds * 1000));
-				urlConnection.getContent();
-
-				return true;
-			} catch (SSLHandshakeException e) {
-				sslException = true;
-			} catch (Exception e) {
-				logger.error(e.getMessage());
-			}
-
-			if (sslException) {
-				// Try HTTP instead...
-				String nonSslUrl = urlToConnect.replace("https", "http");
-				nonSslUrl = nonSslUrl.replace("8443", "8080");
-				nonSslUrl = nonSslUrl.replace("81", "80");
-				return connect(nonSslUrl);
-			}
-
-			return false;
-		}
 	}
 }
