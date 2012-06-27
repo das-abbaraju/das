1eca13e2329f01f53957ce4421c3a531e48ceaba
Kirk Chase <kchase@picsauditing.com>
2012-06-27 13:30:03 -0700

    PICS-6188 Notes regarding a flag status change show codes instead of actual description -- update Note records

--- deployments/upgrade.sql
+++ deployments/upgrade.sql
@@ -5,3 +5,24 @@
 -- REFER TO config_tables.txt FOR A FULL LIST OF CONFIG TABLES
 -- SEE upgradeConfig.sql FOR CONFIG CHANGES
 -- -----------------------------------------------------------------------------------------------
+
+-- PICS-6188 Update Notes
+Update note n 
+join app_translation a on a.msgKey='FlagCriteria.837.description' and a.locale='en' 
+set n.body=a.msgValue 
+where n.body='FlagCriteria.837.description'; 
+
+Update note n 
+join app_translation a on a.msgKey='FlagCriteria.833.description' and a.locale='en' 
+set n.body=a.msgValue 
+where n.body='FlagCriteria.833.description'; 
+
+Update note n 
+join app_translation a on a.msgKey='FlagCriteria.832.description' and a.locale='en' 
+set n.body=a.msgValue 
+where n.body='FlagCriteria.832.description'; 
+
+Update note n 
+join app_translation a on a.msgKey='FlagCriteria.796.description' and a.locale='en' 
+set n.body=a.msgValue 
+where n.body='FlagCriteria.796.description';
