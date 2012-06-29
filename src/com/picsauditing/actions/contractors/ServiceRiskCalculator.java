8095854f9a6de1e65280287087ab95d95d8ba51e
Lani Aung <uaung@picsauditing.com>
2012-06-29 10:19:39 -0700

    PICS-6050 -- Risk Ranking Questions. Reformatting.

--- src/com/picsauditing/actions/contractors/ServiceRiskCalculator.java
+++ src/com/picsauditing/actions/contractors/ServiceRiskCalculator.java
@@ -1,6 +1,8 @@
 package com.picsauditing.actions.contractors;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.picsauditing.jpa.entities.AuditData;
 import com.picsauditing.jpa.entities.LowMedHigh;
@@ -8,6 +10,10 @@ import com.picsauditing.jpa.entities.YesNo;
 import com.picsauditing.util.Strings;
 
 public class ServiceRiskCalculator {
+	// Risk types
+	public static final String SAFETY = "Safety";
+	public static final String PRODUCT = "Product";
+	public static final String TRANSPORTATION = "Transportation";
 	// Service Safety Evaluation
 	public static final int SAFETY_CONDUCTED_FROM_OFFICE = 12341;
 	public static final int SAFETY_HAND_POWER_PNEUMATIC_TOOLS = 12342;
@@ -17,6 +23,10 @@ public class ServiceRiskCalculator {
 	public static final int SAFETY_PERFORMS_HIGH_RISK = 12346;
 	// Self Service Safety Evaluation
 	public static final int SAFETY_LEVEL_HAZARD_EXPOSURE = 12347;
+
+	public final int[] SAFETY_QUESTIONS = new int[] { SAFETY_CONDUCTED_FROM_OFFICE, SAFETY_HAND_POWER_PNEUMATIC_TOOLS,
+			SAFETY_PERSONAL_PROTECTIVE_EQUIPMENT, SAFETY_PERMIT_TO_WORK, SAFETY_MOBILE_EQUIPMENT,
+			SAFETY_PERFORMS_HIGH_RISK, SAFETY_LEVEL_HAZARD_EXPOSURE };
 
 	public LowMedHigh getRiskLevel(AuditData auditData) {
 		switch (auditData.getQuestion().getId()) {
@@ -34,18 +44,35 @@ public class ServiceRiskCalculator {
 		}
 	}
 
-	public LowMedHigh getHighestRiskLevel(List<AuditData> auditDatas) {
-		LowMedHigh highestRisk = LowMedHigh.None;
-
+	public Map<String, LowMedHigh> getHighestRiskLevel(List<AuditData> auditDatas) {
+		Map<String, LowMedHigh> highestRisks = new HashMap<String, LowMedHigh>();
+		
 		for (AuditData auditData : auditDatas) {
+			if (isSafetyQuestion(auditData.getQuestion().getId())) {
+				if (highestRisks.containsKey(SAFETY)) {
+					LowMedHigh currentSafety = getRiskLevel(auditData);
+				}
+			}
+			
 			LowMedHigh risk = getRiskLevel(auditData);
+			LowMedHigh highestRisk = LowMedHigh.None;
 
 			if (risk.ordinal() > highestRisk.ordinal()) {
 				highestRisk = risk;
 			}
 		}
 
-		return highestRisk;
+		return highestRisks;
+	}
+	
+	private boolean isSafetyQuestion(int auditQuestionID) {
+		for (int safetyQuestion : SAFETY_QUESTIONS) {
+			if (auditQuestionID == safetyQuestion) {
+				return true;
+			}
+		}
+		
+		return false;
 	}
 
 	private LowMedHigh determineRiskLevel(String answer, LowMedHigh yes, LowMedHigh no) {
