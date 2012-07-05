2b3609b5e27a39e27d2a169f5c47f5f8c7d41447
gmeurer <gmeurer@picsauditing.com>
2012-07-05 15:58:00 -0600

    test improvement
    setup of springockito and a spring runner test for lei

--- tests/com/picsauditing/mail/EventSubscriptionBuilderTest.java
+++ tests/com/picsauditing/mail/EventSubscriptionBuilderTest.java
@@ -6,9 +6,13 @@ import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.powermock.reflect.Whitebox;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.picsauditing.actions.TranslationActionSupport;
 import com.picsauditing.dao.EmailSubscriptionDAO;
@@ -19,33 +23,26 @@ import com.picsauditing.jpa.entities.EmailTemplate;
 import com.picsauditing.jpa.entities.Note;
 import com.picsauditing.util.SpringUtils;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"EventSubscriptionBuilderTest-context.xml"})
 public class EventSubscriptionBuilderTest {
 	private EventSubscriptionBuilder eventSubscriptionBuilder;
 	
-	@Mock
-	private ContractorAudit audit;
-	@Mock
-	private EmailBuilder emailBuilder;
-	@Mock
-	private EmailQueue emailQueue;
-	@Mock
-	private EmailSenderSpring emailSender;
-	@Mock
-	private TranslationActionSupport tas;
-	@Mock
-	private EmailTemplate et;		
-	@Mock
-	protected NoteDAO noteDAO;
-	@Mock
-	private SpringUtils springUtils;
-		
+	@Mock private ContractorAudit audit;
+	@Mock private EmailBuilder emailBuilder;
+	@Mock private EmailQueue emailQueue;
+	@Mock private TranslationActionSupport tas;
+	@Mock private EmailTemplate et;		
+
+	@Autowired private EmailSenderSpring emailSender;
+	@Autowired private NoteDAO noteDAO;
+	@Autowired private EmailSubscriptionDAO emailSubscriptionDAO;
+	
 	@Before
 	public void setUp() throws Exception {
 	
-//		MockitoAnnotations.initMocks(this);
-//
-//		eventSubscriptionBuilder = new EventSubscriptionBuilder();
-//	
+		MockitoAnnotations.initMocks(this);
+
 //		when(tas.getText(any(String.class))).thenReturn(any(String.class));
 //		verify(et).setSubject(any(String.class));
 //		verify(et).setBody(any(String.class));		
@@ -69,10 +66,9 @@ public class EventSubscriptionBuilderTest {
 
 	@Test
 	public void testNotifyUpcomingImplementationAudit() throws Exception {
-//		Whitebox.invokeMethod(eventSubscriptionBuilder, "notifyUpcomingImplementationAudit", audit);		
+		EventSubscriptionBuilder.notifyUpcomingImplementationAudit(audit);
 //		verify(emailSender).send(emailQueue);
 //		verify(noteDAO).save(any(Note.class));
-		
 	}
 	
 	@Test
