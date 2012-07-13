ffe064662065c53e4f756b68f466b90fbe850c9a
Mike Nichols <mnichols@picsauditing.com>
2012-07-12 17:59:24 -0700

    Removing indentation.

--- src/com/picsauditing/report/tables/JpaFieldExtractor.java
+++ src/com/picsauditing/report/tables/JpaFieldExtractor.java
@@ -12,58 +12,65 @@ import com.picsauditing.report.fields.Field;
 import com.picsauditing.util.Strings;
 
 public class JpaFieldExtractor {
-	static public Set<Field> addFields(Class<?> clazz, String prefix, String alias) {
-		// TODO: Revise this to allow for super class methods
+	public static Set<Field> addFields(Class<?> clazz, String prefix, String alias) {
 		Set<Field> fields = new HashSet<Field>();
+
 		for (Method method : clazz.getMethods()) {
-			if (clazz.equals(method.getDeclaringClass())) {
-				ReportField fieldAnnotation = getReportFieldAnnotation(method);
-				if (fieldAnnotation != null) {
-					Field field = new Field(fieldAnnotation);
-					String fieldName = stripGetFromMethodName(method.getName());
-					field.setFieldClass(method.getReturnType());
-					field.setName(prefix + fieldName);
-					fields.add(field);
-					if (!Strings.isEmpty(fieldAnnotation.sql())) {
-						field.setDatabaseColumnName(fieldAnnotation.sql().replace("{ALIAS}", alias));
-					} else {
-						Column columnAnnotation = getColumnAnnotation(method);
-						if (columnAnnotation == null || Strings.isEmpty(columnAnnotation.name())) {
-							field.setDatabaseColumnName(alias + "." + fieldName.toLowerCase());
-						} else {
-							field.setDatabaseColumnName(alias + "." + columnAnnotation.name());
-						}
-					}
-				}
+			// TODO: Revise this to allow for super class methods
+			if (!clazz.equals(method.getDeclaringClass()))
+				continue;
+
+			ReportField fieldAnnotation = getReportFieldAnnotation(method);
+			if (fieldAnnotation == null)
+				continue;
+
+			Field field = new Field(fieldAnnotation);
+			String fieldName = stripGetFromMethodName(method.getName());
+			field.setFieldClass(method.getReturnType());
+			field.setName(prefix + fieldName);
+			fields.add(field);
+
+			if (!Strings.isEmpty(fieldAnnotation.sql())) {
+				field.setDatabaseColumnName(fieldAnnotation.sql().replace("{ALIAS}", alias));
+				continue;
+			}
+
+			Column columnAnnotation = getColumnAnnotation(method);
+			if (columnAnnotation == null || Strings.isEmpty(columnAnnotation.name())) {
+				field.setDatabaseColumnName(alias + "." + fieldName.toLowerCase());
+			} else {
+				field.setDatabaseColumnName(alias + "." + columnAnnotation.name());
 			}
 		}
+
 		return fields;
 	}
 
-	static private String stripGetFromMethodName(String methodName) {
+	private static String stripGetFromMethodName(String methodName) {
 		if (methodName.startsWith("get"))
 			return methodName.substring(3);
+
 		if (methodName.startsWith("is"))
 			return methodName.substring(2);
+
 		return methodName;
 	}
 
-	static private ReportField getReportFieldAnnotation(Method method) {
+	private static ReportField getReportFieldAnnotation(Method method) {
 		for (Annotation annotation : method.getAnnotations()) {
-			if (annotation.annotationType().equals(ReportField.class)) {
+			if (annotation.annotationType().equals(ReportField.class))
 				return (ReportField) annotation;
-			}
 		}
+
 		return null;
 	}
 
-	static private Column getColumnAnnotation(Method method) {
+	private static Column getColumnAnnotation(Method method) {
 		for (Annotation annotation : method.getAnnotations()) {
-			if (annotation.annotationType().equals(Column.class)) {
+			if (annotation.annotationType().equals(Column.class))
 				return (Column) annotation;
-			}
 		}
+
 		return null;
 	}
-
 }
