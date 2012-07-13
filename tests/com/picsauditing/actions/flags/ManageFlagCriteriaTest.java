591d29522be21c4a7572027a29d3db722f929e06
Kirk Chase <kchase@picsauditing.com>
2012-07-13 14:36:46 -0700

    PICS-4613 Keep Flag Criteria Management from introducing errors

--- tests/com/picsauditing/actions/flags/ManageFlagCriteriaTest.java
+++ tests/com/picsauditing/actions/flags/ManageFlagCriteriaTest.java
@@ -1,26 +1,26 @@
 package com.picsauditing.actions.flags;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.MockitoAnnotations;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import com.picsauditing.EntityFactory;
 import com.picsauditing.PicsTest;
 import com.picsauditing.jpa.entities.FlagCriteria;
 import com.picsauditing.jpa.entities.TranslatableString;
-import static org.mockito.Mockito.when;
 
 public class ManageFlagCriteriaTest extends PicsTest {
 	ManageFlagCriteria manageFlagCriteria;
-	
+
 	@Mock
 	TranslatableString emptyTranslation;
-	
+
 	@Mock
 	TranslatableString somethingTranslation;
-	
 
 	@Before
 	public void setUp() throws Exception {
@@ -29,21 +29,21 @@ public class ManageFlagCriteriaTest extends PicsTest {
 
 		manageFlagCriteria = new ManageFlagCriteria();
 		autowireEMInjectedDAOs(manageFlagCriteria);
-		
+
 		when(somethingTranslation.toString()).thenReturn("Test translation");
 	}
 
 	@Test
 	public void testSave_InputValidation() throws Exception {
 		FlagCriteria flagCriteria = new FlagCriteria();
-		
+
 		flagCriteria.setLabel(somethingTranslation);
 		flagCriteria.setDescription(somethingTranslation);
 		flagCriteria.setCategory("Audits");
 		flagCriteria.setRequiredLanguages("[\"en\"]");
 		flagCriteria.setDefaultValue("test");
 		manageFlagCriteria.setCriteria(flagCriteria);
-		
+
 		// minimum valid criteria
 		manageFlagCriteria.save();
 		assertTrue(!manageFlagCriteria.hasActionErrors());
@@ -53,6 +53,50 @@ public class ManageFlagCriteriaTest extends PicsTest {
 		manageFlagCriteria.save();
 		assertTrue(manageFlagCriteria.hasActionErrors());
 		flagCriteria.setCategory("Audits");
-}
+
+		// bad display order
+		flagCriteria.setDisplayOrder(-1);
+		manageFlagCriteria.save();
+		assertTrue(manageFlagCriteria.hasActionErrors());
+		flagCriteria.setDisplayOrder(999);
+
+		// bad label
+		flagCriteria.setLabel(emptyTranslation);
+		manageFlagCriteria.save();
+		assertTrue(manageFlagCriteria.hasActionErrors());
+		flagCriteria.setLabel(somethingTranslation);
+
+		// bad description
+		flagCriteria.setDescription(emptyTranslation);
+		manageFlagCriteria.save();
+		assertTrue(manageFlagCriteria.hasActionErrors());
+		flagCriteria.setDescription(somethingTranslation);
+
+		// bad data type
+		flagCriteria.setDataType("");
+		manageFlagCriteria.save();
+		assertTrue(manageFlagCriteria.hasActionErrors());
+		flagCriteria.setDataType("string");
+
+		// bad data type
+		flagCriteria.setDataType("boolean");
+		manageFlagCriteria.save();
+		assertTrue(manageFlagCriteria.hasActionErrors());
+		flagCriteria.setDataType("string");
+
+		// bad data type
+		flagCriteria.setDataType("number");
+		manageFlagCriteria.save();
+		assertTrue(manageFlagCriteria.hasActionErrors());
+		flagCriteria.setDataType("string");
+
+		// audit and question
+		flagCriteria.setAuditType(EntityFactory.makeAuditType(1));
+		flagCriteria.setQuestion(EntityFactory.makeAuditQuestion());
+		manageFlagCriteria.save();
+		assertTrue(manageFlagCriteria.hasActionErrors());
+		flagCriteria.setAuditType(null);
+		flagCriteria.setQuestion(null);
+	}
 
 }
