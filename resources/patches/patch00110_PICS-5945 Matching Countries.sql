9b35c9a13ebf0ecce914497334fff0805c7a0741
Michael Do <mdo@picsauditing.com>
2012-07-17 09:41:42 -0700

    PICS-5945 Corrected the script.

--- resources/patches/patch00110_PICS-5945 Matching Countries.sql
+++ resources/patches/patch00110_PICS-5945 Matching Countries.sql
@@ -1,6 +1,4 @@
 /*
-replace into ref_state (isoCode, countryCode, english) select concat(isoCode,'-'), isoCode, 'N/A' from ref_country;
-replace into ref_country_subdivision (isoCode, countryCode, english) select concat(isoCode,'-'), isoCode, 'N/A' from ref_country;
 insert into ref_country_subdivision (isoCode, countryCode, english) values ('PR', 'PR', 'Puerto Rico');
 insert into ref_country_subdivision (isoCode, countryCode, english) values ('GU', 'GU', 'Guam');
 insert into ref_country_subdivision (isoCode, countryCode, english) values ('UM', 'UM', 'United States Minor Islands');
