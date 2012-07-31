60b717a229753db479aef6cac2da3f95d6db5bac
Carey Hinoki <chinoki@picsauditing.com>
2012-07-30 18:26:46 -0700

    PICS-6334 Grouping header trigger hover

--- WebContent/v7/js/extjs/pics/app/view/report/available-field/AvailableFieldList.js
+++ WebContent/v7/js/extjs/pics/app/view/report/available-field/AvailableFieldList.js
@@ -11,11 +11,47 @@ Ext.define('PICS.view.report.available-field.AvailableFieldList', {
         flex: 1
     }],
     enableColumnHide: false,
-    hideHeaders: true,
-    id: 'available_field_list',
     features: Ext.create('Ext.grid.feature.Grouping', {
         groupHeaderTpl: 'Category: {name} <span class="number-of-items">({rows.length} item{[values.rows.length != 1 ? "s" : ""]})</span>'
     }),
+    hideHeaders: true,
+    id: 'available_field_list',
+    listeners: {
+        render: function (cmp, eOpts) {
+            this.mon(cmp.el, 'mouseover', function (event, html, eOpts) {
+                var class_names = this.getGroupClassNamesWithoutOver(html);
+
+                class_names.push('x-over');
+                html.className = class_names.join(' ');
+            }, cmp, {
+                delegate: '.x-grid-group-hd'
+            });
+
+            this.mon(cmp.el, 'mouseout', function (event, html, eOpts) {
+                var class_names = this.getGroupClassNamesWithoutOver(html);
+
+                html.className = class_names.join(' ');
+            }, cmp, {
+                delegate: '.x-grid-group-hd'
+            });
+        }
+    },
     rowLines: false,
-    selModel: Ext.create('Ext.selection.CheckboxModel')
+    selModel: Ext.create('Ext.selection.CheckboxModel'),
+
+    getGroupClassNamesWithoutOver: function (html) {
+        var class_names = html.className.split(' '),
+            class_names_length = class_names.length,
+            new_class_names = [];
+
+        while (class_names_length--) {
+            var class_name = class_names[class_names_length];
+
+            if (class_name != 'x-over') {
+                new_class_names.push(class_name);
+            }
+        }
+
+        return new_class_names;
+    }
 });
