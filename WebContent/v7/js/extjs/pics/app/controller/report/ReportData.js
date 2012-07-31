7836e91e0d0bd2a59e8475fcd0826b9f69792bb7
Carey Hinoki <chinoki@picsauditing.com>
2012-07-30 18:27:48 -0700

    PICS-6330 Controller refactor + update to display info for paging toolbar (# of results)

--- WebContent/v7/js/extjs/pics/app/controller/report/ReportData.js
+++ WebContent/v7/js/extjs/pics/app/controller/report/ReportData.js
@@ -1,6 +1,11 @@
 Ext.define('PICS.controller.report.ReportData', {
     extend: 'Ext.app.Controller',
 
+    refs: [{
+        ref: 'reportPagingToolbar',
+        selector: 'reportpagingtoolbar'
+    }],
+
     stores: [
         'report.Reports',
         'report.DataSets'
@@ -8,78 +13,96 @@ Ext.define('PICS.controller.report.ReportData', {
 
     init: function () {
         this.control({
-            'reportpagingtoolbar': {
-                beforerender: function (cmp, eOpts) {
-                    var report_data_store = this.getReportDataSetsStore();
-
-                    report_data_store.configureProxyUrl();
-                    report_data_store.load({
-                        callback: function () {
-                            cmp.updateDisplayInfo();
-                        }
-                    });
-                }
+            'reportdata': {
+                reconfigure: this.onReportReconfigure
             },
 
             'reportpagingtoolbar button[itemId=refresh]': {
-                click: function () {
-                    this.application.fireEvent('refreshreport');
-                }
+                click: this.onReportRefreshClick
             },
 
             'reportpagingtoolbar combo[name=rows_per_page]': {
-                select: function (combo, records, options) {
-                    this.getReportDataSetsStore().updateReportPaging(records[0].get('field1'));
-                }
+                select: this.onRowsPerPageSelect
             },
 
             'reportpagingtoolbar button[action=add-column]': {
-                click: function () {
-                    this.application.fireEvent('showavailablefieldmodal', 'column');
-                }
+                click: this.onAddColumn
             },
 
             'menu[name=report_data_header_menu] menuitem[name=sort_asc]': {
-                click: function (cmp, event, eOpts) {
-                    var sort_store = this.getReportReportsStore().first().sorts(),
-                        name = cmp.up('menu').activeHeader.dataIndex;
-
-                    sort_store.removeAll();
-                    sort_store.add(Ext.create('PICS.model.report.Sort', {
-                        name: name,
-                        direction: 'SUPERMAN' // lawl could be anything (ASC)
-                    }));
-
-                    this.application.fireEvent('refreshreport');
-                }
+                click: this.onColumnSortAsc
             },
 
             'menu[name=report_data_header_menu] menuitem[name=sort_desc]': {
-                click: function (cmp, event, eOpts) {
-                    var sort_store = this.getReportReportsStore().first().sorts(),
-                        name = cmp.up('menu').activeHeader.dataIndex;
-
-                    sort_store.removeAll();
-                    sort_store.add(Ext.create('PICS.model.report.Sort', {
-                        name: name,
-                        direction: 'DESC'
-                    }));
-
-                    this.application.fireEvent('refreshreport');
-                }
+                click: this.onColumnSortDesc
             },
 
             'menu[name=report_data_header_menu] menuitem[name=remove_column]': {
-                click: function (cmp, event, eOpts) {
-                    var column_store = this.getReportReportsStore().first().columns(),
-                        column_name = cmp.up('menu').activeHeader.dataIndex,
-                        column = column_store.findRecord('name', column_name);
-
-                    column_store.remove(column);
-
-                    this.application.fireEvent('refreshreport');
-                }
+                click: this.onColumnRemove
             }
         });
+
+        this.application.on({
+            refreshreportdisplayinfo: this.refreshReportDisplayInfo,
+            scope: this
+        });
+    },
+
+    onAddColumn: function (cmp, event, eOpts) {
+        this.application.fireEvent('showavailablefieldmodal', 'column');
+    },
+
+    onColumnRemove: function (cmp, event, eOpts) {
+        var column_store = this.getReportReportsStore().first().columns(),
+            column_name = cmp.up('menu').activeHeader.dataIndex,
+            column = column_store.findRecord('name', column_name);
+
+        column_store.remove(column);
+
+        this.application.fireEvent('refreshreport');
+    },
+
+    onColumnSortAsc: function (cmp, event, eOpts) {
+        var sort_store = this.getReportReportsStore().first().sorts(),
+            name = cmp.up('menu').activeHeader.dataIndex;
+
+        sort_store.removeAll();
+        sort_store.add(Ext.create('PICS.model.report.Sort', {
+            name: name,
+            direction: 'SUPERMAN' // lawl could be anything (ASC)
+        }));
+
+        this.application.fireEvent('refreshreport');
+    },
+
+    onColumnSortDesc: function (cmp, event, eOpts) {
+        var sort_store = this.getReportReportsStore().first().sorts(),
+            name = cmp.up('menu').activeHeader.dataIndex;
+
+        sort_store.removeAll();
+        sort_store.add(Ext.create('PICS.model.report.Sort', {
+            name: name,
+            direction: 'DESC'
+        }));
+
+        this.application.fireEvent('refreshreport');
+    },
+
+    onReportReconfigure: function (cmp, eOpts) {
+        this.application.fireEvent('refreshreportdisplayinfo');
+    },
+
+    onReportRefreshClick: function (cmp, event, eOpts) {
+        this.application.fireEvent('refreshreport');
+    },
+
+    onRowsPerPageSelect: function (cmp, records, options) {
+        this.getReportDataSetsStore().updateReportPaging(parseInt(cmp.getValue()));
+    },
+
+    refreshReportDisplayInfo: function () {
+        var report_paging_toolbar = this.getReportPagingToolbar();
+
+        report_paging_toolbar.updateDisplayInfo();
     }
 });
