0ced2d8803fceaf47ebdd9f5a45e0a74202bdbdc
Carey Hinoki <chinoki@picsauditing.com>
2012-08-07 19:22:27 -0700

    PICS-6163 Refactored report header load + update

--- WebContent/v7/js/extjs/pics/app/view/report/header/ReportSummary.js
+++ WebContent/v7/js/extjs/pics/app/view/report/header/ReportSummary.js
@@ -4,9 +4,16 @@ Ext.define('PICS.view.report.header.ReportSummary', {
 
     border: false,
     height: 90,
-    html: new Ext.Template([
-        '<h1 class="name"></h1>',
-        '<h2 class="description"></h2>'
-    ]),
-    id: 'report_summary'
+    id: 'report_summary',
+    tpl: '<h1 class="name">{name}</h1><h2 class="description">{description}</h2>',
+
+    update: function (report) {
+        if (!report || report.modelName != 'PICS.model.report.Report') {
+            throw 'Invalid report';
+        }
+
+        var data = report ? report.data : {};
+
+        this.callParent([data]);
+    }
 });
