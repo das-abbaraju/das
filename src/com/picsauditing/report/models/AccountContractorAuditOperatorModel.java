896b2b264cc5f5e680c5969bf04c90e6af89a593
Michael Do <mdo@picsauditing.com>
2012-08-28 14:32:30 -0700

    PICS-6668 Added a CAOP where clause generator, just as we currently have in our existing reports.

--- src/com/picsauditing/report/models/AccountContractorAuditOperatorModel.java
+++ src/com/picsauditing/report/models/AccountContractorAuditOperatorModel.java
@@ -1,6 +1,8 @@
 package com.picsauditing.report.models;
 
+import com.picsauditing.access.Permissions;
 import com.picsauditing.report.tables.ContractorAuditOperatorTable;
+import com.picsauditing.util.Strings;
 
 public class AccountContractorAuditOperatorModel extends AccountContractorAuditModel {
 	public AccountContractorAuditOperatorModel() {
@@ -10,5 +12,22 @@ public class AccountContractorAuditOperatorModel extends AccountContractorAuditM
 		rootTable.addAllFieldsAndJoins(caoTable);
 		
 		parentTable = caoTable;
+	}
+	
+	@Override
+	public String getWhereClause(Permissions permissions) {
+		String where = super.getWhereClause(permissions);
+
+		// TODO: This looks like it can be further improved. Find a way to do this without having to implode all of the ids.
+		if (permissions.isOperatorCorporate()) {
+			String opIDs = permissions.getAccountIdString();
+			if (permissions.isCorporate())
+				opIDs = Strings.implode(permissions.getOperatorChildren());
+
+			where += "\n AND cao.id IN (SELECT caoID FROM contractor_audit_operator_permission WHERE opID IN (" + opIDs
+					+ "))";
+		}
+		
+		return where;
 	}
 }
