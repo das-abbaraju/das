121c665f22a69da1125ac8ddacd3210d21142614
gmeurer <gmeurer@picsauditing.com>
2012-09-06 11:36:05 -0600

    fix BetaPoolTest (found as part of PICS-6058)

--- tests/com/picsauditing/access/BetaPoolTest.java
+++ tests/com/picsauditing/access/BetaPoolTest.java
@@ -28,27 +28,59 @@ public class BetaPoolTest {
 		assertEquals(BetaPool.BetaTester,BetaPool.getBetaPoolByBetaLevel(3));
 		assertEquals(BetaPool.Global,BetaPool.getBetaPoolByBetaLevel(4));
 	}
+
+	// if user is a developer, then features meant for anybody will be visible
 	@Test
-	public void testDeveloper() throws Exception {
-		// FIXME Why is this test called testDeveloper()?  There is not anything specifically Developer about it.
+	public void testIsUserBetaTester_UserIsGroupDeveloper() throws Exception {
+		permissions.getGroups().add(User.GROUP_DEVELOPER);
+
+		nobodySeesNoneAndEverybodySeesGlobal();
+		
+		assertTrue(BetaPool.isUserBetaTester(permissions, BetaPool.Developer));
+		assertTrue(BetaPool.isUserBetaTester(permissions, BetaPool.Stakeholder));
+		assertTrue(BetaPool.isUserBetaTester(permissions, BetaPool.BetaTester));
+	}
+
+	// if user is a stakeholder, then features meant for developers should not
+	// be visible to them
+	// however, things meant for beta testers and for global will be visible
+	@Test
+	public void testIsUserBetaTester_UserIsGroupStakeHolder() throws Exception {
 		permissions.getGroups().add(User.GROUP_STAKEHOLDER);
 
-		assertFalse(BetaPool.isUserBetaTester(permissions, BetaPool.None));
+		nobodySeesNoneAndEverybodySeesGlobal();
+
 		assertFalse(BetaPool.isUserBetaTester(permissions, BetaPool.Developer));
 		assertTrue(BetaPool.isUserBetaTester(permissions, BetaPool.Stakeholder));
 		assertTrue(BetaPool.isUserBetaTester(permissions, BetaPool.BetaTester));
-		assertTrue(BetaPool.isUserBetaTester(permissions, BetaPool.Global));
+	}
+
+	// if user is a betatester, then things meant for developers and
+	// stakeholders are not visible
+	@Test
+	public void testIsUserBetaTester_UserIsGroupBetaTest() throws Exception {
+		permissions.getGroups().add(User.GROUP_BETATESTER);
+
+		nobodySeesNoneAndEverybodySeesGlobal();
+
+		assertFalse(BetaPool.isUserBetaTester(permissions, BetaPool.Developer));
+		assertFalse(BetaPool.isUserBetaTester(permissions, BetaPool.Stakeholder));
+		assertTrue(BetaPool.isUserBetaTester(permissions, BetaPool.BetaTester));
 	}
 
 	@Test
-	public void testGlobal() throws Exception {
-		// FIXME How is this test different from testDeveloper()?  Other than doing less?  
-		// FIXME Why is this test called testGlobal()?  There is not anything specifically Global about it.
-		assertFalse(BetaPool.isUserBetaTester(permissions, BetaPool.None));
+	public void testIsUserBetaTester_NotAMemberOfAnyBetaGroup() throws Exception {
+		// only global should be true
+		nobodySeesNoneAndEverybodySeesGlobal();
 		assertFalse(BetaPool.isUserBetaTester(permissions, BetaPool.Developer));
-		assertTrue(BetaPool.isUserBetaTester(permissions, BetaPool.Global));
+		assertFalse(BetaPool.isUserBetaTester(permissions, BetaPool.Stakeholder));
 	}
 	
+	private void nobodySeesNoneAndEverybodySeesGlobal() {
+		assertFalse(BetaPool.isUserBetaTester(permissions, BetaPool.None));
+		assertTrue(BetaPool.isUserBetaTester(permissions, BetaPool.Global));
+	}
+
 	@Test
 	public void testGetBetaPoolByBetaLevel_NegativeNumbersReturnsNone() throws Exception {
 		BetaPool result = BetaPool.getBetaPoolByBetaLevel(-1);
@@ -63,14 +95,4 @@ public class BetaPoolTest {
 		assertThat(result, is(BetaPool.None));
 	}
 
-	@Test
-	public void testGetBetaPoolByBetaLevel_GoodNumberNotNone() throws Exception {
-		int numValues = BetaPool.values().length;
-		// FIXME - doing unit tests against random numbers is not a good practice.  Better would be to test the first and last values in the range (if not all of them)
-		int betaLevelToTry = new Random().nextInt(numValues);
-		
-		BetaPool result = BetaPool.getBetaPoolByBetaLevel(betaLevelToTry);
-		
-		assertNotNull(result);
-	}
 }
