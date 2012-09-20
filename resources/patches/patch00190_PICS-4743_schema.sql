47415f493f498bf38320cb3b03d9d4ef735cb6cd
Lani Aung <uaung@picsauditing.com>
2012-09-20 16:02:35 -0700

    PICS-6935 -- Migrate columns. Changes covered in 150

--- resources/patches/patch00190_PICS-4743_schema.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE `contractor_info`
-	CHANGE COLUMN `priority` `insideSalesPriority` VARCHAR(5) NULL DEFAULT NULL AFTER `welcomeAuditor_id`,
-	CHANGE COLUMN `closedOnDate` `expiresOnDate` DATE NULL DEFAULT NULL AFTER `reasonForDecline`,
-	ADD COLUMN `registrationHash` VARCHAR(100) NULL DEFAULT NULL AFTER `languageID`;
-
-ALTER TABLE `contractor_info`
-	DROP COLUMN `reasonForDecline`;
