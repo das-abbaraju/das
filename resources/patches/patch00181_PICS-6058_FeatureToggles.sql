65fc1585a36c4ea4f904f5ba084053f35dccdf3f
gmeurer <gmeurer@picsauditing.com>
2012-09-20 15:00:00 -0600

    per tech talk, s/replace/insert ignore/

--- resources/patches/patch00181_PICS-6058_FeatureToggles.sql
+++ resources/patches/patch00181_PICS-6058_FeatureToggles.sql
@@ -1,27 +1,27 @@
 alter table app_properties add column ticklerDate datetime;
 
 select @Badge:=value from app_properties where property = 'Toggle.Badge';
-replace into app_properties (property, value) values ('Toggle.Badge_v2', concat('releaseToUserAudienceLevel(',
+insert ignore into app_properties (property, value) values ('Toggle.Badge_v2', concat('releaseToUserAudienceLevel(',
 @Badge,
 ')'));
 
 select @DynamicReports:=value from app_properties where property = 'Toggle.DynamicReports';
-replace into app_properties (property, value) values ('Toggle.DynamicReports_v2', concat('releaseToUserAudienceLevel(',
+insert ignore into app_properties (property, value) values ('Toggle.DynamicReports_v2', concat('releaseToUserAudienceLevel(',
 @DynamicReports,
 ')'));
 
 select @LcCor:=value from app_properties where property = 'Toggle.LcCor';
-replace into app_properties (property, value) values ('Toggle.LcCor_v2', concat('releaseToApplicationAudienceLevel(',
+insert ignore into app_properties (property, value) values ('Toggle.LcCor_v2', concat('releaseToApplicationAudienceLevel(',
 @LcCor,
 ')'));
 
 select @LiveAgent:=value from app_properties where property = 'Toggle.LiveAgent';
-replace into app_properties (property, value) values ('Toggle.LiveAgent_v2', concat('releaseToUserAudienceLevel(',
+insert ignore into app_properties (property, value) values ('Toggle.LiveAgent_v2', concat('releaseToUserAudienceLevel(',
 @LiveAgent,
 ')'));
 
 select @SwitchUserServer:=value from app_properties where property = 'Toggle.SwitchUserServer';
-replace into app_properties (property, value) values ('Toggle.SwitchUserServer_v2', concat('releaseToUserAudienceLevel(',
+insert ignore into app_properties (property, value) values ('Toggle.SwitchUserServer_v2', concat('releaseToUserAudienceLevel(',
 @SwitchUserServer,
 ')'));
 
--- resources/patches/patch00230_PICS-4191_FlagHistoryWidget.sql
+++ resources/patches/patch00230_PICS-4191_FlagHistoryWidget.sql
@@ -1,40 +1,40 @@
-replace into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
+insert ignore into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
 values ('Month.Short.Jan', 'en', 'Jan', 1, 1, now(), now(), 'en', 0);
-replace into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
+insert ignore into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
 values ('Month.Short.Feb', 'en', 'Feb', 1, 1, now(), now(), 'en', 0);
-replace into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
+insert ignore into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
 values ('Month.Short.Mar', 'en', 'Mar', 1, 1, now(), now(), 'en', 0);
-replace into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
+insert ignore into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
 values ('Month.Short.Apr', 'en', 'Apr', 1, 1, now(), now(), 'en', 0);
-replace into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
+insert ignore into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
 values ('Month.Short.May', 'en', 'May', 1, 1, now(), now(), 'en', 0);
-replace into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
+insert ignore into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
 values ('Month.Short.Jun', 'en', 'Jun', 1, 1, now(), now(), 'en', 0);
-replace into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
+insert ignore into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
 values ('Month.Short.Jul', 'en', 'Jul', 1, 1, now(), now(), 'en', 0);
-replace into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
+insert ignore into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
 values ('Month.Short.Aug', 'en', 'Aug', 1, 1, now(), now(), 'en', 0);
-replace into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
+insert ignore into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
 values ('Month.Short.Sep', 'en', 'Sep', 1, 1, now(), now(), 'en', 0);
-replace into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
+insert ignore into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
 values ('Month.Short.Oct', 'en', 'Oct', 1, 1, now(), now(), 'en', 0);
-replace into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
+insert ignore into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
 values ('Month.Short.Nov', 'en', 'Nov', 1, 1, now(), now(), 'en', 0);
-replace into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
+insert ignore into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
 values ('Month.Short.Dec', 'en', 'Dec', 1, 1, now(), now(), 'en', 0);
 
-replace into widget (widgetID, caption, widgetType, synchronous, url, chartType) 
+insert ignore into widget (widgetID, caption, widgetType, synchronous, url, chartType) 
 values (37, 'Operator Flag Year History', 'Chart', '0', 'OperatorFlagYearHistoryAjax.action', 'StackedColumn2D');
 
-replace into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
+insert ignore into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
 values ('Widget.37.caption', 'en', 'Operator Flag History', 1, 1, now(), now(), 'en', 0);
-replace into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
+insert ignore into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
 values ('Widget.37.caption', 'de', 'Auftraggeber Flaggengeschichte', 1, 1, now(), now(), 'en', 0);
-replace into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
+insert ignore into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
 values ('Widget.37.caption', 'es', 'Operador de Historia de la bandera', 1, 1, now(), now(), 'en', 0);
-replace into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
+insert ignore into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
 values ('Widget.37.caption', 'fi', 'Operaattorin merkkihistoria', 1, 1, now(), now(), 'en', 0);
-replace into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
+insert ignore into app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, sourceLanguage, contentDriven)
 values ('Widget.37.caption', 'fr', 'Historique des signalements par opérateur', 1, 1, now(), now(), 'en', 0);
 
 update widget_user set widgetID = 37 where widgetID = 24 and userID = 616;
