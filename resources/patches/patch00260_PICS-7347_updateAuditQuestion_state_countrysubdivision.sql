d0e676bdbccd3358003d92ff4b00ed25382052a6
Lei Wang <lwang@picsauditing.com>
2012-09-21 15:39:35 -0700

    PICS-7347, the previous commit was a blank file, no some odd reason.  here is the actual sql

---
+++ resources/patches/patch00260_PICS-7347_updateAuditQuestion_state_countrysubdivision.sql
@@ -0,0 +1,8 @@
+update app_translation a join (select
+          english
+        from ref_state
+        where countrycode in ('US', 'CA')) b
+    on a.msgValue = b.english
+set concat ('AuditQuestion.US-', SUBSTR(msgkey, 15, 10))
+where not ceil(substr(msgkey, 14, 5))
+    and msgkey like 'AuditQuestion%'
