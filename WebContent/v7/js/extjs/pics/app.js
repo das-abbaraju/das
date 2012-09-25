0986009fbc25aba28c605cf46534ce4f92a5dcba
chinoki <chinoki@picsauditing.com>
2012-09-25 13:51:37 -0700

    PICS-7013 Updating decorators + adding JSBuilder3 (build + deploy)

---
+++ WebContent/v7/js/extjs/pics/app.js
@@ -0,0 +1,109 @@
+Ext.Loader.setConfig({
+    enabled: true
+});
+
+Ext.application({
+    name: 'PICS',
+    appFolder: '/v7/js/extjs/pics/app',
+    
+    requires: [
+        'PICS.view.report.Viewport',
+        'Ext.layout.container.Border',
+        'Ext.resizer.Splitter',
+        'Ext.grid.RowNumberer',
+        'Ext.grid.column.Number'
+    ],
+    
+    constants: {
+        NUMBERSTORE: [
+            ['Equals', '='],
+            ['GreaterThan', '>'],
+            ['LessThan', '<'],
+            ['GreaterThanOrEquals', '>='],
+            ['LessThanOrEquals', '<='],
+            ['Empty', 'blank']
+        ],
+        TEXTSTORE: [
+            ['Contains', 'contains'],
+            ['NotContains', 'does not contain'],
+            ['BeginsWith', 'begins with'],
+            ['NotBeginsWith', 'does not begin with'],
+            ['EndsWith', 'ends with'],
+            ['NotEndsWith', 'does not end with'],
+            ['Equals', 'equals'],
+            ['NotEquals', 'does not equal'],
+            ['Empty', 'blank'],
+            ['NotEmpty', 'not blank']
+        ]
+    },
+    
+    controllers: [
+        'report.AvailableFieldModal',
+        'report.ColumnFunctionModal',
+        'report.Filter',
+        'report.Report',
+        'report.ReportData',
+        'report.ReportHeader',
+        'report.SettingsModal'
+    ],
+
+    launch: function() {
+        var that = this;
+
+        // save reference to application
+        PICS.app = this;
+
+        this.getConfiguration({
+            success: function () {
+                that.createViewport.apply(that);
+            }
+        });
+    },
+    
+    getConfiguration: function (options) {
+        var url = Ext.Object.fromQueryString(document.location.search);
+
+        Ext.Ajax.request({
+           url: '/ReportDynamic!configuration.action?report=' + url.report,
+           success: function (result) {
+               var result = Ext.decode(result.responseText);
+
+               // configuration closure
+               PICS.app.configuration = (function config() {
+                   return {
+                       isEditable: function () {
+                           return result.editable;
+                       },
+                       isFavorite: function () {
+                           return result.favorite
+                       },
+                       setIsFavorite: function (bool) {
+                           result.favorite = bool;
+                       }
+
+                   };
+               }());
+
+               // success callback
+               if (options && options.success && typeof options.success == 'function') {
+                   options.success();
+               }
+           }
+        });
+    },
+
+    createViewport: function () {
+        Ext.create('PICS.view.report.Viewport', {
+            listeners: {
+                render: function (component, eOpts) {
+                    // remove loading background
+                    var loading = Ext.get('loading_page');
+                    
+                    if(loading) {
+                        loading.remove();
+                    }
+                }
+            }
+        });
+    }
+});
