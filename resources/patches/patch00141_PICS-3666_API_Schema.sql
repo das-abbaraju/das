df6c28ff923ca153aa377da6720568d8c140f0c1
Craig Jones <cjones@picsauditing.com>
2012-10-03 17:06:58 -0700

    PICS-3666 Minor correction to the SQL patch that modifies the schema -- Various corrections to the usergroup schema to bring it up to snuff.

--- resources/patches/patch00141_PICS-3666_API_Schema.sql
+++ resources/patches/patch00141_PICS-3666_API_Schema.sql
@@ -1,7 +1,19 @@
 -- PICS-3666 
 -- Adding support for an API key to be used with the REST API to link the request to a (special) user account.
 
-ALTER TABLE `users` 
+alter table `users` 
     add column `apiKey` varchar(36) DEFAULT NULL,
     add key `apiKey` (`apiKey`);    
 
+-- Various corrections to the usergroup schema to bring it up to snuff
+delete from `usergroup` 
+where `userId` not in (select `id` from `users`)
+or `groupId` not in (select `id` from `users`);
+    
+alter table `usergroup` 
+	change `userID` `userID` int(11) NOT NULL, 
+	change `groupID` `groupID` int(11) NOT NULL,
+	change `creationDate` `creationDate` datetime NULL, 
+	change `createdBy` `createdBy` int(11) NULL,
+	add constraint `FK_usergroup_user` FOREIGN KEY (`userID`) REFERENCES `users` (`id`),
+	add constraint `FK_usergroup_group` FOREIGN KEY (`groupID`) REFERENCES `users` (`id`);
