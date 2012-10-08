a3c97ee6cd5ad400211daf5d64985a26299ffe88
chinoki <chinoki@picsauditing.com>
2012-10-08 15:00:01 -0700

    ExtJS 4 dependency build

--- WebContent/v7/js/extjs/pics/app/model/report/Column.js
+++ WebContent/v7/js/extjs/pics/app/model/report/Column.js
@@ -1,8 +1,18 @@
 Ext.define('PICS.model.report.Column', {
-	extend: 'Ext.data.Model',
+    extend: 'Ext.data.Model',
+    
+    requires: [
+        'PICS.ux.grid.column.Boolean',
+        'PICS.ux.grid.column.Date',
+        'PICS.ux.grid.column.Flag',
+        'PICS.ux.grid.column.Float',
+        'PICS.ux.grid.column.Int',
+        'PICS.ux.grid.column.Number',
+        'PICS.ux.grid.column.Column'
+    ],
 
-	// http://www.sencha.com/forum/showthread.php?180111-4.1-B2-HasOne-constructor-does-not-work
-	associations: [{
+    // http://www.sencha.com/forum/showthread.php?180111-4.1-B2-HasOne-constructor-does-not-work
+    associations: [{
         type: 'hasOne',
         model: 'PICS.model.report.AvailableField',
         associationKey: 'field',
@@ -57,41 +67,41 @@ Ext.define('PICS.model.report.Column', {
         };
 
         switch (type) {
-        	// <i class="icon-ok"></i>
-        	case 'boolean':
-            	grid_column = Ext.create('PICS.ux.grid.column.Boolean', config);
+            // <i class="icon-ok"></i>
+            case 'boolean':
+                grid_column = Ext.create('PICS.ux.grid.column.Boolean', config);
 
                 break;
             // Y-m-d
             case 'date':
-            	grid_column = Ext.create('PICS.ux.grid.column.Date', config);
+                grid_column = Ext.create('PICS.ux.grid.column.Date', config);
 
                 break;
             // <i class="icon-flag"></i>
             case 'flag':
-            	grid_column = Ext.create('PICS.ux.grid.column.Flag', config);
+                grid_column = Ext.create('PICS.ux.grid.column.Flag', config);
 
                 break;
             // 1,234.00
             case 'float':
-            	grid_column = Ext.create('PICS.ux.grid.column.Float', config);
+                grid_column = Ext.create('PICS.ux.grid.column.Float', config);
 
                 break;
             // 1234
             case 'integer':
-            	grid_column = Ext.create('PICS.ux.grid.column.Int', config);
+                grid_column = Ext.create('PICS.ux.grid.column.Int', config);
 
                 break;
             // 1,234
             case 'number':
-            	grid_column = Ext.create('PICS.ux.grid.column.Number', config);
+                grid_column = Ext.create('PICS.ux.grid.column.Number', config);
 
                 break;
-        	// text
+            // text
             case 'string':
             default:
-            	grid_column = Ext.create('PICS.ux.grid.column.Column', config);
-            	
+                grid_column = Ext.create('PICS.ux.grid.column.Column', config);
+                
                 break;
         }
         
