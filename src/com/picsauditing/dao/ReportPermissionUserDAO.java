06605f8a1fab1c61b35ab02a4f25de667d006a46
Alex Ananighian <aananighian@picsauditing.com>
2012-10-12 15:36:30 -0700

    PICS-7582 Fixed the issue where the report is not displayed on the Report Page when a user shares with a Group

--- src/com/picsauditing/dao/ReportPermissionUserDAO.java
+++ src/com/picsauditing/dao/ReportPermissionUserDAO.java
@@ -13,27 +13,27 @@ public class ReportPermissionUserDAO extends PicsDAO {
 	
 	private static final String FIND_REPORTS_FOR_USER = "SELECT rpu.*, r.name, ru.lastViewedDate FROM report_permission_user rpu " + 
 			"JOIN report r ON r.id = rpu.reportID " +
-			"JOIN report_user ru on ru.reportID = rpu.reportID AND ru.userID = :userId " +
+			"LEFT JOIN report_user ru on ru.reportID = rpu.reportID AND ru.userID = :userId " +
 			"WHERE rpu.userID = :userId ";
 	
 	private static final String FIND_REPORTS_FOR_GROUP = "SELECT rpu.*, r.name, ru.lastViewedDate FROM usergroup ug " + 
 			"JOIN report_permission_user rpu ON rpu.userID = ug.groupID " +
 			"JOIN report r ON r.id = rpu.reportID " +
-			"JOIN report_user ru on ru.reportID = rpu.reportID AND ru.userID = :userId " +
+			"LEFT JOIN report_user ru on ru.reportID = rpu.reportID AND ru.userID = :userId " +
 			"WHERE ug.userID = :userId ";
 
 	private static final String FIND_REPORTS_BASED_ON_USER_PERMISSIONS = "SELECT DISTINCT * FROM ( " + 
 			"%s UNION %s ) t ";
 	
 	private static final String FIND_ORDERED_REPORTS_BASED_ON_USER_PERMISSIONS = 
-			FIND_REPORTS_BASED_ON_USER_PERMISSIONS + " ORDER BY %s";
+			FIND_REPORTS_BASED_ON_USER_PERMISSIONS + " ORDER BY %s ";
 	
 	public ReportPermissionUser findOne(int userId, int reportId) throws NoResultException, NonUniqueResultException {
-		String findReportForUser = FIND_REPORTS_FOR_USER + " AND rpu.reportID = :reportId";
-		String findReportForGroup = FIND_REPORTS_FOR_GROUP + " AND rpu.reportID = :reportId";
+		String findReportForUser = FIND_REPORTS_FOR_USER + " AND rpu.reportID = :reportId ";
+		String findReportForGroup = FIND_REPORTS_FOR_GROUP + " AND rpu.reportID = :reportId ";
 
 		// The reason a Limit is needed is that we are searching for user permissions and group permissions 
-		String sql = String.format(FIND_REPORTS_BASED_ON_USER_PERMISSIONS, findReportForUser, findReportForGroup) + " LIMIT 1";
+		String sql = String.format(FIND_REPORTS_BASED_ON_USER_PERMISSIONS, findReportForUser, findReportForGroup) + " LIMIT 1 ";
 		
 		Query query = em.createNativeQuery(sql, ReportPermissionUser.class);
 		query.setParameter("userId", userId);
