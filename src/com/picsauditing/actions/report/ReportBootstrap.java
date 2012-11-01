d450f7459e55f5cbf5e359414929117421da3547
Alex Ananighian <aananighian@picsauditing.com>
2012-10-31 17:00:10 -0700

    PICS-7891 redirect the user to their manage reports landing page if they do not have permission to view a report

--- src/com/picsauditing/actions/report/ReportBootstrap.java
+++ src/com/picsauditing/actions/report/ReportBootstrap.java
@@ -2,29 +2,33 @@ package com.picsauditing.actions.report;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.opensymphony.xwork2.ActionContext;
 import com.picsauditing.actions.PicsActionSupport;
 import com.picsauditing.model.report.ReportModel;
 
 @SuppressWarnings("serial")
 public class ReportBootstrap extends PicsActionSupport {
 
+	private static final String URL_FOR_REDIRECT_FOR_NOT_VIEWABLE = "ManageReports!favoritesList.action";
+
+	private static final String ERROR_MESSAGE_I18N_KEY = "Report.Error.ViewPermissions";
+	
 	@Autowired
 	private ReportModel reportModel;
 
 	private String name;
 	private int report;
 
-	public String execute() {
+	public String execute() throws Exception {
 		if (report == 0) {
 			name = "Missing report ID parameter";
 			return SUCCESS;
 		}
 
 		if (!reportModel.canUserViewAndCopy(permissions, report)) {
-			String errorMessage = "You do not have permissions to view that report.";
-			ActionContext.getContext().getSession().put("errorMessage", errorMessage);
+			addActionError(getText(ERROR_MESSAGE_I18N_KEY));
+			return setUrlForRedirect(URL_FOR_REDIRECT_FOR_NOT_VIEWABLE);
 		}
+		
 		name = "Loading Report " + report + " ...";
 
 		return SUCCESS;
@@ -34,6 +38,7 @@ public class ReportBootstrap extends PicsActionSupport {
 		return name;
 	}
 
+	@Deprecated
 	public void setId(int id) {
 	}
 
