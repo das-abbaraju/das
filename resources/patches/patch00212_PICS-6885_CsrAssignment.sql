7392e6f5a04e585136d000baa4070981cad14f2a
Jared Griffith <jgriffith@picsauditing.com>
2012-10-31 21:06:24 -0700

    PICS-6885 - Commenting out the drop for the contractor_info table since the colum doesn't exist and it was causing autopatch to fail for this particular patch

--- resources/patches/patch00212_PICS-6885_CsrAssignment.sql
+++ resources/patches/patch00212_PICS-6885_CsrAssignment.sql
@@ -1,5 +1,5 @@
 ALTER IGNORE TABLE `users` DROP `dontReassign`;
 ALTER IGNORE TABLE `users` DROP assignmentCapacity;
 ALTER IGNORE TABLE `users` add column `assignmentCapacity` TINYINT UNSIGNED NOT NULL DEFAULT 0;
-ALTER IGNORE TABLE `contractor_info` DROP `dontReassign`;
+-- ALTER IGNORE TABLE `contractor_info` DROP `dontReassign`;
 ALTER IGNORE TABLE `contractor_info` add column `dontReassign` TINYINT NOT NULL DEFAULT 0;
