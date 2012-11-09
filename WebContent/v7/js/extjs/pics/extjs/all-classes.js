110362dc8f9b11dfa86d3bbe004510a000629223
Jason Roos <jroos@picsauditing.com>
2012-11-09 12:38:57 -0800

    js recompile

--- WebContent/v7/js/extjs/pics/extjs/all-classes.js
+++ WebContent/v7/js/extjs/pics/extjs/all-classes.js
@@ -71432,11 +71432,13 @@ Ext.define('PICS.view.report.filter.base.BooleanFilter', {
     },
 
     createCheckbox: function (record) {
-        var value = record.get('not');
+        var value = record.get('value'),
+            checked = value == 'true' ? true : false;
 
         return {
             xtype: 'checkbox',
             boxLabel: 'True',
+            checked: value == 'true' ? true : false,
             inputValue: true,
             name: 'filter_value',
             uncheckedValue: false,
@@ -93496,11 +93498,11 @@ Ext.define('PICS.view.layout.Menu', {
             return;
         }
 
-        items.splice(1, 1, {
+        items.splice(2, 1, {
             xtype: 'menuseparator'
         });
 
-        items.splice(2, 1, {
+        items.splice(3, 1, {
             xtype: 'tbtext',
             cls: 'menu-title',
             // TODO pass in translated "Favorites"
