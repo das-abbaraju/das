7a4ce8bb0767c43fe9240f7c8390a86973437678
Jason Roos <jroos@picsauditing.com>
2012-11-13 15:17:36 -0800

    PICS-7510 JS for restricting the list of functions visible based on type (needs ajax url)

--- WebContent/v7/js/extjs/pics/app/view/report/column-function/ColumnFunctionModal.js
+++ WebContent/v7/js/extjs/pics/app/view/report/column-function/ColumnFunctionModal.js
@@ -5,57 +5,9 @@ Ext.define('PICS.view.report.column-function.ColumnFunctionModal', {
     border: 0,
     closeAction: 'destroy',
     draggable: false,
-    dockedItems: [{
-        xtype: 'toolbar',
-        border: 0,
-        defaults: {
-            height: 35
-        },
-        dock: 'top',
-        id: 'column_function_list',
-        items: [{
-            action: '',
-            height: 40,
-            text: 'None',
-            textAlign: 'left'
-        },{
-            action: 'Average',
-            height: 40,
-            text: 'Average',
-            textAlign: 'left'
-        }, {
-            action: 'Count',
-            height: 40,
-            text: 'Count',
-            textAlign: 'left'
-        }, {
-            action: 'Min',
-            height: 40,
-            text: 'Min',
-            textAlign: 'left'
-        }, {
-            action: 'Max',
-            height: 40,
-            text: 'Max',
-            textAlign: 'left'
-        }, {
-            action: 'Sum',
-            height: 40,
-            text: 'Sum',
-            textAlign: 'left'
-        }],
-        layout: 'vbox'
-    }, {
-        xtype: 'panel',
-        border: 0,
-        dock: 'bottom',
-        height: 10,
-        id: 'column_function_modal_footer'
-    }],
     header: {
         height: 44
     },
-    height: 295,
     id: 'column_function_modal',
     layout: 'fit',
     modal: true,
@@ -70,5 +22,82 @@ Ext.define('PICS.view.report.column-function.ColumnFunctionModal', {
         }
 
         this.callParent(arguments);
-    }
+
+//      this.getFunctions(this.column, this.addDockedItems);
+        var functions = ['Max', 'Min'];
+        this.addDockedItems(functions);
+        this.height = 40 * functions.length + 55;
+    },
+
+    addDockedItems: function (functions) {
+       var items = this.getFunctionItems(functions);
+
+       this.addDocked({
+           xtype: 'toolbar',
+           border: 0,
+           defaults: {
+               height: 35
+           },
+           dock: 'top',
+           id: 'column_function_list',
+           items: items,
+           layout: 'vbox'
+           });
+
+       this.addDocked({
+           xtype: 'panel',
+           border: 0,
+           dock: 'bottom',
+           height: 10,
+           id: 'column_function_modal_footer'
+       });
+   },
+
+   getFunctionItem: function (fn) {
+       return {
+           action: fn,
+           height: 40,
+           text: fn,
+           textAlign: 'left'
+       };
+   },
+
+   getFunctionItems: function (functions) {
+       var items = [{
+           action: '',
+           height: 40,
+           text: 'None',
+           textAlign: 'left'
+       }];
+
+       var that = this;
+       Ext.each(functions, function (fn) {
+           items.push(that.getFunctionItem(fn));
+       });
+
+       return items;
+   },
+
+   getFunctions: function (column, callback) {
+       /*
+       var displayType = this.column.raw.field.displayType;
+           url = 'ReportDynamic!copy.action';
+
+       Ext.Ajax.request({
+           url: url,
+           params: displayType,
+           success: function (result) {
+               var result = Ext.decode(result.responseText);
+
+               if (result.error) {
+                   Ext.Msg.alert('Status', result.error);
+               } else {
+                   callback(result);
+               }
+           }
+       });
+       */
+       var result = ['Max', 'Min'];
+       callback(result).apply(this);
+   },
 });
