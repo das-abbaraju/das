0c4357c401c2c799c6e48d00d1a6e432626d1494
Philip Schlesinger <pschlesinger@picsauditing.com>
2012-11-14 15:14:08 -0800

    PICS-8057 Initial add of format param retrieve Signed-off-by:Philip Schlesinger <pschlesinger@picsauditing.com>

--- src/com/picsauditing/actions/rest/api/DataFeed.java
+++ src/com/picsauditing/actions/rest/api/DataFeed.java
@@ -13,67 +13,74 @@ import com.picsauditing.actions.report.ReportData;
 
 @SuppressWarnings("serial")
 public class DataFeed extends ReportData implements ParameterAware {
-	private static final Logger logger = LoggerFactory.getLogger(DataFeed.class);
-	private String reportIdSpecified = "";
 
-	@Override
-	@Api
-	public String execute() {
-		try {
-			initialize();
-			runQuery();
+    private static final Logger logger = LoggerFactory.getLogger(DataFeed.class);
+    private String reportIdSpecified = "";
+    private String outputFormat = "";
 
-			converter.convertForExtJS();
-			json.put("data", converter.getReportResults().toJson());
+    @Override
+    @Api
+    public String execute() {
+	try {
+	    initialize();
+	    runQuery();
 
-			if (permissions.isAdmin() || permissions.getAdminID() > 0) {
-				json.put("sql", debugSQL);
-			}
-			json.put("success", true);
-		} catch (ReportValidationException error) {
-			writeJsonError(error.getMessage());
-		} catch (Exception e) {
-			if (report == null) {
-				String err = "Invalid report ID: " + reportIdSpecified;
-				logger.error(err);
-				writeJsonError(err);
-			} else {
-				logger.error("Report: {} {} SQL: {}", new Object[] { report.getId(), e.getMessage(), debugSQL });
-				if (permissions.has(OpPerms.Debug) || permissions.getAdminID() > 0) {
-					writeJsonError(e);
-					json.put("sql", debugSQL);
-				} else {
-					writeJsonError("Invalid Query");
-				}
-			}
+	    converter.convertForExtJS();
+	    json.put("data", converter.getReportResults().toJson());
+
+	    if (permissions.isAdmin() || permissions.getAdminID() > 0) {
+		json.put("sql", debugSQL);
+	    }
+	    json.put("success", true);
+	} catch (ReportValidationException error) {
+	    writeJsonError(error.getMessage());
+	} catch (Exception e) {
+	    if (report == null) {
+		String err = "Invalid report ID: " + reportIdSpecified;
+		logger.error(err);
+		writeJsonError(err);
+	    } else {
+		logger.error("Report: {} {} SQL: {}", new Object[]{report.getId(), e.getMessage(), debugSQL});
+		if (permissions.has(OpPerms.Debug) || permissions.getAdminID() > 0) {
+		    writeJsonError(e);
+		    json.put("sql", debugSQL);
+		} else {
+		    writeJsonError("Invalid Query");
 		}
-		return JSON;
+	    }
 	}
+	return JSON;
+    }
 
-	/**
-	 * We need to set the apiKey parameter early (earlier than when the
-	 * JpaEntityInterceptor/JpaEntityConverter code normally kicks in to set the
-	 * parameters), so that the apiKey will be available to the
-	 * SecurityInterceptor.
+    /**
+     * We need to set the apiKey parameter early (earlier than when the
+     * JpaEntityInterceptor/JpaEntityConverter code normally kicks in to set the
+     * parameters), so that the apiKey will be available to the
+     * SecurityInterceptor.
+     */
+    @Override
+    public void setParameters(Map<String, String[]> parameters) {
+	setApiKey(lookupParam(parameters, "apiKey"));
+	/*
+	 * While we are here, let's memorize the report ID specified in the
+	 * paramers, just in case the JpaEntityConverter cannot find the report
+	 * in question, so that we can report on the requested ID number in the
+	 * error message (above).
 	 */
-	@Override
-	public void setParameters(Map<String, String[]> parameters) {
-		setApiKey(lookupParam(parameters, "apiKey"));
-		/*
-		 * While we are here, let's memorize the report ID specified in the
-		 * paramers, just in case the JpaEntityConverter cannot find the report
-		 * in question, so that we can report on the requested ID number in the
-		 * error message (above).
-		 */
-		reportIdSpecified = lookupParam(parameters, "report");
-	}
+	reportIdSpecified = lookupParam(parameters, "report");
 
-	private String lookupParam(Map<String, String[]> parameters, String key) {
-		String paramValue = null;
-		String[] param = parameters.get(key);
-		if (param != null && param.length > 0) {
-			paramValue = param[0];
-		}
-		return paramValue;
+	outputFormat = lookupParam(parameters, "format");
+	if (outputFormat == null || outputFormat.isEmpty()) {
+	    outputFormat = "json";
 	}
+    }
+
+    private String lookupParam(Map<String, String[]> parameters, String key) {
+	String paramValue = null;
+	String[] param = parameters.get(key);
+	if (param != null && param.length > 0) {
+	    paramValue = param[0];
+	}
+	return paramValue;
+    }
 }
