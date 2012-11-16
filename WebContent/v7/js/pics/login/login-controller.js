d8991267f0ad6dc3f8b41fffb224f362893825b2
Jason Roos <jroos@picsauditing.com>
2012-11-16 14:12:45 -0800

    PISC-8073 New "language undefined" fix: deleted the line (It wasn't needed)

--- WebContent/v7/js/pics/login/login-controller.js
+++ WebContent/v7/js/pics/login/login-controller.js
@@ -17,14 +17,12 @@
                 PICS.ajax({
                     url: "Login!loginform.action",
                     data: {
-                        request_locale: supported_locale_element.val()
+                        request_locale: locale
                     },
                     success: function(data, textStatus, jqXHR) {
-                        var login_container_element = supported_locale_element.closest('.login-container'),
-                            language = login_container_element.find('#current_locale').val();
-                        
+                        var login_container_element = supported_locale_element.closest('.login-container');
+
                         login_container_element.html(data);
-                        supported_locale_element.val(locale);
                     }
                 });
             }
