e17c86eca79cd0e7c3471f76879b27f1920645fb
Jason Roos <jroos@picsauditing.com>
2012-11-15 16:27:58 -0800

    PICS-7510 ExtJS rebuild

--- WebContent/v7/js/extjs/pics/extjs/all-classes.js
+++ WebContent/v7/js/extjs/pics/extjs/all-classes.js
@@ -68836,57 +68836,9 @@ Ext.define('PICS.view.report.column-function.ColumnFunctionModal', {
     border: 0,
     closeAction: 'destroy',
     draggable: false,
-    dockedItems: [{
-        xtype: 'toolbar',
-        border: 0,
-        defaults: {
-            height: 35
-        },
-        dock: 'top',
-        id: 'column_function_list',
-        items: [{
-            action: '',
-            height: 40,
-            text: 'None',
-            textAlign: 'left'
-        },{
-            action: 'Average',
-            height: 40,
-            text: 'Average',
-            textAlign: 'left'
-        }, {
-            action: 'Count',
-            height: 40,
-            text: 'Count',
-            textAlign: 'left'
-        }, {
-            action: 'Min',
-            height: 40,
-            text: 'Min',
-            textAlign: 'left'
-        }, {
-            action: 'Max',
-            height: 40,
-            text: 'Max',
-            textAlign: 'left'
-        }, {
-            action: 'Sum',
-            height: 40,
-            text: 'Sum',
-            textAlign: 'left'
-        }],
-        layout: 'vbox'
-    }, {
-        xtype: 'panel',
-        border: 0,
-        dock: 'bottom',
-        height: 10,
-        id: 'column_function_modal_footer'
-    }],
     header: {
         height: 44
     },
-    height: 295,
     id: 'column_function_modal',
     layout: 'fit',
     modal: true,
@@ -68896,11 +68848,67 @@ Ext.define('PICS.view.report.column-function.ColumnFunctionModal', {
     width: 300,
 
     initComponent: function () {
-        if (!this.column || this.column.modelName != 'PICS.model.report.Column') {
+        if (!this.column
+                || this.column.modelName != 'PICS.model.report.Column') {
             Ext.Error.raise('Invalid column record');
         }
 
         this.callParent(arguments);
+
+        var field = this.column.getAvailableField(),
+            column_functions = field.get('functions'),
+            column_function_items = this.getColumnFunctionItems(column_functions);
+
+        this.addDockedItems(column_function_items);
+
+        this.height = (40 * column_functions.length) + 95;
+    },
+
+    addDockedItems: function (column_function_items) {
+        this.addDocked({
+            xtype: 'toolbar',
+            border: 0,
+            defaults: {
+                height: 35
+            },
+            dock: 'top',
+            id: 'column_function_list',
+            items: column_function_items,
+            layout: 'vbox'
+        });
+
+        this.addDocked({
+            xtype: 'panel',
+            border: 0,
+            dock: 'bottom',
+            height: 10,
+            id: 'column_function_modal_footer'
+        });
+    },
+
+    getColumnFunctionItem: function (column_function) {
+        return {
+            action: column_function.key,
+            height: 40,
+            text: column_function.value,
+            textAlign: 'left'
+        };
+    },
+
+    getColumnFunctionItems: function (column_functions) {
+        var items = [{
+            action: '',
+            height: 40,
+            text: 'None',
+            textAlign: 'left'
+        }];
+
+        var that = this;
+        Ext.each(column_functions, function (column_function) {
+            items.push(that.getColumnFunctionItem(column_function));
+        });
+
+        return items;
     }
 });
 Ext.define('PICS.controller.report.ColumnFunctionModal', {
@@ -97179,6 +97187,9 @@ Ext.define('PICS.model.report.AvailableField', {
 	extend: 'Ext.data.Model',
 
 	fields: [{
+        name: 'functions',
+        type: 'auto'
+    }, {
 	    // field category (categorizes fields in available field modal - column, filter picker)
 	    name: 'category',
 	    type: 'string'
