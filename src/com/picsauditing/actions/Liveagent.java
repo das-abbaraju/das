5f8139e36f594b76e78e2820f3059c4728b49f70
Jason Roos <jroos@picsauditing.com>
2012-11-19 18:47:48 -0800

    OTHER-213 Made Liveagent.action publicly accessible

--- src/com/picsauditing/actions/Liveagent.java
+++ src/com/picsauditing/actions/Liveagent.java
@@ -1,7 +1,10 @@
 package com.picsauditing.actions;
 
+import com.picsauditing.access.Anonymous;
+
 @SuppressWarnings("serial")
 public class Liveagent extends PicsActionSupport {
+	@Anonymous
 	public String execute() throws Exception {
 		return SUCCESS;
 	}
