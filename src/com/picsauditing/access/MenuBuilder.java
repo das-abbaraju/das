2f00b49d8a9fca0cd59fbf92cb1fac6df3c2929f
Alex Ananighian <aananighian@picsauditing.com>
2012-11-19 17:16:52 -0800

    PICS-8147 fix for bad import

--- src/com/picsauditing/access/MenuBuilder.java
+++ src/com/picsauditing/access/MenuBuilder.java
@@ -4,7 +4,6 @@ import java.util.Collections;
 import java.util.List;
 
 import org.apache.commons.collections.CollectionUtils;
-import org.eclipse.jdt.internal.compiler.ast.ThisReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
