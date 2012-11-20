a165e357d4a8a5649f1982e508c855b0a5e35ac7
Jason Roos <jroos@picsauditing.com>
2012-11-19 18:33:16 -0800

    OTHER-213 Added liveagent test page (Similar to Trevor.action) for Live Agent's own developers to access

---
+++ src/com/picsauditing/actions/Liveagent.java
@@ -0,0 +1,8 @@
+package com.picsauditing.actions;
+
+@SuppressWarnings("serial")
+public class Liveagent extends PicsActionSupport {
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+}
