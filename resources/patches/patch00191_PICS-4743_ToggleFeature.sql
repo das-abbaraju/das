a2bf9895d1e407abb759bde02b2c9a23a5894bc5
Lani Aung <uaung@picsauditing.com>
2012-11-30 10:05:54 -0800

    PICS-7950 -- Strip out backwards compatibility and use Feature Toggle. Fixing feature toggle script

--- resources/patches/patch00191_PICS-4743_ToggleFeature.sql
+++ resources/patches/patch00191_PICS-4743_ToggleFeature.sql
@@ -1,2 +1,3 @@
 insert IGNORE into app_properties (property, value)
-VALUES ('Toggle.RequestNewContractorAccount', 'permissions.accountId in [1957]) || permissions.picsEmployee');
+-- VALUES ('Toggle.RequestNewContractorAccount', 'permissions.accountId in [1957]) || permissions.picsEmployee');
+VALUES ('Toggle.RequestNewContractorAccount', 'permissions.picsEmployee');
