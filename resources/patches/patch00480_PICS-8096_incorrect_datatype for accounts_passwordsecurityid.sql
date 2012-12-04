9e014907282bd021b006b60dd6cda08852c303e6
Jared Griffith <jgriffith@picsauditing.com>
2012-12-03 16:19:59 -0800

    PICS-8092 - Fixing sql

--- resources/patches/patch00480_PICS-8096_incorrect_datatype for accounts_passwordsecurityid.sql
+++ resources/patches/patch00480_PICS-8096_incorrect_datatype for accounts_passwordsecurityid.sql
@@ -2,7 +2,7 @@
 -- ALTER TABLE `accounts`
 -- DROP COLUMN `passwordSecurityLevelId`;
 
-ALTER TABLE `accounts` CHANGE `passwordSecurityLevelId` `passwordSecurityLevelId` TINYINT( 4 ) NULL DEFAULT '0'
+ALTER TABLE `accounts` CHANGE `passwordSecurityLevelId` `passwordSecurityLevelId` TINYINT( 4 ) NULL DEFAULT '0';
 
 -- Reapply setting originally in patch00450: Set PasswordSecurityLevel = High for all Nassco acccounts
 UPDATE accounts SET passwordSecurityLevelId = 2 WHERE id IN (33250, 33251, 33252, 33253, 34502);
