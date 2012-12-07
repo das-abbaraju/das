adf116aef8fa351055e3247e2b074e2ff6a05936
Lani Aung <uaung@picsauditing.com>
2012-12-07 14:08:20 -0800

    PICS-8253 -- Refund policy link doesn't load page. Adding RefundPolicy after PicsPolicy got renamed to PrivacyPolicy

---
+++ src/com/picsauditing/actions/RefundPolicy.java
@@ -0,0 +1,10 @@
+package com.picsauditing.actions;
+
+@SuppressWarnings("serial")
+public class RefundPolicy extends PicsActionSupport {
+
+	@Override
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+}
