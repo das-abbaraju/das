cf05613b0e8dfb5a54ac0e5a1b75408a24b5dd3b
Alex Ananighian <aananighian@picsauditing.com>
2012-12-18 14:48:08 -0800

    PICS-8059 some changes for sales commission break-down on Invoice

---
+++ src/com/picsauditing/search/CommissionAuditRowMapper.java
@@ -0,0 +1,23 @@
+package com.picsauditing.search;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.picsauditing.jpa.entities.FeeClass;
+import com.picsauditing.salecommission.invoice.strategy.CommissionAudit;
+
+public class CommissionAuditRowMapper implements RowMapper<CommissionAudit> {
+
+	@Override
+	public CommissionAudit mapRow(ResultSet rs, int rowNum) throws SQLException {
+		CommissionAudit commissionAudit = new CommissionAudit();
+		commissionAudit.setInvoiceId(rs.getInt(1));
+		commissionAudit.setClientSiteId(rs.getInt(2));
+		commissionAudit.setFeeClass(FeeClass.valueOf(FeeClass.class, rs.getString(3)));
+
+		return commissionAudit;
+	}
+
+}
