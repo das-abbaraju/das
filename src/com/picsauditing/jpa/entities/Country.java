0d5c1e95e7a2c9663f87357b87bb58d72fd88473
Michael Do <mdo@picsauditing.com>
2012-12-26 11:53:24 -0800

    PICS-8422 Formatting

--- src/com/picsauditing/jpa/entities/Country.java
+++ src/com/picsauditing/jpa/entities/Country.java
@@ -5,7 +5,6 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -30,237 +29,237 @@ import com.picsauditing.util.Strings;
 @Table(name = "ref_country")
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE, region = "daily")
 public class Country extends BaseTranslatable implements Comparable<Country>, Serializable, Autocompleteable, IsoCode {
-    private static final long serialVersionUID = 6312208192653925848L;
+	private static final long serialVersionUID = 6312208192653925848L;
 
+	/*
+	 * TODO We hard-coded the following two lists as a stop-gap measure to get
+	 * PICS-6555 going. We considered adding an isEuropeanUnion column to
+	 * ref_country, but that begged the question -- Are there any other country
+	 * affiliations that we might want to know about in the future? -- So, we
+	 * decided that it would be short-sighted to add a European-Union-specific
+	 * column, when down the line we'll really want a 1:M table that shows
+	 * multiple affiliations. So, we're invoking the Rule of Three here, and
+	 * this is only strike one.
+	 */
 
-    /*
-      * TODO We hard-coded the following two lists as a stop-gap measure to get
-      * PICS-6555 going. We considered adding an isEuropeanUnion column to
-      * ref_country, but that begged the question -- Are there any other country
-      * affiliations that we might want to know about in the future? -- So, we
-      * decided that it would be short-sighted to add a European-Union-specific
-      * column, when down the line we'll really want a 1:M table that shows
-      * multiple affiliations. So, we're invoking the Rule of Three here, and
-      * this is only strike one.
-      */
+	// The 17 Countries in the EuroZone (that have adopted the Euro)
+	private static final String[] EUROZONE = { "AT", "BE", "CY", "DE", "EE", "ES", "FI", "FR", "GR", "IE", "IT", "LU",
+			"MT", "NL", "PT", "SI", "SK" };
+	// The 10 additional countries that are in the European Union, but not in
+	// the EuroZone (i.e. that have not yet adopted the Euro, or never will)
+	private static final String[] EUROPEAN_UNION_ALSO = { "BG", "CZ", "DK", "GB", "GI", "HU", "IT", "LT", "LV", "PL",
+			"RO", "SE" };
 
-    // The 17 Countries in the EuroZone (that have adopted the Euro)
-    private static final String[] EUROZONE = {"AT", "BE", "CY", "DE", "EE", "ES", "FI", "FR", "GR", "IE", "IT", "LU", "MT", "NL", "PT", "SI", "SK"};
-    // The 10 additional countries that are in the European Union, but not in the EuroZone (i.e. that have not yet adopted the Euro, or never will)
-    private static final String[] EUROPEAN_UNION_ALSO = {"BG", "CZ", "DK", "GB", "GI", "HU", "IT", "LT", "LV", "PL", "RO", "SE"};
+	protected String isoCode;
+	protected TranslatableString name;
+	protected String english;
+	protected Double corruptionPerceptionIndex;
+	protected Currency currency = Currency.USD;
+	protected User csr;
 
+	private List<InvoiceFeeCountry> amountOverrides = new ArrayList<InvoiceFeeCountry>();
 
-    protected String isoCode;
-    protected TranslatableString name;
-    protected String english;
-    protected Double corruptionPerceptionIndex;
-    protected Currency currency = Currency.USD;
-    protected User csr;
+	public Country() {
+	}
 
-    private List<InvoiceFeeCountry> amountOverrides = new ArrayList<InvoiceFeeCountry>();
+	public Country(String isoCode) {
+		this.isoCode = isoCode;
+	}
 
-    public Country() {
-    }
+	public Country(String isoCode, String english) {
+		this.isoCode = isoCode;
+		this.english = english;
+	}
 
-    public Country(String isoCode) {
-        this.isoCode = isoCode;
-    }
+	@Id
+	@Column(nullable = false, length = 2)
+	@ReportField()
+	public String getIsoCode() {
+		return isoCode;
+	}
 
-    public Country(String isoCode, String english) {
-        this.isoCode = isoCode;
-        this.english = english;
-    }
+	public void setIsoCode(String isoCode) {
+		this.isoCode = isoCode;
+	}
 
-    @Id
-    @Column(nullable = false, length = 2)
-    @ReportField()
-    public String getIsoCode() {
-        return isoCode;
-    }
+	@Transient
+	@ReportField()
+	public String getName() {
+		return name.toString();
+	}
 
-    public void setIsoCode(String isoCode) {
-        this.isoCode = isoCode;
-    }
+	public void setName(TranslatableString name) {
+		this.name = name;
+	}
 
-    @Transient
-    @ReportField()
-    public String getName() {
-        return name.toString();
-    }
+	public String getEnglish() {
+		return english;
+	}
 
-    public void setName(TranslatableString name) {
-        this.name = name;
-    }
+	public void setEnglish(String english) {
+		this.english = english;
+	}
 
-    public String getEnglish() {
-        return english;
-    }
+	public static String convertToCode(String tempCountry) {
+		if (Strings.isEmpty(tempCountry))
+			return null;
 
-    public void setEnglish(String english) {
-        this.english = english;
-    }
+		tempCountry = tempCountry.trim();
+		if (tempCountry.length() == 2)
+			return tempCountry;
+		if (tempCountry.equals("Canada"))
+			return "CA";
+		else if (tempCountry.equals("United States"))
+			return "US";
 
-    public static String convertToCode(String tempCountry) {
-        if (Strings.isEmpty(tempCountry))
-            return null;
+		return "???";
+	}
 
-        tempCountry = tempCountry.trim();
-        if (tempCountry.length() == 2)
-            return tempCountry;
-        if (tempCountry.equals("Canada"))
-            return "CA";
-        else if (tempCountry.equals("United States"))
-            return "US";
+	@Column(name = "perceivedCorruption")
+	public Double getCorruptionPerceptionIndex() {
+		return this.corruptionPerceptionIndex;
+	}
 
-        return "???";
-    }
+	public void setCorruptionPerceptionIndex(Double corruptionPerceptionIndex) {
+		this.corruptionPerceptionIndex = corruptionPerceptionIndex;
+	}
 
-    @Column(name = "perceivedCorruption")
-    public Double getCorruptionPerceptionIndex() {
-        return this.corruptionPerceptionIndex;
-    }
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	@ReportField()
+	public Currency getCurrency() {
+		return currency;
+	}
 
-    public void setCorruptionPerceptionIndex(Double corruptionPerceptionIndex) {
-        this.corruptionPerceptionIndex = corruptionPerceptionIndex;
-    }
+	public void setCurrency(Currency curreny) {
+		this.currency = curreny;
+	}
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    @ReportField()
-    public Currency getCurrency() {
-        return currency;
-    }
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "csrID")
+	public User getCsr() {
+		return csr;
+	}
 
-    public void setCurrency(Currency curreny) {
-        this.currency = curreny;
-    }
+	public void setCsr(User csr) {
+		this.csr = csr;
+	}
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "csrID")
-    public User getCsr() {
-        return csr;
-    }
+	@OneToMany(mappedBy = "country")
+	public List<InvoiceFeeCountry> getAmountOverrides() {
+		return this.amountOverrides;
+	}
 
-    public void setCsr(User csr) {
-        this.csr = csr;
-    }
+	public void setAmountOverrides(List<InvoiceFeeCountry> amountOverrides) {
+		this.amountOverrides = amountOverrides;
+	}
 
-    @OneToMany(mappedBy = "country")
-    public List<InvoiceFeeCountry> getAmountOverrides() {
-        return this.amountOverrides;
-    }
+	@Transient
+	public boolean isHasCountrySubdivisions() {
+		return isUS() || isCanada() || isUK();
+	}
 
-    public void setAmountOverrides(List<InvoiceFeeCountry> amountOverrides) {
-        this.amountOverrides = amountOverrides;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		return obj instanceof Country && this.isoCode.equals(((Country) obj).getIsoCode());
+	}
 
-    @Transient
-    public boolean isHasCountrySubdivisions() {
-        return isUS() || isCanada() || isUK();
-    }
+	@Override
+	public String toString() {
+		return Strings.isEmpty(english) ? isoCode : english;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        return obj instanceof Country && this.isoCode.equals(((Country) obj).getIsoCode());
-    }
+	@Transient
+	public String getAutocompleteResult() {
+		return isoCode + "_C";
+	}
 
-    @Override
-    public String toString() {
-        return Strings.isEmpty(english) ? isoCode : english;
-    }
+	@Transient
+	public String getAutocompleteItem() {
+		return isoCode;
+	}
 
-    @Transient
-    public String getAutocompleteResult() {
-        return isoCode + "_C";
-    }
+	@Transient
+	public String getAutocompleteValue() {
+		return getName();
+	}
 
-    @Transient
-    public String getAutocompleteItem() {
-        return isoCode;
-    }
+	@Transient
+	public JSONObject toJSON() {
+		return toJSON(false);
+	}
 
-    @Transient
-    public String getAutocompleteValue() {
-        return getName();
-    }
+	@Transient
+	@SuppressWarnings("unchecked")
+	public JSONObject toJSON(boolean full) {
+		JSONObject obj = new JSONObject();
+		obj.put("isoCode", isoCode);
 
-    @Transient
-    public JSONObject toJSON() {
-        return toJSON(false);
-    }
+		if (full) {
+			obj.put("english", english);
+			obj.put("CSR", csr == null ? null : csr.toJSON());
+		}
+		return obj;
+	}
 
-    @Transient
-    @SuppressWarnings("unchecked")
-    public JSONObject toJSON(boolean full) {
-        JSONObject obj = new JSONObject();
-        obj.put("isoCode", isoCode);
+	@Transient
+	public String getI18nKey() {
+		return getClass().getSimpleName() + "." + isoCode;
+	}
 
-        if (full) {
-            obj.put("english", english);
-            obj.put("CSR", csr == null ? null : csr.toJSON());
-        }
-        return obj;
-    }
+	@Transient
+	public String getI18nKey(String property) {
+		return getI18nKey();
+	}
 
-    @Transient
-    public String getI18nKey() {
-        return getClass().getSimpleName() + "." + isoCode;
-    }
+	@Override
+	public int compareTo(Country o) {
+		return this.getI18nKey().compareTo(o.getI18nKey());
+	}
 
-    @Transient
-    public String getI18nKey(String property) {
-        return getI18nKey();
-    }
+	@Transient
+	public boolean isUS() {
+		return "US".equals(isoCode);
+	}
 
-    @Override
-    public int compareTo(Country o) {
-        return this.getI18nKey().compareTo(o.getI18nKey());
-    }
+	@Transient
+	public boolean isCanada() {
+		return "CA".equals(isoCode);
+	}
 
-    @Transient
-    public boolean isUS() {
-        return "US".equals(isoCode);
-    }
+	@Transient
+	public boolean isUAE() {
+		return "AE".equals(isoCode);
+	}
 
-    @Transient
-    public boolean isCanada() {
-        return "CA".equals(isoCode);
-    }
+	@Transient
+	public boolean isUK() {
+		return "GB".equals(isoCode);
+	}
 
-    @Transient
-    public boolean isUAE() {
-        return "AE".equals(isoCode);
-    }
+	@Transient
+	public boolean isFrance() {
+		return "FR".equals(isoCode);
+	}
 
-    @Transient
-    public boolean isUK() {
-        return "GB".equals(isoCode);
-    }
+	@Transient
+	public boolean isEuroZone() {
+		return (Arrays.binarySearch(EUROZONE, isoCode) >= 0);
 
-    @Transient
-    public boolean isFrance() {
-        return "FR".equals(isoCode);
-    }
+	}
 
-    @Transient
-    public boolean isEuroZone() {
-        return (Arrays.binarySearch(EUROZONE, isoCode) >= 0);
+	@Transient
+	public boolean isEuropeanUnion() {
+		return (isEuroZone() || Arrays.binarySearch(EUROPEAN_UNION_ALSO, isoCode) >= 0);
+	}
 
-    }
+	@Transient
+	public BigDecimal getAmount(InvoiceFee invoiceFee) {
+		for (InvoiceFeeCountry countryFeeAmountOverride : getAmountOverrides()) {
+			if (countryFeeAmountOverride.getInvoiceFee().equals(invoiceFee))
+				return countryFeeAmountOverride.getAmount();
+		}
 
-    @Transient
-    public boolean isEuropeanUnion() {
-        return (isEuroZone() || Arrays.binarySearch(EUROPEAN_UNION_ALSO, isoCode) >= 0);
-    }
-
-    @Transient
-    public BigDecimal getAmount(InvoiceFee invoiceFee) {
-        for (InvoiceFeeCountry countryFeeAmountOverride : getAmountOverrides()) {
-            if (countryFeeAmountOverride.getInvoiceFee().equals(invoiceFee))
-                return countryFeeAmountOverride.getAmount();
-        }
-
-        return invoiceFee.getAmount();
-    }
-
+		return invoiceFee.getAmount();
+	}
 }
