2112754a52f3a92ec2bf7565f2729690ff5711cd
Trevor Allred <tallred@picsauditing.com>
2012-12-28 14:40:42 -0800

    added basic new year mailer

---
+++ src/com/picsauditing/mail/NewYearMailer.java
@@ -0,0 +1,14 @@
+package com.picsauditing.mail;
+
+import com.picsauditing.actions.PicsActionSupport;
+
+@SuppressWarnings("serial")
+public class NewYearMailer extends PicsActionSupport {
+	
+	public String queueEmails() {
+		
+		// TODO send emails to queue
+		return SUCCESS;
+	}
+	
+}
