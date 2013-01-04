6192390bb5295857f19f90ec2e569017176cdb27
chinoki <chinoki@picsauditing.com>
2013-01-04 15:54:34 -0800

    PICS-6886 forgot string grid column include

--- WebContent/v7/js/extjs/pics/app/model/report/Column.js
+++ WebContent/v7/js/extjs/pics/app/model/report/Column.js
@@ -2,13 +2,14 @@ Ext.define('PICS.model.report.Column', {
     extend: 'Ext.data.Model',
     
     requires: [
+        'PICS.ux.grid.column.Column',
         'PICS.ux.grid.column.Boolean',
         'PICS.ux.grid.column.Date',
         'PICS.ux.grid.column.Flag',
         'PICS.ux.grid.column.Float',
         'PICS.ux.grid.column.Int',
         'PICS.ux.grid.column.Number',
-        'PICS.ux.grid.column.Column'
+        'PICS.ux.grid.column.String'
     ],
 
     // http://www.sencha.com/forum/showthread.php?180111-4.1-B2-HasOne-constructor-does-not-work
