65cd1c55bf4528bb2216db994b9f1b41e456bdfc
Michael Do <mdo@picsauditing.com>
2013-01-04 10:28:24 -0800

    Update PICS Version

--- src/com/picsauditing/util/PicsOrganizerVersion.java
+++ src/com/picsauditing/util/PicsOrganizerVersion.java
@@ -2,7 +2,7 @@ package com.picsauditing.util;
 
 public class PicsOrganizerVersion {
 	final static public int major = 6;
-	final static public int minor = 33;
+	final static public int minor = 34;
 	final static public int patch = 0;
 
 	@SuppressWarnings("unused")
