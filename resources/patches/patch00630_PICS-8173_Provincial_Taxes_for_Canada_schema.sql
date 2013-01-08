f0db320a6db72f71aad8942173db7ee9fdbc479f
dalvarado <dalvarado@picsauditing.com>
2013-01-08 09:33:23 -0800

    PPICS-8173 Canadian Tax. Split up patches. Make InvoiceService prototype scope.

---
+++ resources/patches/patch00630_PICS-8173_Provincial_Taxes_for_Canada_schema.sql
@@ -0,0 +1,19 @@
+-- Add ratePercent and effectiveDate columns to invoice_fee table
+ALTER TABLE `invoice_fee`
+ADD COLUMN `ratePercent` decimal(6,3) NULL default 0 after `defaultAmount`,
+ADD COLUMN `effectiveDate` DATETIME NULL after `updateDate`;
+
+-- Fix the size of the invoice_fee_country.country column or adding a FK later on will fail. country is a FK to ref_country.isoCode and should be varchar(10), not varchar(25).
+alter table `invoice_fee_country`
+modify `country` varchar(10) NOT NULL;
+
+-- Add subdivision, ratePercent and effectiveDate columns to invoice_fee table
+ALTER TABLE `invoice_fee_country`
+ADD COLUMN `subdivision` varchar(10) NULL after `country`,
+ADD COLUMN `ratePercent` decimal(6,3) NULL default 0 after `amount`,
+ADD COLUMN `effectiveDate` DATETIME NULL after `updateDate`;
+
+-- Update the composite unique index to include subdivision and effectiveDate.
+ALTER TABLE `invoice_fee_country`
+DROP INDEX countryFee,
+ADD UNIQUE INDEX feeCountrySubdivision (feeID, country, subdivision, effectiveDate);
--- resources/patches/patch00631_PICS-8173_Provincial_Taxes_for_Canada_data.sql
+++ resources/patches/patch00631_PICS-8173_Provincial_Taxes_for_Canada_data.sql
@@ -1,23 +1,3 @@
--- Add ratePercent and effectiveDate columns to invoice_fee table
-ALTER TABLE `invoice_fee`
-ADD COLUMN `ratePercent` decimal(6,3) NULL default 0 after `defaultAmount`,
-ADD COLUMN `effectiveDate` DATETIME NULL after `updateDate`;
-
--- Fix the size of the invoice_fee_country.country column or adding a FK later on will fail. country is a FK to ref_country.isoCode and should be varchar(10), not varchar(25).
-alter table `invoice_fee_country`
-modify `country` varchar(10) NOT NULL;
-
--- Add subdivision, ratePercent and effectiveDate columns to invoice_fee table
-ALTER TABLE `invoice_fee_country`
-ADD COLUMN `subdivision` varchar(10) NULL after `country`,
-ADD COLUMN `ratePercent` decimal(6,3) NULL default 0 after `amount`,
-ADD COLUMN `effectiveDate` DATETIME NULL after `updateDate`;
-
--- Update the composite unique index to include subdivision and effectiveDate.
-ALTER TABLE `invoice_fee_country`
-DROP INDEX countryFee,
-ADD UNIQUE INDEX feeCountrySubdivision (feeID, country, subdivision, effectiveDate);
-
 -- Add an invoice_fee for each type of Canadian province tax.  Do not modify the exising row for GST fee(id=200).
 insert into `invoice_fee`
 (`id`, `fee`, `defaultAmount`, `ratePercent`, `visible`, `feeClass`, `minFacilities`, `maxFacilities`, `qbFullName`, `createdBy`, `updatedBy`, `creationDate`, `updateDate`, `effectiveDate`, `displayOrder`, `commissionEligible`)
