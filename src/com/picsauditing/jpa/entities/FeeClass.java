3e8605e8ad15dcb25acaa58ca452b93154d37d37
Lucas Kam <lkam@picsauditing.com>
2013-01-31 16:36:18 -0800

    PICS-9074 Removed Oldcastle and Sunoco IG Exemptions. Paired with Michael (mdo)

--- src/com/picsauditing/jpa/entities/FeeClass.java
+++ src/com/picsauditing/jpa/entities/FeeClass.java
@@ -190,9 +190,7 @@ public enum FeeClass implements Translatable {
 		Map<Integer, Date> exclusions = new HashMap<Integer, Date>();
 		exclusions.put(OperatorAccount.BASF, BASFInsureGUARDAndAuditGUARDPricingEffectiveDate);
 		exclusions.put(OperatorAccount.AI, Jan2013InsureGUARDPricingEffectiveDate);
-		exclusions.put(OperatorAccount.OLDCASTLE, Jan2013InsureGUARDPricingEffectiveDate);
 		exclusions.put(OperatorAccount.SUNCOR, SuncorInsureGUARDPricingEffectiveDate);
-		exclusions.put(OperatorAccount.SUNOCO, Jan2013InsureGUARDPricingEffectiveDate);
 		return exclusions;
 	}
 
