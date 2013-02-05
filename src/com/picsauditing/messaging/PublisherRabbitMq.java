85c54e913a4b303bfcc4b03c27c375046ae71235
Alex Ananighian <aananighian@picsauditing.com>
2013-02-05 14:55:24 -0800

    Fix failing tests and slight code formatting

--- src/com/picsauditing/messaging/PublisherRabbitMq.java
+++ src/com/picsauditing/messaging/PublisherRabbitMq.java
@@ -2,14 +2,15 @@ package com.picsauditing.messaging;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 import com.picsauditing.toggle.FeatureToggle;
 
 public class PublisherRabbitMq implements Publisher {
+
+	@SuppressWarnings("unused")
 	private final Logger logger = LoggerFactory.getLogger(PublisherRabbitMq.class);
-	
+
 	private RabbitTemplate amqpTemplate;
 	private FeatureToggle featureToggleChecker;
 	private String featureToggleName = FeatureToggle.TOGGLE_BPROC;
@@ -21,22 +22,22 @@ public class PublisherRabbitMq implements Publisher {
 	public void setFeatureToggleChecker(FeatureToggle featureToggleChecker) {
 		this.featureToggleChecker = featureToggleChecker;
 	}
-	
+
 	public void setFeatureToggleName(String featureToggleName) {
 		this.featureToggleName = featureToggleName;
 	}
-	
+
 	@Override
 	public void publish(Object message) {
-			if (featureToggleChecker == null || featureToggleChecker.isFeatureEnabled(featureToggleName)) {
-				amqpTemplate.convertAndSend(message);
-			}
+		if (featureToggleChecker == null || featureToggleChecker.isFeatureEnabled(featureToggleName)) {
+			amqpTemplate.convertAndSend(message);
+		}
 	}
 
 	public void publish(Object message, String routingKey) {
-			if (featureToggleChecker.isFeatureEnabled(featureToggleName)) {
-				amqpTemplate.convertAndSend(routingKey, message);
-			}
+		if (featureToggleChecker.isFeatureEnabled(featureToggleName)) {
+			amqpTemplate.convertAndSend(routingKey, message);
+		}
 	}
 
 }
