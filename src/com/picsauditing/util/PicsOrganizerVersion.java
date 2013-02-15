ed91bda331062dd064ae208be3899bd6f759db36
Jared Griffith <jgriffith@picsauditing.com>
2013-02-15 14:48:47 -0800

    Updating the version on master

--- src/com/picsauditing/util/PicsOrganizerVersion.java
+++ src/com/picsauditing/util/PicsOrganizerVersion.java
@@ -2,7 +2,7 @@ package com.picsauditing.util;
 
 public class PicsOrganizerVersion {
 	final static public int major = 6;
-	final static public int minor = 37;
+	final static public int minor = 38;
 	final static public int patch = 0;
 
 	@SuppressWarnings("unused")
