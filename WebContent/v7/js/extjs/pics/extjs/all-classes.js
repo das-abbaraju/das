edbc7f2a6781bd88c6f1956b07451e3bac27775b
Jason Roos <jroos@picsauditing.com>
2013-02-19 10:44:11 -0800

    PICS-6886 Success alert messages now close automatically after 5 seconds

--- WebContent/v7/js/extjs/pics/extjs/all-classes.js
+++ WebContent/v7/js/extjs/pics/extjs/all-classes.js
@@ -67144,6 +67144,16 @@ Ext.define('PICS.view.report.alert.Success', {
     extend: 'Ext.window.Window',
     alias: 'widget.reportalertsuccess',
 
+    listeners: {
+        show: function (cmp, eOpts) {
+            var that = this;
+
+            setTimeout(function () {
+                that.close();
+            }, 5000);
+        }
+    },
+
     draggable: false,
     id: 'success_message',
     resizable: false,
