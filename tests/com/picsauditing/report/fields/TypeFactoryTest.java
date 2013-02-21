6ea29e4a6701fc23f9b282de06e807883f07df8d
Alex Ananighian <aananighian@picsauditing.com>
2013-02-20 19:57:12 -0800

    PICS-9372 some more experimentation

--- tests/com/picsauditing/report/fields/TypeFactoryTest.java
+++ tests/com/picsauditing/report/fields/TypeFactoryTest.java
@@ -4,13 +4,11 @@ import java.util.Set;
 import java.util.TreeSet;
 
 import org.approvaltests.Approvals;
-import org.approvaltests.reporters.JunitReporter;
-import org.approvaltests.reporters.UseReporter;
 import org.junit.Test;
 
 import com.picsauditing.util.Strings;
 
-@UseReporter(JunitReporter.class)
+//@UseReporter(JunitReporter.class)
 public class TypeFactoryTest {
 
 	@Test
