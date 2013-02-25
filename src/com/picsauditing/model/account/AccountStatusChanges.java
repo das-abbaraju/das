b19df2351a3a9a63825bd98436371e7e23040859
Michael Do <mdo@picsauditing.com>
2013-02-25 14:13:06 -0800

    PICS-9453 Null check for permissions. If permissions are null, meaning the user is anonymous, we should still run the deactivate but stamp as system.

--- src/com/picsauditing/model/account/AccountStatusChanges.java
+++ src/com/picsauditing/model/account/AccountStatusChanges.java
@@ -17,30 +17,32 @@ import com.picsauditing.jpa.entities.User;
 import com.picsauditing.util.Strings;
 
 public class AccountStatusChanges {
-    // All of the following reasons are offered up in a drop-down presented to the user by ReportFilterContractor
-    // .getDeactivationReasons(). Some of them are also automatically set by the system.
-    public static final String BID_ONLY_ACCOUNT_REASON = "Bid Only Account";
-    public static final String CHARGEBACK_REASON = "ChargeBack";
-    public static final String DID_NOT_COMPLETE_PICS_PROCESS_REASON = "Did not Complete PICS process";
-    public static final String DOES_NOT_WORK_FOR_OPERATOR_REASON = "Does not work for operator";
-    public static final String DUPLICATE_MERGED_ACCOUNT_REASON = "Duplicate/Merged Account";
-    public static final String OPERATOR_EXEMPTION_REASON = "Operator Exemption";
-    public static final String PAYMENTS_NOT_CURRENT_REASON = "Payments not Current";
+	// All of the following reasons are offered up in a drop-down presented to
+	// the user by ReportFilterContractor
+	// .getDeactivationReasons(). Some of them are also automatically set by the
+	// system.
+	public static final String BID_ONLY_ACCOUNT_REASON = "Bid Only Account";
+	public static final String CHARGEBACK_REASON = "ChargeBack";
+	public static final String DID_NOT_COMPLETE_PICS_PROCESS_REASON = "Did not Complete PICS process";
+	public static final String DOES_NOT_WORK_FOR_OPERATOR_REASON = "Does not work for operator";
+	public static final String DUPLICATE_MERGED_ACCOUNT_REASON = "Duplicate/Merged Account";
+	public static final String OPERATOR_EXEMPTION_REASON = "Operator Exemption";
+	public static final String PAYMENTS_NOT_CURRENT_REASON = "Payments not Current";
 
-    // The following reasons are only automatically set by the system.
-    public static final String DEACTIVATED_NON_RENEWAL_ACCOUNT_REASON = "Deactivated non-renewal account";
-    public static final String DEACTIVATED_PENDING_ACCOUNT_REASON = "Deactivated pending account";
-    public static final String OPERATOR_MANUALLY_DEACTIVATED_REASON = "Operator manually deactivated";
-    public static final String ACCOUNT_EXPIRED_REASON = "Account expired";
-    public static final String ACCOUNT_ABOUT_TO_EXPIRE_REASON = "Account about to expire";
+	// The following reasons are only automatically set by the system.
+	public static final String DEACTIVATED_NON_RENEWAL_ACCOUNT_REASON = "Deactivated non-renewal account";
+	public static final String DEACTIVATED_PENDING_ACCOUNT_REASON = "Deactivated pending account";
+	public static final String OPERATOR_MANUALLY_DEACTIVATED_REASON = "Operator manually deactivated";
+	public static final String ACCOUNT_EXPIRED_REASON = "Account expired";
+	public static final String ACCOUNT_ABOUT_TO_EXPIRE_REASON = "Account about to expire";
 
-    @Autowired
+	@Autowired
 	private AccountDAO accountDAO;
 	@Autowired
 	private NoteDAO noteDAO;
 
 	public void deactivateClientSite(OperatorAccount clientSite, Permissions permissions, String deactivationReason,
-                                     String notation) {
+			String notation) {
 		validate(clientSite, notation);
 
 		setDeactivationStatus(clientSite, permissions, deactivationReason);
@@ -48,8 +50,8 @@ public class AccountStatusChanges {
 		accountDAO.save(clientSite);
 	}
 
-	public void deactivateContractor(ContractorAccount contractor, Permissions permissions,
-                                     String deactivationReason, String notation) {
+	public void deactivateContractor(ContractorAccount contractor, Permissions permissions, String deactivationReason,
+			String notation) {
 		validate(contractor, notation);
 
 		setDeactivationStatus(contractor, permissions, deactivationReason);
@@ -69,9 +71,14 @@ public class AccountStatusChanges {
 	}
 
 	private void setDeactivationStatus(Account account, Permissions permissions, String deactivationReason) {
-        account.setReason(deactivationReason);
+		account.setReason(deactivationReason);
 		account.setStatus(AccountStatus.Deactivated);
-		account.setDeactivatedBy(new User(permissions.getUserId()));
+
+		if (permissions == null)
+			account.setDeactivatedBy(new User(User.SYSTEM));
+		else
+			account.setDeactivatedBy(new User(permissions.getUserId()));
+
 		account.setDeactivationDate(new Date());
 	}
 
