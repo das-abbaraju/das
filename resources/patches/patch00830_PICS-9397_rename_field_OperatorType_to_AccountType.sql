a50d0a1895c203eb0e8633ba393a17db5f5eb5c3
Mike Nichols <mnichols@picsauditing.com>
2013-02-26 15:47:42 -0800

    PICS-9397 Data migration.

---
+++ resources/patches/patch00830_PICS-9397_rename_field_OperatorType_to_AccountType.sql
@@ -0,0 +1,3 @@
+update report_column set name = 'AccountType' where name = 'OperatorType';
+update report_filter set name = 'AccountType' where name = 'OperatorType';
+update report_sort set name = 'AccountType' where name = 'OperatorType';
