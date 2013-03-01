1d13adaabf4d30493f329ab02a999d00f21a26dd
Kirk Chase <kchase@picsauditing.com>
2013-03-01 11:08:03 -0800

    PICS-9530 update AU expires datess for 2009 and after

---
+++ resources/patches/patch00860_PICS-9530_AUExpireDate.sql
@@ -0,0 +1,29 @@
+-- update 2009 AU
+Update contractor_audit ca
+set ca.expiresDate = '2013-03-15 23:59:59'
+where
+  ca.auditTypeID=11
+  and ca.auditFor='2009';
+
+-- update 2010 AU
+Update contractor_audit ca
+set ca.expiresDate = '2014-03-15 23:59:59'
+where
+  ca.auditTypeID=11
+  and ca.auditFor='2010';
+
+-- update 2011 AU
+Update contractor_audit ca
+set ca.expiresDate = '2015-03-15 23:59:59'
+where
+  ca.auditTypeID=11
+  and ca.auditFor='2011';
+
+-- update 2012 AU
+Update contractor_audit ca
+set ca.expiresDate = '2016-03-15 23:59:59'
+where
+  ca.auditTypeID=11
+  and ca.auditFor='2012';
+
+
