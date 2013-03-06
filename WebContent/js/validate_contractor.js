41495eccabe2323aedc1d311279a4b0483387e1c
Joel Brownell <jbrownell@picsauditing.com>
2013-03-05 16:35:44 -0800

    PICS-8875: Refactored js and updated functionality to include validation on OSHA numbers.

--- WebContent/js/validate_contractor.js
+++ WebContent/js/validate_contractor.js
@@ -1,7 +1,55 @@
-function showAudit(auditID) {
-	// if this is an ajax call, then get the form elements and then post them through ajax and return the results to a div
-	startThinking({div:'verification_audit', type:'large', message: 'Retrieving form...'});
-	$('#verification_audit').load('VerifyAuditAjax.action',{'auditID': auditID});
-	$(window).scrollTo('#verification_audit',0,0);
+(function ($) {
+    PICS.define('audit.VerifyController', {
+        methods: {
+            init: function () {
+                if ($('#verification_audit').length > 0) {
+                    var that = this;
 
-}
+                    $('.verifyAudit').on('click', function (event) {
+                        that.loadAudit(event);
+                    });
+                }
+            },
+
+            loadAudit: function () {
+                var element = $(event.currentTarget);
+                    auditID = element.attr('data-id'),
+                    that = this;
+
+                startThinking({div:'verification_audit', type:'large', message: 'Retrieving form...'});                
+                $(window).scrollTo('#verification_audit',0,0);
+
+                PICS.ajax({
+                    url: 'VerifyAuditAjax.action',
+                    data: {
+                        auditID: auditID
+                    },
+                    success: function (data, textStatus, jqXHR) {
+                        var element = $('#verification_audit');
+
+                        element.html(data);
+
+                        that.validateOSHANumber();
+                    }
+                });
+            },
+
+            validateOSHANumber: function () {
+                $('.oshanum').on('change', function () {
+                    var element = $(this),
+                        error_container = element.siblings('.error'),
+                        error_message = "<div class='error'>Please enter a whole number.</div>",
+                        pattern = /^\d+$/g,
+                        isInteger = pattern.test(element.val());
+
+                    if (!isInteger) {
+                        element.after(error_message);
+                    } else {
+                        error_container.remove();
+                    }
+                });
+            }
+        }
+    });
+}(jQuery));
+
