d6f11baa42a5791b6b4aeaf15ef4bcc8a455bbf6
Michael Do <mdo@picsauditing.com>
2013-03-18 16:07:10 -0700

    PICS-9597 changing schema changes to match desired output.

--- resources/patches/patch00920_PICS-9597-DB_Schema.sql
+++ resources/patches/patch00920_PICS-9597-DB_Schema.sql
@@ -9,22 +9,3 @@ CHANGE `name` `name` varchar(100) COLLATE utf8_general_ci NOT NULL after `modelT
 CHANGE `description` `description` text COLLATE utf8_general_ci NULL after `name`, 
 CHANGE `parameters` `parameters` text COLLATE utf8_general_ci NULL after `description`, 
 CHANGE `filterExpression` `filterExpression` varchar(100) COLLATE utf8_general_ci NULL after `parameters`, COMMENT='';
-
-CREATE TABLE `report_permission_group`(
-`id` int(11) NOT NULL auto_increment , 
-`createdBy` int(11) NULL , 
-`updatedBy` int(11) NULL , 
-`creationDate` datetime NULL , 
-`updateDate` datetime NULL , 
-`reportID` int(11) NOT NULL , 
-`userID` int(11) NULL , 
-`editable` tinyint(1) NOT NULL DEFAULT '0' , 
-`owner` tinyint(1) NOT NULL DEFAULT '0' , 
-`permissionLevel` varchar(20) COLLATE utf8_general_ci NOT NULL DEFAULT ''View'' , 
-PRIMARY KEY (`id`) , 
-UNIQUE KEY `user_report`(`userID`,`reportID`) , 
-KEY `user_report_report`(`reportID`) , 
-KEY `user_report_user`(`userID`) 
-) ENGINE=InnoDB DEFAULT CHARSET='utf8';
-
-Truncate table report_permission_group;
--- resources/patches/patch00921_PICS-9597-DB_SQL.sql
+++
@@ -1,7 +0,0 @@
-insert into report_permission_group (id, createdBy, updatedBy, creationDate, updateDate, reportID, userID, editable)
-select rpu.id, rpu.createdBy, rpu.updatedBy, rpu.creationDate, rpu.updateDate, rpu.reportID, rpu.userID, rpu.editable
-from report_permission_user rpu
-join users u on rpu.userID = u.id and u.isGroup = 'Yes';
-
-delete rpu from report_permission_user rpu
-join users u on rpu.userID = u.id and u.isGroup = 'Yes';
