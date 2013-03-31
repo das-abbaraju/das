8c11e5cb06afdc68a3af737180c2f07dbdc3d992
David Alvarado <dalvarado@picsauditing.com>
2013-03-30 19:41:53 -0700

    PICS-9598 Add soft delete column for reports

--- resources/patches/patch00970_PICS-9598_Report_Sharing.sql
+++ resources/patches/patch00970_PICS-9598_Report_Sharing.sql
@@ -1,4 +1,7 @@
 ALTER TABLE `report_user`
 ADD COLUMN `hidden` tinyint(1) NOT NULL DEFAULT 0;
 
+ALTER TABLE `report`
+ADD COLUMN `deleted` tinyint(1) NOT NULL DEFAULT 0;
+
 UPDATE report SET ownerID = createdBy WHERE createdBy IS NOT NULL;
