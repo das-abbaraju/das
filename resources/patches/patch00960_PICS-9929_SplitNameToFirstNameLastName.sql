fa6cab033269fea8803ae9979e7cdb6f3545f67e
Lani Aung <uaung@picsauditing.com>
2013-04-01 09:30:57 -0700

    PICS-9929 -- Convert data for users display to first and last name. Adding manual trim

--- resources/patches/patch00960_PICS-9929_SplitNameToFirstNameLastName.sql
+++ resources/patches/patch00960_PICS-9929_SplitNameToFirstNameLastName.sql
@@ -1,6 +1,6 @@
 update users u
-set u.firstName = trim(left(u.name, locate(substring_index(trim(replace(left(u.name, locate(' jr', u.name)), ',', '')), ' ', -1), u.name))),
-u.lastName = substring(u.name, locate(substring_index(trim(replace(left(u.name, locate(' jr', u.name)), ',', '')), ' ', -1), u.name)),
+set u.firstName = left(trim(left(u.name, locate(substring_index(trim(replace(left(u.name, locate(' jr', u.name)), ',', '')), ' ', -1), u.name))), 50),
+u.lastName = left(substring(u.name, locate(substring_index(trim(replace(left(u.name, locate(' jr', u.name)), ',', '')), ' ', -1), u.name)), 50),
 u.updateDate = now(),
 u.updatedBy = 23157
 where u.name like '% jr%'
@@ -10,8 +10,8 @@ and u.isActive = 'Yes'
 and u.isGroup = 'No';
 
 update users u
-set u.firstName = trim(left(u.name, locate(substring_index(trim(replace(left(u.name, locate(' sr', u.name)), ',', '')), ' ', -1), u.name))),
-u.lastName = substring(u.name, locate(substring_index(trim(replace(left(u.name, locate(' sr', u.name)), ',', '')), ' ', -1), u.name)),
+set u.firstName = left(trim(left(u.name, locate(substring_index(trim(replace(left(u.name, locate(' sr', u.name)), ',', '')), ' ', -1), u.name))), 50),
+u.lastName = left(substring(u.name, locate(substring_index(trim(replace(left(u.name, locate(' sr', u.name)), ',', '')), ' ', -1), u.name)), 50),
 u.updateDate = now(),
 u.updatedBy = 23157
 where u.name like '% sr%'
@@ -21,8 +21,8 @@ and u.isActive = 'Yes'
 and u.isGroup = 'No';
 
 update users u
-set u.firstName = trim(substring_index(u.name, ',', 1)),
-u.lastName = trim(substring_index(u.name, ',', -1)),
+set u.firstName = left(trim(substring_index(u.name, ',', 1)), 50),
+u.lastName = left(trim(substring_index(u.name, ',', -1)), 50),
 u.updateDate = now(),
 u.updatedBy = 23157
 where u.name like '%,%'
@@ -33,18 +33,14 @@ and u.isActive = 'Yes'
 and u.isGroup = 'No';
 
 update users u
-set u.firstName = trim(left(u.name, locate(substring_index(trim(u.name), ' ', -1), u.name) - 1)),
-u.lastName = substring_index(trim(u.name), ' ', -1),
+set u.firstName = left(trim(left(u.name, locate(substring_index(trim(u.name), ' ', -1), u.name) - 1)), 50),
+u.lastName = left(substring_index(trim(u.name), ' ', -1), 50),
 u.updateDate = now(),
 u.updatedBy = 23157
-where u.firstName is null
-and u.lastName is null
-and u.name not like 'Account%'
+where u.name not like 'Account%'
 and u.name not like '%Admin%'
 and u.name not like 'test%'
 and u.name not like 'Customer Service%'
-and u.firstName is null
-and u.lastName is null
 and u.isActive = 'Yes'
 and u.isGroup = 'No';
 
@@ -57,7 +53,7 @@ where u.lastName is not null
 and length(u.firstName) = 0;
 
 update users u
-set u.firstName = u.name,
+set u.firstName = left(u.name, 50),
 u.updateDate = now(),
 u.updatedBy = 23157
 where u.firstName is null
