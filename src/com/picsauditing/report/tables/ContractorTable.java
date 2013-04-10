4ccffe156e4d73c685b56c72b87d01e57a7db295
Michael Do <mdo@picsauditing.com>
2013-04-10 09:51:56 -0700

    PICS-9990 Modified the CSR join to bring only current users

--- src/com/picsauditing/report/tables/ContractorTable.java
+++ src/com/picsauditing/report/tables/ContractorTable.java
@@ -1,51 +1,54 @@
 package com.picsauditing.report.tables;
 
-import com.picsauditing.jpa.entities.*;
+import com.picsauditing.jpa.entities.AuditType;
+import com.picsauditing.jpa.entities.ContractorAccount;
+import com.picsauditing.jpa.entities.FeeClass;
+import com.picsauditing.jpa.entities.UserAccountRole;
 import com.picsauditing.report.fields.Field;
 import com.picsauditing.report.fields.FieldType;
 
 public class ContractorTable extends AbstractTable {
 
-	public static final String Account = "Account";
+    public static final String Account = "Account";
     public static final String AccountUser = "AccountUser";
-	public static final String CustomerService = "CustomerService";
-	public static final String RecommendedCSR = "RecommendedCSR";
-	public static final String PQF = "PQF";
-	public static final String Flag = "Flag";
-	public static final String FlagCriteriaContractor = "FlagCriteriaContractor";
-	public static final String RequestedBy = "RequestedBy";
-	public static final String Watch = "Watch";
-	public static final String Tag = "Tag";
-	public static final String ContractorStatistics = "ContractorStatistics";
-	public static final String ContractorTrade = "ContractorTrade";
-	public static final String WelcomeCall = "WelcomeCall";
+    public static final String CustomerService = "CustomerService";
+    public static final String RecommendedCSR = "RecommendedCSR";
+    public static final String PQF = "PQF";
+    public static final String Flag = "Flag";
+    public static final String FlagCriteriaContractor = "FlagCriteriaContractor";
+    public static final String RequestedBy = "RequestedBy";
+    public static final String Watch = "Watch";
+    public static final String Tag = "Tag";
+    public static final String ContractorStatistics = "ContractorStatistics";
+    public static final String ContractorTrade = "ContractorTrade";
+    public static final String WelcomeCall = "WelcomeCall";
 
-	public ContractorTable() {
-		super("contractor_info");
-		addFields(ContractorAccount.class);
+    public ContractorTable() {
+        super("contractor_info");
+        addFields(ContractorAccount.class);
 
-		addContractorFee(FeeClass.DocuGUARD);
-		addContractorFee(FeeClass.AuditGUARD);
-		addContractorFee(FeeClass.InsureGUARD);
-		addContractorFee(FeeClass.EmployeeGUARD);
-	}
+        addContractorFee(FeeClass.DocuGUARD);
+        addContractorFee(FeeClass.AuditGUARD);
+        addContractorFee(FeeClass.InsureGUARD);
+        addContractorFee(FeeClass.EmployeeGUARD);
+    }
 
-	private void addContractorFee(FeeClass feeClass) {
-		Field field = new Field(feeClass.toString(), "EXISTS(SELECT * FROM contractor_fee cf WHERE cf.conID = "
-				+ ReportOnClause.ToAlias + ".id AND cf.currentAmount > 0 AND cf.feeClass = '" + feeClass.toString()
-				+ "')", FieldType.Boolean);
-		field.setCategory(FieldCategory.Billing);
-		field.setWidth(100);
-		addField(field);
-	}
+    private void addContractorFee(FeeClass feeClass) {
+        Field field = new Field(feeClass.toString(), "EXISTS(SELECT * FROM contractor_fee cf WHERE cf.conID = "
+                + ReportOnClause.ToAlias + ".id AND cf.currentAmount > 0 AND cf.feeClass = '" + feeClass.toString()
+                + "')", FieldType.Boolean);
+        field.setCategory(FieldCategory.Billing);
+        field.setWidth(100);
+        addField(field);
+    }
 
-	protected void addJoins() {
-		addRequiredKey(new ReportForeignKey(Account, new AccountTable(), new ReportOnClause("id", "id",
-				ReportOnClause.ToAlias + ".type = 'Contractor'")));
+    protected void addJoins() {
+        addRequiredKey(new ReportForeignKey(Account, new AccountTable(), new ReportOnClause("id", "id",
+                ReportOnClause.ToAlias + ".type = 'Contractor'")));
 
-		ReportForeignKey flagKey = addRequiredKey(new ReportForeignKey(Flag, new ContractorOperatorTable(),
-				new ReportOnClause("id", "subID", ReportOnClause.ToAlias + ".genID = " + ReportOnClause.AccountID)));
-		flagKey.setMinimumImportance(FieldImportance.Low);
+        ReportForeignKey flagKey = addRequiredKey(new ReportForeignKey(Flag, new ContractorOperatorTable(),
+                new ReportOnClause("id", "subID", ReportOnClause.ToAlias + ".genID = " + ReportOnClause.AccountID)));
+        flagKey.setMinimumImportance(FieldImportance.Low);
 
 //		ReportForeignKey oldCsrKey = addOptionalKey(new ReportForeignKey(CustomerService, new UserTable(),
 //				new ReportOnClause("welcomeAuditor_id")));
@@ -56,44 +59,46 @@ public class ContractorTable extends AbstractTable {
                 new ReportForeignKey(
                         CustomerService,
                         new AccountUserTable(),
-                        new ReportOnClause("id", "accountID", ReportOnClause.ToAlias + ".role = '" + UserAccountRole.PICSCustomerServiceRep + "'")
+                        new ReportOnClause("id", "accountID", ReportOnClause.ToAlias + ".role = '" +
+                                UserAccountRole.PICSCustomerServiceRep + "' AND " + ReportOnClause.ToAlias +
+                                ".startDate < NOW() AND " + ReportOnClause.ToAlias + ".endDate >= NOW()")
                 )
         );
 
-		csrKey.setCategory(FieldCategory.CustomerServiceRepresentatives);
+        csrKey.setCategory(FieldCategory.CustomerServiceRepresentatives);
         csrKey.setMinimumImportance(FieldImportance.Average);
 
         ReportForeignKey pqfKey = addOptionalKey(new ReportForeignKey(PQF, new ContractorAuditTable(),
-				new ReportOnClause("id", "conID", ReportOnClause.ToAlias + ".auditTypeID = " + AuditType.PQF)));
+                new ReportOnClause("id", "conID", ReportOnClause.ToAlias + ".auditTypeID = " + AuditType.PQF)));
         pqfKey.setMinimumImportance(FieldImportance.Required);
 
         ReportForeignKey welcomeCallKey = addOptionalKey(new ReportForeignKey(WelcomeCall, new ContractorAuditTable(),
-				new ReportOnClause("id", "conID", ReportOnClause.ToAlias + ".auditTypeID = " + AuditType.WELCOME)));
+                new ReportOnClause("id", "conID", ReportOnClause.ToAlias + ".auditTypeID = " + AuditType.WELCOME)));
         welcomeCallKey.setMinimumImportance(FieldImportance.Required);
 
         ReportForeignKey requestedBy = addOptionalKey(new ReportForeignKey(RequestedBy, new AccountTable(),
-				new ReportOnClause("requestedByID")));
+                new ReportOnClause("requestedByID")));
         requestedBy.setMinimumImportance(FieldImportance.Required);
         requestedBy.setCategory(FieldCategory.RequestingClientSite);
 
         addOptionalKey(new ReportForeignKey(Watch, new ContractorWatchTable(), new ReportOnClause("id", "conID",
-				ReportOnClause.ToAlias + ".userID = " + ReportOnClause.UserID)));
+                ReportOnClause.ToAlias + ".userID = " + ReportOnClause.UserID)));
 
         addOptionalKey(new ReportForeignKey(Tag, new ContractorTagView(), new ReportOnClause("id", "conID",
-				ReportOnClause.ToAlias + ".opID IN (" + ReportOnClause.VisibleAccountIDs + ")")));
+                ReportOnClause.ToAlias + ".opID IN (" + ReportOnClause.VisibleAccountIDs + ")")));
 
         addRequiredKey(new ReportForeignKey(ContractorStatistics, new ContractorStatisticsView(), new ReportOnClause(
-				"id", "conID")));
+                "id", "conID")));
 
         addOptionalKey(new ReportForeignKey(ContractorTrade, new ContractorTradeTable(), new ReportOnClause("id",
-				"conID")));
+                "conID")));
 
         ReportForeignKey recommendedCsrKey = addOptionalKey(new ReportForeignKey(RecommendedCSR, new UserTable(),
-				new ReportOnClause("recommendedCsrID")));
+                new ReportOnClause("recommendedCsrID")));
         recommendedCsrKey.setMinimumImportance(FieldImportance.Required);
         recommendedCsrKey.setCategory(FieldCategory.CustomerServiceRepresentatives);
 
         addOptionalKey(new ReportForeignKey(FlagCriteriaContractor, new FlagCriteriaContractorTable(),
-				new ReportOnClause("id", "conID")));
+                new ReportOnClause("id", "conID")));
     }
 }
