ff714ea304efe8e036f961068709c6b74d9d44b9
Philip H. Schlesinger <pschlesinger@picsauditing.com>
2013-04-11 10:50:57 -0700

    PICS-10038 PICS-10110 Cleaning up test code and adding in null testing

--- tests/com/picsauditing/model/l10n/InvoiceLocaleUtilTest.java
+++ tests/com/picsauditing/model/l10n/InvoiceLocaleUtilTest.java
@@ -1,12 +1,12 @@
 package com.picsauditing.model.l10n;
 
-import static junit.framework.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.when;
-
-import java.util.List;
-import java.util.Locale;
-
+import com.picsauditing.dao.AppPropertyDAO;
+import com.picsauditing.dao.CountryDAO;
+import com.picsauditing.jpa.entities.AppProperty;
+import com.picsauditing.jpa.entities.ContractorAccount;
+import com.picsauditing.jpa.entities.Country;
+import com.picsauditing.toggle.FeatureToggle;
+import com.picsauditing.util.SpringUtils;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -17,13 +17,12 @@ import org.mockito.MockitoAnnotations;
 import org.powermock.reflect.Whitebox;
 import org.springframework.context.ApplicationContext;
 
-import com.picsauditing.dao.AppPropertyDAO;
-import com.picsauditing.dao.CountryDAO;
-import com.picsauditing.jpa.entities.AppProperty;
-import com.picsauditing.jpa.entities.ContractorAccount;
-import com.picsauditing.jpa.entities.Country;
-import com.picsauditing.toggle.FeatureToggle;
-import com.picsauditing.util.SpringUtils;
+import java.util.List;
+import java.util.Locale;
+
+import static junit.framework.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 public class InvoiceLocaleUtilTest {
 
@@ -56,6 +55,23 @@ public class InvoiceLocaleUtilTest {
 	public static void classTeardown() {
 		Whitebox.setInternalState(SpringUtils.class, "applicationContext", (ApplicationContext) null);
 		Whitebox.setInternalState(InvoiceLocaleUtil.class, "localesToEmailInvoicesInBPROCS", (List<Locale>) null);
+	}
+
+	private void setupNullLocaleSpecifiedForBPROCSEmail() {
+		when(appProperty.getValue()).thenReturn(null);
+		when(appPropertyDAO.find(FeatureToggle.TOGGLE_INVOICE_LOCALES_TO_EMAIL_VIA_BPROCS)).thenReturn(appProperty);
+	}
+
+	@Test
+	public void testInvoiceIsToBeEmailedViaBPROCS_NoLocalesSpecified() {
+		Locale localeToTest = Locale.GERMANY;
+		String isoCodeToTest = Country.GERMANY_ISO_CODE;
+
+		setupNullLocaleSpecifiedForBPROCSEmail();
+		setupCountryDAOToReturnLocaleWhenGivenIsoCode(isoCodeToTest, localeToTest);
+		boolean result = InvoiceLocaleUtil.invoiceIsToBeEmailedViaBPROCS(contractorAccount);
+
+		assertFalse(result);
 	}
 
 	private void setupSingleLocaleForEmail() {
@@ -94,9 +110,13 @@ public class InvoiceLocaleUtilTest {
 	}
 
 	private void setupInvoiceLocaleUtilEmailListContainsLocaleTest() {
+		Locale localeToTest = Locale.FRANCE;
+		String isoCodeToTest = Country.FRANCE_ISO_CODE;
+
 		setupSingleLocaleForEmail();
-		setupCountryLocale(Country.FRANCE_ISO_CODE, Locale.FRANCE);
-		addLocaleToEmailList(Locale.FRANCE);
+		setupContractorAccountBillingCountryTo(isoCodeToTest);
+		setupCountryDAOToReturnLocaleWhenGivenIsoCode(isoCodeToTest, localeToTest);
+		addLocaleToEmailList(localeToTest);
 	}
 
 	@Test
@@ -109,9 +129,13 @@ public class InvoiceLocaleUtilTest {
 	}
 
 	private void setupInvoiceLocaleUtilEmailListDoesNotContainsLocaleTest() {
+		Locale localeToTest = Locale.GERMANY;
+		String isoCodeToTest = Country.GERMANY_ISO_CODE;
+
 		setupSingleLocaleForEmail();
-		setupCountryLocale(Country.GERMANY_ISO_CODE, Locale.GERMANY);
-		removeLocaleFromEmailList(Locale.GERMANY);
+		setupContractorAccountBillingCountryTo(isoCodeToTest);
+		setupCountryDAOToReturnLocaleWhenGivenIsoCode(isoCodeToTest, localeToTest);
+		removeLocaleFromEmailList(localeToTest);
 	}
 
 	@Test
@@ -123,9 +147,13 @@ public class InvoiceLocaleUtilTest {
 		assertFalse(result);
 	}
 
-	private void setupCountryLocale(String isoCode, Locale locale) {
+	private void setupContractorAccountBillingCountryTo(String isoCode) {
 		when(country.getIsoCode()).thenReturn(isoCode);
 		when(contractorAccount.getBillingCountry()).thenReturn(country);
+	}
+
+
+	private void setupCountryDAOToReturnLocaleWhenGivenIsoCode(String isoCode, Locale locale) {
 		when(countryDAO.findLocaleByCountryISO(isoCode)).thenReturn(locale);
 	}
 
