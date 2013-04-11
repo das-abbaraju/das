b7e0e65c7b153b8612783321d435af20b8f3ee45
Philip H. Schlesinger <pschlesinger@picsauditing.com>
2013-04-11 11:59:29 -0700

    PICS-10039 PICS-10116 Renaming src/test to src/sample

---
+++ src/sample/EventSubscriptionBuilderSample.java
@@ -0,0 +1,14 @@
+package sample;
+
+import com.picsauditing.mail.EventSubscriptionBuilder;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pschlesinger
+ * Date: 4/11/13
+ * Time: 11:57 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class EventSubscriptionBuilderSample extends EventSubscriptionBuilder {
+
+}
