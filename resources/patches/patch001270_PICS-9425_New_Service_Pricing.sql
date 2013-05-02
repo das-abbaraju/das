c607efcc27116ab5741e163f8151e98a389f2b30
Jason Roos <jroos@picsauditing.com>
2013-05-02 15:40:22 -0700

    PICS-9425 : Add patch for new service pricing
    -- invoice_fee_country table changes

---
+++ resources/patches/patch001270_PICS-9425_New_Service_Pricing.sql
@@ -0,0 +1,14 @@
+-- Better values for null
+update invoice_fee_country
+	set effectiveDate = '1970-01-01'
+	where effectiveDate is null;
+	
+-- Encourage better dates
+alter table `pics_alpha1`.`invoice_fee_country` 
+	change `effectiveDate` `effectiveDate` 
+	datetime default '1970-01-01 00:00:00' NOT NULL;
+
+-- add expiration date
+alter table `pics_alpha1`.`invoice_fee_country` 
+	add column `expirationDate` 
+	datetime DEFAULT '4000-01-01 23:59:59' NOT NULL after `effectiveDate`;
