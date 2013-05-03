9cace3ce1f67fa8356c2bba8c389dd472404439e
Michael Do <mdo@picsauditing.com>
2013-05-03 09:43:07 -0700

    PICS-10631 Fixing the build, removing code that wasn't intended for this ticket

--- src/com/picsauditing/report/models/ModelType.java
+++ src/com/picsauditing/report/models/ModelType.java
@@ -6,6 +6,7 @@ public enum ModelType {
 	ContractorAudits,
 	ContractorAuditDatas,
 	ContractorAuditFiles,
+	ContractorAuditOperators,
 	ContractorAuditOperatorWorkflows,
 	ContractorFees,
 	ContractorFlagDatas,
