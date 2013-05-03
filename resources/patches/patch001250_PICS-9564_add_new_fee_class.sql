29b5632db58eec315e20e22ba84ab1cbe36fd1cb
Alex Ananighian <aananighian@picsauditing.com>
2013-05-03 16:36:51 -0700

    PICS-9564 : Change to auto-patch script

--- resources/patches/patch001250_PICS-9564_add_new_fee_class.sql
+++ resources/patches/patch001250_PICS-9564_add_new_fee_class.sql
@@ -1,15 +1,8 @@
--- Adding SSIP fee
-
-INSERT INTO invoice_fee (fee, defaultamount, visible, feeClass, minFacilities, maxFacilities,
-                         qbFullName, createdBy, updatedBy, creationDate, updateDate,
-                         effectiveDate, displayOrder, commissionEligible)
-  VALUES ("SSIP Activation Fee Discount", -199, 1, "SSIPDiscountFee", 1, 10000, "SSIP01", 53137, 53137, NOW(), NOW(), NOW(), 255, 1);
-
--- Add translation item
 INSERT INTO app_translation (msgKey, locale, msgValue, createdBy, updatedBy, creationDate, updateDate, lastUsed,
                              qualityRating, applicable, sourceLanguage, contentDriven)
   VALUES ((SELECT
              CONCAT("InvoiceFee.", id, ".fee")
            FROM invoice_fee
-           WHERE fee = "SSIP Activation Discount"), 'en',
-          'SSIP Discount', 53137, 53137, NOW(), NOW(), null, 1, 1, 'en', 0);
+           WHERE fee = "SSIP Activation Fee Discount"), 'en',
+          'SSIP Discount', 53137, 53137, NOW(), NOW(), null, 1, 1, 'en', 0)
+  ON DUPLICATE KEY UPDATE msgValue = 'SSIP Activation Fee Discount' ;
