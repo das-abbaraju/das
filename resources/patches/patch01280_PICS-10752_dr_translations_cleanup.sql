ac323802b4e1e35341127d84b742971491dcbbb6
David Alvarado <dalvarado@picsauditing.com>
2013-05-03 12:48:51 -0700

    PICS-10752 Fix typo in patch.

--- resources/patches/patch01280_PICS-10752_dr_translations_cleanup.sql
+++ resources/patches/patch01280_PICS-10752_dr_translations_cleanup.sql
@@ -1,4 +1,4 @@
 -- Add the js column with sensible defaults. Alex said that he'd take care of updating the appropriate rows
 -- manually at a later time.
 ALTER TABLE `app_translation`
-ADD COLUMN `js2` TINYINT(1) DEFAULT 0  NOT NULL  COMMENT '1 indicates translation applies to javascript' AFTER `contentDriven`;
+ADD COLUMN `js` TINYINT(1) DEFAULT 0  NOT NULL  COMMENT '1 indicates translation applies to javascript' AFTER `contentDriven`;
