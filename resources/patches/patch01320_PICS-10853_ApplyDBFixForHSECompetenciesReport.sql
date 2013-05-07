396656336839e8cbced62bcd18c1e411a2ad5643
Jared Griffith <jgriffith@picsauditing.com>
2013-05-07 15:34:06 -0700

    PICS-10853 - Adjusting where indexes are put

--- resources/patches/patch01320_PICS-10853_ApplyDBFixForHSECompetenciesReport.sql
+++ resources/patches/patch01320_PICS-10853_ApplyDBFixForHSECompetenciesReport.sql
@@ -1,4 +1,12 @@
 -- Remove duplicate records from employee_competency first
+ALTER TABLE	employee_competency
+ADD UNIQUE INDEX 	idxEmployee_Competency
+(
+	employeeID
+,	competencyID
+)
+;
+
 DELETE t1
 FROM
 	employee_competency	AS t1
@@ -10,14 +18,15 @@ AND	t1.competencyID	= t2.competencyID
 AND	t1.id	< t2.id
 ;
 
-ALTER TABLE	employee_competency
-ADD UNIQUE INDEX 	idxEmployee_Competency
+-- Remove duplicate records from employee_role first
+ALTER TABLE	employee_role
+ADD UNIQUE INDEX	idxEmployee_Role
 (
 	employeeID
-,	competencyID
+,	jobRoleID
 )
 ;
--- Remove duplicate records from employee_role first
+
 DELETE t1
 FROM
 	employee_role	AS t1
@@ -27,13 +36,5 @@ WHERE
 	t1.employeeID	= t2.employeeID
 AND	t1.jobRoleID	= t2.jobRoleID
 AND	t1.id	< t2.id
-;
-
-ALTER TABLE	employee_role
-ADD UNIQUE INDEX	idxEmployee_Role
-(
-	employeeID
-,	jobRoleID
-)
 ;
 
