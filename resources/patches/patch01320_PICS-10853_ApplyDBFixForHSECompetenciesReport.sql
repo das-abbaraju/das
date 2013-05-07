dc027c99e5ce8c84720aecf540af162732aae41a
Solomon Shacter <sshacter@picsauditing.com>
2013-05-07 14:30:06 -0700

    PICS-10853 -- HSE Competencies Report - Apply DB Recommendations

---
+++ resources/patches/patch01320_PICS-10853_ApplyDBFixForHSECompetenciesReport.sql
@@ -0,0 +1,39 @@
+-- Remove duplicate records from employee_competency first
+DELETE t1
+FROM
+	employee_competency	AS t1
+JOIN
+	employee_competency	AS t2
+WHERE
+	t1.employeeID	= t2.employeeID
+AND	t1.competencyID	= t2.competencyID
+AND	t1.id	< t2.id
+;
+
+ALTER TABLE	employee_competency
+ADD UNIQUE INDEX 	idxEmployee_Competency
+(
+	employeeID
+,	competencyID
+)
+;
+-- Remove duplicate records from employee_role first
+DELETE t1
+FROM
+	employee_role	AS t1
+JOIN
+	employee_role	AS t2
+WHERE
+	t1.employeeID	= t2.employeeID
+AND	t1.jobRoleID	= t2.jobRoleID
+AND	t1.id	< t2.id
+;
+
+ALTER TABLE	employee_role
+ADD UNIQUE INDEX	idxEmployee_Role
+(
+	employeeID
+,	jobRoleID
+)
+;
+
