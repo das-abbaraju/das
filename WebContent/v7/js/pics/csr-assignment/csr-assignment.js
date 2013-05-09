268543d5bb7706a8adbd3e06b796745a23774137
Joel Brownell <jbrownell@picsauditing.com>
2013-05-09 12:32:20 -0700

    PICS-9079: Code review updates and minification.

--- WebContent/v7/js/pics/csr-assignment/csr-assignment.js
+++ WebContent/v7/js/pics/csr-assignment/csr-assignment.js
@@ -3,61 +3,54 @@
         methods:{
             init:function() {
                 var that = this;
-                
-                $('#accept_all').on('change', function (event) {
-                    that.toggleRecommendations.call(that, event);
-                });
-                
-                $('#apply_assignments').on('click', function (event) {
-                    that.applyRecommendedCSR();
-                });
 
-                $('.accept-recommended').on('change', function (event) {
-                    that.updateSelectedRecords();
-                });
+                $('#accept_all').on('change', $.proxy(this, "toggleRecommendations"));
+                
+                $('#apply_selected_assignments').on('click', $.proxy(this, "applyRecommendedCSR"));
+
+                $('.accept-recommended').on('change', this.updateSelectedRecords);
             },
             
             applyRecommendedCSR: function () {
-                var $accept_recommended = $('.accept-recommended'),
-                    accepted_contractor_id = [],
-                    contractor_ids = '';
-
-                function getAcceptedValues() {
-                    $accept_recommended.each(function () {
-                        if ($(this).is(":checked")) {
-                            accepted_contractor_id.push(this.value);
-                        }
-                    });
-                }
-
-                getAcceptedValues();
-
-                contractor_ids = accepted_contractor_id.join();
+                log(this)
+                var $acceptedCSR = $('#acceptedCSR'),
+                    accepted_values = this.getAcceptedValues();
 
                 //update form value and submit form
-                $('#acceptedCSR').val(contractor_ids);
+                $acceptedCSR.val(accepted_values.join());
                 $('#save_approved_csr').submit();
+            },
 
+            getAcceptedValues: function () {
+                var $accept_recommended = $('.accept-recommended'),
+                    accepted_values = [];
+
+                $accept_recommended.each(function () {
+                    if ($(this).is(":checked")) {
+                        accepted_values.push(this.value);
+                    }
+                });
+
+                return accepted_values;
             },
 
             toggleRecommendations: function (event) {
                 var $element = $(event.target),
-                    $accept_recommended = $('.accept-recommended'),
-                    that = this;
-                
+                    $accept_recommended = $('.accept-recommended')
+
                 if ($element.is(":checked")) {
                     $accept_recommended.prop('checked', true);
                 } else {
                     $accept_recommended.prop('checked', false);
                 }
 
-                that.updateSelectedRecords();
+                this.updateSelectedRecords();
             },
 
             updateSelectedRecords: function () {
                 var $accept_recommended = $('.accept-recommended'),
-                    record_count = 0,
-                    $selected_records = $('#selected_records');
+                    $selected_records = $('#selected_records'),
+                    record_count = 0;
 
                 $accept_recommended.each(function () {
                     if ($(this).is(":checked")) {
