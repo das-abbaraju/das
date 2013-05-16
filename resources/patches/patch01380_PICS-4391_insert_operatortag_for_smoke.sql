143d61860bae8d0a8d3c17d5d512bddc8babea03
gmeurer <gmeurer@picsauditing.com>
2013-05-16 13:06:54 -0600

    PICS-4391: patch to create SmokeTestContractor operator tag for PICS Global

---
+++ resources/patches/patch01380_PICS-4391_insert_operatortag_for_smoke.sql
@@ -0,0 +1,2 @@
+insert ignore into operator_tag (tag, active, opID, createdBy, updatedBy, creationDate, updateDate, visibleToContractor, inheritable)
+values ('SmokeTestContractor', 1, 4, 1, 1, now(), now(), 0, 0);
