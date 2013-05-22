e1a72a5ca41618db073d277a5c3c1bad769ed25c
Jared Griffith <jgriffith@picsauditing.com>
2013-05-22 09:00:17 -0700

    PICS-1410 - Fixing errant = sign

--- resources/patches/patch01410_PICS-10791_Delete_Tag.sql
+++ resources/patches/patch01410_PICS-10791_Delete_Tag.sql
@@ -1,4 +1,4 @@
 --     PICS-10791 "Please remove the Bulk FM 2012-Find Alternative Tag (# 829)"
 
-    DELETE from contractor_tag where tagId = 829;
-    DELETE from operator_tag where id = 829 and tag = 'Bulk FM 2012-Find Alternative'; =
+DELETE from contractor_tag where tagId = 829;
+DELETE from operator_tag where id = 829 and tag = 'Bulk FM 2012-Find Alternative'; 
