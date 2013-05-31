669ba4d7b9f1e6a3a525c68cfdcc0e0b36ce61c8
Michael Do <mdo@picsauditing.com>
2013-05-31 09:46:36 -0700

    PICS-9400 Added tag to Contractor Operator Model

--- src/com/picsauditing/report/tables/ContractorTagView.java
+++ src/com/picsauditing/report/tables/ContractorTagView.java
@@ -8,8 +8,8 @@ public class ContractorTagView extends AbstractTable {
 	public ContractorTagView() {
 		super("(SELECT c.conID, o.opID, c.tagID, o.tag FROM contractor_tag c "
 				+ "JOIN operator_tag o ON c.tagID = o.id AND o.active = 1)");
-		addField(new Field("Tag", "GROUP_CONCAT(" + ReportOnClause.ToAlias + ".tag SEPARATOR ', ')", FieldType.String))
-				.setFilterable(false);
+//		addField(new Field("Tag", "GROUP_CONCAT(" + ReportOnClause.ToAlias + ".tag SEPARATOR ', ')", FieldType.String))
+//				.setFilterable(false);
 		Field tagID = addField(new Field("TagID", "tagID", FieldType.OperatorTag));
 		tagID.setVisible(false);
 	}
