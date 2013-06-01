f12a5ae51c269ca03a3657a90bff733596fa0a01
chinoki <chinoki@picsauditing.com>
2013-05-31 18:37:58 -0700

    PICS-10974 Fixing a library bug

--- WebContent/v7/js/pics/layout/menu/menu.js
+++ WebContent/v7/js/pics/layout/menu/menu.js
@@ -105,7 +105,38 @@
                     }
 
                     return this.hide();
-                }
+                };
+                
+                // fix for https://github.com/twitter/bootstrap/issues/2954
+                search_query_element.data('typeahead').move = function (e) {
+                    if (!this.shown) return
+
+                    switch(e.keyCode) {
+                        case 9: // tab
+                        case 13: // enter
+                        case 27: // escape
+                            e.preventDefault()
+                            break
+                        
+                        case 38: // up arrow
+                            if (!e.charCode) {
+                                e.preventDefault()
+                                this.prev()
+                            }
+                            
+                            break
+                        
+                        case 40: // down arrow
+                            if (!e.charCode) {
+                                e.preventDefault()
+                                this.next()
+                            }
+                            
+                            break
+                    }
+
+                    e.stopPropagation()
+                };
             },
 
             search: function (query, process) {
