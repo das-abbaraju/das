896c5172c3968cda179d02653e2d0d3dca0dd259
Bryan Latner <blatner@picsauditing.com>
2013-06-06 14:11:00 -0700

    Prevent Demo accounts from being marked to sync with quickbooks or SAP.

--- src/com/picsauditing/model/billing/AccountingSystemSynchronization.java
+++ src/com/picsauditing/model/billing/AccountingSystemSynchronization.java
@@ -8,11 +8,13 @@ import com.picsauditing.jpa.entities.Transaction;
 public class AccountingSystemSynchronization {
 
 	public static void setToSynchronize(Transaction transaction) {
+        if (transaction.getAccount().isDemo()) return;
 		transaction.setQbSync(true);
         transaction.setSapSync(true);
 	}
 
 	public static void setNotToSynchronize(Transaction transaction) {
+        if (transaction.getAccount().isDemo()) return;
 		transaction.setQbSync(false);
         transaction.setSapSync(false);
 	}
