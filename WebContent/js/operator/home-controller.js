456303279cf446437336c708214448274b9ffdb0
Jason Roos <jroos@picsauditing.com>
2013-06-07 16:15:19 -0700

    PICS-9185 More JS refactoring

--- WebContent/js/operator/home-controller.js
+++ WebContent/js/operator/home-controller.js
@@ -1,3 +1,86 @@
+PICS.Charts = {};
+
+PICS.Charts.Chart = function (config) {
+    if (!config) {
+        return;
+    }
+
+    this.style_type = config.type,
+    this.chart_type = config.chart_type, // FIXME: Should not be hard-coded
+    this.data = config.data,
+    this.container = config.container;
+};
+PICS.Charts.Chart.prototype.draw = function () {
+    var data_table = new google.visualization.DataTable(this.data),
+        options = this.getOptions(),
+        google_chart = this.getGoogleChart();
+
+    google_chart.draw(data_table, options);
+};
+PICS.Charts.Chart.prototype.getOptions = function () {
+    // Return generic, default options
+};
+PICS.Charts.Chart.prototype.getGoogleChart = function () {
+    switch (this.chart_type) {
+        case 'Bar':
+            return new google.visualization.BarChart(this.container);        
+        case 'Pie':
+        default:
+            return new google.visualization.PieChart(this.container);
+    }
+};
+PICS.Charts.Chart.prototype.getColumnNames = function () {
+    var column_names = [],
+        rows = this.data.rows;
+
+    $.each(rows, function(index, row) {
+        cell = row.c,
+        cell_column = cell[0],
+        column_name = cell_column.v;
+
+        column_names.push(column_name);
+    });
+
+    return column_names;
+};
+PICS.Charts.Chart.prototype.getColorsOrderedLikeColumnNames = function (column_names) {
+    var colors = [];
+
+    $.each(this.colors, function (column_name, color) {
+        colors[column_names.indexOf(column_name)] = color;
+    });
+
+    return colors;
+};
+
+PICS.Charts.FlagsChart = function (config) {
+    PICS.Charts.Chart.call(this, config);
+
+    this.colors = {
+        'Clear': '#FFF',
+        'Red': '#CF0F0F',
+        'Amber': '#FFCF3F',
+        'Green': '#3F9F0F'
+    };
+};
+PICS.Charts.FlagsChart.prototype = new PICS.Charts.Chart();
+PICS.Charts.FlagsChart.constructor = PICS.Charts.FlagsChart;
+PICS.Charts.FlagsChart.prototype.getOptions = function () {
+    var column_names = this.getColumnNames(this.data),
+        colors = this.getColorsOrderedLikeColumnNames(column_names);
+
+    return {
+        width: 400,
+        height: 300,
+        colors: colors,
+        is3D: true,
+        legend: {
+            position: 'top',
+            alignment: 'center'
+        }
+    };
+};
+
 (function ($) {
     PICS.define('home.HomeController', {
         methods: {
@@ -5,7 +88,7 @@
                 if ($('#Home__page').length) {
                     var that = this;
 
-                    $('.panel_content[data-widget-type=Chart]').each(function (key, value) {
+                    $('.panel_content[data-widget-type=Chart][data-widget-id=23]').each(function (key, value) {
                         var chart_container = this,
                             $chart_container = $(chart_container),
                             widget_id = $chart_container.data('widget-id');
@@ -17,78 +100,23 @@
                             },
                             type: 'GET',
                             dataType: 'json',
-                            success: function(response, textStatus, jqXHR) {
-                                var chart_data = response.data,
-                                    chart_options;
+                            success: function(data, textStatus, jqXHR) {
+                                var config = data,
+                                    chart;
 
-                                if (chart_data) {
-                                    chart_options = that.getChartOptions(response);
-                                    that.createChart(chart_container, chart_data, chart_options);
+                                config.chart_type = 'Pie';
+                                config.container = chart_container;
+
+                                switch (config.type) {
+                                    case 'Flags':
+                                        chart = new PICS.Charts.FlagsChart(config);
                                 }
+
+                                chart.draw();
                             }
                         });
                     });
                 }
-            },
-
-            createChart: function (container, data, options) {
-                var data_table = new google.visualization.DataTable(data),
-                    chart = new google.visualization.PieChart(container);
-
-                chart.draw(data_table, options);
-            },
-
-            getChartOptions: function (response) {
-                var chart_type = response.type,
-                    chart_data = response.data;
-
-                switch (chart_type) {
-                    case 'Flags':
-                        return this.getFlagsOptions(chart_data);
-                }
-            },
-
-            getColorsFromColumns: function (column_names) {
-                var colors = [];
-
-                colors[column_names.indexOf('Clear')] = '#FFF';
-                colors[column_names.indexOf('Red')] = '#CF0F0F';
-                colors[column_names.indexOf('Amber')] = '#FFCF3F';
-                colors[column_names.indexOf('Green')] = '#3F9F0F';
-
-                return colors;
-            },
-
-            getColumnNames: function (chart_data) {
-                var column_names = [],
-                    rows = chart_data.rows;
-
-                $.each(rows, function(index, row) {
-                    cell = row.c,
-                    cell_column = cell[0],
-                    column_name = cell_column.v;
-
-                    column_names.push(column_name);
-                });
-
-                return column_names;
-            },
-
-            getFlagsOptions: function(chart_data) {
-                var column_names = this.getColumnNames(chart_data),
-                    colors = this.getColorsFromColumns(column_names),
-                    rows = chart_data.rows;
-
-                return {
-                    width: 400,
-                    height: 300,
-                    colors: colors,
-                    is3D: true,
-                    legend: {
-                        position: 'top',
-                        alignment: 'center'
-                    }
-                };
             }
         }
     });
