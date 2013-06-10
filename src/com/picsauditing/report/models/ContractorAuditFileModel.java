a2957844690a804eeb865d86a358c15d30df1280
Michael Do <mdo@picsauditing.com>
2013-06-10 12:06:40 -0700

    PICS-11067 Added join to CAO for CAF Model

--- src/com/picsauditing/report/models/ContractorAuditFileModel.java
+++ src/com/picsauditing/report/models/ContractorAuditFileModel.java
@@ -4,11 +4,15 @@ import com.picsauditing.access.Permissions;
 import com.picsauditing.jpa.entities.Filter;
 import com.picsauditing.report.fields.Field;
 import com.picsauditing.report.tables.*;
+import com.picsauditing.util.Strings;
 
 import java.util.List;
 import java.util.Map;
 
 public class ContractorAuditFileModel extends AbstractModel {
+
+    private static final String AUDIT_OPERATOR = "AuditOperator";
+    private static final String CONTRACTOR_OPERATOR = "ContractorOperator";
 
 	public static final String ContractorAuditFile = "ContractorAuditFile";
 
@@ -33,18 +37,54 @@ public class ContractorAuditFileModel extends AbstractModel {
 		contractorInfo.alias = "ContractorInfo";
         contractorInfo.minimumImportance = FieldImportance.Required;
 
-		// Now, join from ContractorTable to ContractorAccountTable
+        // Now, join from ContractorTable to ContractorAccountTable
 		ModelSpec contractorAccount = contractorInfo.join(ContractorTable.Account);
 		contractorAccount.alias = "Account"; // Needs to agree to the param in the setUrl method below
         contractorAccount.minimumImportance = FieldImportance.Required;
 
-		return contractorAuditFile;
+		ModelSpec auditor = contractorAudit.join(ContractorAuditTable.Auditor);
+        auditor.minimumImportance = FieldImportance.Required;
+
+		ModelSpec closingAuditor = contractorAudit.join(ContractorAuditTable.ClosingAuditor);
+        closingAuditor.minimumImportance = FieldImportance.Required;
+
+        ModelSpec cao = contractorAudit.join(ContractorAuditTable.SingleCAO);
+        cao.alias = AUDIT_OPERATOR;
+        cao.minimumImportance = FieldImportance.Required;
+
+        ModelSpec caoOperator = cao.join(ContractorAuditOperatorTable.Operator);
+        caoOperator.alias = "AuditOperatorAccount";
+        caoOperator.minimumImportance = FieldImportance.Required;
+
+        return contractorAuditFile;
 	}
 
 	@Override
 	public String getWhereClause(List<Filter> filters) {
-		super.getWhereClause(filters);
-		return permissionQueryBuilder.buildWhereClause();
+        super.getWhereClause(filters);
+        permissionQueryBuilder.setContractorOperatorAlias(CONTRACTOR_OPERATOR);
+
+        String where = permissionQueryBuilder.buildWhereClause();
+
+        if (!where.isEmpty()) {
+            where += " AND ";
+        }
+
+        where += "AuditOperator.visible = 1";
+
+        if (permissions.isOperatorCorporate()) {
+            // TODO: This looks like it can be further improved. Find a way to
+            // do this without having to implode all of the ids.
+            String opIDs = permissions.getAccountIdString();
+            if (permissions.isCorporate()) {
+                opIDs = Strings.implode(permissions.getOperatorChildren());
+            }
+
+            where += "\n AND " + AUDIT_OPERATOR
+                    + ".id IN (SELECT caoID FROM contractor_audit_operator_permission WHERE opID IN (" + opIDs + "))";
+        }
+
+        return where;
 	}
 
 	@Override
