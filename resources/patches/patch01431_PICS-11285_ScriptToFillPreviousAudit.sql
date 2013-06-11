49b0fee0878c12a2506eb22d0ecb9fa48644cf76
Michael Do <mdo@picsauditing.com>
2013-06-11 10:24:07 -0700

    PICS-11285 updating fill in script

--- resources/patches/patch01431_PICS-11285_ScriptToFillPreviousAudit.sql
+++ resources/patches/patch01431_PICS-11285_ScriptToFillPreviousAudit.sql
@@ -1,12 +1,9 @@
 DROP TABLE IF EXISTS temp_previous_audit;
 
 CREATE TEMPORARY TABLE temp_previous_audit AS
-SELECT a.`name` accountName, ca.id AS audit1, ca2.id AS audit2, ca.`auditTypeID`, ca.`expiresDate` AS expires1, ca2.`expiresDate` AS expires2 FROM 
-accounts a
-JOIN contractor_audit ca ON a.id = ca.`conID` AND ca.id = (SELECT MAX(ca3.id) FROM contractor_audit ca3 WHERE ca.`conID` = ca3.`conID` AND ca.`auditTypeID` = ca3.`auditTypeID`)
-JOIN contractor_audit ca2 ON ca.`conID` = ca2.`conID` AND ca.`auditTypeID` = ca2.`auditTypeID` AND ca.id > ca2.`id`  AND ca2.id = (SELECT MAX(ca4.id) FROM contractor_audit ca4 WHERE ca.`conID` = ca4.`conID` AND ca.`auditTypeID` = ca4.`auditTypeID` AND ca.id > ca4.`id`)
-JOIN audit_type audt ON ca.`auditTypeID` = audt.`id` AND audt.`renewable` = 0 AND audt.`hasMultiple` = 0
-ORDER BY a.id, ca.`auditTypeID`, ca.id, ca2.id;
+SELECT ca.id AS audit1, pre.id AS audit2, ca.`auditTypeID`, ca.`expiresDate` AS expires1, pre.`expiresDate` AS expires2 FROM contractor_audit ca
+JOIN audit_type a ON a.id = ca.`auditTypeID` AND a.`renewable` = 0 AND (a.`hasMultiple` = 0 OR a.id = 11)
+JOIN contractor_audit pre ON pre.id = (SELECT t.id FROM contractor_audit t WHERE ca.`conID` = t.`conID` AND ca.`auditTypeID` = t.`auditTypeID` AND t.`creationDate` < ca.`creationDate` ORDER BY t.`creationDate` DESC LIMIT 1);
 
 UPDATE contractor_audit ca
 JOIN temp_previous_audit tpa ON ca.`id` = tpa.audit1
