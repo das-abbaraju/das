09dfb87f0f99fb952bac4c4018167fb41c4b0995
Alex Ananighian <aananighian@picsauditing.com>
2013-06-18 15:17:12 -0700

    PICS-9672 PICS-11199 PICS-11484 : Change test to use new PicsTranslationTest parent class

--- tests/com/picsauditing/report/data/ReportDataConverterTest.java
+++ tests/com/picsauditing/report/data/ReportDataConverterTest.java
@@ -1,6 +1,7 @@
 package com.picsauditing.report.data;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,39 +9,24 @@ import java.util.Locale;
 
 import org.apache.commons.beanutils.BasicDynaBean;
 import org.json.simple.JSONArray;
-import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.powermock.reflect.Whitebox;
 
 import com.picsauditing.EntityFactory;
-import com.picsauditing.PICS.I18nCache;
+import com.picsauditing.PicsTranslationTest;
 import com.picsauditing.access.OpPerms;
 import com.picsauditing.access.Permissions;
 import com.picsauditing.jpa.entities.Column;
-import com.picsauditing.search.Database;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class ReportDataConverterTest {
-    private static final Logger logger = LoggerFactory.getLogger(ReportDataConverterTest.class);
-
-	@Mock
-	Database databaseForTesting;
+public class ReportDataConverterTest extends PicsTranslationTest {
 
 	Permissions permissions;
-
-	@AfterClass
-	public static void classTearDown() {
-		Whitebox.setInternalState(I18nCache.class, "databaseForTesting", (Database) null);
-	}
 
 	@Before
 	public void setup() throws Exception {
 		MockitoAnnotations.initMocks(this);
-		Whitebox.setInternalState(I18nCache.class, "databaseForTesting", databaseForTesting);
+		super.resetTranslationService();
 
 		permissions = EntityFactory.makePermission();
 		EntityFactory.addUserPermission(permissions, OpPerms.Billing);
@@ -59,12 +45,12 @@ public class ReportDataConverterTest {
 		JSONArray json = runJsonConverter(queryResults);
 
 		assertEquals(1, json.size());
-        String expected = "\\[\\{\"AccountID\":1,\"AccountName\":\"Test 1\",\"AccountCreationDate\":\"1970-01-14 @ 2\\d:56\","
-                + "\"ContractorMembershipDate__Month\":\"janvier\",\"ContractorLastUpgradeDate\":null,\"AccountZip\":\"92614\","
-                + "\"ContractorMembershipDate\":\"1970-01-14\"\\}\\]";
+		String expected = "\\[\\{\"AccountID\":1,\"AccountName\":\"Test 1\",\"AccountCreationDate\":\"1970-01-14 @ 2\\d:56\","
+				+ "\"ContractorMembershipDate__Month\":\"janvier\",\"ContractorLastUpgradeDate\":null,\"AccountZip\":\"92614\","
+				+ "\"ContractorMembershipDate\":\"1970-01-14\"\\}\\]";
 
-        assertTrue(json.toString().matches(expected));
-    }
+		assertTrue(json.toString().matches(expected));
+	}
 
 	@Test
 	public void testConvertQueryResultsToJson_Simple() {
@@ -88,7 +74,7 @@ public class ReportDataConverterTest {
 		}
 	}
 
-    private JSONArray runJsonConverter(List<BasicDynaBean> queryResults) {
+	private JSONArray runJsonConverter(List<BasicDynaBean> queryResults) {
 		List<Column> columns = DynaBeanBuilder.makeColumns(permissions);
 
 		ReportDataConverter converter = new ReportDataConverter(columns, queryResults);
