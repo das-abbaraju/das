980cf1e0615ae3936b9f4f237ccfc7df9de59615
Jason Roos <jroos@picsauditing.com>
2013-06-19 18:28:06 -0700

    PICS-9185 : Modify options to use p property of cols

--- WebContent/js/operator/home-controller.js
+++ WebContent/js/operator/home-controller.js
@@ -23,52 +23,79 @@
                     dataType: 'json',
                     success: function(data, textStatus, jqXHR) {
                         var config = data,
-                            google_data = config.data,
-                            style_type = config.style_type || '',
-                            options = $.extend({}, that.getOptionsByStyleType(style_type), that.getDefaultOptions());
+                            google_data = config.data;
+                        google_data = {
+                          cols: [{id: 'A', label: 'NEW A', type: 'string', p: { class_name: 'green-flag'}},
+                                 {id: 'B', label: 'B-label', type: 'number', p: { class_name: 'red-flag'}},
+                                 {id: 'C', label: 'C-label', type: 'date', p: { class_name: 'yellow-flag'}}
+                                ],
+                          rows: [{c:[{v: 'a'}, {v: 1.0, f: 'One'}, {v: new Date(2008, 1, 28, 0, 31, 26), f: '2/28/08 12:31 AM'}]},
+                                 {c:[{v: 'b'}, {v: 2.0, f: 'Two'}, {v: new Date(2008, 2, 30, 0, 31, 26), f: '3/30/08 12:31 AM'}]},
+                                 {c:[{v: 'c'}, {v: 3.0, f: 'Three'}, {v: new Date(2008, 3, 30, 0, 31, 26), f: '4/30/08 12:31 AM'}]}
+                                ],
+                          p: {foo: 'hello', bar: 'world!'}
+                        };
 
-                        switch (chart_type) {
-                            case 'Pie':
-                                chart =  new google.visualization.PieChart(chart_container);
-                                break;
-                            case 'Bar':
-                                chart = new google.visualization.BarChart(chart_container);
-                                break;
-                            case 'Column':
-                                chart = new google.visualization.ColumnChart(chart_container);
-                                options.isStacked = true;
-                                break;
-                        }
 
-                        var google_data = new google.visualization.DataTable(config.data);
-                        chart.draw(google_data, options);
+var
+                            style_type = config.style_type || 'Flags',
+                            chart = that.getChartByType(chart_type, chart_container),
+                            data_table = new google.visualization.DataTable(google_data),
+                            options = $.extend({}, that.getChartOptionsForStyleType(style_type, data_table), that.getDefaultChartOptions());
+
+                        chart.draw(data_table, options);
                     }
                 });
             },
 
-            getFlagStyleOptions: function () {
-                return {
-                    colors: [
-                        'red',
-                        'green',
-                        'yellow'
-                    ]
+            getChartByType: function (chart_type, chart_container) {
+                switch (chart_type) {
+                    case 'Pie':
+                        return chart = new google.visualization.PieChart(chart_container);
+                    case 'Bar':
+                        return chart = new google.visualization.BarChart(chart_container);
+                    case 'Column':
+                        return chart = new google.visualization.ColumnChart(chart_container);
+                    default:
+                        throw new Error('Invalid chart type');
                 }
             },
 
-            getOptionsByStyleType: function (style_type) {
+            getDefaultChartOptions: function () {
+                return {
+                    height: 400
+                }
+            },
+
+            getChartOptionsForFlagStyleType: function (data_table) {
+                var column_index = 0,
+                    num_columns = data_table.getNumberOfColumns(),
+                    colors = [];
+
+                var color_options = {
+                    'red-flag': 'red',
+                    'yellow-flag': 'yellow',
+                    'green-flag': 'green'
+                };
+
+                for (; column_index < num_columns; column_index += 1) {
+                    class_name = data_table.getColumnProperty(column_index, 'class_name');
+
+                    colors.push(color_options[class_name]);
+                }
+
+                return {
+                    colors: colors
+                }
+            },
+
+            getChartOptionsForStyleType: function (style_type, data_table) {
                 switch (style_type) {
                     case 'Flags':
-                        return this.getFlagStyleOptions();
+                        return this.getChartOptionsForFlagStyleType(data_table);
                         break;
                     default:
                         return {};
-                }
-            },
-
-            getDefaultOptions: function () {
-                return {
-                    height: 400
                 }
             }
         }
