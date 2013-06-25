105039c399c30d233f52ff0941cdaedd0b14a2be
Michael Do <mdo@picsauditing.com>
2013-06-25 12:37:53 -0700

    PICS-11568 Added Client Site and Account Manager fields

--- src/com/picsauditing/report/models/ForcedFlagsModel.java
+++ src/com/picsauditing/report/models/ForcedFlagsModel.java
@@ -3,6 +3,7 @@ package com.picsauditing.report.models;
 import com.picsauditing.access.Permissions;
 import com.picsauditing.jpa.entities.Filter;
 import com.picsauditing.report.fields.Field;
+import com.picsauditing.report.fields.FieldType;
 import com.picsauditing.report.tables.*;
 import com.picsauditing.util.Strings;
 
@@ -21,10 +22,24 @@ public class ForcedFlagsModel extends AbstractModel {
 		ModelSpec spec = new ModelSpec(null, "ContractorFlag");
 
 		ModelSpec contractor = spec.join(ForcedFlagView.Contractor);
-        contractor.minimumImportance = FieldImportance.Required;
 		ModelSpec account = contractor.join(ContractorTable.Account);
         account.alias = "Account";
         account.minimumImportance = FieldImportance.Required;
+
+        ModelSpec operator = spec.join(ForcedFlagView.Operator);
+        operator.alias = "Operator";
+        operator.category = FieldCategory.ReportingClientSite;
+        ModelSpec opAccount = operator.join(OperatorTable.Account);
+        opAccount.alias = "ContractorOperatorOperator";
+        opAccount.minimumImportance = FieldImportance.Required;
+        opAccount.category = FieldCategory.ReportingClientSite;
+
+        ModelSpec accountManager = opAccount.join(AccountTable.AccountManager);
+        accountManager.alias = "AccountManager";
+        accountManager.category = FieldCategory.CustomerService;
+        ModelSpec accountManagerUser = accountManager.join(AccountUserTable.User);
+        accountManagerUser.alias = "AccountManagerUser";
+        accountManagerUser.category = FieldCategory.CustomerService;
 
         ModelSpec forcedBy = spec.join(ForcedFlagView.ForcedByUser);
         forcedBy.alias = "ContractorOperatorForcedByUser";
@@ -40,6 +55,11 @@ public class ForcedFlagsModel extends AbstractModel {
 		Field accountName = fields.get("AccountName".toUpperCase());
 		accountName.setUrl("ContractorView.action?id={AccountID}");
 
-		return fields;
+        Field accountType = new Field("ContractorOperatorOperatorType", "ContractorOperatorOperator.type", FieldType.AccountType);
+        accountType.setCategory(FieldCategory.ReportingClientSite);
+        accountType.setTranslationPrefixAndSuffix("AccountType", "");
+        fields.put("ContractorOperatorOperatorType".toUpperCase(), accountType);
+
+        return fields;
 	}
 }
