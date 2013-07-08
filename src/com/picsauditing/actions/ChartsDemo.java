98d1082c0c1d3d12b38dba3bb94c78222d252b9e
Jason Roos <jroos@picsauditing.com>
2013-07-08 15:00:43 -0700

    PICS-9185: Add dedicated charts demo action

---
+++ src/com/picsauditing/actions/ChartsDemo.java
@@ -0,0 +1,9 @@
+package com.picsauditing.actions;
+
+@SuppressWarnings("serial")
+public class ChartsDemo extends PicsActionSupport {
+
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+}
