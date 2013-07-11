c5a3aa584c7ec0b6188a99cb67db876ecdd2959f
Kirk Chase <kchase@picsauditing.com>
2013-07-11 13:31:52 -0700

    PICS-11046 update sales number

--- resources/patches/patch01760_PICS-11870_UpdateDBSales.sql
+++ resources/patches/patch01760_PICS-11870_UpdateDBSales.sql
@@ -1,2 +1,6 @@
 UPDATE ref_country
-SET salesPhone = '877-725-3022';
+SET salesPhone = '+1-877-725-3022';
+
+UPDATE ref_country
+SET salesPhone = '1-877-725-3022'
+where isoCode='US' or isoCode='CA';
