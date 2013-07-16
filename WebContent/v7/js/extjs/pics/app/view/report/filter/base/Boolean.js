b4802aabb95598a7f235ca4509514169bede6043
Jason Roos <jroos@picsauditing.com>
2013-07-16 16:45:21 -0700

    Revert "PICS-6475: Change Checkbox filter to Yes/No/Any radio options"
    This reverts commit a8b9a01070d78daddb64837158989a8f3f3575b0.

--- WebContent/v7/js/extjs/pics/app/view/report/filter/base/Boolean.js
+++ WebContent/v7/js/extjs/pics/app/view/report/filter/base/Boolean.js
@@ -11,26 +11,11 @@ Ext.define('PICS.view.report.filter.base.Boolean', {
 
     createValueField: function () {
         return {
-            xtype      : 'fieldcontainer',
-            defaultType: 'radiofield',
-            defaults: {
-                flex: 1,
-                name: 'value',
-                margin: '0 10 0 0'
-            },
-            layout: 'hbox',
-            items: [
-                {
-                    boxLabel  : 'Yes',
-                    inputValue: true,
-                }, {
-                    boxLabel  : 'No',
-                    inputValue: false
-                }, {
-                    boxLabel  : 'All',
-                    inputValue: ''
-                }
-            ]
+            xtype: 'checkbox',
+            boxLabel: 'True',
+            inputValue: true,
+            name: 'value',
+            uncheckedValue: false
         };
     }
 });
