e4cc619ea19ccdec41d5e7b6cc5a3b3bd04cca23
Bryan Latner <blatner@picsauditing.com>
2013-07-17 10:53:32 -0700

    Mild changes based on code review.

--- src/com/picsauditing/actions/billing/InvoiceRefundController.scala
+++ src/com/picsauditing/actions/billing/InvoiceRefundController.scala
@@ -4,11 +4,13 @@ import com.opensymphony.xwork2.Preparable
 import com.picsauditing.actions.contractors.ContractorActionSupport
 import com.picsauditing.dao.{CreditMemoDAO, InvoiceDAO}
 import org.springframework.beans.factory.annotation.Autowired
-import com.picsauditing.jpa.entities.{RefundItem, CreditMemoAppliedToInvoice, Invoice}
+import com.picsauditing.jpa.entities.{InvoiceItem, RefundItem, CreditMemoAppliedToInvoice, Invoice}
 import scala.beans.BeanProperty
 import scala.collection.JavaConversions._
 import com.picsauditing.access.{OpPerms, RequiredPermission}
 import scala.Some
+import java.lang.{Integer => int}
+import java.util.{List => StrutsList}
 
 @RequiredPermission(OpPerms.Billing)
 class InvoiceRefundController extends ContractorActionSupport with Preparable {
@@ -19,7 +21,7 @@ class InvoiceRefundController extends ContractorActionSupport with Preparable {
   private val invoiceDAO: InvoiceDAO = null
 
   @BeanProperty
-  var refunds: java.util.List[java.lang.Integer] = null.asInstanceOf[java.util.List[java.lang.Integer]]
+  var refunds: StrutsList[int] = null.asInstanceOf[StrutsList[int]]
   @BeanProperty
   var invoice: Invoice = null
 
@@ -41,7 +43,7 @@ class InvoiceRefundController extends ContractorActionSupport with Preparable {
 
   private def refundFor(invoice: Invoice) = {
     val refund = CreditMemoAppliedToInvoice.from(invoice)
-    selectedRefundItemsFor(invoice) foreach { item => item match {
+    refundItemsPickedFrom(invoice) foreach { item => item match {
         case Some(refundItem) => refund.getCreditMemo.getItems.add(refundItem)
         case None => {} // Do nothing.
       }
@@ -49,19 +51,32 @@ class InvoiceRefundController extends ContractorActionSupport with Preparable {
     refund
   }
 
-  private def selectedRefundItemsFor(invoice: Invoice): List[Option[RefundItem]] = {
-    refunds map { id => invoice.getItems.find(item => item.getId == id ) match {
-        case Some(item) => {
-          item.setRefunded(true)
-          Some(new RefundItem(item))
-        }
-        case None => {
-          addActionError("Item number " + id + " did not exist on this invoice.")
-          None
-        }
-      }
+  private def refundItemsPickedFrom(invoice: Invoice): List[Option[RefundItem]] = {
+    refunds map { proposedRefundId => generatedReturnItem { foundInvoiceItemFor(proposedRefundId) }}}.toList
+
+  private def foundInvoiceItemFor(proposedRefundItemId: int) = {
+    val matchingItem = existingUnrefundedInvoiceItem(_)(proposedRefundItemId)
+
+    invoice.getItems find matchingItem match {
+      case Some(item) => Right(item)
+      case None => Left(proposedRefundItemId)
     }
-  }.toList
+  }
+
+  private def existingUnrefundedInvoiceItem(invoiceItem: InvoiceItem)(lookingFor: int) = {
+    invoiceItem.getId == lookingFor && !invoiceItem.isRefunded
+  }
+
+  private def generatedReturnItem(invoiceItem: Either[int, InvoiceItem]) = invoiceItem match {
+    case Right(item) => {
+      item.setRefunded(true)
+      Some(new RefundItem(item))
+    }
+    case Left(id) => {
+      addActionError("Item number " + id + " did not exist on this invoice.")
+      None
+    }
+  }
 
   private def save(creditMemo: CreditMemoAppliedToInvoice) = {
     if (!creditMemo.getCreditMemo.getItems.isEmpty)
