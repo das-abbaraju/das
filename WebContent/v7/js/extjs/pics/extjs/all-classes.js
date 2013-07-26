a1ba62164b7d05ce5aaf0a288c6599eebfe93ada
Jason Roos <jroos@picsauditing.com>
2013-07-26 12:29:50 -0700

    PICS-11252: Add legend + Adjust layout + Fix active tab width

--- WebContent/v7/js/extjs/pics/extjs/all-classes.js
+++ WebContent/v7/js/extjs/pics/extjs/all-classes.js
@@ -83411,34 +83411,38 @@ Ext.define('PICS.view.report.settings.SubscribeSetting', {
     id: 'report_subscribe',
 
     items: [{
-        xtype: 'radiogroup',
-        defaults: {
-            flex: 1,
-            name: 'subscription_frequency',
-            margin: '0 0 10 0'
-        },
-        layout: 'vbox',
-        items: [{
-            boxLabel: PICS.text('Report.execute.subscribeSetting.labelNever'),
-            inputValue: 'None'
-        }, {
-            boxLabel: PICS.text('Report.execute.subscribeSetting.labelDaily'),
-            inputValue: 'Daily'
-        }, {
-            boxLabel: PICS.text('Report.execute.subscribeSetting.labelWeekly'),
-            inputValue: 'Weekly'
-        }, {
-            boxLabel: PICS.text('Report.execute.subscribeSetting.labelMonthly'),
-            inputValue: 'Monthly'
+      xtype: 'fieldset',
+      title: PICS.text('Report.execute.subscribeSetting.legendTitle') + ':',
+      items: [{
+            xtype: 'radiogroup',
+            defaults: {
+                flex: 1,
+                name: 'subscription_frequency',
+                margin: '0 0 10 0'
+            },
+            layout: 'vbox',
+            align: 'left',
+            items: [{
+                boxLabel: PICS.text('Report.execute.subscribeSetting.labelNever'),
+                inputValue: 'None'
+            }, {
+                boxLabel: PICS.text('Report.execute.subscribeSetting.labelDaily'),
+                inputValue: 'Daily'
+            }, {
+                boxLabel: PICS.text('Report.execute.subscribeSetting.labelWeekly'),
+                inputValue: 'Weekly'
+            }, {
+                boxLabel: PICS.text('Report.execute.subscribeSetting.labelMonthly'),
+                    inputValue: 'Monthly'
+            }]
         }]
     }],
 
     layout: {
-        type: 'vbox',
-        align: 'center',
+        type: 'form'
     },
 
-    padding: '40 0 0 0',
+    margin: '0 20 0 20',
 
     // custom config
     modal_title: PICS.text('Report.execute.subscribeSetting.title'),
