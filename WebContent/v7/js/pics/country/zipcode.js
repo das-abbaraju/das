3b7deba03a3a4a705969ff46ab6d80e07e6913c3
Joel Brownell <jbrownell@picsauditing.com>
2013-08-01 13:26:17 -0700

    Updated zipcode class function name.

--- WebContent/v7/js/pics/country/zipcode.js
+++ WebContent/v7/js/pics/country/zipcode.js
@@ -1,6 +1,6 @@
 PICS.define('country.Zipcode', {
     methods: {
-        render: function (selected_country, $zipcode) {
+        modifyZipcodeDisplay: function (selected_country, $zipcode) {
             if ($zipcode.length) {
                 if (selected_country == 'AE') {
                     $zipcode.slideUp(400);
