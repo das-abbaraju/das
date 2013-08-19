dfedb3cd42930363381ce1f14dc190657c5f32c2
Philip H. Schlesinger <pschlesinger@picsauditing.com>
2013-08-19 09:50:03 -0700

    PICS-12285 : Add filter code; add model to approved list

--- src/com/picsauditing/report/models/ContractorTradesModel.java
+++ src/com/picsauditing/report/models/ContractorTradesModel.java
@@ -1,11 +1,11 @@
 package com.picsauditing.report.models;
 
 import com.picsauditing.access.Permissions;
-import com.picsauditing.jpa.entities.AccountStatus;
 import com.picsauditing.jpa.entities.Filter;
 import com.picsauditing.report.fields.Field;
 import com.picsauditing.report.fields.FieldType;
-import com.picsauditing.report.tables.*;
+import com.picsauditing.report.tables.ContractorTradeTable;
+import com.picsauditing.report.tables.FieldCategory;
 
 import java.util.List;
 import java.util.Map;
@@ -41,6 +41,15 @@ public class ContractorTradesModel extends AbstractModel {
 
         Field tradeName = fields.get("TradeName".toUpperCase());
         tradeName.setVisible(true);
+
+		Field clientSite = new Field("ContractorWorksAtClientSite","Account.id",FieldType.Operator);
+		clientSite.setVisible(false);
+		clientSite.setPrefixValue("SELECT co.subID " +
+				"FROM generalcontractors co " +
+				"WHERE co.genID IN ");
+		clientSite.setSuffixValue("");
+		clientSite.setCategory(FieldCategory.ReportingClientSite);
+		fields.put(clientSite.getName().toUpperCase(), clientSite);
 
         return fields;
 	}
