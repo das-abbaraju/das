74c0cb2af5e9bd67deb3b8fc1591499b4d79efac
Jared Griffith <jgriffith@picsauditing.com>
2013-08-21 09:21:12 -0700

    PICS-11263 - Commenting out the index addition

--- resources/patches/patch01970_PICS-11263_Improve_Timezone_Display.sql
+++ resources/patches/patch01970_PICS-11263_Improve_Timezone_Display.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS ref_time_zone (
   zoneName varchar(200) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (id)
 );
-
-ALTER TABLE ref_time_zone
-  ADD  UNIQUE INDEX ak_ref_time_zone (countryCode, zoneName);
+-- Commenting this out since the table already exists as does the index
+-- ALTER TABLE IGNORE ref_time_zone
+--  ADD  UNIQUE INDEX ak_ref_time_zone (countryCode, zoneName);
