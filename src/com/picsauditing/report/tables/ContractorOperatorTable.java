4e564e98fb1e32ed7bc87334b8b4b12e26407412
Michael Do <mdo@picsauditing.com>
2013-08-21 20:52:37 -0700

    PICS-12345 Added tags to Contractor Lists.

--- src/com/picsauditing/report/tables/ContractorOperatorTable.java
+++ src/com/picsauditing/report/tables/ContractorOperatorTable.java
@@ -55,6 +55,13 @@ public class ContractorOperatorTable extends AbstractTable {
 		networkLevel.setVisible(false);
 		networkLevel.setFilterable(false);
 		addField(networkLevel);
+
+        Field contractorOperatorTag = new Field("Tag", "(SELECT GROUP_CONCAT(o.tag ORDER BY o.tag SEPARATOR ', ') FROM contractor_tag c " +
+                " JOIN operator_tag o ON c.tagID = o.id AND o.active = 1 " +
+                " WHERE " + ReportOnClause.ToAlias + ".subID = c.conID AND " + ReportOnClause.ToAlias + ".genID = o.opID)", FieldType.String);
+        contractorOperatorTag.setFilterable(false);
+        contractorOperatorTag.setWidth(300);
+        addField(contractorOperatorTag);
 	}
 
 	public void addJoins() {
