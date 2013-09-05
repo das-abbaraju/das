6938499caa976614edba170fc32ea62b56e22bf2
Solomon Shacter <sshacter@picsauditing.com>
2013-09-05 16:42:26 -0700

    PICS-12692

--- resources/patches/patch02130_PICS-12692_CreateAuthenticationTable.sql
+++ resources/patches/patch02130_PICS-12692_CreateAuthenticationTable.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS app_user
 	id	INT(11) NOT NULL AUTO_INCREMENT COMMENT "Authentication / Authorization ID"
 ,	username	VARCHAR(64) NOT NULL COMMENT "Unique user name"
 ,	`password` 	VARCHAR(64) NOT NULL COMMENT "The user's encrypted password"
-,	hashSalt 	VARCHAR(64) NOT NULL COMMENT "Random data that are used as an additional input to a one-way function that hashes a password or passphrase"
-,	resetHash 	VARCHAR(64) NOT NULL COMMENT "Password revocery hash"
+,	hashSalt 	VARCHAR(64) NULL COMMENT "Random data that are used as an additional input to a one-way function that hashes a password or passphrase"
+,	resetHash 	VARCHAR(64) NULL COMMENT "Password revocery hash"
 ,	lastLogin	DATETIME NULL COMMENT "The last date and time this user logged in"
 ,	PRIMARY KEY (id)
 ,	UNIQUE INDEX akApp_User (username)
