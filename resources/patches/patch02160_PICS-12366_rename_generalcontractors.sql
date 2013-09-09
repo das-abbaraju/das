e8c76cd2854d92c69086f4a3f00e7f7fb4bdcee7
Michael Do <mdo@picsauditing.com>
2013-09-09 15:43:41 -0700

    PICS-12366 rename generalcontractors.

---
+++ resources/patches/patch02160_PICS-12366_rename_generalcontractors.sql
@@ -0,0 +1,20 @@
+ALTER TABLE `generalcontractors`   
+  DROP INDEX `subID`,
+  DROP INDEX `genID`,
+  DROP FOREIGN KEY `FK_generalcontractors_con`,
+  DROP FOREIGN KEY `FK_generalcontractors_op`;
+
+ALTER TABLE `generalcontractors`   
+  CHANGE `genID` `opID` INT(11) NULL,
+  CHANGE `subID` `conID` INT(11) NULL, 
+  ADD  UNIQUE INDEX `conID` (`conID`, `opID`),
+  ADD  INDEX `opID` (`opID`),
+  ADD CONSTRAINT `FK_contractor_operator_op` FOREIGN KEY (`opID`) REFERENCES `pics_yesterday`.`accounts`(`id`),
+  ADD CONSTRAINT `FK_contractor_operator_con` FOREIGN KEY (`conID`) REFERENCES `pics_yesterday`.`accounts`(`id`);
+
+RENAME TABLE `pics_yesterday`.`generalcontractors` TO `pics_yesterday`.`contractor_operator`;
+
+DROP VIEW IF EXISTS generalcontractors;
+CREATE VIEW `generalcontractors` AS
+(SELECT id,opID AS genID,conID AS subID,gcID,TYPE,createdBy,creationDate,updatedBy,updateDate,workStatus,flag,lastStepToGreenDate,waitingOn,forceFlag,forceBegin,forceEnd,forcedBy,relationshipType,processCompletion,flagLastUpdated,forceReason,contractorType,baselineFlag,baselineApprover,baselineApproved,flagDetail,baselineFlagDetail,requestedByUserID,requestedByUser,deadline,reasonForRegistration
+FROM contractor_operator);
