0797b298790ec3f5c17046bc4224fad29d1f2578
Michael Do <mdo@picsauditing.com>
2013-09-10 11:37:27 -0700

    PICS-12366 changed sequence to better apply for live DBs

--- resources/patches/patch02160_PICS-12366_rename_generalcontractors.sql
+++ resources/patches/patch02160_PICS-12366_rename_generalcontractors.sql
@@ -1,18 +1,18 @@
-ALTER TABLE `generalcontractors`   
+RENAME TABLE `generalcontractors` TO `contractor_operator`;
+
+ALTER TABLE `contractor_operator`
   DROP INDEX `subID`,
   DROP INDEX `genID`,
   DROP FOREIGN KEY `FK_generalcontractors_con`,
   DROP FOREIGN KEY `FK_generalcontractors_op`;
 
-ALTER TABLE `generalcontractors`   
+ALTER TABLE `contractor_operator`
   CHANGE `genID` `opID` INT(11) NULL,
   CHANGE `subID` `conID` INT(11) NULL, 
   ADD  UNIQUE INDEX `conID` (`conID`, `opID`),
   ADD  INDEX `opID` (`opID`),
   ADD CONSTRAINT `FK_contractor_operator_op` FOREIGN KEY (`opID`) REFERENCES `accounts`(`id`),
   ADD CONSTRAINT `FK_contractor_operator_con` FOREIGN KEY (`conID`) REFERENCES `accounts`(`id`);
-
-RENAME TABLE `generalcontractors` TO `contractor_operator`;
 
 DROP VIEW IF EXISTS generalcontractors;
 CREATE VIEW `generalcontractors` AS
