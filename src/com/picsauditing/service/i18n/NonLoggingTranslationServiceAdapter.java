92b1c65bbf0cdb368e2391c1668410963b7ec452
gmeurer <gmeurer@picsauditing.com>
2013-09-12 09:57:24 -0600

    PICS-12091: fixed registering strategy in non logging adapter

--- src/com/picsauditing/service/i18n/NonLoggingTranslationServiceAdapter.java
+++ src/com/picsauditing/service/i18n/NonLoggingTranslationServiceAdapter.java
@@ -3,15 +3,26 @@ package com.picsauditing.service.i18n;
 import com.picsauditing.model.i18n.TranslationWrapper;
 
 public class NonLoggingTranslationServiceAdapter extends TranslationServiceAdapter {
-    private static final TranslationService INSTANCE = new NonLoggingTranslationServiceAdapter();
+    private static TranslationService INSTANCE;
 
     private NonLoggingTranslationServiceAdapter() {
         super();
     }
 
     public static TranslationService getInstance() {
+        TranslationService service = INSTANCE;
+        if (service == null) {
+            synchronized (TranslationServiceAdapter.class) {
+                service = INSTANCE;
+                if (service == null) {
+                    registerTranslationTransformStrategy();
+                    INSTANCE = new NonLoggingTranslationServiceAdapter();
+                }
+            }
+        }
         return INSTANCE;
     }
+
 
     @Override
     public String getText(String key, String locale) {
