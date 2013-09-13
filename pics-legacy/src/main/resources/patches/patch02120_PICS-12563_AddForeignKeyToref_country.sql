ac33397b51e13d9ed10bf53ebf85169253f7d332
Solomon Shacter <sshacter@picsauditing.com>
2013-09-13 11:43:13 -0700

    PICS-12563 Add FK reference on businessUnitID

--- pics-legacy/src/main/resources/patches/patch02120_PICS-12563_AddForeignKeyToref_country.sql
+++ pics-legacy/src/main/resources/patches/patch02120_PICS-12563_AddForeignKeyToref_country.sql
@@ -1,3 +1,5 @@
 -- per ticket instructions. set this column to nullable instead of adding a FK
 ALTER TABLE ref_country
   CHANGE `businessUnitID` `businessUnitID` INT(8) DEFAULT 1  NULL;
+ALTER TABLE `pics_alpha1`.`ref_country`
+  ADD CONSTRAINT `fk1_ref_country` FOREIGN KEY (`businessUnitID`) REFERENCES `pics_alpha1`.`ref_sap_business_unit`(`id`) ON DELETE SET NULL;
