47335520b61a48378f8642e20a7ea962094b076b
Lucas Kam <lkam@picsauditing.com>
2013-10-02 14:57:41 -0700

    PICS-13059 Added timeouts, exception handling and resource closing
    -- Should fix some memory leaks and performance issues.

--- pics-legacy/src/main/java/com/picsauditing/validator/VATWebValidator.java
+++ pics-legacy/src/main/java/com/picsauditing/validator/VATWebValidator.java
@@ -1,9 +1,5 @@
 package com.picsauditing.validator;
 
-import com.picsauditing.jpa.entities.EmailQueue;
-import com.picsauditing.mail.EmailBuilder;
-import com.picsauditing.util.EmailAddressUtils;
-import org.apache.struts2.ServletActionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,10 +8,10 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class VATWebValidator {
-    private final Logger logger = LoggerFactory.getLogger(VATWebValidator.class);
+    private static final Logger logger = LoggerFactory.getLogger(VATWebValidator.class);
 
     public void webValidate(String vatCode) throws ValidationException {
-        String countryPrefix = vatCode.substring(0,2);
+        String countryPrefix = vatCode.substring(0, 2);
         String numbers = vatCode.substring(2, vatCode.length());
 
         String result = null;
@@ -32,20 +28,46 @@ public class VATWebValidator {
         }
     }
 
-
     private String runValidation(String countryPrefix, String numbers) throws IOException {
-        HttpURLConnection connection = (HttpURLConnection) new URL(
-                "http://isvat.appspot.com/" + countryPrefix + "/" + numbers +"/"
-        ).openConnection();
-        connection.setRequestMethod("GET");
-        connection.setDoOutput(true);
-        OutputStream output = connection.getOutputStream();
-        output.close();
+        HttpURLConnection connection = null;
+        OutputStream output = null;
+        BufferedReader br = null;
+        String result = null;
+        try {
+            connection = (HttpURLConnection) new URL(
+                    "http://isvat.appspot.com/" + countryPrefix + "/" + numbers + "/"
+            ).openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            connection.setDoOutput(true);
+            output = connection.getOutputStream();
 
-        InputStream input = connection.getInputStream();
-        BufferedReader br = new BufferedReader(new InputStreamReader(input));
-        String lin = br.readLine();
+            InputStream input = connection.getInputStream();
 
-        return lin;
+            br = new BufferedReader(new InputStreamReader(input));
+            result = br.readLine();
+        } catch (IOException e) {
+            // Don't keep the contractor from registering if there's a connection problem.
+            result = "true" ;
+            throw e;
+        }
+        finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+            if (output != null) {
+                try {
+                    output.close();
+                } catch (IOException e) {}
+            }
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {}
+            }
+
+           return result;
+        }
     }
 }
