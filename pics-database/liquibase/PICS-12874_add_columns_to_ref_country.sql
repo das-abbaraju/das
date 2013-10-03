e8d10787e533dae70b664c63bb71675511c7c0ba
Solomon Shacter <sshacter@picsauditing.com>
2013-10-03 14:48:19 -0700

    Add Liquibase to PICSORG

--- pics-database/liquibase/PICS-12874_add_columns_to_ref_country.sql
+++ pics-database/liquibase/PICS-12874_add_columns_to_ref_country.sql
@@ -1,10 +1,9 @@
--- liquibase formatted sql
+--liquibase formatted sql
 
--- changeset mdo:5
-
+--changeset mdo:5
+--preConditions onFail MARK_RAN
+--onUpdateSQL IGNORE
 -- Remove the bad My Schedule timeslots for Samuel Duckworth-Essilfie which are not removeable from the UI due to bug in our calendar plugin.
 ALTER TABLE `ref_country`
   ADD COLUMN `csrAddress2` VARCHAR(50) NULL AFTER `csrAddress`,
   ADD COLUMN `csrCountry` VARCHAR(10) NULL AFTER `csrCity`;
-
--- rollback select 1 from dual;
--- pics-database/liquibase/PICS-12878_change_all_users_to_allow_switch_to_v7_except_BASF.sql
+++ pics-database/liquibase/PICS-12878_change_all_users_to_allow_switch_to_v7_except_BASF.sql
@@ -1,10 +1,8 @@
--- liquibase formatted sql
+--liquibase formatted sql
 
--- changeset mdo:4
+--changeset mdo:4
 
 -- Flip a toggle from inclusive to exclusive to exclude BASF
 UPDATE app_properties
 SET VALUE = '!(permissions.primaryCorporateAccountID in [6115])'
 WHERE property = 'Toggle.v7Menus';
-
--- rollback select 1 from dual;
