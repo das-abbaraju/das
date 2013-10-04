4443c0a1bf608f141c176dbd89cf1160f450b171
Philip H. Schlesinger <pschlesinger@picsauditing.com>
2013-10-04 10:53:59 -0700

    PICS-12857 : Add patches to adjust table structure for SAP

---
+++ pics-database/liquibase/pschlesinger1_PICS-12857_ModifyBizUnitTblStructureAddSapCode.sql
@@ -0,0 +1,21 @@
+--liquibase formatted sql
+
+--changeset pschlesinger:1
+SELECT IFNULL(column_name, null) INTO @colName
+FROM information_schema.columns
+WHERE table_name = 'ref_sap_business_unit'
+      AND column_name = 'sapCode'
+      and table_schema = database();
+
+select if(@colName is not null, 'select 1', 'ALTER TABLE `ref_sap_business_unit` ADD COLUMN `sapCode` varchar(20) NOT NULL DEFAULT \'Undefined\' ') into @addStatement;
+
+prepare addColumn from @addStatement;
+
+execute addColumn;
+
+deallocate prepare addColumn;
+
+set @colName = null;
+COLLATE=utf8_general_ci CHECKSUM=0 DELAY_KEY_WRITE=0
+;
+
