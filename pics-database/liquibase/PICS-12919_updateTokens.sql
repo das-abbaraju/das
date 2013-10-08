1029c10fad4fa9e911d421a914ed3a4b9bfff21b
Kirk Chase <kchase@picsauditing.com>
2013-10-08 13:45:19 -0700

    PICS-12919 added CSRCountry, updated CSR and ISR to get the info from their current setting

--- pics-database/liquibase/PICS-12919_updateTokens.sql
+++ pics-database/liquibase/PICS-12919_updateTokens.sql
@@ -3,19 +3,21 @@
 --changeset kchase:2
 -- update tokens
 update token set velocityCode='${contractor.currentCsr.name}' where tokenName='CSRName';
-update token set velocityCode='${contractor.country.csrPhone}' where tokenName='CSRPhone';
-update token set velocityCode='${contractor.country.csrFax}' where tokenName='CSRFax';
-update token set velocityCode='${contractor.country.csrEmail}' where tokenName='CSREmail';
+update token set velocityCode='${contractor.currentCsr.country.csrPhone}' where tokenName='CSRPhone';
+update token set velocityCode='${contractor.currentCsr.country.csrFax}' where tokenName='CSRFax';
+update token set velocityCode='${contractor.currentCsr.country.csrEmail}' where tokenName='CSREmail';
 update token set velocityCode='${contractor.country.businessUnit.displayName}' where tokenName='CountrySpecificPicsName';
 update token set velocityCode='${contractor.country.businessUnit.addressSingleLine}' where tokenName='CountrySpecificOfficeAddress';
 update token set velocityCode='${contractor.country.phone}' where tokenName='CountrySpecificMainPhone';
 update token set velocityCode='${contractor.country.salesPhone}' where tokenName='CountrySpecificSalesPhone';
 update token set velocityCode='${contractor.country.picsEmail}' where tokenName='CountrySpecificEmail';
+update token set velocityCode='${contractor.currentCsr.country.english}' where tokenName='CSRCountry';
 
 update app_translation set msgValue="${contractor.currentCsr.name}" where msgKey="Token.31.velocityCode";
-update app_translation set msgValue="${contractor.country.csrPhone}" where msgKey="Token.32.velocityCode";
-update app_translation set msgValue="${contractor.country.csrFax}" where msgKey="Token.34.velocityCode";
-update app_translation set msgValue="${contractor.country.csrEmail}" where msgKey="Token.33.velocityCode";
+update app_translation set msgValue="${contractor.currentCsr.country.csrPhone}" where msgKey="Token.32.velocityCode";
+update app_translation set msgValue="${contractor.currentCsr.country.csrFax}" where msgKey="Token.34.velocityCode";
+update app_translation set msgValue="${contractor.currentCsr.country.csrEmail}" where msgKey="Token.33.velocityCode";
+update app_translation set msgValue="${contractor.currentCsr.country.english}" where msgKey="Token.85.velocityCode";
 update app_translation set msgValue="${contractor.country.businessUnit.displayName}" where msgKey="Token.46.velocityCode";
 update app_translation set msgValue="${contractor.country.businessUnit.addressSingleLine}" where msgKey="Token.47.velocityCode";
 update app_translation set msgValue="${contractor.country.phone}" where msgKey="Token.43.velocityCode";
@@ -27,8 +29,8 @@ update app_translation set msgValue="${contractor.country.csrCity}" where msgKey
 update app_translation set msgValue="${contractor.country.csrCountrySubdivision}" where msgKey="Token.70.velocityCode";
 update app_translation set msgValue="${contractor.country.csrZip}" where msgKey="Token.71.velocityCode";
 update app_translation set msgValue="${contractor.country.currency}" where msgKey="Token.72.velocityCode";
-update app_translation set msgValue="${contractor.country.isrPhone}" where msgKey="Token.73.velocityCode";
-update app_translation set msgValue="${contractor.country.isrFax}" where msgKey="Token.74.velocityCode";
-update app_translation set msgValue="${contractor.country.isrEmail}" where msgKey="Token.75.velocityCode";
+update app_translation set msgValue="${contractor.currentInsideSalesRepresentativecountry.isrPhone}" where msgKey="Token.73.velocityCode";
+update app_translation set msgValue="${contractor.currentInsideSalesRepresentativecountry.isrFax}" where msgKey="Token.74.velocityCode";
+update app_translation set msgValue="${contractor.currentInsideSalesRepresentativecountry.isrEmail}" where msgKey="Token.75.velocityCode";
 
 
