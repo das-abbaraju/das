e0a39f838c39533dfb90ee20b4e5678f93736215
Kirk Chase <kchase@picsauditing.com>
2013-10-09 11:13:44 -0700

    PICS-12980 patch script updated to include all operators

--- pics-database/liquibase/kchase1_PICS-12980_UpdateWorkStatus.sql
+++ pics-database/liquibase/kchase1_PICS-12980_UpdateWorkStatus.sql
@@ -2,7 +2,21 @@
 
 --changeset kchase:1
 -- update Johnson & Johnson contractors work status
-update generalcontractors gc
-set gc.workStatus='Y'
+-- original script
+-- update generalcontractors gc
+-- set gc.workStatus='Y'
+-- where gc.workStatus='P'
+-- and gc.genID=41063;
+
+update
+generalcontractors gc
+join accounts a on gc.genID = a.id
+join accounts con on gc.subID = con.id
+join operators o on a.id = o.id
+set gc.workStatus = 'Y'
 where gc.workStatus='P'
-and gc.genID=41063;
+and a.autoApproveRelationships = 1
+and a.status = 'Active'
+and con.status = 'Active'
+and o.inPicsConsortium = 0
+;
