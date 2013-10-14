43f42c1baf56879073597b80ee2e37a3a4c36f7f
Jared Griffith <jared@Nickel-Mac.local>
2013-10-14 10:26:03 -0700

    Fixing Phil's patch back to what it was

--- pics-database/liquibase/pschlesinger3_PICS-12857_AddingKnownSapCodes.sql
+++ pics-database/liquibase/pschlesinger3_PICS-12857_AddingKnownSapCodes.sql
@@ -4,3 +4,4 @@
 update ref_sap_business_unit set sapCode = 'US' where businessUnit = 'US';
 update ref_sap_business_unit set sapCode = 'CA' where businessUnit = 'Canada';
 update ref_sap_business_unit set sapCode = 'EMEA' where businessUnit = 'UK_EU';
+;
