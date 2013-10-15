9dad4a86e51dd7cc47795c9dfce914f3416f0087
Jason Roos <jroos@picsauditing.com>
2013-10-14 18:51:57 -0700

    PICS-13106: Add ability to turn any string into a uri slug

--- pics-web/src/main/webapp/v7/js/script.js
+++ pics-web/src/main/webapp/v7/js/script.js
@@ -21347,6 +21347,33 @@ PICS.define('country.Country', {
         }
     });
 }(jQuery));
+(function(window, $) {
+    function toSlug() {
+        slug = this
+            // Make all letters lower-case
+            .toLowerCase()
+            // Replace 1 or more hyphens with a single space
+            .replace(/-+/g, ' ')
+            // Replace 1 or more spaces with a single hyphen
+            .replace(/\s+/g, '-');
+
+        if (isValidSlug(slug)) {
+            return slug;
+        }
+
+        return false;
+    }
+
+    function isValidSlug(slug) {
+        var regex_uri = /^((?:[a-z0-9.-]|%[0-9A-F]{2}){3,})(?::(\d+))?((?:\/(?:[a-z0-9-._~!$&'()*+,;=:@]|%[0-9A-F]{2})*)*)(?:\?((?:[a-z0-9-._~!$&'()*+,;=:\/?@]|%[0-9A-F]{2})*))?(?:#((?:[a-z0-9-._~!$&'()*+,;=:\/?@]|%[0-9A-F]{2})*))?$/i;
+
+        return regex_uri.test(slug);
+    }
+
+    $.extend(window.String.prototype, {
+        toSlug: toSlug
+    });
+}(window, jQuery));
 /**
  * Validate
  *
