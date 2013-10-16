efc230d3af045032b19d07e83b228e299efd5d7b
Michael Do <mdo@picsauditing.com>
2013-10-16 09:25:10 -0700

    PICS-13047 Changed user group table to use calc table and U Model.

--- pics-legacy/src/main/java/com/picsauditing/report/tables/UserGroupTable.java
+++ pics-legacy/src/main/java/com/picsauditing/report/tables/UserGroupTable.java
@@ -6,13 +6,12 @@ public class UserGroupTable extends AbstractTable {
 	public static final String Group = "Group";
 
 	public UserGroupTable() {
-		super("usergroup");
+		super("calc_inherited_user_group");
 		addFields(UserGroup.class);
 	}
 
 	protected void addJoins() {
 		ReportForeignKey group = new ReportForeignKey(Group, new UserTable(), new ReportOnClause("groupID","id", ReportOnClause.ToAlias + ".isGroup = 'Yes'"));
-		group.setMinimumImportance(FieldImportance.Average);
 		addOptionalKey(group);
 	}
 }
