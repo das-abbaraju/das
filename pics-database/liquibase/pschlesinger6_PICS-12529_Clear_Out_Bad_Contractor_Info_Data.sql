f1e912332c8c0981440e6ec2031497f7826bae33
Philip H. Schlesinger <pschlesinger@picsauditing.com>
2013-10-25 15:46:08 -0700

    PICS-12529 : patch to clean up contractor_info -- data is junk, causing clashes with good data on accounts

---
+++ pics-database/liquibase/pschlesinger6_PICS-12529_Clear_Out_Bad_Contractor_Info_Data.sql
@@ -0,0 +1,23 @@
+--liquibase formatted sql
+
+--changeset pschlesinger:6
+update
+    contractor_info
+join
+  accounts
+on
+  contractor_info.id = accounts.id
+set
+  billingAddress = null,
+  billingCity = null,
+  billingCountrySubdivision = null,
+  billingCountry = null
+where
+  accounts.status != 'Demo'
+and
+      country != billingCountry
+and
+      billingCountry is not null
+and
+(billingAddress is null or billingAddress = '');
+
