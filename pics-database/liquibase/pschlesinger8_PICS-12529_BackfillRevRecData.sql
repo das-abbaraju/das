748242920947d10fc3fd2dd58d551adfa02ede31
Philip H. Schlesinger <pschlesinger@picsauditing.com>
2013-10-27 22:02:59 -0700

    PICS-12529 : Add SQL to backfill rev rec data for Activation and Renewal invoices

--- pics-database/liquibase/pschlesinger8_PICS-12529_BackfillRevRecData.sql
+++ pics-database/liquibase/pschlesinger8_PICS-12529_BackfillRevRecData.sql
@@ -5,8 +5,8 @@
 UPDATE	invoice_item
 ,	invoice
 SET
-invoice_item.startDate	= DATE(invoice.creationDate)
-,	invoice_item.endDate	= DATE(DATE_ADD(invoice.creationDate, INTERVAL 1 YEAR))
+invoice_item.revenueStartDate	= DATE(invoice.creationDate)
+,	invoice_item.revenueFinishDate	= DATE(DATE_ADD(invoice.creationDate, INTERVAL 1 YEAR))
 WHERE	1=1
 AND	invoice.invoiceType = "Activation"
 AND	invoice.id = invoice_item.invoiceID
@@ -16,8 +16,8 @@ AND	invoice.id = invoice_item.invoiceID
 UPDATE	invoice_item
 ,	invoice
 SET
-invoice_item.startDate	= DATE(DATE_ADD(invoice.creationDate, INTERVAL 1 MONTH))
-,	invoice_item.endDate	= DATE(DATE_ADD(DATE_ADD(invoice.creationDate, INTERVAL 1 MONTH), INTERVAL 1 YEAR))
+invoice_item.revenueStartDate	= DATE(DATE_ADD(invoice.creationDate, INTERVAL 1 MONTH))
+,	invoice_item.revenueFinishDate	= DATE(DATE_ADD(DATE_ADD(invoice.creationDate, INTERVAL 1 MONTH), INTERVAL 1 YEAR))
 WHERE	1=1
 AND	invoice.invoiceType = "Renewal"
 AND	invoice.id = invoice_item.invoiceID
