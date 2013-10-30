df6c9e477473154df5bd63ca15f4d086b437dd51
gmeurer <gmeurer@picsauditing.com>
2013-10-30 15:19:12 -0600

    PICS-13390: reverted TranslationKeyValidator, urlencoding translate,

--- pics-legacy/src/main/java/com/picsauditing/service/i18n/TranslationKeyValidator.java
+++ pics-legacy/src/main/java/com/picsauditing/service/i18n/TranslationKeyValidator.java
@@ -2,23 +2,14 @@ package com.picsauditing.service.i18n;
 
 import com.picsauditing.util.Strings;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class TranslationKeyValidator {
-    private static Pattern pattern = Pattern.compile("^[a-zA-Z0-9.]+$");
 
     public boolean validateKey(String key) {
-        if (Strings.isEmpty(key) || key.contains(" ") || containsInvalidCharacters(key)) {
+        if (Strings.isEmpty(key) || key.contains(" ")) {
             return false;
         } else {
             return true;
         }
-    }
-
-    private boolean containsInvalidCharacters(String key) {
-        Matcher matcher = pattern.matcher(key);
-        return !matcher.matches();
     }
 
 }
