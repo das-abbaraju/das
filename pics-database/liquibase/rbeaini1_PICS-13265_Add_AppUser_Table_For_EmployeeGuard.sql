6b8d021fc1b897232514f4bda569bf64083bb16f
Jared Griffith <jgriffith@picsauditing.com>
2013-10-30 10:56:26 -0700

    PICS-13265 #time 1h 20m #comment This should have been a patch in the first place

--- pics-database/liquibase/rbeaini1_PICS-13265_Add_AppUser_Table_For_EmployeeGuard.sql
+++ pics-database/liquibase/rbeaini1_PICS-13265_Add_AppUser_Table_For_EmployeeGuard.sql
@@ -4,8 +4,8 @@
 CREATE TABLE IF NOT EXISTS app_user
  (
    id  INT(11) NOT NULL AUTO_INCREMENT COMMENT "Authentication / Authorization ID"
- ,  username  VARCHAR(64) NOT NULL COMMENT "Unique user name"
- ,  password   VARCHAR(64) NOT NULL COMMENT "The user's encrypted password"
+ ,  username  VARCHAR(100) NOT NULL COMMENT "Unique user name"
+ ,  password   VARCHAR(100) NOT NULL COMMENT "The user's encrypted password"
  ,  hashSalt   VARCHAR(64) NULL COMMENT "Random data that are used as an additional input to a oneway function that hashes a password or passphrase"
  ,  resetHash   VARCHAR(64) NULL COMMENT "Password revocery hash"
  ,  lastLogin  DATETIME NULL COMMENT "The last date and time this user logged in"
