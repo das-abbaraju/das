ea51d5d8a2afcfb9213e682304c26fda51c69ee4
Jason Roos <jroos@picsauditing.com>
2013-11-05 12:09:49 -0800

    PICS-13412: Move event re-binding to controller, where it belongs

--- pics-web/src/main/webapp/v7/js/extjs/pics/app/view/report/modal/column-filter/FilterList.js
+++ pics-web/src/main/webapp/v7/js/extjs/pics/app/view/report/modal/column-filter/FilterList.js
@@ -7,16 +7,6 @@ Ext.define('PICS.view.report.modal.column-filter.FilterList', {
     id: 'filter_list',
 
     selModel: Ext.create('Ext.selection.CheckboxModel', {
-        mode: 'SIMPLE',
-
-        listeners: {
-            // This probably belongs in the controller. When defined there, however, selectionchange
-            // doesn't fire on subsequent loads of the modal.
-            selectionchange: function () {
-                var add_button = Ext.ComponentQuery.query('reportfiltermodal button[action=add]')[0];
-
-                this.getCount() ? add_button.setDisabled(false) : add_button.setDisabled(true);
-            }
-        }
+        mode: 'SIMPLE'
     }),
 });
