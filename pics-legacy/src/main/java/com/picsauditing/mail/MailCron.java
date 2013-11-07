b91ad595c0f3dade0fa6c918d2fb4dd74747780b
Trevor Allred <tallred@picsauditing.com>
2013-11-06 22:09:45 -0800

    minor refactoring of MailCron

--- pics-legacy/src/main/java/com/picsauditing/mail/MailCron.java
+++ pics-legacy/src/main/java/com/picsauditing/mail/MailCron.java
@@ -4,54 +4,52 @@ import com.picsauditing.access.Anonymous;
 import com.picsauditing.actions.PicsActionSupport;
 import com.picsauditing.service.mail.MailCronService;
 import com.picsauditing.validator.ValidationException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-/**
- * Run the email task every minute Send 5 emails at a time This translates into 300/hour or 7200/day If this seems slow,
- * it is because of strict limits by gmail
- *
- * @author Trevor
- *
- */
 @SuppressWarnings("serial")
 public class MailCron extends PicsActionSupport {
 
-	@Autowired
-	private MailCronService mailCronService;
-	private int subscriptionID = 0;
+    @Autowired
+    MailCronService mailCronService;
+    private int subscriptionID = 0;
 
-	private final Logger logger = LoggerFactory.getLogger(MailCron.class);
-	
-	@Anonymous
-	public String execute() {
+    @Anonymous
+    public String execute() {
+        // TODO Don't call this anymore to send mail or create email subscriptions, call subscription() and send() directly
+        subscription();
+        send();
+        return ACTION_MESSAGES;
+    }
 
-		try {
-			mailCronService.processEmailSubscription(subscriptionID, getPermissions());
-		} catch (ValidationException e) {
-			addActionError(e.getMessage());
-			// todo: Consider not returning and instead, continue with processPendingEmails()
-			return ACTION_MESSAGES;
-		}
+    @Anonymous
+    public String subscription() {
+        try {
+            mailCronService.processEmailSubscription(subscriptionID, getPermissions());
+        } catch (ValidationException e) {
+            addActionError(e.getMessage());
+        }
+        return ACTION_MESSAGES;
+    }
 
-		String statusMessage = mailCronService.processPendingEmails(permissions);
-		addActionMessage(statusMessage);
-		return ACTION_MESSAGES;
-	}
+    @Anonymous
+    public String send() {
+        String statusMessage = mailCronService.processPendingEmails(permissions);
+        addActionMessage(statusMessage);
+        return ACTION_MESSAGES;
+    }
 
-	@Anonymous
-	public String listAjax() {
-		output = mailCronService.getSubscriptionIdsToSendAsCommaDelimited();
-		return PLAIN_TEXT;
-	}
+    @Anonymous
+    public String listAjax() {
+        output = mailCronService.getSubscriptionIdsToSendAsCommaDelimited();
+        return PLAIN_TEXT;
+    }
 
-	public void setSubscriptionID(int subscriptionID) {
-		this.subscriptionID = subscriptionID;
-	}
+    public int getSubscriptionID() {
+        return subscriptionID;
+    }
 
-	public int getSubscriptionID() {
-		return subscriptionID;
-	}
+    public void setSubscriptionID(int subscriptionID) {
+        this.subscriptionID = subscriptionID;
+    }
 
 }
---
+++ pics-legacy/src/test/java/com/picsauditing/mail/MailCronTest.java
@@ -0,0 +1,54 @@
+package com.picsauditing.mail;
+
+import com.picsauditing.PicsActionTest;
+import com.picsauditing.actions.PicsActionSupport;
+import com.picsauditing.service.mail.MailCronService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.when;
+import static junit.framework.Assert.*;
+
+public class MailCronTest extends PicsActionTest {
+    @Mock
+    MailCronService service;
+    private MailCron cron;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        cron = new MailCron();
+        super.setUp(cron);
+
+        cron.mailCronService = service;
+    }
+
+    @Test
+    public void testList() throws Exception {
+        when(service.getSubscriptionIdsToSendAsCommaDelimited()).thenReturn("1,2,3");
+
+        String result = cron.listAjax();
+
+        assertEquals(PicsActionSupport.PLAIN_TEXT, result);
+        assertEquals("1,2,3", cron.getOutput());
+    }
+
+    @Test
+    public void testSubscription() throws Exception {
+        String result = cron.subscription();
+
+        assertEquals(0, cron.getActionMessages().size());
+        assertEquals(PicsActionSupport.ACTION_MESSAGES, result);
+    }
+
+    @Test
+    public void testSend() throws Exception {
+        String result = cron.send();
+
+        assertEquals(1, cron.getActionMessages().size());
+        assertEquals(PicsActionSupport.ACTION_MESSAGES, result);
+    }
+
+}
