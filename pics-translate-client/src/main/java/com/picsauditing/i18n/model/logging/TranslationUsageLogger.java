a32667f800bb8ecebcab98f51f116e784d327d81
gmeurer <gmeurer@picsauditing.com>
2013-11-21 10:24:18 -0800

    PICS-13641: removed pics-translate-client maven module and updated poms to use jar from new project

--- pics-translate-client/src/main/java/com/picsauditing/i18n/model/logging/TranslationUsageLogger.java
+++
@@ -1,7 +0,0 @@
-package com.picsauditing.i18n.model.logging;
-
-import com.picsauditing.i18n.model.TranslationLookupData;
-
-public interface TranslationUsageLogger {
-    void logTranslationUsage(TranslationLookupData usage);
-}
