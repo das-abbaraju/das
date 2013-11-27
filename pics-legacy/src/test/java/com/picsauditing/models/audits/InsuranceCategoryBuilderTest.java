7bafea5b366a6725e2599a4d6d59ebbde10b2b0a
Lani Aung <uaung@picsauditing.com>
2013-11-27 15:19:17 -0800

    PICS-13738 -- Setup Insurance Category - Slug Error, clean up test

--- pics-legacy/src/test/java/com/picsauditing/models/audits/InsuranceCategoryBuilderTest.java
+++ pics-legacy/src/test/java/com/picsauditing/models/audits/InsuranceCategoryBuilderTest.java
@@ -8,7 +8,6 @@ import com.picsauditing.jpa.entities.AuditType;
 import com.picsauditing.jpa.entities.BaseTable;
 import com.picsauditing.jpa.entities.OperatorAccount;
 import com.picsauditing.search.Database;
-import com.picsauditing.util.SlugService;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -20,41 +19,39 @@ import static org.mockito.Mockito.when;
 
 public class InsuranceCategoryBuilderTest extends PicsTranslationTest {
 
-    private Permissions permissions;
-    @Mock
-    private AuditTypeDAO typeDAO;
-    @Mock
-    private Database databaseForTesting;
+	private Permissions permissions;
 	@Mock
-	private SlugService slugService;
+	private AuditTypeDAO typeDAO;
+	@Mock
+	private Database databaseForTesting;
 
-    @Before
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
 
-        when(typeDAO.save((BaseTable) any())).thenReturn(null);
-        permissions = Permissions.builder().userId(1).build();
-    }
+		when(typeDAO.save((BaseTable) any())).thenReturn(null);
+		permissions = Permissions.builder().userId(1).build();
+	}
 
-    @Test
-    public void testBuild() throws Exception {
-        AuditType insuranceType = AuditType.builder()
-                .name("General Liability")
-                .categories(AuditCategory.builder()
-                        .name("General Liability")
-                        .build())
-                .build();
-        OperatorAccount operator = OperatorAccount.builder()
-                .name("Wolfram & Hart")
-                .build();
+	@Test
+	public void testBuild() throws Exception {
+		AuditType insuranceType = AuditType.builder()
+				.name("General Liability")
+				.categories(AuditCategory.builder()
+						.name("General Liability")
+						.build())
+				.build();
+		OperatorAccount operator = OperatorAccount.builder()
+				.name("Wolfram & Hart")
+				.build();
 
-        AuditCategory insuranceCategory = InsuranceCategoryBuilder.builder().build(typeDAO, insuranceType, permissions, operator);
+		AuditCategory insuranceCategory = InsuranceCategoryBuilder.builder().build(typeDAO, insuranceType, permissions, operator);
 
-        assertEquals(2, insuranceType.getCategories().size());
-        assertEquals(operator.getName(), insuranceCategory.getName());
-        assertEquals(insuranceCategory.getQuestions().get(0).getName(),
-                "Upload a Certificate of Insurance or other supporting documentation for this policy.");
-        assertEquals(insuranceCategory.getQuestions().get(1).getName(),
-                "This insurance policy complies with all additional " + operator.getName() + " requirements.");
-    }
+		assertEquals(2, insuranceType.getCategories().size());
+		assertEquals(operator.getName(), insuranceCategory.getName());
+		assertEquals(insuranceCategory.getQuestions().get(0).getName(),
+				"Upload a Certificate of Insurance or other supporting documentation for this policy.");
+		assertEquals(insuranceCategory.getQuestions().get(1).getName(),
+				"This insurance policy complies with all additional " + operator.getName() + " requirements.");
+	}
 }
