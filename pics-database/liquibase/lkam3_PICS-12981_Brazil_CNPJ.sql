d33b5de68562ae92e7fd6ddb3a40a5a836de737e
Lucas Kam <lkam@picsauditing.com>
2013-12-11 08:57:38 -0800

    PICS-12981 Fixed the SQL patch so it works on all databases

--- pics-database/liquibase/lkam3_PICS-12981_Brazil_CNPJ.sql
+++ pics-database/liquibase/lkam3_PICS-12981_Brazil_CNPJ.sql
@@ -2,5 +2,5 @@
 
 --changeset mdo:3
 --preConditions onFail MARK_RAN
-ALTER TABLE `pics_alpha1`.`contractor_info`
-  CHANGE `europeanUnionVATnumber` `europeanUnionVATnumber` VARCHAR(30) CHARSET latin1 COLLATE latin1_swedish_ci NULL;
+ALTER TABLE contractor_info
+  CHANGE europeanUnionVATnumber europeanUnionVATnumber VARCHAR(30) CHARSET latin1 COLLATE latin1_swedish_ci NULL;
