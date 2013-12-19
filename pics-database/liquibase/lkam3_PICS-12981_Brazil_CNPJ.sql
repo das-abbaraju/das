b185708024be1751c2c252d5e07c5b9a50cdc2d9
Jared Griffith <jgriffith@picsauditing.com>
2013-12-18 16:04:40 -0800

    Fixing lucas sql comment

--- pics-database/liquibase/lkam3_PICS-12981_Brazil_CNPJ.sql
+++ pics-database/liquibase/lkam3_PICS-12981_Brazil_CNPJ.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset mdo:3
+--changeset lkam:3
 --preConditions onFail MARK_RAN
 ALTER TABLE contractor_info
   CHANGE europeanUnionVATnumber europeanUnionVATnumber VARCHAR(30) CHARSET latin1 COLLATE latin1_swedish_ci NULL;
