c141719a59d32b8745cc8f7ba7ef3fd0287b321a
Michael Do <mdo@picsauditing.com>
2013-12-19 13:15:59 -0800

    PICS-13692 added new script to cover missed items.

---
+++ pics-database/liquibase/mdo23_PICS-13692_updateReactivationAndDeclinedAccounts.sql
@@ -0,0 +1,26 @@
+--liquibase formatted sql
+
+--changeset mdo:23
+--preConditions onFail MARK_RAN
+--onUpdateSQL IGNORE
+UPDATE invoice
+JOIN invoice_item ON invoice_item.invoiceID = invoice.id
+JOIN accounts ON accounts.id = invoice.accountID
+JOIN invoice_fee ON invoice_item.feeID = invoice_fee.id
+SET invoice.updatedBy = 37951, invoice.updateDate = NOW(), invoice.invoiceType = 'Activation'
+WHERE accounts.status IN ('Declined')
+AND invoice.status != 'Void'
+AND invoice.invoiceType = 'Renewal'
+AND invoice_fee.feeClass = 'Activation'
+AND invoice.creationDate > '2011-11-27';
+
+UPDATE invoice
+JOIN invoice_item ON invoice_item.invoiceID = invoice.id
+JOIN accounts ON accounts.id = invoice.accountID
+JOIN invoice_fee ON invoice_item.feeID = invoice_fee.id
+SET invoice.updatedBy = 37951, invoice.updateDate = NOW(), invoice.invoiceType = 'Activation'
+WHERE accounts.status NOT IN ('Demo')
+AND invoice.status != 'Void'
+AND invoice.invoiceType = 'Renewal'
+AND invoice_fee.feeClass = 'Reactivation'
+AND invoice.creationDate > '2011-11-27';
