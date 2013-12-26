4503917614ba0328b74704eb14d36698e2567fb5
Joel Brownell <jbrownell@picsauditing.com>
2013-12-26 10:54:52 -0800

    PICS-14058: Code review updates.

--- pics-web/src/main/webapp/bootstrap3/js/pics/select2/select2.js
+++ pics-web/src/main/webapp/bootstrap3/js/pics/select2/select2.js
@@ -3,32 +3,32 @@
 PICS.define('select2.Select2', {
     methods: (function () {
         function init() {
-            createSelect2DefaultElement();
-            createSelect2MinElement();
+            configureSelect2();
+            configureSelect2NoSearch();
         }
 
-        function createSelect2DefaultElement() {
-            var select2Default = $('.select2');
+        function configureSelect2() {
+            var $select2Default = $('.select2');
 
-            if (select2Default.length) {
-                $('.select2').select2({
+            if ($select2Default.length) {
+                $select2Default.select2({
                     width: getSelect2Width()
                 });
 
-                select2Default.trigger('intialized.select2');
+                $select2Default.trigger('intialized.select2');
             }
         }
 
-        function createSelect2MinElement() {
-            var select2Min = $('.select2Min');
+        function configureSelect2NoSearch() {
+            var $select2Min = $('.select2Min');
 
-            if (select2Min.length) {
-                select2Min.select2({
+            if ($select2Min.length) {
+                $select2Min.select2({
                     minimumResultsForSearch: -1,
                     width: getSelect2Width()
                 });
 
-                select2Min.trigger('intialized.select2');
+                $select2Min.trigger('intialized.select2');
             }
         }
 
