16b890cffac9e96ae746901f1985d94a67a1a7dc
gmeurer <gmeurer@picsauditing.com>
2014-01-02 11:45:35 -0700

    PICS-14155: added cache to JdbcAppPropertyProvider to reduce load on app_property table

--- pics-legacy/src/main/java/com/picsauditing/dao/jdbc/JdbcAppPropertyProvider.java
+++ pics-legacy/src/main/java/com/picsauditing/dao/jdbc/JdbcAppPropertyProvider.java
@@ -5,6 +5,7 @@ import com.picsauditing.dao.mapper.GenericQueryMapper;
 import com.picsauditing.jpa.entities.AppProperty;
 import com.picsauditing.model.general.AppPropertyProvider;
 import com.picsauditing.search.Database;
+import net.sf.ehcache.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,11 +14,25 @@ import java.util.List;
 public class JdbcAppPropertyProvider implements AppPropertyProvider {
     private static final Logger logger = LoggerFactory.getLogger(JdbcAppPropertyProvider.class);
     private static final String FIND_APP_PROPERTY_BY_NAME = "SELECT * FROM app_properties WHERE property = ?";
+    private static final String cacheName = "app_properties";
+    // to inject for test
+    private CacheManager cacheManager;
 
     public JdbcAppPropertyProvider() {
     }
 
     public String findAppProperty(String appPropertyName) {
+        String property = propertyFromCache(appPropertyName);
+        if (property == null) {
+            property = findAppPropertyFromDatabase(appPropertyName);
+            if (property != null) {
+                cacheProperty(appPropertyName, property);
+            }
+        }
+        return property;
+    }
+
+    private String findAppPropertyFromDatabase(String appPropertyName) {
         try {
             List<AppProperty> appProperties
                     = Database.select(FIND_APP_PROPERTY_BY_NAME,
@@ -32,4 +47,45 @@ public class JdbcAppPropertyProvider implements AppPropertyProvider {
         }
         return null;
     }
+
+    private void cacheProperty(String appPropertyName, String propertyValue) {
+        Cache cache = cache();
+        if (cache != null) {
+            cache.put(new Element(appPropertyName, propertyValue));
+        } else {
+            logger.error("cache configuration issue - no cache named " + cacheName);
+        }
+    }
+
+    private String propertyFromCache(String appPropertyName) {
+        Cache cache = cache();
+        if (cache != null) {
+            Element element = cache.get(appPropertyName);
+            if (element != null) {
+                Object object = element.getObjectValue();
+                if (object instanceof String) {
+                    return (String) object;
+                }
+            }
+        }
+        return null;
+    }
+
+    private Cache cache() {
+        Cache cache = null;
+        CacheManager cacheManager = cacheManager();
+        if (cacheManager != null) {
+            cache = cacheManager.getCache(cacheName);
+        }
+        return cache;
+    }
+
+    private CacheManager cacheManager() {
+        if (cacheManager == null) {
+            return CacheManager.getInstance();
+        } else {
+            return cacheManager;
+        }
+    }
+
 }
