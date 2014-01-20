02768866ad31a7dfe632ca2ac938b5b799a2100b
Michael Do <mdo@picsauditing.com>
2014-01-20 14:28:46 -0800

    PICS-13435 added forced flag expiration to all flag data reports. (cherry picked from commit 89f3d94fa8d40c78a0ae91a9735bf8a827239ddc)
    Conflicts:
    
    	pics-legacy/src/test/java/com/picsauditing/report/models/AllModelsTest.testAllModels.approved.txt

--- pics-database/liquibase/mdo25_PICS-13435_updateFlagChangesSubscriptionToDRSubscription.sql
+++ pics-database/liquibase/mdo25_PICS-13435_updateFlagChangesSubscriptionToDRSubscription.sql
@@ -7,6 +7,7 @@ UPDATE IGNORE email_subscription
 SET subscription = 'DynamicReports', updatedBy = 37951, updateDate = NOW(), reportID = 1542
 WHERE subscription = 'FlagChanges';
 
-UPDATE IGNORE email_subscription
-SET timePeriod = 'None', updatedBy = 37951, updateDate = NOW()
-WHERE subscription = 'FlagChanges';
+UPDATE email_subscription es
+JOIN email_subscription e ON es.userID = e.userID AND es.subscription = 'FlagChanges' AND e.subscription = 'DynamicReports' AND e.reportID = 1542
+AND es.timePeriod != 'None'
+SET e.timePeriod = es.timePeriod, e.updatedBy = 37951, e.updateDate = NOW(), es.timePeriod = 'None', es.updatedBy = 37951, es.updateDate = NOW();
