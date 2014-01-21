b9548807c1bd072a2db0e8a13deb87bb30f7cc31
Michael Do <mdo@picsauditing.com>
2014-01-21 10:29:05 -0800

    PICS-14176

--- pics-legacy/src/main/java/com/picsauditing/actions/cron/DeactivateNonRenewalAccounts.java
+++ pics-legacy/src/main/java/com/picsauditing/actions/cron/DeactivateNonRenewalAccounts.java
@@ -7,6 +7,8 @@ import com.picsauditing.jpa.entities.ContractorAccount;
 import com.picsauditing.jpa.entities.User;
 import com.picsauditing.model.account.AccountStatusChanges;
 import com.picsauditing.util.IndexerEngine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
@@ -23,6 +25,8 @@ public class DeactivateNonRenewalAccounts implements CronTask {
     @Autowired
     AccountStatusChanges accountStatusChanges;
 
+    private static final Logger logger = LoggerFactory.getLogger(DeactivateNonRenewalAccounts.class);
+
     public String getDescription() {
         return "Deactivate non-renewing accounts when payment expires";
     }
@@ -38,10 +42,12 @@ public class DeactivateNonRenewalAccounts implements CronTask {
     }
 
     public CronTaskResult run() {
+        logger.debug("Starting Deactivation of Contractors who are not renewing");
         CronTaskResult results = new CronTaskResult(true, "");
         String where = "a.status = 'Active' AND a.renew = 0 AND paymentExpires < NOW()";
         List<ContractorAccount> conAcctList = contractorAccountDAO.findWhere(where);
         for (ContractorAccount contractor : conAcctList) {
+            logger.debug("Deactivating" + contractor.getName() + " (" + contractor.getId() + ")");
             final String reason = contractor.getAccountLevel().isBidOnly() ? AccountStatusChanges
                     .BID_ONLY_ACCOUNT_REASON : AccountStatusChanges.DEACTIVATED_NON_RENEWAL_ACCOUNT_REASON;
             billingService.syncBalance(contractor);
