7a9106e6a9e97e1d81b27c66edbb6629f82f0f35
Lani Aung <uaung@picsauditing.com>
2014-02-07 15:47:50 -0800

    TA113 -- Job Role employee counts

--- pics-legacy/src/main/java/com/picsauditing/employeeguard/viewmodel/operator/factory/OperatorSiteAssignmentModelFactory.java
+++ pics-legacy/src/main/java/com/picsauditing/employeeguard/viewmodel/operator/factory/OperatorSiteAssignmentModelFactory.java
@@ -1,15 +1,18 @@
 package com.picsauditing.employeeguard.viewmodel.operator.factory;
 
 import com.picsauditing.employeeguard.entities.Employee;
+import com.picsauditing.employeeguard.forms.operator.RoleInfo;
 import com.picsauditing.employeeguard.viewmodel.operator.SiteAssignmentModel;
 
 import java.util.List;
+import java.util.Map;
 
 public class OperatorSiteAssignmentModelFactory {
 
-	public SiteAssignmentModel create(final List<Employee> employeesAtSite) {
+	public SiteAssignmentModel create(final List<Employee> employeesAtSite, Map<RoleInfo, Integer> roleCounts) {
 		return new SiteAssignmentModel.Builder()
 				.totalEmployeesAssignedToSite(employeesAtSite.size())
+				.roleEmployeeCount(roleCounts)
 				.build();
 	}
 }
