f014bf7d98deab6ea3947d601e84871671dba5bc
Lani Aung <uaung@picsauditing.com>
2014-02-07 16:15:25 -0800

    TA111 - Adding unit tests...

--- pics-legacy/src/test/java/com/picsauditing/employeeguard/controllers/operator/SiteAssignmentActionTest.java
+++ pics-legacy/src/test/java/com/picsauditing/employeeguard/controllers/operator/SiteAssignmentActionTest.java
@@ -2,8 +2,11 @@ package com.picsauditing.employeeguard.controllers.operator;
 
 import com.picsauditing.PicsActionTest;
 import com.picsauditing.employeeguard.entities.Employee;
+import com.picsauditing.employeeguard.entities.Role;
+import com.picsauditing.employeeguard.entities.RoleEmployee;
 import com.picsauditing.employeeguard.services.AccountService;
 import com.picsauditing.employeeguard.services.EmployeeService;
+import com.picsauditing.employeeguard.services.RoleService;
 import com.picsauditing.employeeguard.services.models.AccountModel;
 import com.picsauditing.employeeguard.viewmodel.operator.SiteAssignmentModel;
 import org.junit.Before;
@@ -12,21 +15,20 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.powermock.reflect.Whitebox;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyListOf;
 import static org.mockito.Matchers.anySetOf;
 import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class SiteAssignmentActionTest extends PicsActionTest {
 
 	public static final int CONTRACTOR_ID_1 = 1;
 	public static final int CONTRACTOR_ID_2 = 2;
 	public static final int SITE_ID = 1234;
+	public static final int CORPORATE_ID = 10000;
 
 	private SiteAssignmentAction siteAssignmentAction;
 
@@ -34,6 +36,8 @@ public class SiteAssignmentActionTest extends PicsActionTest {
 	private AccountService accountService;
 	@Mock
 	private EmployeeService employeeService;
+	@Mock
+	private RoleService roleService;
 
 	@Before
 	public void setUp() throws Exception {
@@ -46,10 +50,20 @@ public class SiteAssignmentActionTest extends PicsActionTest {
 		Whitebox.setInternalState(siteAssignmentAction, "accountService", accountService);
 		Whitebox.setInternalState(siteAssignmentAction, "employeeService", employeeService);
 		Whitebox.setInternalState(siteAssignmentAction, "permissions", permissions);
+		Whitebox.setInternalState(siteAssignmentAction, "roleService", roleService);
 	}
 
 	@Test
-	public void testStatus_ClientSite() throws Exception {
+	public void testStatus_ClientSite_TotalCountOfEmployees() throws Exception {
+		setupMocksForTotalCount();
+
+		assertEquals("status", siteAssignmentAction.status());
+
+		SiteAssignmentModel siteAssignmentModel = siteAssignmentAction.getSiteAssignmentModel();
+		performAssertionsOnTotalCount(siteAssignmentModel);
+	}
+
+	private void setupMocksForTotalCount() {
 		List<AccountModel> contractors = Arrays.asList(
 				new AccountModel.Builder().id(CONTRACTOR_ID_1).name("Contractor 1").build(),
 				new AccountModel.Builder().id(CONTRACTOR_ID_2).name("Contractor 2").build()
@@ -57,20 +71,78 @@ public class SiteAssignmentActionTest extends PicsActionTest {
 
 		List<Employee> employees = Arrays.asList(new Employee(), new Employee(), new Employee());
 
+		setupPermissions();
 		when(accountService.getContractors(SITE_ID)).thenReturn(contractors);
 		when(employeeService.getEmployeesAssignedToSite(anySetOf(Integer.class), eq(SITE_ID))).thenReturn(employees);
-		when(permissions.getAccountId()).thenReturn(SITE_ID);
-		when(permissions.isOperator()).thenReturn(true);
+		when(roleService.getRolesForAccounts(anyListOf(Integer.class))).thenReturn(Collections.<Role>emptyList());
+		when(roleService.getSiteToCorporateRoles(SITE_ID)).thenReturn(Collections.<Role, Role>emptyMap());
+	}
 
-		assertEquals("status", siteAssignmentAction.status());
-
-		SiteAssignmentModel siteAssignmentModel = siteAssignmentAction.getSiteAssignmentModel();
+	private void performAssertionsOnTotalCount(SiteAssignmentModel siteAssignmentModel) {
 		assertNotNull(siteAssignmentModel);
 		assertEquals(3, siteAssignmentModel.getTotalEmployeesAssignedToSite());
 
 		verify(accountService).getAccountById(SITE_ID);
 		verify(accountService).getContractors(SITE_ID);
 		verify(employeeService).getEmployeesAssignedToSite(anySetOf(Integer.class), eq(SITE_ID));
+	}
+
+	private void setupPermissions() {
+		when(permissions.getAccountId()).thenReturn(SITE_ID);
+		when(permissions.isOperator()).thenReturn(true);
+	}
+
+	@Test
+	public void testStatus_ClientSite_RoleInfo() throws Exception {
+		List<Integer> corporateIds = setupMocksForClientSiteRoleInfoAndReturnCorporateIds();
+
+		assertEquals("status", siteAssignmentAction.status());
+
+		SiteAssignmentModel siteAssignmentModel = siteAssignmentAction.getSiteAssignmentModel();
+
+		performAssertionsOnRoleInfo(corporateIds, siteAssignmentModel);
+	}
+
+	private void performAssertionsOnRoleInfo(List<Integer> corporateIds, SiteAssignmentModel siteAssignmentModel) {
+		assertNotNull(siteAssignmentModel);
+		assertEquals(2, siteAssignmentModel.getTotalEmployeesAssignedToSite());
+		assertEquals(2, siteAssignmentModel.getRoleEmployee().size());
+		assertTrue(siteAssignmentModel.getRoleEmployee().values().contains(1));
+
+		verify(roleService).getRolesForAccounts(corporateIds);
+		verify(roleService).getSiteToCorporateRoles(SITE_ID);
+	}
+
+	private List<Integer> setupMocksForClientSiteRoleInfoAndReturnCorporateIds() {
+		Role siteRole1 = mock(Role.class);
+		Role siteRole2 = mock(Role.class);
+		Role corporateRole1 = mock(Role.class);
+		Role corporateRole2 = mock(Role.class);
+
+		RoleEmployee roleEmployee = mock(RoleEmployee.class);
+		Employee employee = mock(Employee.class);
+
+		List<Integer> corporateIds = Arrays.asList(CORPORATE_ID);
+		List<Role> roles = Arrays.asList(corporateRole1, corporateRole2);
+
+		Map<Role, Role> siteToCorporateRoles = new HashMap<>();
+		siteToCorporateRoles.put(siteRole1, corporateRole1);
+		siteToCorporateRoles.put(siteRole2, corporateRole2);
+
+		setupPermissions();
+		when(accountService.getContractors(SITE_ID)).thenReturn(Collections.<AccountModel>emptyList());
+		when(accountService.getTopmostCorporateAccountIds(SITE_ID)).thenReturn(corporateIds);
+		when(corporateRole1.getName()).thenReturn("Corporate Role 1");
+		when(corporateRole2.getName()).thenReturn("Corporate Role 2");
+		when(employeeService.getEmployeesAssignedToSite(anySetOf(Integer.class), eq(SITE_ID)))
+				.thenReturn(Arrays.asList(employee, mock(Employee.class)));
+		when(employee.getRoles()).thenReturn(Arrays.asList(roleEmployee));
+		when(roleEmployee.getRole()).thenReturn(siteRole1);
+		when(roleService.getRolesForAccounts(anyListOf(Integer.class))).thenReturn(roles);
+		when(roleService.getSiteToCorporateRoles(SITE_ID)).thenReturn(siteToCorporateRoles);
+		when(siteRole1.getName()).thenReturn("Site Role 1");
+		when(siteRole2.getName()).thenReturn("Site Role 2");
+		return corporateIds;
 	}
 
 	@Test
