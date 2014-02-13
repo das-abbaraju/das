5a9c6495c9094dcb658c45e6379cd21e0f15fb61
Lani Aung <uaung@picsauditing.com>
2014-02-13 14:28:24 -0800

    US302: TA412

--- pics-legacy/src/main/java/com/picsauditing/employeeguard/controllers/operator/EmployeeAction.java
+++ pics-legacy/src/main/java/com/picsauditing/employeeguard/controllers/operator/EmployeeAction.java
@@ -1,20 +1,54 @@
 package com.picsauditing.employeeguard.controllers.operator;
 
 import com.picsauditing.controller.PicsRestActionSupport;
+import com.picsauditing.employeeguard.entities.AccountSkill;
+import com.picsauditing.employeeguard.entities.Employee;
+import com.picsauditing.employeeguard.entities.Role;
+import com.picsauditing.employeeguard.services.EmployeeService;
+import com.picsauditing.employeeguard.services.RoleService;
+import com.picsauditing.employeeguard.services.SkillService;
+import com.picsauditing.employeeguard.services.StatusCalculatorService;
+import com.picsauditing.employeeguard.viewmodel.employee.EmployeeProjectAndRoleStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Map;
 
 public class EmployeeAction extends PicsRestActionSupport {
 
-	private int siteId;
+	@Autowired
+	private EmployeeService employeeService;
+	@Autowired
+	private RoleService roleService;
+	@Autowired
+	private StatusCalculatorService statusCalculatorService;
+	@Autowired
+	private SkillService skillService;
+
+	private int contractorId;
+
+	private EmployeeProjectAndRoleStatus employeeProjectAndRoleStatus;
 
 	public String show() {
+		Employee employee = employeeService.findEmployee(id, contractorId);
+		Map<Role, Role> siteToCorporateRoles = roleService.getSiteToCorporateRoles(permissions.getAccountId());
+		List<AccountSkill> siteRequiredSkills = skillService.getRequiredSkillsForSiteAndCorporates(permissions.getAccountId());
+
+		employeeProjectAndRoleStatus = statusCalculatorService.getEmployeeStatusesForProjectsAndJobRoles(
+				employee, siteToCorporateRoles, siteRequiredSkills);
+
 		return SHOW;
 	}
 
-	public int getSiteId() {
-		return siteId;
+	public int getContractorId() {
+		return contractorId;
 	}
 
-	public void setSiteId(int siteId) {
-		this.siteId = siteId;
+	public void setContractorId(int contractorId) {
+		this.contractorId = contractorId;
+	}
+
+	public EmployeeProjectAndRoleStatus getEmployeeProjectAndRoleStatus() {
+		return employeeProjectAndRoleStatus;
 	}
 }
---
+++ pics-legacy/src/main/java/com/picsauditing/employeeguard/forms/factory/EmployeeModelFactory.java
@@ -0,0 +1,11 @@
+package com.picsauditing.employeeguard.forms.factory;
+
+import com.picsauditing.employeeguard.entities.Employee;
+import com.picsauditing.employeeguard.viewmodel.employee.EmployeeModel;
+
+public class EmployeeModelFactory {
+
+	public EmployeeModel create(final Employee employee) {
+		return null;
+	}
+}
