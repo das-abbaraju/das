7bdc41d04ce4b6f13c4edeaffff6ca440a3b7766
Alex Ananighian <aananighian@picsauditing.com>
2014-02-12 17:26:45 -0800

    US301: Added route for Front-End to begin work on JSP

---
+++ pics-legacy/src/main/java/com/picsauditing/employeeguard/controllers/operator/EmployeeAction.java
@@ -0,0 +1,11 @@
+package com.picsauditing.employeeguard.controllers.operator;
+
+import com.picsauditing.controller.PicsRestActionSupport;
+
+public class EmployeeAction extends PicsRestActionSupport {
+
+	public String show() {
+		return SHOW;
+	}
+
+}
