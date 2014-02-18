577cc45a95a62852b009c19c3ea5e03229ecc030
Alex Ananighian <aananighian@picsauditing.com>
2014-02-18 11:19:35 -0800

    US301: Updated the Controller and struts routing file to include siteId

--- pics-legacy/src/main/java/com/picsauditing/employeeguard/controllers/operator/EmployeeAction.java
+++ pics-legacy/src/main/java/com/picsauditing/employeeguard/controllers/operator/EmployeeAction.java
@@ -4,8 +4,17 @@ import com.picsauditing.controller.PicsRestActionSupport;
 
 public class EmployeeAction extends PicsRestActionSupport {
 
+	private int siteId;
+
 	public String show() {
 		return SHOW;
 	}
 
+	public int getSiteId() {
+		return siteId;
+	}
+
+	public void setSiteId(int siteId) {
+		this.siteId = siteId;
+	}
 }
