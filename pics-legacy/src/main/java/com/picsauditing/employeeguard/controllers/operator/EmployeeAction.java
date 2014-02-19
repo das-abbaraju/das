02b20309379a200e9cf1613b76bef7951730f647
Lani Aung <uaung@picsauditing.com>
2014-02-18 16:06:20 -0800

    US302: TA412

--- pics-legacy/src/main/java/com/picsauditing/employeeguard/controllers/operator/EmployeeAction.java
+++ pics-legacy/src/main/java/com/picsauditing/employeeguard/controllers/operator/EmployeeAction.java
@@ -1,17 +1,14 @@
 package com.picsauditing.employeeguard.controllers.operator;
 
 import com.picsauditing.controller.PicsRestActionSupport;
-import com.picsauditing.employeeguard.entities.AccountSkill;
 import com.picsauditing.employeeguard.entities.Employee;
-import com.picsauditing.employeeguard.entities.Role;
-import com.picsauditing.employeeguard.services.*;
+import com.picsauditing.employeeguard.services.AccountService;
+import com.picsauditing.employeeguard.services.EmployeeService;
 import com.picsauditing.employeeguard.services.models.AccountModel;
 import com.picsauditing.employeeguard.viewmodel.employee.EmployeeModel;
-import com.picsauditing.employeeguard.viewmodel.employee.EmployeeProjectAndRoleStatus;
 import com.picsauditing.employeeguard.viewmodel.factory.ViewModelFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.List;
 import java.util.Map;
 
 public class EmployeeAction extends PicsRestActionSupport {
@@ -20,25 +17,13 @@ public class EmployeeAction extends PicsRestActionSupport {
 	private AccountService accountService;
 	@Autowired
 	private EmployeeService employeeService;
-	@Autowired
-	private RoleService roleService;
-	@Autowired
-	private StatusCalculatorService statusCalculatorService;
-	@Autowired
-	private SkillService skillService;
 
 	private int contractorId;
 
 	private EmployeeModel employee;
-	private EmployeeProjectAndRoleStatus employeeProjectAndRoleStatus;
 
 	public String show() {
 		Employee employeeEntity = employeeService.findEmployee(id, contractorId);
-		Map<Role, Role> siteToCorporateRoles = roleService.getSiteToCorporateRoles(permissions.getAccountId());
-		List<AccountSkill> siteRequiredSkills = skillService.getRequiredSkillsForSiteAndCorporates(permissions.getAccountId());
-
-		employeeProjectAndRoleStatus = statusCalculatorService.getEmployeeStatusesForProjectsAndJobRoles(
-				employeeEntity, siteToCorporateRoles, siteRequiredSkills);
 
 		Map<Integer, AccountModel> contractors = accountService.getContractorsForEmployee(employeeEntity);
 		employee = ViewModelFactory.getEmployeeModelFactory().create(employeeEntity, contractors);
@@ -56,9 +41,5 @@ public class EmployeeAction extends PicsRestActionSupport {
 
 	public EmployeeModel getEmployee() {
 		return employee;
-	}
-
-	public EmployeeProjectAndRoleStatus getEmployeeProjectAndRoleStatus() {
-		return employeeProjectAndRoleStatus;
 	}
 }
