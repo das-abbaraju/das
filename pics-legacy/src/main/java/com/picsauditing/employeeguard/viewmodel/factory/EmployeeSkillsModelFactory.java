00784c504ac6153e411fd2e4e9c30ea69f5c01bf
Lani Aung <uaung@picsauditing.com>
2014-02-20 11:58:14 -0800

    US308: TA423, TA536

--- pics-legacy/src/main/java/com/picsauditing/employeeguard/viewmodel/factory/EmployeeSkillsModelFactory.java
+++ pics-legacy/src/main/java/com/picsauditing/employeeguard/viewmodel/factory/EmployeeSkillsModelFactory.java
@@ -1,18 +1,21 @@
 package com.picsauditing.employeeguard.viewmodel.factory;
 
+import com.picsauditing.employeeguard.entities.AccountSkill;
 import com.picsauditing.employeeguard.entities.Employee;
-import com.picsauditing.employeeguard.entities.ProjectSkill;
+import com.picsauditing.employeeguard.entities.ProjectRole;
 import com.picsauditing.employeeguard.entities.Role;
 import com.picsauditing.employeeguard.viewmodel.employee.EmployeeSkillsModel;
 
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class EmployeeSkillsModelFactory {
 
 	public EmployeeSkillsModel create(final Employee employee,
+	                                  final List<ProjectRole> siteProjectRoles,
+	                                  final List<Role> siteRoles,
 	                                  final Map<Role, Role> siteToCorporateRoles,
-	                                  final Set<ProjectSkill> projectSkills) {
+	                                  final List<AccountSkill> siteSkills) {
 		return null;
 	}
 }
--- pics-legacy/src/test/java/com/picsauditing/employeeguard/viewmodel/factory/EmployeeSkillsModelFactoryTest.java
+++ pics-legacy/src/test/java/com/picsauditing/employeeguard/viewmodel/factory/EmployeeSkillsModelFactoryTest.java
@@ -1,23 +1,44 @@
 package com.picsauditing.employeeguard.viewmodel.factory;
 
+import com.picsauditing.PICS.DateBean;
 import com.picsauditing.employeeguard.entities.*;
+import com.picsauditing.employeeguard.viewmodel.employee.EmployeeSkillsModel;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.when;
 
 public class EmployeeSkillsModelFactoryTest {
 	private EmployeeSkillsModelFactory factory;
 
 	@Mock
+	private AccountSkill skillForCorporateRequirement;
+	@Mock
 	private AccountSkill skillForCorporateRole;
 	@Mock
 	private AccountSkill skillForProjectRole;
+	@Mock
+	private AccountSkill skillForProjectSkill;
+	@Mock
+	private AccountSkill skillForSiteRequirement;
+	@Mock
+	private AccountSkillEmployee employeeSkillForCorporateRequirement;
+	@Mock
+	private AccountSkillEmployee employeeSkillForCorporateRole;
+	@Mock
+	private AccountSkillEmployee employeeSkillForProjectRole;
+	@Mock
+	private AccountSkillEmployee employeeSkillForProjectSkill;
+	@Mock
+	private AccountSkillEmployee employeeSkillForSiteRequirement;
 	@Mock
 	private AccountSkillRole roleSkillForCorporateRole;
 	@Mock
@@ -38,6 +59,8 @@ public class EmployeeSkillsModelFactoryTest {
 	private ProjectRole projectRole;
 	@Mock
 	private ProjectRoleEmployee projectRoleEmployee;
+	@Mock
+	private ProjectSkill projectSkill;
 
 	@Before
 	public void setUp() throws Exception {
@@ -48,14 +71,73 @@ public class EmployeeSkillsModelFactoryTest {
 
 	@Test
 	public void testCreate() throws Exception {
-		when(roleForCorporate.getSkills()).thenReturn(Arrays.asList(roleSkillForCorporateRole));
+		setupMocks();
+
+		Map<Role, Role> siteToCorporateRoles = new HashMap<>();
+		siteToCorporateRoles.put(roleForSite, roleForCorporate);
+
+		EmployeeSkillsModel model = factory.create(employee, Arrays.asList(projectRole), Arrays.asList(roleForSite),
+				siteToCorporateRoles, Arrays.asList(skillForCorporateRequirement, skillForSiteRequirement));
+
+		performAssertions(model);
+	}
+
+	private void setupMocks() {
 		when(employee.getRoles()).thenReturn(Arrays.asList(roleEmployee));
 		when(employee.getProjectRoles()).thenReturn(Arrays.asList(projectRoleEmployee));
+		when(project.getSkills()).thenReturn(Arrays.asList(projectSkill));
 		when(projectRole.getProject()).thenReturn(project);
 		when(projectRole.getRole()).thenReturn(roleForProjectRole);
 		when(projectRoleEmployee.getProjectRole()).thenReturn(projectRole);
+		when(projectSkill.getSkill()).thenReturn(skillForProjectSkill);
 		when(roleEmployee.getRole()).thenReturn(roleForSite);
+		when(roleForCorporate.getSkills()).thenReturn(Arrays.asList(roleSkillForCorporateRole));
 		when(roleSkillForCorporateRole.getSkill()).thenReturn(skillForCorporateRole);
+
+		setupEmployeeSkillMocks();
+		setupMockNames();
+	}
+
+	private void setupEmployeeSkillMocks() {
+		when(employee.getSkills()).thenReturn(Arrays.asList(employeeSkillForCorporateRequirement,
+				employeeSkillForCorporateRole, employeeSkillForProjectRole, employeeSkillForProjectSkill,
+				employeeSkillForSiteRequirement));
+
+		Date expired = DateBean.today();
+		Date completed = DateBean.addYears(expired, 1);
+		Date expiring = DateBean.addDays(expired, 15);
+
+		when(employeeSkillForCorporateRequirement.getSkill()).thenReturn(skillForCorporateRequirement);
+		when(employeeSkillForCorporateRequirement.getEndDate()).thenReturn(completed);
+
+		when(employeeSkillForCorporateRole.getSkill()).thenReturn(skillForCorporateRole);
+		when(employeeSkillForCorporateRequirement.getEndDate()).thenReturn(expiring);
+
+		when(employeeSkillForProjectRole.getSkill()).thenReturn(skillForProjectRole);
+		when(employeeSkillForProjectRole.getEndDate()).thenReturn(expired);
+
+		when(employeeSkillForProjectSkill.getSkill()).thenReturn(skillForProjectSkill);
+		when(employeeSkillForProjectSkill.getEndDate()).thenReturn(completed);
+
+		when(employeeSkillForSiteRequirement.getSkill()).thenReturn(skillForSiteRequirement);
+		when(employeeSkillForSiteRequirement.getEndDate()).thenReturn(expiring);
+	}
+
+	private void setupMockNames() {
+		when(roleForCorporate.getName()).thenReturn("Corporate Role");
+		when(roleForProjectRole.getName()).thenReturn("Project Role");
+
+		when(skillForCorporateRequirement.getName()).thenReturn("Corporate Required Skill");
+		when(skillForCorporateRole.getName()).thenReturn("Corporate Role Skill");
+		when(skillForProjectRole.getName()).thenReturn("Project Role Skill");
+		when(skillForProjectSkill.getName()).thenReturn("Project Skill");
+		when(skillForSiteRequirement.getName()).thenReturn("Site Required Skill");
+	}
+
+	private void performAssertions(EmployeeSkillsModel model) {
+		assertNotNull(model);
+		assertNotNull(model.getEmployeeSkills());
+		assertFalse(model.getEmployeeSkills().isEmpty());
 
 		fail("Not implemented");
 	}
