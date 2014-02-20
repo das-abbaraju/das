d6de09ddbf9cf49ff8de9bae006cd3d6762746cd
Michael Do <mdo@picsauditing.com>
2014-02-20 09:45:59 -0800

    PICS-13646 updated to non-trade SS and none trade risk

--- pics-database/liquibase/mdo46_PICS-13646_tradeSS_DDL.sql
+++ pics-database/liquibase/mdo46_PICS-13646_tradeSS_DDL.sql
@@ -7,4 +7,4 @@ DROP VIEW IF EXISTS vwaccounttradesafetysensitivity;
 
 ALTER TABLE contractor_info
   ADD COLUMN tradeSafetySensitive TINYINT(4) DEFAULT 0  NOT NULL AFTER insideSalesPriority,
-  ADD COLUMN tradeSafetyRisk TINYINT(3) DEFAULT 2  NULL AFTER tradeSafetySensitive;
+  ADD COLUMN tradeSafetyRisk TINYINT(3) DEFAULT 0  NULL AFTER tradeSafetySensitive;
