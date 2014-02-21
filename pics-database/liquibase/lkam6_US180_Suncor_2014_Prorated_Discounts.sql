77976c4adbda94e22a79908392cfcea0d2dc9214
Jared Griffith <jgriffith@picsauditing.com>
2014-02-20 16:49:39 -0800

    US180 Fixing patch file and adding to changelog.xml

--- pics-database/liquibase/lkam6_US180_Suncor_2014_Prorated_Discounts.sql
+++ pics-database/liquibase/lkam6_US180_Suncor_2014_Prorated_Discounts.sql
@@ -3,4 +3,4 @@
 --changeset lkam:6
 --preConditions onFail MARK_RAN
 
-ALTER TABLE pics_alpha1.contractor_info ADD COLUMN logoForSingleOperatorContractor INT(11) DEFAULT NULL NULL AFTER autoAddClientSite;
+ALTER TABLE contractor_info ADD COLUMN logoForSingleOperatorContractor INT(11) DEFAULT NULL NULL AFTER autoAddClientSite;
