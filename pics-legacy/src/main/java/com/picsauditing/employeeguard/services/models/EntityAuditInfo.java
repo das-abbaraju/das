f2b8c41ea373cfabd2aba882dfd8065b14f8d9be
Alex Ananighian <aananighian@picsauditing.com>
2014-02-26 13:49:43 -0800

    US451: Updated the JUnit tests and also experimented with a model that contains all the userId and timestamp for entity audit tracking

---
+++ pics-legacy/src/main/java/com/picsauditing/employeeguard/services/models/EntityAuditInfo.java
@@ -0,0 +1,4 @@
+package com.picsauditing.employeeguard.services.models;
+
+public class EntityAuditInfo {
+}
---
+++ pics-legacy/src/test/java/com/picsauditing/employeeguard/services/entity/RoleServiceTest.java
@@ -0,0 +1,41 @@
+package com.picsauditing.employeeguard.services.entity;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RoleServiceTest {
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	@Test
+	public void testFind() throws Exception {
+
+	}
+
+	@Test
+	public void testSearch() throws Exception {
+
+	}
+
+	@Test
+	public void testSave() throws Exception {
+
+	}
+
+	@Test
+	public void testUpdate() throws Exception {
+
+	}
+
+	@Test
+	public void testDelete() throws Exception {
+
+	}
+
+	@Test
+	public void testDeleteById() throws Exception {
+
+	}
+}
