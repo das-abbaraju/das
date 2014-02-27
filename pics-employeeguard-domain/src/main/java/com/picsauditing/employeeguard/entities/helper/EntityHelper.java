fd65f83feba11217c1f17bbd79267f7a7806886f
Lani Aung <uaung@picsauditing.com>
2014-02-27 15:12:13 -0800

    US451: Filling out service tests, updated skill action to use new skill service

--- pics-employeeguard-domain/src/main/java/com/picsauditing/employeeguard/entities/helper/EntityHelper.java
+++ pics-employeeguard-domain/src/main/java/com/picsauditing/employeeguard/entities/helper/EntityHelper.java
@@ -1,8 +1,6 @@
 package com.picsauditing.employeeguard.entities.helper;
 
 import com.picsauditing.employeeguard.entities.BaseEntity;
-import com.picsauditing.employeeguard.entities.Employee;
-import com.picsauditing.employeeguard.entities.ProfileDocument;
 import com.picsauditing.employeeguard.models.EntityAuditInfo;
 import org.apache.commons.collections.CollectionUtils;
 
@@ -10,90 +8,90 @@ import java.util.*;
 
 public class EntityHelper {
 
-    public static <E extends BaseEntity> E setCreateAuditFields(final E entity,
-																final int createdBy,
-																final Date createdDate) {
-        entity.setCreatedBy(createdBy);
-        entity.setCreatedDate(createdDate);
+	public static <E extends BaseEntity> E setCreateAuditFields(final E entity,
+	                                                            final int createdBy,
+	                                                            final Date createdDate) {
+		entity.setCreatedBy(createdBy);
+		entity.setCreatedDate(createdDate);
 		return entity;
-    }
+	}
 
-    public static <E extends BaseEntity> Collection<E> setCreateAuditFields(final Collection<E> entities,
-																			final int createdBy,
-																			final Date createdDate) {
-        if (CollectionUtils.isEmpty(entities)) {
-            return Collections.emptyList();
-        }
+	public static <E extends BaseEntity> Collection<E> setCreateAuditFields(final Collection<E> entities,
+	                                                                        final int createdBy,
+	                                                                        final Date createdDate) {
+		if (CollectionUtils.isEmpty(entities)) {
+			return Collections.emptyList();
+		}
 
-        for (BaseEntity entity : entities) {
-            setCreateAuditFields(entity, createdBy, createdDate);
-        }
+		for (BaseEntity entity : entities) {
+			setCreateAuditFields(entity, createdBy, createdDate);
+		}
 
 		return entities;
-    }
+	}
 
-    public static <E extends BaseEntity> E setUpdateAuditFields(final E entity,
-																   final int createdBy,
-																   final Date createdDate) {
-        entity.setUpdatedBy(createdBy);
-        entity.setUpdatedDate(createdDate);
+	public static <E extends BaseEntity> E setUpdateAuditFields(final E entity,
+	                                                            final int createdBy,
+	                                                            final Date createdDate) {
+		entity.setUpdatedBy(createdBy);
+		entity.setUpdatedDate(createdDate);
 		return entity;
-    }
+	}
 
-    public static <E extends BaseEntity> Collection<E> setUpdateAuditFields(final Collection<E> entities,
-																   final int createdBy,
-																   final Date createdDate) {
-        if (CollectionUtils.isEmpty(entities)) {
-            return Collections.emptyList();
-        }
+	public static <E extends BaseEntity> Collection<E> setUpdateAuditFields(final Collection<E> entities,
+	                                                                        final int createdBy,
+	                                                                        final Date createdDate) {
+		if (CollectionUtils.isEmpty(entities)) {
+			return Collections.emptyList();
+		}
 
-        for (BaseEntity entity : entities) {
-            setUpdateAuditFields(entity, createdBy, createdDate);
-        }
+		for (BaseEntity entity : entities) {
+			setUpdateAuditFields(entity, createdBy, createdDate);
+		}
 
 		return entities;
-    }
+	}
 
-    public static <E extends BaseEntity> E softDelete(final E entity, final int deletedBy) {
-        entity.setDeletedBy(deletedBy);
+	public static <E extends BaseEntity> E softDelete(final E entity, final int deletedBy) {
+		entity.setDeletedBy(deletedBy);
 		return entity;
-    }
+	}
 
-    public static <E extends BaseEntity> void softDelete(final Collection<E> entities, final int deletedBy) {
-        if (CollectionUtils.isEmpty(entities)) {
-            return;
-        }
+	public static <E extends BaseEntity> void softDelete(final Collection<E> entities, final int deletedBy) {
+		if (CollectionUtils.isEmpty(entities)) {
+			return;
+		}
 
-        for (BaseEntity entity : entities) {
-            softDelete(entity, deletedBy);
-        }
-    }
+		for (BaseEntity entity : entities) {
+			softDelete(entity, deletedBy);
+		}
+	}
 
-    public static <E extends BaseEntity> List<Integer> getIdsForEntities(final List<E> entities) {
-        if (CollectionUtils.isEmpty(entities)) {
-            return Collections.emptyList();
-        }
+	public static <E extends BaseEntity> List<Integer> getIdsForEntities(final List<E> entities) {
+		if (CollectionUtils.isEmpty(entities)) {
+			return Collections.emptyList();
+		}
 
-        List<Integer> ids = new ArrayList<>();
-        for (E entity : entities) {
-            ids.add(entity.getId());
-        }
+		List<Integer> ids = new ArrayList<>();
+		for (E entity : entities) {
+			ids.add(entity.getId());
+		}
 
-        return ids;
-    }
+		return ids;
+	}
 
-    public static <E extends BaseEntity> Set<Integer> getUniqueIdsForEntities(final List<E> entities) {
-        if (CollectionUtils.isEmpty(entities)) {
-            return Collections.emptySet();
-        }
+	public static <E extends BaseEntity> Set<Integer> getUniqueIdsForEntities(final List<E> entities) {
+		if (CollectionUtils.isEmpty(entities)) {
+			return Collections.emptySet();
+		}
 
-        Set<Integer> ids = new HashSet<>();
-        for (E entity : entities) {
-            ids.add(entity.getId());
-        }
+		Set<Integer> ids = new HashSet<>();
+		for (E entity : entities) {
+			ids.add(entity.getId());
+		}
 
-        return ids;
-    }
+		return ids;
+	}
 
 	public static <E extends BaseEntity> E setCreateAuditFields(final E entity, final EntityAuditInfo entityAuditInfo) {
 		entity.setCreatedBy(entityAuditInfo.getAppUserId());
@@ -105,5 +103,29 @@ public class EntityHelper {
 		entity.setUpdatedBy(entityAuditInfo.getAppUserId());
 		entity.setUpdatedDate(entityAuditInfo.getTimestamp());
 		return entity;
+	}
+
+	public static <E extends BaseEntity> List<E> setCreateAuditFields(final List<E> entities, final EntityAuditInfo entityAuditInfo) {
+		if (CollectionUtils.isEmpty(entities)) {
+			return Collections.emptyList();
+		}
+
+		for (E entity : entities) {
+			setCreateAuditFields(entity, entityAuditInfo);
+		}
+
+		return entities;
+	}
+
+	public static <E extends BaseEntity> List<E> setUpdateAuditFields(final List<E> entities, final EntityAuditInfo entityAuditInfo) {
+		if (CollectionUtils.isEmpty(entities)) {
+			return Collections.emptyList();
+		}
+
+		for (E entity : entities) {
+			setUpdateAuditFields(entities, entityAuditInfo);
+		}
+
+		return entities;
 	}
 }
