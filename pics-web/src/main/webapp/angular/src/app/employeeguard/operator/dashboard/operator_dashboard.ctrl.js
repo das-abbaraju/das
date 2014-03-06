c41b2054b3b3ba755645cffb24b3af702f5aa30e
jbrownell <jbrownell@picsauditing.com>
2014-03-05 16:45:02 -0800

    TA558: Added fancy arc chart for site status. Conflicts: 	pics-web/src/main/webapp/angular/build/script.js 	pics-web/src/main/webapp/angular/src/app/employeeguard/operator/dashboard/_site-status.tpl.html

--- pics-web/src/main/webapp/angular/src/app/employeeguard/operator/dashboard/operator_dashboard.ctrl.js
+++ pics-web/src/main/webapp/angular/src/app/employeeguard/operator/dashboard/operator_dashboard.ctrl.js
@@ -7,6 +7,7 @@ angular.module('PICS.employeeguard')
 
     function onSuccess(result) {
         $scope.site = result;
+        $scope.loadAssignmentChart();
     }
 
     function onError(error) {
@@ -16,7 +17,6 @@ angular.module('PICS.employeeguard')
     $scope.calculateStatusPercentage = function (amount, total) {
         return (amount / total) * 100;
     };
-
 
     $scope.getProjectStatus = function (project) {
         var progress_bar = {},
@@ -38,5 +38,61 @@ angular.module('PICS.employeeguard')
         };
 
         return progress_bar;
+    };
+
+    $scope.redrawChart = function () {
+        d3.select('.assignment-chart').html('');
+        $scope.loadAssignmentChart();
+    };
+
+    $scope.loadAssignmentChart = function () {
+        var height = 200,
+            width = 420,
+            arcStartAngle = -0.5 * Math.PI,
+            arcEndAngle = 0.5 * Math.PI;
+
+        var data = [{
+                        amount: $scope.site.completed + $scope.site.pending,
+                        color: '#33b679'
+                    },{
+                        amount: $scope.site.expiring,
+                        color: '#f4b400'
+                    },{
+                        amount: $scope.site.expired,
+                        color: '#e74c3c'
+                    }];
+
+        var createArc = d3.svg.arc()
+            .outerRadius(height)
+            .innerRadius(height - 80);
+
+        var pie = d3.layout.pie()
+            .sort(null)
+            .startAngle(arcStartAngle)
+            .endAngle(arcEndAngle)
+            .value(function(d) { return d.amount; });
+
+
+        var chart = d3.select('.assignment-chart')
+                        .append('svg')
+                        .attr('height', height)
+                        .attr('width', width)
+                        .append('g')
+                        .attr('transform', 'translate(' + width/2 + ',' + height + ')');
+
+        chart.selectAll('.arc')
+            .data(pie(data))
+            .enter()
+            .append('g')
+            .append('path')
+            .attr('fill', function(d, i) { return d.data.color; })
+            .transition()
+            .duration(1500)
+            .attrTween('d', animatePie);
+
+        function animatePie(d) {
+            var i = d3.interpolate({startAngle: arcStartAngle, endAngle: arcStartAngle}, d);
+            return function(time) { return createArc(i(time)); };
+        }
     };
 });
