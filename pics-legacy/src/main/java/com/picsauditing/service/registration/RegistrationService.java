fa0544e73737c26e5b0d2967b38415b53290f1ed
Bryan Latner <blatner@picsauditing.com>
2014-03-07 09:36:37 -0800

    Remove duplicative registration validator.

--- pics-legacy/src/main/java/com/picsauditing/service/registration/RegistrationService.java
+++ pics-legacy/src/main/java/com/picsauditing/service/registration/RegistrationService.java
@@ -1,19 +1,10 @@
 package com.picsauditing.service.registration;
 
-import com.picsauditing.service.registration.validator.RegistrationValidator;
-
 public class RegistrationService {
 
-    private final RegistrationValidator validator;
-
-    public RegistrationService(
-            RegistrationValidator rv
-    ) {
-        this.validator = rv;
-    }
 
     public RegistrationSubmission newRegistration() {
-        return new RegistrationSubmission(validator, this);
+        return new RegistrationSubmission(this);
     }
 
     void doRegistration(RegistrationSubmission registrationSubmission) {
--- pics-legacy/src/main/java/com/picsauditing/service/registration/RegistrationSubmission.java
+++ pics-legacy/src/main/java/com/picsauditing/service/registration/RegistrationSubmission.java
@@ -1,23 +1,125 @@
 package com.picsauditing.service.registration;
 
-import com.picsauditing.service.registration.validator.RegistrationValidator;
 
 public class RegistrationSubmission {
-    private final RegistrationValidator passedValidator;
     private final RegistrationService parentService;
+    private String contractorName;
+    private String userName;
+    private String password;
+    private String email;
+    private String userFirstName;
+    private String userLastName;
+    private String address;
+    private String phoneNumber;
+    private String zip;
+    private String countryISO;
+    private String timeZone;
+    private String vatID;
 
     RegistrationSubmission (
-            RegistrationValidator v,
             RegistrationService s
     ) {
-        this.passedValidator = v;
         this.parentService = s;
     }
 
     public void submit() {
-        passedValidator.validate(this);
         parentService.doRegistration(this);
     }
 
 
+    public void setContractorName(String contractorName) {
+        this.contractorName = contractorName;
+    }
+
+    public String getContractorName() {
+        return contractorName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setUserFirstName(String userFirstName) {
+        this.userFirstName = userFirstName;
+    }
+
+    public String getUserFirstName() {
+        return userFirstName;
+    }
+
+    public void setUserLastName(String userLastName) {
+        this.userLastName = userLastName;
+    }
+
+    public String getUserLastName() {
+        return userLastName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setCountryISO(String countryISO) {
+        this.countryISO = countryISO;
+    }
+
+    public String getCountryISO() {
+        return countryISO;
+    }
+
+    public void setTimeZone(String timeZone) {
+        this.timeZone = timeZone;
+    }
+
+    public String getTimeZone() {
+        return timeZone;
+    }
+
+    public void setVatID(String vatID) {
+        this.vatID = vatID;
+    }
+
+    public String getVatID() {
+        return vatID;
+    }
 }
