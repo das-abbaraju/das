bfbb8473bd125afbab0384c32aea8001a20eaa8c
Lucas Kam <lkam@picsauditing.com>
2014-03-17 16:17:22 -0700

    US580 Corrected SQL patch

--- pics-database/liquibase/2014/Q1/lkam8_US580_Add_EmployeeGUARD_Rules.sql
+++ pics-database/liquibase/2014/Q1/lkam8_US580_Add_EmployeeGUARD_Rules.sql
@@ -3,7 +3,7 @@
 --changeset lkam:8
 --preConditions onFail MARK_RAN
 ALTER TABLE contractor_info
-ADD COLUMN hasEmployeeGuard TINYINT(4) NOT NULL DEFAULT '0' AFTER logoForSingleOperatorContractor;
+ADD COLUMN hasEmployeeGuard TINYINT(4) NOT NULL DEFAULT '0' AFTER salesRepSalesForceID;
 
 ALTER TABLE operators
 ADD COLUMN requiresEmployeeGuard TINYINT(4) NOT NULL DEFAULT '0' AFTER salesForceID;
