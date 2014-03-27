8ff238a7f02c9d48484e18a5d2d6cd84f6648699
Jared Griffith <jgriffith@picsauditing.com>
2014-03-27 16:46:12 -0700

    PICS-14871 #time 10m #comment Splitting sql files and adding them to the changelog

--- pics-database/liquibase/2014/Q1/kchase37_PICS-14871_UpdateResubmittedPQF_DML.sql
+++ pics-database/liquibase/2014/Q1/kchase37_PICS-14871_UpdateResubmittedPQF_DML.sql
@@ -1,18 +1,6 @@
 --liquibase formatted sql
 
---changeset kchase:35
-
--- Determine appropriate PQFs and CAOs to update
-DROP TABLE IF EXISTS temp_caos_to_update;
-
-CREATE TABLE temp_caos_to_update AS
-SELECT cao.id as caoID, cao.status as caoStatus from contractor_audit as ca
-join contractor_audit_operator as cao on cao.`auditID` = ca.id
-where ca.auditTypeID=1
-and ca.`expiresDate` > NOW()
-and cao.visible = 1
-and cao.status in ('Resubmit', 'Resubmitted')
-;
+--changeset kchase:37
 
 -- Update cao status
 UPDATE contractor_audit_operator cao
