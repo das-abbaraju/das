a9f614917aca1ad1c6342c0142d0f5a58ad12385
jbrownell <jbrownell@picsauditing.com>
2014-03-31 16:44:15 -0700

    TA244: Updated routing for operator assignment page. Conflicts: 	pics-legacy/src/main/resources/struts-employee-guard.xml

--- pics-web/src/main/webapp/angular/src/app/employeeguard/employeeguard.js
+++ pics-web/src/main/webapp/angular/src/app/employeeguard/employeeguard.js
@@ -14,6 +14,9 @@ angular.module('PICS.employeeguard', [
         .when('/employee-guard/operators/dashboard', {
             templateUrl: '/angular/src/app/employeeguard/operator/dashboard/dashboard.tpl.html'
         })
+        .when('/employee-guard/operators/assignments', {
+            templateUrl: '/angular/src/app/employeeguard/operator/assignmentlist/assignmentlist.tpl.html'
+        })
         .when('/employee-guard/operators/projects', {
             templateUrl: '/angular/src/app/employeeguard/operator/project/project_list.tpl.html'
         })
