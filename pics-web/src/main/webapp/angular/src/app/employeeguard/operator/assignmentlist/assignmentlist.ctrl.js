127e85c6bd4216599464a7188b4bebc9c9858f00
jbrownell <jbrownell@picsauditing.com>
2014-04-02 15:13:57 -0700

    Added select2 to ui and ui enhancements.

--- pics-web/src/main/webapp/angular/src/app/employeeguard/operator/assignmentlist/assignmentlist.ctrl.js
+++ pics-web/src/main/webapp/angular/src/app/employeeguard/operator/assignmentlist/assignmentlist.ctrl.js
@@ -4,6 +4,9 @@ angular.module('PICS.employeeguard')
     $scope.siteList = SiteList.query(function(sites) {
         if ($scope.hasSites(sites)) {
             $scope.loadSelectedSiteData(sites[0].id);
+        } else {
+            $scope.site_assignments = SiteAssignments.get();
+            $scope.project_assignments = ProjectAssignments.query();
         }
     });
 
@@ -13,11 +16,22 @@ angular.module('PICS.employeeguard')
 
     $scope.loadSelectedSiteData = function(site_id) {
         $scope.selected_site = site_id;
+        $scope.selected_site_name = $scope.getSiteNameFromSiteList(site_id);
         $scope.site_assignments = SiteAssignments.get({id: site_id});
         $scope.project_assignments = ProjectAssignments.query({id: site_id});
     };
 
     $scope.updateSelectedSite = function() {
-        $scope.loadSelectedSiteData($scope.selected_site);
+        if ($scope.selected_site !== 'null') {
+            $scope.loadSelectedSiteData($scope.selected_site);
+        }
+    };
+
+    $scope.getSiteNameFromSiteList = function (id) {
+        for (var i = 0; i < $scope.siteList.length; i++) {
+            if ($scope.siteList[i].id == $scope.selected_site) {
+                return $scope.siteList[i].name;
+            }
+        }
     };
 });
