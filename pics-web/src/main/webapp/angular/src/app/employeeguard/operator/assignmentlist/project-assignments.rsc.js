c866e6087f9bb8f59041a9eeb72d7e503c0c3cd2
jbrownell <jbrownell@picsauditing.com>
2014-04-03 11:22:21 -0700

    TA-1132: Moved project and site assignment resources to EG common.

--- pics-web/src/main/webapp/angular/src/app/employeeguard/operator/assignmentlist/project-assignments.rsc.js
+++
@@ -1,6 +0,0 @@
-angular.module('PICS.employeeguard')
-
-.factory('ProjectAssignments', function($resource, $routeParams) {
-    // return $resource('/employee-guard/corporates/sites');
-    return $resource('/angular/json/operator/assignmentlist/project_assignments:id.json');
-});
--- pics-web/src/main/webapp/angular/src/app/employeeguard/operator/assignmentlist/site_details.rsc.js
+++
@@ -1,5 +0,0 @@
-angular.module('PICS.employeeguard')
-
-.factory('SiteAssignmentDetails', function($resource, $routeParams) {
-    return $resource('/employee-guard/corporates/sites/:id');
-});
