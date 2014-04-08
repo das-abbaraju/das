f7fe26b3fee15457962404cafaade9e3062696c9
Bryan Latner <blatner@picsauditing.com>
2014-04-08 11:56:08 -0700

    Add in forgotten 'Compiled' directive.

--- pics-database/src/main/scala/com/picsauditing/persistence/provider/UserDataProvider.scala
+++ pics-database/src/main/scala/com/picsauditing/persistence/provider/UserDataProvider.scala
@@ -1,20 +1,21 @@
 package com.picsauditing.persistence.provider
 
-import com.picsauditing.persistence.model.{Profile, UserAccessDAO, UserDAO}
+import com.picsauditing.persistence.model.{UserContactInfo, Profile, UserAccessDAO, UserDAO}
 
 
 class UserDataProvider extends UserDAO with UserAccessDAO { this: Profile =>
   import profile.simple._
 
 
-  def findAccountContactByRole(role: String, accountID: Long)(implicit session: Session) = contactRoleQuery(role, accountID).list
+  def findAccountContactByRole(role: String, accountID: Long)(implicit session: Session): List[UserContactInfo] =
+    contactRoleQuery(role, accountID).list
+    // The error above is a tooling error. It compiles and runs.
 
-  val contactRoleQuery = (role: Column[String], accountID: Column[Long]) => {
+  private val contactRoleQuery = Compiled { (role: Column[String], accountID: Column[Long]) => {
     for {
       user <- users if user.isActive === "Yes" && user.accountID === accountID
-      ua <- userAccess  if user.id === ua.userID  && ua.accessType === role
+      ua <- userAccess if user.id === ua.userID && ua.accessType === role
     } yield user.contactinfo
-  }
+  }}
 
-
- }
+}
