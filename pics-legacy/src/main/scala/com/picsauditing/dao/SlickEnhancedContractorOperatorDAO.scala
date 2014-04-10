683f62dd1664b4a9b6a6205deedb5720cba54ac4
Bryan Latner <blatner@picsauditing.com>
2014-04-09 17:22:38 -0700

    Alter new CO Dao
    New methods to replace CO entity transient methods.
    Hopefully with names that make more sense.

--- pics-legacy/src/main/scala/com/picsauditing/dao/SlickEnhancedContractorOperatorDAO.scala
+++ pics-legacy/src/main/scala/com/picsauditing/dao/SlickEnhancedContractorOperatorDAO.scala
@@ -2,13 +2,27 @@ package com.picsauditing.dao
 
 import com.picsauditing.persistence.provider.FacilityRelationshipDataProvider
 import com.picsauditing.persistence.model.MySQLProfile
+import com.picsauditing.jpa.entities.{ApprovalStatus, ContractorOperator}
 
 class SlickEnhancedContractorOperatorDAO extends ContractorOperatorDAO with PICSDataAccess {
 
   lazy val frdProvider = new FacilityRelationshipDataProvider with MySQLProfile
 
-  def findFacilitiesForCorporateOperatorID(id: Long) = db withSession { implicit session =>
-    toJava (frdProvider findFacilityIDsForCorporateOperator id)
+  private def findCorporateChildWithWorkStatusForContractor(co: ContractorOperator, findWorkStatus: String)(notCorporate: ApprovalStatus => Boolean) = {
+    if (co.getOperatorAccount.isCorporate)
+      frdProvider.childrenWorkstatusEquals(
+        workStatus = findWorkStatus,
+        corporateID = co.getOperatorAccount.getId.toLong,
+        contractorID = co.getContractorAccount.getId.toLong
+      )
+    else
+      notCorporate(co.getWorkStatus)
   }
+
+  def workStatusIsContractor = findCorporateChildWithWorkStatusForContractor(_, "C")( notCorporate = _.isContractor )
+  def workStatusIsPending = findCorporateChildWithWorkStatusForContractor(_ , "P")( notCorporate = _.isPending )
+  def workStatusIsRejected = findCorporateChildWithWorkStatusForContractor(_, "N")( notCorporate = _.isNo )
+  def workStatusIsApproved = findCorporateChildWithWorkStatusForContractor(_, "Y")( notCorporate = _.isYes )
+
 
 }
