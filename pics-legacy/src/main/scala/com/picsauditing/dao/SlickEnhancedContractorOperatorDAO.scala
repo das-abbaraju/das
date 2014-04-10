94c6e43ac99b798e30a3b1b71b53879bb3f4a2d9
Bryan Latner <blatner@picsauditing.com>
2014-04-09 17:31:34 -0700

    Add missing database session to query execution.

--- pics-legacy/src/main/scala/com/picsauditing/dao/SlickEnhancedContractorOperatorDAO.scala
+++ pics-legacy/src/main/scala/com/picsauditing/dao/SlickEnhancedContractorOperatorDAO.scala
@@ -8,16 +8,17 @@ class SlickEnhancedContractorOperatorDAO extends ContractorOperatorDAO with PICS
 
   lazy val frdProvider = new FacilityRelationshipDataProvider with MySQLProfile
 
-  private def findCorporateChildWithWorkStatusForContractor(co: ContractorOperator, findWorkStatus: String)(notCorporate: ApprovalStatus => Boolean) = {
+  private def findCorporateChildWithWorkStatusForContractor(co: ContractorOperator, findWorkStatus: String)(notCorporate: ApprovalStatus => Boolean) =
     if (co.getOperatorAccount.isCorporate)
-      frdProvider.childrenWorkstatusEquals(
-        workStatus = findWorkStatus,
-        corporateID = co.getOperatorAccount.getId.toLong,
-        contractorID = co.getContractorAccount.getId.toLong
-      )
+      db withSession {
+        frdProvider.childrenWorkstatusEquals(
+          workStatus = findWorkStatus,
+          corporateID = co.getOperatorAccount.getId.toLong,
+          contractorID = co.getContractorAccount.getId.toLong
+        )
+      }
     else
       notCorporate(co.getWorkStatus)
-  }
 
   def workStatusIsContractor = findCorporateChildWithWorkStatusForContractor(_, "C")( notCorporate = _.isContractor )
   def workStatusIsPending = findCorporateChildWithWorkStatusForContractor(_ , "P")( notCorporate = _.isPending )
