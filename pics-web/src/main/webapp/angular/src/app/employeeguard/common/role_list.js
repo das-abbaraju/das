2c0aa6022d692418e8ad13cda23bc7ef3a0354e1
jbrownell <jbrownell@picsauditing.com>
2014-04-10 16:44:18 -0700

    Removed unused url

--- pics-web/src/main/webapp/angular/src/app/employeeguard/common/role_list.js
+++ pics-web/src/main/webapp/angular/src/app/employeeguard/common/role_list.js
@@ -1,6 +1,5 @@
 angular.module('PICS.employeeguard')
 
 .factory('RoleList', function($resource) {
-    // return $resource('/employee-guard/operators/roles/list');
     return $resource('/angular/json/operator/role_list.json');
 });
