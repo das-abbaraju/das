2fa63428da11a4338c61ce94b478001708bd6eda
jbrownell <jbrownell@picsauditing.com>
2014-04-11 17:26:55 -0700

    TA1292: Fixed table striping issue caused by ie8 js override.

--- pics-web/src/main/webapp/bootstrap3/js/pics/widget/TableStripe.js
+++ pics-web/src/main/webapp/bootstrap3/js/pics/widget/TableStripe.js
@@ -4,15 +4,15 @@
 // e.g., to stripe search results
 PICS.define('widget.TableStripe', {
     methods: (function () {
-        function init(selector) {
-            var row_selector = selector || '.table-striped tr';
+        function init() {
+            $('.table-striped').each(function() {
+                var row_selector = $(this).find('tr'),
+                    odd_rows = row_selector.filter(':odd'),
+                    even_rows = row_selector.filter(':even');
 
-            var table_striped_rows = $(row_selector),
-                odd_rows = table_striped_rows.filter(':odd'),
-                even_rows = table_striped_rows.filter(':even');
-
-            odd_rows.addClass('odd');
-            even_rows.addClass('even');
+                odd_rows.addClass('odd');
+                even_rows.addClass('even');
+            });
         }
 
         return {
