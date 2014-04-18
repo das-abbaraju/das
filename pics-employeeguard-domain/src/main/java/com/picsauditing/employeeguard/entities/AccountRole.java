f8f1e12c04029f00ed9660bf87418edb7a01e6c3
aphatarphekar <aphatarphekar@picsauditing.com>
2014-04-17 17:09:04 -0700

    DE505:Added functionality to update/add roles to a skill which was missing
    (cherry picked from commit 130da96ab9c89b8e8caa9c415a756f72e4d60ae8)

---
+++ pics-employeeguard-domain/src/main/java/com/picsauditing/employeeguard/entities/AccountRole.java
@@ -0,0 +1,127 @@
+package com.picsauditing.employeeguard.entities;
+
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "account_group")
+@Where(clause = "deletedDate IS NULL")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "type", discriminatorType = DiscriminatorType.STRING)
+public abstract class AccountRole implements BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	protected int id;
+
+	@Column(name = "accountID", nullable = false)
+	protected int accountId;
+
+	protected String name;
+
+	protected String description;
+
+	protected int createdBy;
+	protected int updatedBy;
+	protected int deletedBy;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date createdDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date updatedDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date deletedDate;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(int accountId) {
+		this.accountId = accountId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(int createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public int getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(int updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	public int getDeletedBy() {
+		return deletedBy;
+	}
+
+	public void setDeletedBy(int deletedBy) {
+		this.deletedBy = deletedBy;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Date getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(Date updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+
+	public Date getDeletedDate() {
+		return deletedDate;
+	}
+
+	public void setDeletedDate(Date deletedDate) {
+		this.deletedDate = deletedDate;
+	}
+
+	public abstract boolean equals(final Object o);
+
+	public abstract int hashCode();
+
+	@Override
+	public String toString() {
+		return id + " " + name;
+	}
+}
