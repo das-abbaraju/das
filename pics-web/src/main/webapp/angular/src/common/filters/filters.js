6dea9dc1fed916abed50d8152f9b3b5ada01a91a
jbrownell <jbrownell@picsauditing.com>
2014-04-22 16:43:31 -0700

    Added duplicate array filter and filter tests.

--- pics-web/src/main/webapp/angular/src/common/filters/filters.js
+++ pics-web/src/main/webapp/angular/src/common/filters/filters.js
@@ -6,4 +6,21 @@ angular.module('PICS.filters', [])
         var str = text.replace(/\s+/g, '-').toLowerCase();
         return str;
     };
+})
+
+.filter('removeDuplicateItemsFromArray', function ($filter) {
+    return function (original_array) {
+        var new_array = [],
+            matches;
+
+        angular.forEach(original_array, function(original_item) {
+            matches = $filter('filter')(new_array, original_item, true);
+
+            if (!matches.length) {
+                new_array.push(original_item);
+            }
+        });
+
+        return new_array;
+    };
 });
