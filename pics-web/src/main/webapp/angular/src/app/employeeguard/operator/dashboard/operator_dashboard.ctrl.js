800493876e4a931d6d4e9fc1e92da0cd5ca5f48f
jbrownell <jbrownell@picsauditing.com>
2014-04-29 14:30:42 -0700

    Updated operator dashboard to pull from Who Am I and fixed app.js pulling in non-existent project.

--- pics-web/src/main/webapp/angular/src/app/employeeguard/operator/dashboard/operator_dashboard.ctrl.js
+++ pics-web/src/main/webapp/angular/src/app/employeeguard/operator/dashboard/operator_dashboard.ctrl.js
@@ -1,34 +1,36 @@
 angular.module('PICS.employeeguard')
 
-.controller('operatorDashboardCtrl', function ($scope, SiteList, SiteAssignments, ProjectAssignments, WhoAmI) {
-    $scope.siteList = SiteList.query(function(sites) {
-        if ($scope.hasSites(sites)) {
-            $scope.loadSelectedSiteData(sites[0].id);
+.controller('operatorDashboardCtrl', function ($scope, $routeParams, SiteList, SiteAssignments, ProjectAssignments, WhoAmI) {
+
+    WhoAmI.get(function(user) {
+        $scope.user = user.type.toLowerCase();
+
+        if ($scope.user === 'corporate') {
+            SiteList.query(function(sites) {
+                $scope.siteList = sites;
+
+                //load first site if none specified in url. Remove when no longer an issue
+                if (!$routeParams.siteId) {
+                    $scope.loadSelectedSiteData(sites[0].id);
+                } else {
+                    $scope.loadSelectedSiteData($routeParams.siteId);
+                }
+            });
         } else {
             $scope.loadSelectedSiteData();
         }
     });
 
-    WhoAmI.get(function(user) {
-        $scope.user = user.type.toLowerCase();
-    });
-
-    $scope.hasSites = function(sites) {
-        return sites.length > 0;
-    };
-
     $scope.loadSelectedSiteData = function(site_id) {
-        if (site_id !== 'null') {
-            $scope.selected_site = site_id;
-            $scope.site_assignments = SiteAssignments.get({id: site_id}, function(site_details){
-                $scope.chartData = [
-                    site_details.completed + site_details.pending,
-                    site_details.expiring,
-                    site_details.expired
-                ];
-            });
-            $scope.project_assignments = ProjectAssignments.query({id: site_id});
-        }
+        $scope.selected_site = site_id;
+        $scope.site_assignments = SiteAssignments.get({id: site_id}, function(site_details){
+            $scope.chartData = [
+                site_details.completed + site_details.pending,
+                site_details.expiring,
+                site_details.expired
+            ];
+        });
+        $scope.project_assignments = ProjectAssignments.query({id: site_id});
     };
 
     $scope.calculateStatusPercentage = function (amount, total) {
