b0cc55e325b2c368cad5b60cd968327f98cdd5d4
Alex Ananighian <aananighian@picsauditing.com>
2014-04-30 10:08:44 -0700

    US371: Fix broken employee image

--- pics-legacy/src/main/java/com/picsauditing/employeeguard/models/factories/OperatorEmployeeModelFactory.java
+++ pics-legacy/src/main/java/com/picsauditing/employeeguard/models/factories/OperatorEmployeeModelFactory.java
@@ -1,14 +1,10 @@
 package com.picsauditing.employeeguard.models.factories;
 
-import com.picsauditing.employeeguard.entities.AccountSkill;
 import com.picsauditing.employeeguard.entities.Employee;
-import com.picsauditing.employeeguard.entities.ProfileDocument;
 import com.picsauditing.employeeguard.models.CompanyModel;
 import com.picsauditing.employeeguard.services.calculator.SkillStatus;
-import com.picsauditing.employeeguard.util.PhotoUtil;
 
 import java.util.List;
-import java.util.Map;
 
 import static com.picsauditing.employeeguard.util.EmployeeGUARDUrlUtils.IMAGE_LINK;
 import static com.picsauditing.employeeguard.util.EmployeeGUARDUrlUtils.buildUrl;
@@ -16,12 +12,12 @@ import static com.picsauditing.employeeguard.util.EmployeeGUARDUrlUtils.buildUrl
 public class OperatorEmployeeModelFactory {
 
 	public OperatorEmployeeModel build(final Employee employee, final SkillStatus employeeStatus,
-									   final ProfileDocument image, final List<CompanyModel> companyModels) {
+									   final List<CompanyModel> companyModels) {
 
 		OperatorEmployeeModel operatorEmployeeModel = new OperatorEmployeeModel();
 
 		operatorEmployeeModel.setId(employee.getId());
-		operatorEmployeeModel.setImage(buildImageFileUrl(image));
+		operatorEmployeeModel.setImage(buildLink(employee));
 		operatorEmployeeModel.setStatus(employeeStatus);
 		operatorEmployeeModel.setFirstName(employee.getFirstName());
 		operatorEmployeeModel.setLastName(employee.getLastName());
@@ -30,12 +26,8 @@ public class OperatorEmployeeModelFactory {
 		return operatorEmployeeModel;
 	}
 
-	private String buildImageFileUrl(final ProfileDocument image) {
-		if (image == null) {
-			return PhotoUtil.DEFAULT_PHOTO_FILE;
-		}
-
-		return buildUrl(IMAGE_LINK, image.getId());
+	private String buildLink(final Employee employee) {
+		return buildUrl(IMAGE_LINK, employee.getAccountId(), employee.getId());
 	}
 
 	public class OperatorEmployeeModel {
@@ -63,15 +55,15 @@ public class OperatorEmployeeModelFactory {
 			this.image = image;
 		}
 
-        public SkillStatus getStatus() {
-            return status;
-        }
+		public SkillStatus getStatus() {
+			return status;
+		}
 
-        public void setStatus(SkillStatus status) {
-            this.status = status;
-        }
+		public void setStatus(SkillStatus status) {
+			this.status = status;
+		}
 
-        public String getFirstName() {
+		public String getFirstName() {
 			return firstName;
 		}
 
