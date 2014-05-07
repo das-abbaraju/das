6cb350680fa534216b3593076c614c5324f6c67e
Alex Ananighian <aananighian@picsauditing.com>
2014-05-07 15:15:50 -0700

    DE605: Add mock JSON Response to integrate with Angular Front-End

--- pics-legacy/src/main/java/com/picsauditing/employeeguard/controllers/employee/ProfileSkillsAction.java
+++ pics-legacy/src/main/java/com/picsauditing/employeeguard/controllers/employee/ProfileSkillsAction.java
@@ -16,13 +16,116 @@ public class ProfileSkillsAction extends PicsRestActionSupport {
 	private ProfileEntityService profileEntityService;
 
 	public String skills() {
-		Profile profile = profileEntityService.findByAppUserId(permissions.getAppUserID());
-		EmployeeSkillsModelFactory.EmployeeSkillsModel employeeSkillsModel =
-				employeeSkillsModelService.buildEmployeeSkillsModel(profile);
+//		Profile profile = profileEntityService.findByAppUserId(permissions.getAppUserID());
+//		EmployeeSkillsModelFactory.EmployeeSkillsModel employeeSkillsModel =
+//				employeeSkillsModelService.buildEmployeeSkillsModel(profile);
+//
+//		jsonString = new Gson().toJson(employeeSkillsModel);
 
-		jsonString = new Gson().toJson(employeeSkillsModel);
+		jsonString = fakeJson();
 
 		return JSON_STRING;
+	}
+
+	private String fakeJson() {
+		return "{\n" +
+				"  \"status\": \"Expired\",\n" +
+				"  \"sites\": [\n" +
+				"    {\n" +
+				"      \"id\": 2,\n" +
+				"      \"name\": \"BASF Houston Texas\",\n" +
+				"      \"status\": \"Expired\",\n" +
+				"      \"projects\": [\n" +
+				"        {\n" +
+				"          \"id\": 3,\n" +
+				"          \"name\": \"Dynamic Reporting\",\n" +
+				"          \"status\": \"Expired\",\n" +
+				"          \"skills\": [\n" +
+				"            {\n" +
+				"              \"id\": 210,\n" +
+				"              \"name\": \"Dynamic Reporting Skill\",\n" +
+				"              \"status\": \"Expiring\"\n" +
+				"            },\n" +
+				"            {\n" +
+				"              \"id\": 5,\n" +
+				"              \"name\": \"Dynamic Reporting Skill 2\",\n" +
+				"              \"status\": \"Completed\"\n" +
+				"            }\n" +
+				"          ]\n" +
+				"        },\n" +
+				"        {\n" +
+				"          \"id\": 3,\n" +
+				"          \"name\": \"Ninja Dojo\",\n" +
+				"          \"status\": \"Expiring\",\n" +
+				"          \"skills\": [\n" +
+				"            {\n" +
+				"              \"id\": 4,\n" +
+				"              \"name\": \"Ninja Dojo Skill 4\",\n" +
+				"              \"status\": \"Expired\"\n" +
+				"            },\n" +
+				"            {\n" +
+				"              \"id\": 5,\n" +
+				"              \"name\": \"Ninja Dojo Skill 5\",\n" +
+				"              \"status\": \"Completed\"\n" +
+				"            }\n" +
+				"          ]\n" +
+				"        }\n" +
+				"      ],\n" +
+				"      \"required\": {\n" +
+				"        \"skills\": [\n" +
+				"          {\n" +
+				"            \"id\": 45,\n" +
+				"            \"name\": \"BASF Site Skill 1\",\n" +
+				"            \"status\": \"Expired\"\n" +
+				"          },\n" +
+				"          {\n" +
+				"            \"id\": 10,\n" +
+				"            \"name\": \"BASF Corp Skill 1\",\n" +
+				"            \"status\": \"Expiring\"\n" +
+				"          }\n" +
+				"        ]\n" +
+				"      }\n" +
+				"    },\n" +
+				"    {\n" +
+				"      \"id\": 6,\n" +
+				"      \"name\": \"PICS\",\n" +
+				"      \"status\": \"Expiring\",\n" +
+				"      \"required\": {\n" +
+				"        \"skills\": [\n" +
+				"          {\n" +
+				"            \"id\": 4,\n" +
+				"            \"name\": \"PICS Site skill\",\n" +
+				"            \"status\": \"Expired\"\n" +
+				"          },\n" +
+				"          {\n" +
+				"            \"id\": 25,\n" +
+				"            \"name\": \"PICS Site skill 2\",\n" +
+				"            \"status\": \"Expiring\"\n" +
+				"          }\n" +
+				"        ]\n" +
+				"      }\n" +
+				"    },\n" +
+				"    {\n" +
+				"      \"id\": 8,\n" +
+				"      \"name\": \"Spectre\",\n" +
+				"      \"status\": \"Completed\",\n" +
+				"      \"projects\": [\n" +
+				"        {\n" +
+				"          \"id\": 32,\n" +
+				"          \"name\": \"Volcano Base\",\n" +
+				"          \"status\": \"Completed\",\n" +
+				"          \"skills\": [\n" +
+				"            {\n" +
+				"              \"id\": 4,\n" +
+				"              \"name\": \"Volcano Base Skill\",\n" +
+				"              \"status\": \"Expired\"\n" +
+				"            }\n" +
+				"          ]\n" +
+				"        }\n" +
+				"      ]\n" +
+				"    }\n" +
+				"  ]\n" +
+				"}";
 	}
 
 }
