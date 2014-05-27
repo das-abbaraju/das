46d9bc685efee442d4baf8c614dcf333301d69b2
rbpics <rbeaini@picsauditing.com>
2014-05-27 10:22:08 -0700

    PICS-15351 - new action (cherry picked from commit 81c40db)
    (cherry picked from commit 9feec9dc810baaae02ac3cb1471c8ef00b65896b)
    
    Conflicts:
    	pics-legacy/src/main/java/com/picsauditing/employeeguard/controllers/ResetPassword.java
    	pics-legacy/src/main/resources/struts-employee-guard.xml

--- pics-legacy/src/main/java/com/picsauditing/employeeguard/controllers/ResetPassword.java
+++ pics-legacy/src/main/java/com/picsauditing/employeeguard/controllers/ResetPassword.java
@@ -1,15 +1,9 @@
 package com.picsauditing.employeeguard.controllers;
 
-import com.picsauditing.access.Anonymous;
 import com.picsauditing.controller.PicsRestActionSupport;
 
 public class ResetPassword extends PicsRestActionSupport {
     private String password;
-
-    @Anonymous
-    public String index() throws Exception {
-        return SUCCESS;
-    }
 
     public String getPassword() {
         return password;
