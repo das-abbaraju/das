f11b904593e0ecaa4effdf3d39e10ca1c2854245
Nalin Kishore <nkishore@picsauditing.com>
2014-06-02 17:30:37 -0700

    US635 TA1976 : UnitTest stubs for removing address for QB Contractors.

---
+++ pics-legacy/src/test/java/com/intuit/developer/adaptors/UpdateContractorsTest.java
@@ -0,0 +1,33 @@
+package com.intuit.developer.adaptors;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UpdateContractorsTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetQbXml() throws Exception {
+       //TODO: Fixme
+    }
+
+
+    @Test
+    public void testGetQbXml() throws Exception {
+        //TODO: Fixme
+    }
+
+    @Test
+    public void testSetBillAddress_ToggleDisabled() throws Exception {
+        //TODO: Fixme
+    }
+
+    @Test
+    public void testSetBillAddress_ToggleEnabled() throws Exception {
+        //TODO: Fixme
+    }
+}
