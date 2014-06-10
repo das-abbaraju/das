ee442d918517eb809d3e3a157e35bee736ac3ebc
jbrownell <jbrownell@picsauditing.com>
2014-06-09 17:07:07 -0700

    US1010: Code update

--- pics-web/src/main/webapp/employee-guard/src/app/employeeguard/common/skill-detail/skill-detail.rsc.js
+++ pics-web/src/main/webapp/employee-guard/src/app/employeeguard/common/skill-detail/skill-detail.rsc.js
@@ -1,5 +1,5 @@
 angular.module('PICS.employeeguard')
 
 .factory('SkillDetail', function($resource) {
-    return $resource('/employee-guard/json/eg-common/skill-detail/skill-detail.json');
+    return $resource('/employee-guard/skillreview/employee/:id/skill/:skillId/info');
 });
