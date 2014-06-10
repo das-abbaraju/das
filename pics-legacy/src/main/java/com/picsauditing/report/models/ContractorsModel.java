bb5116c23cd9ed4da9160a6c681c371d50d65048
Michael Do <mdo@picsauditing.com>
2014-06-10 16:51:30 -0700

    PICS-14335 Added new link to Billable Sites

--- pics-legacy/src/main/java/com/picsauditing/report/models/ContractorsModel.java
+++ pics-legacy/src/main/java/com/picsauditing/report/models/ContractorsModel.java
@@ -105,6 +105,9 @@ public class ContractorsModel extends AbstractModel {
 		Field accountName = fields.get("AccountName".toUpperCase());
 		accountName.setUrl("ContractorView.action?id={AccountID}");
 
+        Field billableSites = fields.get("ContractorPayingFacilities".toUpperCase());
+        billableSites.setUrl("ContractorFacilities.action?id={AccountID}");
+
         if (permissions.isOperatorCorporate()) {
             Field flagColor = fields.get("ContractorFlagFlagColor".toUpperCase());
             flagColor.setUrl("ContractorFlag.action?id={AccountID}");
