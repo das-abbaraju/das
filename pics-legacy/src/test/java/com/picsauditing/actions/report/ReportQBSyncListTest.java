026be1c1f6cb1f9de5a78a958698a3ae9fb4aeca
Nalin Kishore <nkishore@picsauditing.com>
2014-06-13 12:56:57 -0700

    US 1023 TA2389 : unit test for com.picsauditing.actions.report.ReportQBSyncList#setQBData

---
+++ pics-legacy/src/test/java/com/picsauditing/actions/report/ReportQBSyncListTest.java
@@ -0,0 +1,13 @@
+package com.picsauditing.actions.report;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class ReportQBSyncListTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+}
