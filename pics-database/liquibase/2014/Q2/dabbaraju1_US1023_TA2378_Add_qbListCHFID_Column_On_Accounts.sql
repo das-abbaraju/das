226179d13ca5c45177a5d63fbde5bdffbec0f518
CFranks <cfranks@picsauditing.com>
2014-06-17 20:11:55 -0700

    fixed sql in US1023_TA2378 add column

--- pics-database/liquibase/2014/Q2/dabbaraju1_US1023_TA2378_Add_qbListCHFID_Column_On_Accounts.sql
+++ pics-database/liquibase/2014/Q2/dabbaraju1_US1023_TA2378_Add_qbListCHFID_Column_On_Accounts.sql
@@ -1,12 +1,9 @@
--- Add new nullable column qbListCHFID on Accounts table for new CHF currency.
-
 --liquibase formatted sql
 
 --changeset dabbaraju:1
-
 -- --preConditions onFail MARK_RAN
 
-alter table
-accounts
-add
-qbListCHFID  VARCHAR(25);
+alter table accounts
+add column qbListCHFID  VARCHAR(25) NULL;
+
+-- Add new nullable column qbListCHFID on Accounts table for new CHF currency.
