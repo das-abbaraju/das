e86acc534ac1524db7f6cfbbceb9a79f3edf4516
CFranks <cfranks@picsauditing.com>
2014-07-15 12:46:53 -0700

    add new zealand pricing to aud script

--- pics-database/liquibase/2014/Q3/akafagy1_PICS-15646_Change_AUS_Pricing.sql
+++ pics-database/liquibase/2014/Q3/akafagy1_PICS-15646_Change_AUS_Pricing.sql
@@ -3,13 +3,13 @@
 --changeset akafagy:1
 
 -- backup data
-CREATE TEMPORARY TABLE temp_invoice_fee_aud AS
+CREATE TEMPORARY TABLE temp_invoice_fee_aud_nzd AS
 SELECT DISTINCT rc.isoCode, rc.currency, fee.fee, fee.minFacilities, fee.maxFacilities, ifc.amount, ifc.`country`
 FROM ref_country rc
 JOIN invoice_fee_country ifc ON rc.isoCode = ifc.country
 JOIN invoice_fee fee ON ifc.feeID = fee.id
-WHERE rc.isoCode = 'AU'
-AND rc.currency = 'AUD';
+WHERE rc.isoCode IN ('AU', 'NZ')
+AND rc.currency IN ('AUD', 'NZD');
 
 UPDATE invoice_fee_country ifc
 JOIN ref_country rc ON rc.isoCode = ifc.country
@@ -133,3 +133,137 @@ AND rc.currency = 'AUD'
 AND fee.minFacilities >= 5
 AND fee.maxFacilities > 4
 AND fee.fee LIKE 'EmployeeGUARD%';
+
+
+
+UPDATE invoice_fee_country ifc
+  JOIN ref_country rc ON rc.isoCode = ifc.country
+  JOIN invoice_fee fee ON ifc.feeID = fee.id
+SET amount = 220.00
+WHERE rc.isoCode = 'NZ'
+      AND rc.currency = 'NZD'
+      AND fee.minFacilities = 1
+      AND fee.maxFacilities = 1
+      AND fee.fee LIKE 'DocuGUARD%';
+
+UPDATE invoice_fee_country ifc
+  JOIN ref_country rc ON rc.isoCode = ifc.country
+  JOIN invoice_fee fee ON ifc.feeID = fee.id
+SET amount = 220.00
+WHERE rc.isoCode = 'NZ'
+      AND rc.currency = 'NZD'
+      AND fee.minFacilities = 2
+      AND fee.maxFacilities = 4
+      AND fee.fee LIKE 'DocuGUARD%';
+
+UPDATE invoice_fee_country ifc
+  JOIN ref_country rc ON rc.isoCode = ifc.country
+  JOIN invoice_fee fee ON ifc.feeID = fee.id
+SET amount = 220.00
+WHERE rc.isoCode = 'NZ'
+      AND rc.currency = 'NZD'
+      AND fee.minFacilities >= 5
+      AND fee.maxFacilities > 4
+      AND fee.fee LIKE 'DocuGUARD%';
+
+
+UPDATE invoice_fee_country ifc
+  JOIN ref_country rc ON rc.isoCode = ifc.country
+  JOIN invoice_fee fee ON ifc.feeID = fee.id
+SET amount = 110.00
+WHERE rc.isoCode = 'NZ'
+      AND rc.currency = 'NZD'
+      AND fee.minFacilities = 1
+      AND fee.maxFacilities = 1
+      AND fee.fee LIKE 'InsureGUARD%';
+
+UPDATE invoice_fee_country ifc
+  JOIN ref_country rc ON rc.isoCode = ifc.country
+  JOIN invoice_fee fee ON ifc.feeID = fee.id
+SET amount = 220.00
+WHERE rc.isoCode = 'NZ'
+      AND rc.currency = 'NZD'
+      AND fee.minFacilities = 2
+      AND fee.maxFacilities = 4
+      AND fee.fee LIKE 'InsureGUARD%';
+
+UPDATE invoice_fee_country ifc
+  JOIN ref_country rc ON rc.isoCode = ifc.country
+  JOIN invoice_fee fee ON ifc.feeID = fee.id
+SET amount = 440.00
+WHERE rc.isoCode = 'NZ'
+      AND rc.currency = 'NZD'
+      AND fee.minFacilities >= 5
+      AND fee.maxFacilities > 4
+      AND fee.fee LIKE 'InsureGUARD%';
+
+
+UPDATE invoice_fee_country ifc
+  JOIN ref_country rc ON rc.isoCode = ifc.country
+  JOIN invoice_fee fee ON ifc.feeID = fee.id
+SET amount = 550.00
+WHERE rc.isoCode = 'NZ'
+      AND rc.currency = 'NZD'
+      AND fee.minFacilities = 1
+      AND fee.maxFacilities = 1
+      AND fee.fee LIKE 'AuditGUARD%';
+
+UPDATE invoice_fee_country ifc
+  JOIN ref_country rc ON rc.isoCode = ifc.country
+  JOIN invoice_fee fee ON ifc.feeID = fee.id
+SET amount = 1100.00
+WHERE rc.isoCode = 'NZ'
+      AND rc.currency = 'NZD'
+      AND fee.minFacilities = 2
+      AND fee.maxFacilities = 4
+      AND fee.fee LIKE 'AuditGUARD%';
+
+UPDATE invoice_fee_country ifc
+  JOIN ref_country rc ON rc.isoCode = ifc.country
+  JOIN invoice_fee fee ON ifc.feeID = fee.id
+SET amount = 2200.00
+WHERE rc.isoCode = 'NZ'
+      AND rc.currency = 'NZD'
+      AND fee.minFacilities >= 5
+      AND fee.maxFacilities > 4
+      AND fee.fee LIKE 'AuditGUARD%';
+
+
+UPDATE invoice_fee_country ifc
+  JOIN ref_country rc ON rc.isoCode = ifc.country
+  JOIN invoice_fee fee ON ifc.feeID = fee.id
+SET amount = 220.00
+WHERE rc.isoCode = 'NZ'
+      AND rc.currency = 'NZD'
+      AND fee.minFacilities = 1
+      AND fee.maxFacilities = 1
+      AND fee.fee LIKE 'EmployeeGUARD%';
+
+UPDATE invoice_fee_country ifc
+  JOIN ref_country rc ON rc.isoCode = ifc.country
+  JOIN invoice_fee fee ON ifc.feeID = fee.id
+SET amount = 440.00
+WHERE rc.isoCode = 'NZ'
+      AND rc.currency = 'NZD'
+      AND fee.minFacilities = 2
+      AND fee.maxFacilities = 4
+      AND fee.fee LIKE 'EmployeeGUARD%';
+
+UPDATE invoice_fee_country ifc
+  JOIN ref_country rc ON rc.isoCode = ifc.country
+  JOIN invoice_fee fee ON ifc.feeID = fee.id
+SET amount = 880.00
+WHERE rc.isoCode = 'NZ'
+      AND rc.currency = 'NZD'
+      AND fee.minFacilities >= 5
+      AND fee.maxFacilities > 4
+      AND fee.fee LIKE 'EmployeeGUARD%';
+
+
+UPDATE invoice_fee_country ifc
+  JOIN ref_country rc ON rc.isoCode = ifc.country
+  JOIN invoice_fee fee ON ifc.feeID = fee.id
+SET amount = 220.00
+WHERE rc.isoCode = 'NZ'
+      AND rc.currency = 'NZD'
+      AND fee.fee LIKE 'Reactivation Fee%';
