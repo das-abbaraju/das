13e10ed7eab9193c60a6ee63349eba9d2c6cdbfc
jbrownell <jbrownell@picsauditing.com>
2014-07-17 16:57:06 -0700

    US1148: JS recompile.

--- pics-web/src/main/webapp/bootstrap3/js/script.js
+++ pics-web/src/main/webapp/bootstrap3/js/script.js
@@ -29127,52 +29127,123 @@ PICS.define('employee-guard.LeftNavigation', {
     }())
 });
 
-PICS.define('employee-guard.Welcome', {
+PICS.define('employee-guard.SignUp', {
     methods: (function () {
-        var welcome_page_el = $('.employee_guard_welcome-page'),
-            signup_page_el = $('.employee_guard_sign_up-page'),
-            login_page_el,
-            switchLanguageURL,
-            selected_language;
+        var sign_up_page_el = $('.employee_guard_sign_up-page'),
+            selected_language,
+            language_select_el,
+            formValues;
 
         function init() {
-            if (welcome_page_el.length > 0) {
-                login_page_el = welcome_page_el;
-                switchLanguageURL = 'login/switchLanguage';
-            } else if (signup_page_el.length > 0) {
-                login_page_el = signup_page_el;
-                switchLanguageURL = 'sign-up/switchLanguage';
+            if (sign_up_page_el.length > 0) {
+                bindEvents();
             }
-
-            bindEvents();
-            login_page_el.find('#supported_locales').val(selected_language);
         }
 
         function bindEvents() {
-            $('#supported_locales').on('change', onSupportedLanguageChange);
+            language_select_el = sign_up_page_el.find('#supported_locales');
+
+            language_select_el.on('change', onSupportedLanguageChange);
+            language_select_el.val(selected_language);
+
+            if (formValues) {
+                prefillFormData(formValues);
+            }
+
+        }
+
+        function prefillFormData (formValues) {
+            var form = sign_up_page_el.find('form');
+
+            form.find('#employee_guard_create_account_hashCode').val(formValues.hashCode);
+            form.find('#employee_guard_create_account_firstName').val(formValues.firstName);
+            form.find('#employee_guard_create_account_lastName').val(formValues.lastName);
+            form.find('#employee_guard_create_account_email').val(formValues.email);
         }
 
         function onSupportedLanguageChange(event) {
-               var language_select_el = $(event.target),
-                    language = language_select_el.val();
+            var language = language_select_el.val();
+            selected_language = language;
+            setActiveLanguage(language);
+            setFormValues(language);
+        }
 
-                updateLanguage(language);
+        function setActiveLanguage(language) {
+            PICS.ajax({
+                url: 'sign-up/switchLanguage',
+                request_locale: language,
+                success: onUpdateLanguageRequestSuccess
+            });
+        }
 
-            function updateLanguage(language) {
-                selected_language = language;
-                PICS.ajax({
-                    url: switchLanguageURL,
-                    data: {
-                        request_locale: language
-                    },
-                    success: onUpdateLanguageRequestSuccess
-                });
+        function setFormValues(language) {
+            var form = sign_up_page_el.find('form');
+
+            formValues = {};
+            formValues.hashCode = form.find('#employee_guard_create_account_hashCode').val();
+            formValues.firstName = form.find('#employee_guard_create_account_firstName').val();
+            formValues.lastName = form.find('#employee_guard_create_account_lastName').val();
+            formValues.email = form.find('#employee_guard_create_account_email').val();
+        }
+
+        function onUpdateLanguageRequestSuccess(new_login_form_html) {
+            sign_up_page_el.find('#login_row').replaceWith(new_login_form_html);
+            init();
+        }
+
+        return {
+            init: init
+        };
+    }())
+});
+PICS.define('employee-guard.Welcome', {
+    methods: (function () {
+        var welcome_page_el = $('.employee_guard_welcome-page'),
+            selected_language,
+            language_select_el;
+
+        function init() {
+            if (welcome_page_el.length > 0) {
+                bindEvents();
             }
+        }
 
-            function onUpdateLanguageRequestSuccess(new_login_form_html) {
-                login_page_el.find('#login_row').replaceWith(new_login_form_html);
-                init();
-            }
+        function bindEvents() {
+            language_select_el = welcome_page_el.find('#supported_locales');
+
+            language_select_el.on('change', onSupportedLanguageChange);
+            language_select_el.val(selected_language);
+        }
+
+        function onSupportedLanguageChange(event) {
+            var language = language_select_el.val();
+
+            selected_language = language;
+            setActiveLanguage(language);
+        }
+
+        function setActiveLanguage(language) {
+            PICS.ajax({
+                url: 'login/switchLanguage',
+                data: formatFormData(language),
+                success: onUpdateLanguageRequestSuccess
+            });
+        }
+
+        function formatFormData(language) {
+            var hashCode = $('#employee_guard_login_hashCode').val();
+
+            var data = {
+                request_locale: language,
+                hashCode: hashCode
+            };
+
+            return data;
+        }
+
+        function onUpdateLanguageRequestSuccess(new_login_form_html) {
+            welcome_page_el.find('#login_row').replaceWith(new_login_form_html);
+            init();
         }
 
         return {
