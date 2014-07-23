c08f20c003edc3d7ee763867424f41efa8f42a90
jbrownell <jbrownell@picsauditing.com>
2014-07-23 11:30:46 -0700

    US1045: Moved skills controller from operator folder.

--- pics-web/src/main/webapp/employee-guard/src/app/employeeguard/common/skill_list.rsc.js
+++
@@ -1,5 +0,0 @@
-angular.module('PICS.employeeguard')
-
-.factory('OperatorSkillList', function($resource) {
-    return $resource('/employee-guard/api/skills');
-});
--- pics-web/src/main/webapp/employee-guard/src/app/employeeguard/operator/skills/skills.js
+++
@@ -1,9 +0,0 @@
-angular.module('PICS.employeeguard.skills', [])
-
-.config(function ($routeProvider, $locationProvider) {
-    $locationProvider.html5Mode(true);
-
-    $routeProvider.when('/employee-guard/skills', {
-        templateUrl: '/employee-guard/src/app/employeeguard/operator/skills/skills.tpl.html'
-    });
-});
