4464500569cfd518cffe507a6b11f5e62a531cd5
Michael Do <mdo@picsauditing.com>
2014-07-31 13:50:24 +0700

    PICS-15605 Update all complete audit reqs to reviewed if prior to 2014-07-08

--- pics-database/liquibase/2014/Q3/mdo60_PICS-15605.sql
+++ pics-database/liquibase/2014/Q3/mdo60_PICS-15605.sql
@@ -1,8 +1,11 @@
 --liquibase formatted sql
 
 --changeset mdo:60
-UPDATE contractor_audit_file
+UPDATE contractor_audit ca
+JOIN contractor_audit_operator cao ON ca.id = cao.auditID
+JOIN contractor_audit_file caf ON ca.id = caf.auditID
 SET reviewed = 1
-WHERE reviewed = 0
-AND creationDate < '2014-07-08'
+WHERE caf.reviewed = 0
+AND cao.status = 'Complete'
+AND caf.creationDate < '2014-07-08'
 ;
