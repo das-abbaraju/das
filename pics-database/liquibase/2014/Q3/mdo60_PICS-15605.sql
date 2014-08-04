5d495afbe537559c7964704968634af7547a12f1
Michael Do <mdo@picsauditing.com>
2014-08-04 09:38:34 +0700

    PICS-15605 update to 7/30

--- pics-database/liquibase/2014/Q3/mdo60_PICS-15605.sql
+++ pics-database/liquibase/2014/Q3/mdo60_PICS-15605.sql
@@ -7,5 +7,5 @@ JOIN contractor_audit_file caf ON ca.id = caf.auditID
 SET reviewed = 1
 WHERE caf.reviewed = 0
 AND cao.status = 'Complete'
-AND caf.creationDate < '2014-07-08'
+AND caf.creationDate < '2014-07-30'
 ;
