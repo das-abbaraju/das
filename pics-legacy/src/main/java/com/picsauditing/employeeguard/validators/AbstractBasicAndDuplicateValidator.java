921d81f441aa1da522511cb6f624527ab6282db6
Alex Ananighian <aananighian@picsauditing.com>
2014-08-08 11:34:58 -0700

    DE791: Added in the new translation keys for the duplicate error message
    (cherry picked from commit 1c8081225c6bff25b419ed433e5c6fdb2ee9bbcf)

--- pics-legacy/src/main/java/com/picsauditing/employeeguard/validators/AbstractBasicAndDuplicateValidator.java
+++ pics-legacy/src/main/java/com/picsauditing/employeeguard/validators/AbstractBasicAndDuplicateValidator.java
@@ -3,12 +3,15 @@ package com.picsauditing.employeeguard.validators;
 import com.opensymphony.xwork2.util.ValueStack;
 import com.opensymphony.xwork2.validator.ValidatorContext;
 import com.picsauditing.employeeguard.daos.DuplicateEntityChecker;
+import com.picsauditing.employeeguard.msgbundle.EGI18n;
 import com.picsauditing.employeeguard.validators.duplicate.DuplicateInfoProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public abstract class AbstractBasicAndDuplicateValidator<T extends DuplicateInfoProvider> extends AbstractBasicValidator<T> {
 
     protected static final String DUPLICATE_ERROR_MSG_KEY = "PICS.DUPLICATE";
+	protected static final String OOPS_YOU_HAVE_A_DUPLICATE_KEY = "PICS.OOPS.DUPLICATE";
+	protected static final String PLEASE_CHECK_FIELDS_HEADING_KEY = "PICS.DUPLICATE.FIELDS";
 
     @Autowired
     protected DuplicateEntityChecker duplicateEntityChecker;
@@ -43,6 +46,13 @@ public abstract class AbstractBasicAndDuplicateValidator<T extends DuplicateInfo
     protected boolean isDuplicate(final T form) {
         return duplicateEntityChecker.isDuplicate(form);
     }
+
+	@Override
+	protected void addFieldErrorIfMessage(final String fieldName, final String errorMessageKey) {
+		super.addFieldErrorIfMessage(fieldName, errorMessageKey);
+		errors.put(OOPS_YOU_HAVE_A_DUPLICATE_KEY, EGI18n.getTextFromResourceBundle("VALIDATION.REQUIRED.DUPLICATE.OOPS.MSG"));
+		errors.put(PLEASE_CHECK_FIELDS_HEADING_KEY, EGI18n.getTextFromResourceBundle("VALIDATION.REQUIRED.DUPLICATE.FIELD.HEADER"));
+	}
 
     protected abstract String getDuplicateErrorMessage();
 }
