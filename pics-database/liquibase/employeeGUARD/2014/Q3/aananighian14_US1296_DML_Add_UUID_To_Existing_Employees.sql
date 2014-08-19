ed69d4ad23a0357dde1f2717eaf4560a582b8dba
aphatarphekar <aphatarphekar@picsauditing.com>
2014-08-19 16:58:55 -0700

    US1296: Fixed the incorrect column name in DB patch
    (cherry picked from commit d1174e3d259536410510251022e7925f3c23b554)

--- pics-database/liquibase/employeeGUARD/2014/Q3/aananighian14_US1296_DML_Add_UUID_To_Existing_Employees.sql
+++ pics-database/liquibase/employeeGUARD/2014/Q3/aananighian14_US1296_DML_Add_UUID_To_Existing_Employees.sql
@@ -3,19 +3,17 @@
 --changeset aananighian:14
 set sql_safe_updates = 0;
 
-
 ALTER TABLE `account_employee`
-  ADD COLUMN `UUID` VARCHAR(40) NULL AFTER `id`,
-ADD UNIQUE INDEX `UUID_UNIQUE_ACCOUNT_EMPLOYEE` (`UUID` ASC);
+  ADD COLUMN `guid` VARCHAR(40) NULL AFTER `id`,
+ADD UNIQUE INDEX `UUID_UNIQUE_ACCOUNT_EMPLOYEE` (`guid` ASC);
 
 
 update account_employee
-	set account_employee.UUID = UUID()
-	where account_employee.UUID is null;
+	set account_employee.guid = UUID()
+	where account_employee.guid is null;
 
 ALTER TABLE `account_employee`
-	CHANGE COLUMN `UUID` `UUID` VARCHAR(40) NOT NULL ;
-
+	CHANGE COLUMN `guid` `guid` VARCHAR(40) NOT NULL ;
 
 
 set sql_safe_updates = 1;
