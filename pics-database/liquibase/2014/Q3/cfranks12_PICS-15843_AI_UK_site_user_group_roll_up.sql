e1f4715c545290375d21411824f723851f89c6b1
CFranks <cfranks@picsauditing.com>
2014-09-02 16:56:01 -0700

    PICS-15843_AI_UK_site_user_group_roll_up with IGNORE

--- pics-database/liquibase/2014/Q3/cfranks12_PICS-15843_AI_UK_site_user_group_roll_up.sql
+++ pics-database/liquibase/2014/Q3/cfranks12_PICS-15843_AI_UK_site_user_group_roll_up.sql
@@ -2,12 +2,12 @@
 
 --changeset cfranks:12
 
-INSERT INTO usergroup
+INSERT IGNORE INTO usergroup
 (userId, groupId, creationDate, createdBy, updatedBy, updateDate)
 VALUES
 (70310 , 111138, NOW(), 135224, 135224,  NOW());
 
-INSERT INTO usergroup
+INSERT IGNORE INTO usergroup
 (userId, groupId, creationDate, createdBy, updatedBy, updateDate)
 VALUES
 (90444  , 111138, NOW(), 135224, 135224,  NOW());
