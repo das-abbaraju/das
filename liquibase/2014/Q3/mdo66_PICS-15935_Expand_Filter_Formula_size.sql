ddca24b53a86dca8ca9e5fdd21f69513ebcf096d
CFranks <cfranks@picsauditing.com>
2014-09-18 11:10:15 -0700

    PICS-15935 Expand_Filter_Formla_size fix

--- liquibase/2014/Q3/mdo66_PICS-15935_Expand_Filter_Formula_size.sql
+++ liquibase/2014/Q3/mdo66_PICS-15935_Expand_Filter_Formula_size.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
 
 --changeset mdo:66
-ALTER TABLE `pics_config`.`report`
+ALTER TABLE `report`
   CHANGE `filterExpression` `filterExpression` VARCHAR(200) CHARSET utf8 COLLATE utf8_general_ci NULL;
