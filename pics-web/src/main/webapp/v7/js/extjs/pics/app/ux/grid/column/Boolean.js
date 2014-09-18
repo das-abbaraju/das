205a50274270cc2ba5620b2be06ffc43e90b0afa
Stephanie Hirschberg <shirschberg@picsauditing.com>
2014-09-18 12:31:56 -0700

    PICS-13454: Updated check icons in CEMEX post eval report

--- pics-web/src/main/webapp/v7/js/extjs/pics/app/ux/grid/column/Boolean.js
+++ pics-web/src/main/webapp/v7/js/extjs/pics/app/ux/grid/column/Boolean.js
@@ -1,16 +1,22 @@
 Ext.define('PICS.ux.grid.column.Boolean', {
 	extend: 'PICS.ux.grid.column.Column',
-	
+
 	align: 'center',
 	width: 50,
-	
+
 	constructor: function () {
 		this.callParent(arguments);
 	},
-	
+
 	renderer: function (value, metaData, record, rowIndex, colIndex, store, view) {
-	    value = value ? '<i class="icon-ok"></i>' : '';
-        
+	    if (value === true) {
+            value = '<i class="icon-check"></i>';
+        } else if (value === false) {
+            value = '<i class="icon-check-empty"></i>';
+        } else {
+            value = '';
+        }
+
         return this.callParent(arguments);
     }
 });
